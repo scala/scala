4c58302ea372416da3269c1088be444fc886865c
paltherr <paltherr@epfl.ch>
2003-02-21 15:19:54 +0000

    - Changed True to Boolean.True

--- test/files/pos/IntSet.scala
+++ test/files/pos/IntSet.scala
@@ -14,7 +14,7 @@ class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
   def contains(x: Int): Boolean =
     if (x < elem) left contains x
     else if (x > elem) right contains x
-    else True;
+    else Boolean.True;
   def incl(x: Int): IntSet =
     if (x < elem) new NonEmpty(elem, left incl x, right)
     else if (x > elem) new NonEmpty(elem, left, right incl x)
