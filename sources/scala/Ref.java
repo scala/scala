68fabb7cc651ae6f8306565a70d65e0d44ebf411
paltherr <paltherr@epfl.ch>
2003-02-21 09:52:23 +0000

    - Added Ref.java

---
+++ sources/scala/Ref.java
@@ -0,0 +1,27 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002, LAMP/EPFL                  **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $OldId: Ref.java,v 1.2 2002/03/12 13:16:04 zenger Exp $
+// $Id$
+
+package scala;
+
+/** @meta class [?T] extends scala.Object;
+ */
+public class Ref extends scala.Object$class {
+
+    /** @meta field ?T;
+     */
+    public java.lang.Object elem = null;
+
+    /** @meta constr (?T);
+     */
+    public Ref(java.lang.Object x) {
+	elem = x;
+    }
+}
