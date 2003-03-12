2142b86eceb2ed291610f2138ff0b3c8fbcda692
schinz <schinz@epfl.ch>
2003-03-12 14:19:30 +0000

    - check that no syntactic representation of typ...
    - check that no syntactic representation of types remain in the tree
    (since all syntactic types have to be replaced by TypeTerm after
    analysis)

--- sources/scalac/checkers/CheckTypes.java
+++ sources/scalac/checkers/CheckTypes.java
@@ -19,7 +19,8 @@ import scalac.util.Debug;
 /**
  * Check the following properties:
  *   1. all tree nodes have a type,
- *   2. TypeRefs supply the correct number of arguments.
+ *   2. TypeRefs supply the correct number of arguments,
+ *   3. type terms are represented as TypeTerm nodes.
  *
  * @author Michel Schinz
  * @version 1.0
@@ -27,6 +28,13 @@ import scalac.util.Debug;
 
 public class CheckTypes extends Checker {
     public CheckTypes(Global global) { super(global); }
+
+    public void checkIsTypeTerm(Tree tree, Tree tpe) {
+        verify(tree,
+               tpe == Tree.Empty || tpe instanceof Tree.TypeTerm,
+               "no syntactic types",
+               "`tpe' part of tree is not an instance of TypeTerm");
+    }
 
     public void check(Tree tree) {
 	verify(tree, tree.type != null, "non-null type", "type of tree is not null");
@@ -42,6 +50,21 @@ public class CheckTypes extends Checker {
                            + " expects " + sym.typeParams().length + " type arguments"
                            + " but is given " + args.length);
                 break;
+            }
+
+            switch (tree) {
+            case ClassDef(_, _, _, _, Tree tpe, _):
+                checkIsTypeTerm(tree, tpe); break;
+            case ModuleDef(_, _, Tree tpe, _):
+                checkIsTypeTerm(tree, tpe); break;
+            case ValDef(_, _, Tree tpe, _):
+                checkIsTypeTerm(tree, tpe); break;
+            case DefDef(_, _, _, _, Tree tpe, _):
+                checkIsTypeTerm(tree, tpe); break;
+            case Typed(_, Tree tpe):
+                checkIsTypeTerm(tree, tpe); break;
+            case Super(Tree tpe):
+                checkIsTypeTerm(tree, tpe); break;
             }
         }
     }
