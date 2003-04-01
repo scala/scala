d8284d61f2f09a72f223c6e9396eec8fa5893d29
Martin Odersky <odersky@gmail.com>
2003-04-01 17:01:10 +0000

    *** empty log message ***

---
+++ test/files/pos/patterns1.scala
@@ -0,0 +1,13 @@
+trait Option[a] {}
+case class Some[a](x: a) extends Option[a];
+case class None[a] extends Option[a];
+
+object test {
+
+  def println(str: String): Unit = java.lang.System.out.println(str);
+
+  def print(opt: Option[String]) = opt match {
+    case Some(x) => println(x);
+    case None => println("nothing");
+  }
+}
