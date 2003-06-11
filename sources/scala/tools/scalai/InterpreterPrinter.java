39f22e7351900793142c44882db8ffecb8bd6038
paltherr <paltherr@epfl.ch>
2003-06-11 13:48:15 +0000

    - Added InterpreterPrinter.java

---
+++ sources/scala/tools/scalai/InterpreterPrinter.java
@@ -0,0 +1,77 @@
+/*     ____ ____  ____ ____  ______                                     *\
+**    / __// __ \/ __// __ \/ ____/    SOcos COmpiles Scala             **
+**  __\_ \/ /_/ / /__/ /_/ /\_ \       (c) 2002, LAMP/EPFL              **
+** /_____/\____/\___/\____/____/                                        **
+\*                                                                      */
+
+// $Id$
+
+package scalai;
+
+import java.io.PrintWriter;
+
+import scala.runtime.InterpreterSupport.DefinitionPrinter;
+
+import scalac.util.Debug;
+
+public class InterpreterPrinter implements DefinitionPrinter {
+
+    //########################################################################
+    // Private State
+
+    private final Interpreter interpreter;
+    private final PrintWriter writer;
+
+    //########################################################################
+    // Public Constructors
+
+    public InterpreterPrinter(Interpreter interpreter, PrintWriter writer) {
+        this.interpreter = interpreter;
+        this.writer = writer;
+    }
+
+    //########################################################################
+    // Public Methods
+
+    public void showDefinition(String signature) {
+        writer.println(signature);
+    }
+
+    public void showValueDefinition(String signature, Object value) {
+        EvaluatorResult result = interpreter.toString(value, null);
+        switch (result) {
+        case Value(Object string, _):
+            writer.println(signature + " = " + string);
+            return;
+        case Error(EvaluatorException exception):
+            writer.print(signature + " = ");
+            writer.println(exception.getScalaErrorMessage(true));
+            return;
+        default:
+            throw Debug.abort("illegal case", result);
+        }
+    }
+
+    public void showResult(EvaluatorResult result, boolean interactive) {
+        switch (result) {
+        case Void:
+            return;
+        case Value(Object value, String type):
+            if (interactive)
+                if (value instanceof String)
+                    writer.println(value + ": " + type);
+                else
+                    showResult(interpreter.toString(value, type), interactive);
+            return;
+        case Error(EvaluatorException exception):
+            String name = Thread.currentThread().getName();
+            writer.print("Exception in thread \"" + name + "\" ");
+            writer.println(exception.getScalaErrorMessage(true));
+            return;
+        default:
+            throw Debug.abort("illegal case", result);
+        }
+    }
+
+    //########################################################################
+}
