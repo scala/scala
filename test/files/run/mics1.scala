5c348d28dac57711b938cb1c910b3b95c47445dd
michelou <michelou@epfl.ch>
2003-06-11 18:31:17 +0000

    *** empty log message ***

---
+++ test/files/run/mics1.scala
@@ -0,0 +1,25 @@
+abstract class IntMap[a] {
+    def lookup(key: Int): a = match {
+        case Empty() => error("key " + key + " not found")
+        case _ => error("ok")
+    }
+}
+
+case class Empty[a]() extends IntMap[a];
+
+object mics1 {
+
+    def main(args: Array[String]) = {
+        val key = 2000;
+        val map: IntMap[String] = new Empty[String];
+
+        System.out.print("lookup(" + key + ") = ");
+        try {
+            System.out.println(map.lookup(key));
+        } except {
+            case e => System.out.println(e);
+        }
+    }
+
+}
+
