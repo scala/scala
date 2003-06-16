be4f8d7916b08ffdec9f9170b10d8ae4d673f966
buraq <buraq@epfl.ch>
2003-06-16 08:35:18 +0000

    removed attributes, need HashMap or something s...
    removed attributes, need HashMap or something similar

--- sources/scala/xml/Element.scala
+++ sources/scala/xml/Element.scala
@@ -1,10 +1,24 @@
 package scala.xml ;
 
+/** interface to XML elements. used by dtd2scala tool and the generated XML parser interfaces.
+ *  sorry, no attributes yet (needs working HashMap).
+ *  future revisions might use sequence instead of list.
+ */
+
 abstract class Element {
 
                 def getName:     String;                // the real element name
                 def getChildren: List[Element];         // the children
-                def getAttribs:  Map[ String, String ];
+//                def getAttribs:  Map[ String, String ]; // disabled
+
+	        def toXML( elems:List[Element] ):String = elems match {
+		    case head :: tail  => head.toString() + toXML( tail );
+                    case Nil           => "";
+		}
+
+                def toXML: String =
+                        "<" + getName +">" + toXML( getChildren ) + "</" + getName +">"
+
 
   /*
                 def toXML : String = {
