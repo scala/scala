3102d7d40fcc65e6174e84d5424cd45d688c0257
michelou <michelou@epfl.ch>
2003-06-23 17:11:16 +0000

    cleaned up

--- sources/scalac/transformer/matching/WordAutomInScala.java
+++ sources/scalac/transformer/matching/WordAutomInScala.java
@@ -1,84 +1,102 @@
-package scalac.transformer.matching ;
+/*     ____ ____  ____ ____  ______                                     *\
+**    / __// __ \/ __// __ \/ ____/    SOcos COmpiles Scala             **
+**  __\_ \/ /_/ / /__/ /_/ /\_ \       (c) 2002, LAMP/EPFL              **
+** /_____/\____/\___/\____/____/                                        **
+\*                                                                      */
+
+// $Id$
+
+package scalac.transformer.matching;
+
+import ch.epfl.lamp.util.Position;
 
 import scalac.*;
 import scalac.ast.Tree;
 import scalac.ast.TreeGen;
 import scalac.symtab.Type;
 import scalac.symtab.Symbol;
+import scalac.transformer.TransMatch.Matcher;
 import scalac.typechecker.*;
 import Tree.*;
 
-import scalac.transformer.TransMatch.Matcher ;
-import java.util.* ;
-import ch.epfl.lamp.util.Position;
-
-public class WordAutomInScala extends Autom2Scala  {
-
-      Tree getMatcherSwitch( Tree selector,
-                             Tree failTree,
-                             Tree body[],
-                             Type resultType) {
+import java.util.*;
 
 
-            Tree run = callFun( new Tree[] {
-                  cf.newIterator( selector ),
-                  gen.mkIntLit(Position.NOPOS,0) } );
+/**
+ */
+public class WordAutomInScala extends Autom2Scala {
 
-            /* return code `var <swres>: scala.Int = <init>' */
+    Tree getMatcherSwitch(Tree selector,
+                          Tree failTree,
+                          Tree body[],
+                          Type resultType) {
 
-            run = _intvar( resultSym, run );
+        Tree run = callFun( new Tree[] {
+                       cf.newIterator(selector),
+                       gen.mkIntLit(Position.NOPOS, 0) } );
 
-            Tree result;
+        /* return code `var <swres>: scala.Int = <init>' */
 
-            // conditions
-            Tree cond[]  = new Tree[ body.length ];
-            //Tree bbody[] = new Tree[ body.length ];
-            for( int i = body.length - 1; i >= 0; i-- )
-                  cond[ i ] = cf.Equals( _swres(), gen.mkIntLit(Position.NOPOS, i ));
+        run = _intvar(resultSym, run);
 
-            result = cf.Switch( selector, cond, body, failTree );
+        Tree result;
 
-            result = cf.make.Block( pos, new Tree[] { theDefDef, run, result } )
+        // conditions
+        Tree cond[] = new Tree[body.length];
+        //Tree bbody[] = new Tree[body.length];
+        for (int i = body.length - 1; i >= 0; i--)
+            cond[i] = cf.Equals(_swres(), gen.mkIntLit(Position.NOPOS, i));
+
+        result = cf.Switch( selector, cond, body, failTree );
+
+        result = cf.make.Block( pos, new Tree[] { theDefDef, run, result } )
                   .setType( resultType );
-            //unit.global.debugPrinter.print( result );
-            return result;
+        //unit.global.debugPrinter.print( result );
+        return result;
+    }
 
+    /** do the translation
+     */
+    public void translate() {
+        initializeSyms();
+        Tree tb = code_body();
+        theDefDef = gen.DefDef(this.funSym, tb);
+    }
 
-      }
+    /** ...
+     * @return returns translation of transition with label from i.
+     * returns null if there is no such transition
+     * (no translation needed)
+     */
+    Tree code_delta(int i, Label label) {
+        Integer target = dfa.delta(i, label);
 
-      /** do the translation
-       */
-      public void translate() {
-            initializeSyms();
-            Tree tb = code_body();
-            theDefDef = gen.DefDef( this.funSym,
-                                    tb );
-      }
+        if (target == null)
+            switch (label) {
+            case DefaultLabel:
+                return code_fail(); // this may not happen !
+            default:
+                return null; // not good
+            }
+        else if (target.intValue() == dfa.nstates - 1) // that one is a dead state
+            return code_fail();
 
-      /** returns translation of transition with label from i.
-       *  returns null if there is no such transition(no translation needed)
-       */
-      Tree code_delta( int i, Label label ) {
-            Integer target = dfa.delta( i, label );
+        return callFun(new Tree[] { _iter(),
+            gen.mkIntLit(Position.NOPOS, target.intValue())} );
+    }
 
-            if( target == null )
-                  switch( label ) {
-                  case DefaultLabel:
-                        return code_fail(); // this may not happen !
-                  default:
-                        return null; // not good
-                  }
-            else if( target.intValue() == dfa.nstates - 1 ) // that one is a dead state
-                  return code_fail();
-
-            return callFun( new Tree[] { _iter(), gen.mkIntLit(Position.NOPOS, target.intValue() )} );
-      }
-
-      public WordAutomInScala( DetWordAutom dfa,
-                               Type elementType,
-                               Symbol owner,
-                               CodeFactory cf ) {
-            super( dfa, elementType, owner, cf );
-      }
+    /** ...
+     * @param dfa ...
+     * @param elementType ...
+     * @param owner ...
+     * @param cf ...
+     * @return ...
+     */
+    public WordAutomInScala(DetWordAutom dfa,
+                            Type elementType,
+                            Symbol owner,
+                            CodeFactory cf) {
+        super(dfa, elementType, owner, cf);
+    }
 
 }
