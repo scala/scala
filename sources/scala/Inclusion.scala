a8722061eee0cb300a32ea30484a267ec6004368
Matthias Zenger <mzenger@gmail.com>
2003-06-25 10:50:11 +0000

    Support files for the Publish/Subscriber mechan...
    Support files for the Publish/Subscriber mechanism; this set of files
    implements, in particular, a rollback mechanism for any collection type.
    The mechanism is designed that it can consistently reset a whole set of
    containers to a consistent state.

---
+++ sources/scala/Inclusion.scala
@@ -0,0 +1,13 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2003, LAMP/EPFL                  **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+** $Id$
+\*                                                                      */
+
+package scala;
+
+
+case class Inclusion[A](elem: A) extends ObservableUpdate[A];
