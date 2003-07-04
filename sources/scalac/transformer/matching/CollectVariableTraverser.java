94cc5fb3985236c6a7a1ef7af7450c49648e06a9
buraq <buraq@epfl.ch>
2003-07-04 12:16:52 +0000

    collect variables from patterns

---
+++ sources/scalac/transformer/matching/CollectVariableTraverser.java
@@ -0,0 +1,35 @@
+package scalac.transformer.matching ;
+
+import scalac.util.Name ;
+import scalac.ast.Tree ;
+import scalac.symtab.Symbol ;
+
+import java.util.Vector;
+
+class CollectVariableTraverser extends VariableTraverser {
+
+      Vector vars;
+
+      boolean isVariableName( Name name ) {
+            return ( name.toString().indexOf("$") == -1 )
+                  && super.isVariableName( name );
+      }
+
+
+      void handleVariableSymbol( Symbol sym ) {
+            vars.add( sym );
+      }
+
+      public CollectVariableTraverser() {
+            this.vars = new Vector();
+      }
+
+      static  boolean containsBinding( Tree  pat ) {
+
+            CollectVariableTraverser cvt = new CollectVariableTraverser();
+            cvt.traverse( pat );
+            return !cvt.vars.isEmpty();
+
+      }
+
+}
