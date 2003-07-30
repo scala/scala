c1c06996b1a9a432fecd1dfc8da89c2eea3f3599
paltherr <paltherr@epfl.ch>
2003-07-30 08:50:00 +0000

    - Added $Id$

--- sources/scalac/DocModule.java
+++ sources/scalac/DocModule.java
@@ -4,6 +4,8 @@
 ** /_____/\____/\___/\____/____/                                        **
 \*                                                                      */
 
+// $Id$
+
 package scalac;
 
 import scalac.Global;
