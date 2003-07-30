8684be678d835f938a063ce9887370058254a22d
paltherr <paltherr@epfl.ch>
2003-07-30 14:45:00 +0000

    - Added TypeNames

---
+++ sources/scalac/util/TypeNames.java
@@ -0,0 +1,15 @@
+/*     ____ ____  ____ ____  ______                                     *\
+**    / __// __ \/ __// __ \/ ____/    SOcos COmpiles Scala             **
+**  __\_ \/ /_/ / /__/ /_/ /\_ \       (c) 2002, LAMP/EPFL              **
+** /_____/\____/\___/\____/____/                                        **
+\*                                                                      */
+
+// $Id$
+
+package scalac.util;
+
+public class TypeNames {
+
+    public static final Name EMPTY = Names.EMPTY.toTypeName();
+
+}
