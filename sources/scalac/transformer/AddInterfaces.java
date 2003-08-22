cc2922163923cbd888e84e58f44e5ddfd2f5ab74
schinz <schinz@epfl.ch>
2003-08-22 11:21:52 +0000

    - handle multiple constructors

--- sources/scalac/transformer/AddInterfaces.java
+++ sources/scalac/transformer/AddInterfaces.java
@@ -301,7 +301,8 @@ class AddInterfaces extends Transformer {
             Tree t = classBody[i];
             Symbol tSym = t.symbol();
 
-            if (t.definesSymbol() && !classMemberMap.containsKey(tSym))
+            if (t.definesSymbol() && !(classMemberMap.containsKey(tSym)
+                                       || tSym.isConstructor()))
                 continue;
 
             Tree newT = transform(t);
