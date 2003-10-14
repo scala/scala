cc2a5f03994399f05845b32d37969f04b0c953e2
michelou <michelou@epfl.ch>
2003-10-14 09:10:17 +0000

    - minor changes

--- sources/scala/tools/scalatest/Main.java
+++ sources/scala/tools/scalatest/Main.java
@@ -17,7 +17,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.io.Writer;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
