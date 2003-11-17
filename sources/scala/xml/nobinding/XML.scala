9a63043f7cea8f46324e65a020b1bb8f85b530d4
buraq <buraq@epfl.ch>
2003-11-17 11:24:07 +0000

    API change for XML data that is not bound to cl...
    API change for XML data that is not bound to classes

---
+++ sources/scala/xml/nobinding/XML.scala
@@ -0,0 +1,39 @@
+package scala.xml.nobinding ;
+
+import java.net.URL;
+import scala.collection.Map ;
+import scala.collection.mutable.HashMap ;
+
+import scala.xml.Utility ;
+/** functions to load and save XML elements. use this when data binding is not
+**  desired.
+**/
+object XML {
+
+  // functions for generic xml loading, saving
+
+  /** loads XML from a given file*/
+  def load( filename:String ):Element =
+    new NoBindingFactoryAdapter().loadXML( filename );
+
+  /** loads XML from a (file) URL */
+  def load( url:URL ):Element =
+    new NoBindingFactoryAdapter().loadXML( url );
+
+  /** saves XML to filename with encoding ISO-8859-1 */
+  def save( filename:String, doc:Element ):Unit = {
+    /* using NIO classes of JDK 1.4 */
+    import java.io.{FileOutputStream,Writer};
+    import java.nio.channels.{Channels,FileChannel};
+
+    val fos = new FileOutputStream( filename );
+    val w:Writer = Channels.newWriter( fos.getChannel(), "ISO-8859-1" );
+
+    /* 2do: optimize by giving writer parameter to toXML*/
+    w.write( Utility.toXML( doc ));
+
+    w.close();
+    fos.close();
+  }
+
+}
