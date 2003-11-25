d4acacd8bfde1983950854c1862a75e54bfc5c27
buraq <buraq@epfl.ch>
2003-11-25 14:55:29 +0000

    fixed error in pow!

--- sources/scala/collection/immutable/Tree.scala
+++ sources/scala/collection/immutable/Tree.scala
@@ -264,9 +264,10 @@ class Tree[KEY,Entry](order:Order[KEY],entryKey:Entry=>KEY) {
     */
     protected val p = 2; // It seems that p = 2 is optimal for sorted keys */
     protected def pow(a:int, b:int):int =
-	a.match {
+      b.match {
 	    case 2 => a * a;
 	    case 1 => a;
+            case 0 => 1;
 	    case x if x > 0 => a * pow(a, b-1);
 	};
     private def div2(x:int) = x >> 1;
