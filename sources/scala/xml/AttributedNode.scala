93fea4d99c64b1a1101d6489ca824c43f23d6227
Martin Odersky <odersky@gmail.com>
2003-11-25 16:05:59 +0000

    *** empty log message ***

--- sources/scala/xml/AttributedNode.scala
+++ sources/scala/xml/AttributedNode.scala
@@ -3,6 +3,7 @@ package scala.xml ;
 abstract class AttributedNode extends Node {
 
 val attribHashCode:int;
-def attributes:Map[String,String];
+def attributes:scala.collection.Map[String,String];
+def toXML = Utility.toXML(this);
 
 }
