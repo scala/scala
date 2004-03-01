ffea5d8f789b42735a8bef47151352375dc8d351
michelou <michelou@epfl.ch>
2004-03-01 17:16:34 +0000

    *** empty log message ***

--- test/files/run/literals.scala
+++ test/files/run/literals.scala
@@ -26,9 +26,71 @@ object Test {
   }
 
   def main(args: Array[String]) = {
+    // char
+    check_success("'\\u0024' == '$'", '\u0024', '$');
+    check_success("'\\u005f' == '_'", '\u005f', '_');
+    check_success("65.asInstanceOf[char] == 'A'", 65.asInstanceOf[char], 'A');
+
+    System.out.println();
+
+    // int
+    check_success("01 == 1", 01, 1);
+    check_success("010 == 8", 010, 8);
+    check_success("0X01 == 1", 0X01, 1);
+    check_success("0x01 == 1", 0x01, 1);
+    check_success("0x10 == 16", 0x10, 16);
+    check_success("0xa == 10", 0xa, 10);
+    check_success("0x0a == 10", 0x0a, 10);
+
+    check_success("+01 == 1", +01, 1);
+    check_success("+010 == 8", +010, 8);
+    check_success("+0x01 == 1", +0x01, 1);
+    check_success("+0x10 == 16", +0x10, 16);
+    check_success("+0xa == 10", +0xa, 10);
+    check_success("+0x0a == 10", +0x0a, 10);
+
+    check_success("-01 == -1", -01, -1);
+    check_success("-010 == -8", -010, -8);
+    check_success("-0x01 == -1", -0x01, -1);
+    check_success("-0x10 == -16", -0x10, -16);
+    check_success("-0xa == -10", -0xa, -10);
+    check_success("-0x0a == -10", -0x0a, -10);
+
+    check_success("017777777777 == 2147483647", 017777777777, 2147483647);
+    check_success("020000000000 == -2147483648", 020000000000, -2147483647 -1);
+    check_success("037777777777 == -1", 037777777777, -1);
+
+    check_success("0x7fffffff == 2147483647", 0x7fffffff, 2147483647);
+    check_success("0x80000000 == -2147483648", 0x80000000, -2147483647 -1);
+    check_success("0xffffffff == -1", 0xffffffff, -1);
+
+    System.out.println();
+
+    // long
+    check_success("1l == 1L", 1l, 1L);
+    check_success("1L == 1l", 1L, 1l);
+    check_success("1.asInstanceOf[long] == 1l", 1.asInstanceOf[long], 1l);
+
+    check_success("0777777777777777777777L == 9223372036854775807L",
+      0777777777777777777777L, 9223372036854775807L);
+    check_success("01000000000000000000000L == -9223372036854775808L",
+      01000000000000000000000L, -9223372036854775807L -1L);
+    check_success("01777777777777777777777L == -1L",
+      01777777777777777777777L, -1L);
+
+    check_success("0x7fffffffffffffffL == 9223372036854775807L",
+      0x7fffffffffffffffL, 9223372036854775807L);
+    check_success("0x8000000000000000L == -9223372036854775808L",
+      0x8000000000000000L, -9223372036854775807L -1L);
+    check_success("0xffffffffffffffffL == -1L",
+      0xffffffffffffffffL, -1L);
+
+    System.out.println();
+
     // see JLS at address:
     // http://java.sun.com/docs/books/jls/second_edition/html/lexical.doc.html#230798
 
+    // float
     check_success("1e1f == 10.0f", 1e1f, 10.0f);
     check_success("2.f == 2.0f", 2.f, 2.0f);
     check_success(".3f == 0.3f", .3f, 0.3f);
@@ -36,7 +98,12 @@ object Test {
     check_success("3.14f == 3.14f", 3.14f, 3.14f);
     check_success("6.022e23f == 6.022e23f", 6.022e23f, 6.022e23f);
     check_success("09f == 9.0f", 09f, 9.0f);
+    check_success("1.asInstanceOf[float] == 1.0", 1.asInstanceOf[float], 1.0f);
+    check_success("1l.asInstanceOf[float] == 1.0", 1l.asInstanceOf[float], 1.0f);
 
+    System.out.println();
+
+    // double
     check_success("1e1 == 10.0", 1e1, 10.0);
     check_success("2. == 2.0", 2., 2.0);
     check_success("2.d == 2.0", 2.d, 2.0);
@@ -46,6 +113,8 @@ object Test {
     check_success("3.14 == 3.14", 3.14, 3.14);
     check_success("1e-9d == 1.0e-9", 1e-9d, 1.0e-9);
     check_success("1e137 == 1.0e137", 1e137, 1.0e137);
+    check_success("1.asInstanceOf[double] == 1.0", 1.asInstanceOf[double], 1.0);
+    check_success("1l.asInstanceOf[double] == 1.0", 1l.asInstanceOf[double], 1.0);
   }
 }
 
