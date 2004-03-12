6c9e15bea04439165712c876dda23a3a9a74f3b5
paltherr <paltherr@epfl.ch>
2004-03-12 22:08:18 +0000

    - Added Similarity.scala and NodeSeq.scala

---
+++ sources/scala/Similarity.scala
@@ -0,0 +1,16 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002, LAMP/EPFL                  **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id$
+
+package scala;
+
+// !!! Just a stub to make things compile.
+trait Similarity {
+  def similar(that: Similarity): boolean = false;
+}
---
+++ sources/scala/xml/NodeSeq.scala
@@ -0,0 +1,14 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002, LAMP/EPFL                  **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id$
+
+package scala.xml;
+
+// !!! Just a stub to make things compile.
+class NodeSeq(any: Any);
