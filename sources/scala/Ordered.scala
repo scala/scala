141324d8253fd57548b4f21ac46dc72ae8b9d626
michelou <michelou@epfl.ch>
2004-04-23 08:19:46 +0000

    - added file header.
    - corrected comment to be XHTML compliant.

--- sources/scala/Ordered.scala
+++ sources/scala/Ordered.scala
@@ -1,4 +1,14 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2003-04, LAMP/EPFL               **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+** $Id$
+\*                                                                      */
+
 package scala;
+
 
 /** A trait for totally ordered data.
  */
@@ -6,9 +16,9 @@ trait Ordered[+a] {
 
   /** Result of comparing `this' with operand `that'.
    *  returns `x' where
-   *  x < 0    iff    this < that
-   *  x == 0   iff    this == that
-   *  x > 0    iff    this > that
+   *  <code>x &lt; 0</code>    iff    <code>this &lt; that</code>
+   *  <code>x == 0</code>   iff    <code>this == that</code>
+   *  <code>x &gt; 0</code>    iff    <code>this &gt; that</code>
    */
   def compareTo [b >: a <% Ordered[b]](that: b): int;
 
--- sources/scala/PartiallyOrdered.scala
+++ sources/scala/PartiallyOrdered.scala
@@ -1,4 +1,14 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2003-04, LAMP/EPFL               **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+** $Id$
+\*                                                                      */
+
 package scala;
+
 
 /** A trait for partially ordered data.
  */
@@ -7,9 +17,9 @@ trait PartiallyOrdered[+a] {
   /** Result of comparing `this' with operand `that'.
    *  Returns `None' if operands are not comparable.
    *  If operands are comparable, returns `Some(x)' where
-   *  x < 0    iff    this < that
-   *  x == 0   iff    this == that
-   *  x > 0    iff    this > that
+   *  <code>x &lt; 0</code>    iff    <code>this &lt; that</code>
+   *  <code>x == 0</code>   iff    <code>this == that</code>
+   *  <code>x &gt; 0</code>    iff    <code>this &gt; that</code>
    */
   def tryCompareTo [b >: a <% PartiallyOrdered[b]](that: b): Option[int];
 
