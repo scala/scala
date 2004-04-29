84ff0a4c40da09630ccd4d84dc0bbe87d44966e5
buraq <buraq@epfl.ch>
2004-04-29 15:59:42 +0000

    added attribute map

--- sources/scala/xml/Elem.scala
+++ sources/scala/xml/Elem.scala
@@ -12,32 +12,32 @@ case class Elem( label:String, child:Node* ) extends Node {
     }
   }
 
-  private val hmap = new HashMap[String,String]();
+  def `@` = new HashMap[String,String]();
 
   /** the attributes axis - default is Nil
   */
-  def attribute: Seq[Pair[String, String]] = hmap.elements.toSeq( hmap.size );
+  def attribute: Seq[Pair[String, String]] = `@`.toList;
 
   /** returns a new element with updated attributes
   */
   final def %(attrs: Seq[Pair[String, String]]):Elem = {
     val newmap = new HashMap[String,String]();
-    for( val p <- hmap.elements ) { newmap += p._1 -> p._2 };
+    for( val p <- `@`.elements ) { newmap += p._1 -> p._2 };
     for( val p <- attrs )         { newmap += p._1 -> p._2 };
     new Elem( label, child:_* ) {
-      private val hmap = newmap;
-      override def attribute = newmap.elements.toSeq( newmap.size );
+      override def `@` = newmap;
+      override def attribute = `@`.toList;
     };
   }
   /** returns a new symbol with updated attribute
   */
   final def %(attr: Pair[String, String]):Elem = {
     val newmap = new HashMap[String,String]();
-    for( val p <- hmap.elements ) { newmap += p._1 -> p._2 };
+    for( val p <- `@`.elements ) { newmap += p._1 -> p._2 };
     newmap += attr._1 -> attr._2;
     new Elem( label, child:_* ) {
-      private val hmap = newmap;
-      override def attribute = newmap.elements.toSeq( newmap.size );
+      override def `@` = newmap;
+      override def attribute = `@`.toList;
     };
   }
 }
