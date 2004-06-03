3b537582a64cfaee371e7f3439b7077ea3e4fc49
buraq <buraq@epfl.ch>
2004-06-03 20:05:37 +0000

    validation works

---
+++ test/files/xml/xhtml.scala
@@ -0,0 +1,33 @@
+// $Id$
+
+import scala.xml.{Node,Text};
+import dtd._;
+
+object Test {
+
+  val n = Node.NoAttributes;
+
+  def main( argv:Array[String] ) = {
+    val link = A( n + "href" -> "http://lampwww.epfl.ch",
+                 Text("link"));
+
+    //val m = new scala.collection.mutable.HashMap[String, String];
+    //m.put("href","http://lampwww.epfl.ch");
+    //link.setAttribs(m);
+
+    val body = Body(n,
+      H1(n,Text("Welcome to xhtml in scala")),
+      P(n,Text( "a paragraph")),
+      P(n,Text("another one, with a "),link,Text(" to the LAMP homepage.")));
+    val page = Html(n,
+                    Head(n,
+                         Base(n+"href" -> "http://here.edu"),
+                         Title(n,Text("a basic xhtml page"))),
+                    body);
+    System.out.println( page ) ;
+
+    val doc = load(argv(0));
+    System.out.println( doc );
+  }
+
+}
