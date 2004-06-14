3b26120ff86f1ca66e7b92c8ae9df085940e4d39
buraq <buraq@epfl.ch>
2004-06-14 10:54:37 +0000

    support for namespaces

---
+++ sources/scala/xml/Namespace.scala
@@ -0,0 +1,12 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2004, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+** $Id$
+\*                                                                      */
+package scala.xml ;
+
+/** an XML namespace URI */
+case class Namespace( uri:String ) ;
