9686e20774b3da3efe87e24d913aefef1805041a
buraq <buraq@epfl.ch>
2004-07-08 17:08:00 +0000

    obsol

--- sources/scala/util/regexp/SyntaxErrorException.scala
+++
@@ -1,8 +0,0 @@
-// $Id$
-
-package scala.util.regexp ;
-
-/** this runtime exception is thrown if an attempt to instantiate a
-  * syntactically incorrect expression is detected */
-class SyntaxErrorExpression(e: String)
-  extends java.lang.RuntimeException(e);
