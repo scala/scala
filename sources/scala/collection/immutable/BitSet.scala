147b761ceaaac0265af38bf4c00f71779a4ed1ea
michelou <michelou@epfl.ch>
2004-07-16 12:00:51 +0000

    - updated some comments.

--- sources/scala/collection/immutable/BitSet.scala
+++ sources/scala/collection/immutable/BitSet.scala
@@ -7,31 +7,41 @@
 ** $Id$
 \*                                                                      */
 
-package scala.collection.immutable ;
+package scala.collection.immutable;
 
-/** An immutable bitset view on a byte array. Instances can conveniently be
- *  created from instances of mutable.ResizableBitSet
- *  n:    number of relevant bits
- *  ba:   array of bytes of length n>>>3
- *  copy: if yes, then ba is copied and updates will not affect this bitset
+/** The class <code>BitSet</code>provides an immutable bitset view on a
+ *  byte array. Instances can conveniently be created from instances of
+ *  <code>mutable.ResizableBitSet</code>.
+ *
+ *  @param <code>n</code> represents the number of relevant bits
+ *  @param ba:   array of bytes of length <code>n</code>&gt;&gt;&gt;3
+ *  @param copy: if yes, then <code>ba</code> is copied and updates will
+ *               not affect this bitset
+ *
  *  @author  Burak Emir
+ *  @version 1.0
  */
-class BitSet(n:Int, ba: Array[Byte], copy:Boolean) extends scala.collection.BitSet {
+class BitSet(n:Int, ba: Array[Byte], copy: Boolean) extends scala.collection.BitSet {
 
-  final def size   = n;
+  final def size = n;
 
-  val array:Array[Byte]  =
-    if( copy ) {
+  val array: Array[Byte]  =
+    if (copy) {
       val arr = new Array[Byte](ba.length);
-      java.lang.System.arraycopy( ba, 0, arr, 0, ba.length );
+      java.lang.System.arraycopy(ba, 0, arr, 0, ba.length);
       arr
-    } else ba;
+    }
+    else
+      ba;
 
   def this(rbs: scala.collection.mutable.ResizableBitSet) = {
     this(rbs.size, rbs.toByteArray, false);
   }
 
-  /** returns true if bit i is set */
+  /** returns true if bit i is set
+   *
+   * @param i
+   */
   def apply(i: Int):Boolean = {
     val j    = (i >>> 3);
     val mask = (1 << (i & 0x07));
