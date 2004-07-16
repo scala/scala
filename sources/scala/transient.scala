548a1b758f20ecadef5deac25074cac0b8e969e9
michelou <michelou@epfl.ch>
2004-07-16 11:58:54 +0000

    - corrected CVS header.

--- sources/scala/transient.scala
+++ sources/scala/transient.scala
@@ -1,12 +1,12 @@
 /*                     __                                               *\
 **     ________ ___   / /  ___     Scala API                            **
-**    / __/ __// _ | / /  / _ |    (c) 2002, LAMP/EPFL                  **
+**    / __/ __// _ | / /  / _ |    (c) 2003-2004, LAMP/EPFL             **
 **  __\ \/ /__/ __ |/ /__/ __ |                                         **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
-** $Id: Attribute.scala
-*/
+** $Id$
+\*                                                                      */
+
 package scala;
 
 class transient extends Attribute {}
-
