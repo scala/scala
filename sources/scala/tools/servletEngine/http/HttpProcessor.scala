9309cf418f5eefb20bfc07dcc4dcfab176c5bba4
buraq <buraq@epfl.ch>
2004-07-26 13:16:39 +0000

    *** empty log message ***

--- sources/scala/tools/servletEngine/http/HttpProcessor.scala
+++
@@ -1,9 +0,0 @@
-package scala.tools.servletEngine.http ;
-
-import java.io._;
-/**
-le processing actuel de la requête du client est reporté aux classes qui implemetent le trait HttpProcessor, ce trait decrit la façon avec laquelle la classe Httpd peut appeler le processeur pour repondre à la requête du client.
-*/
- trait HttpProcessor {
-   def processRequest(out: HttpOutputStream): Unit ={};
-}
