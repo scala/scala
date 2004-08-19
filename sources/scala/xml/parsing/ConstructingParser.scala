d2bb97849985f367f5e5101222d20c7f5a06d61c
buraq <buraq@epfl.ch>
2004-08-19 17:29:21 +0000

    print an error, instead of dying with fatal error

--- sources/scala/xml/parsing/ConstructingParser.scala
+++ sources/scala/xml/parsing/ConstructingParser.scala
@@ -5,11 +5,19 @@ object ConstructingParser {
     fromSource(new ConstructingHandler(), inp);
 
   def fromSource(theHandle: ConstructingHandler, inp: scala.io.Source): ConstructingParser  = {
+
+
     val p = new ConstructingParser() {
       val input = inp;
       override val handle = theHandle;
       def nextch = { ch = input.next; pos = input.pos; }
       override val preserveWS = true;
+
+      /** report a syntax error */
+      def reportSyntaxError(str: String): Unit = {
+        Console.println(inp.descr+":"+scala.io.Position.toString(pos)+":"+str);
+      }
+
     };
     p.nextch;
     p
@@ -21,9 +29,6 @@ object ConstructingParser {
 abstract class ConstructingParser extends MarkupParser[Node] {
 
   val handle = new ConstructingHandler();
-
-  /** report a syntax error */
-  def reportSyntaxError(str: String): Unit = throw FatalError(str);
 
   /** this method assign the next character to ch and advances in input */
   def nextch: Unit;
