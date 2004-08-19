5f9cb270e802e2e667adf4e985f92a1d200065fd
buraq <buraq@epfl.ch>
2004-08-19 17:16:44 +0000

    added constructors from scala.io.Source

--- sources/scala/xml/parsing/ConstructingParser.scala
+++ sources/scala/xml/parsing/ConstructingParser.scala
@@ -1,5 +1,20 @@
 package scala.xml.parsing ;
 
+object ConstructingParser {
+  def fromSource(inp: scala.io.Source): ConstructingParser =
+    fromSource(new ConstructingHandler(), inp);
+
+  def fromSource(theHandle: ConstructingHandler, inp: scala.io.Source): ConstructingParser  = {
+    val p = new ConstructingParser() {
+      val input = inp;
+      override val handle = theHandle;
+      def nextch = { ch = input.next; pos = input.pos; }
+      override val preserveWS = true;
+    };
+    p.nextch;
+    p
+  }
+}
 
 /** an xml parser. parses XML and invokes callback methods of a MarkupHandler
  */
@@ -7,16 +22,10 @@ abstract class ConstructingParser extends MarkupParser[Node] {
 
   val handle = new ConstructingHandler();
 
-  //val enableEmbeddedExpressions: Boolean;
-
   /** report a syntax error */
   def reportSyntaxError(str: String): Unit = throw FatalError(str);
 
   /** this method assign the next character to ch and advances in input */
   def nextch: Unit;
-
-  /** this method should assign the first character of the input to ch */
-  def init: Unit;
-
 
 }
