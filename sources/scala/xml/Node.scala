b586442ff3d3b1e37bb486973cfc690a9d028431
buraq <buraq@epfl.ch>
2004-08-20 10:32:08 +0000

    change in doc

--- sources/scala/xml/Node.scala
+++ sources/scala/xml/Node.scala
@@ -37,7 +37,8 @@ abstract class Node extends NodeSeq {
   final def apply(i: Int) = if( i==0 ) this else error("singleton sequence");
   */
 
-  /** used internally. Text = -1 PI = -2 Comment = -3 CDATA = -4 EntityRef = -5 */
+  /** used internally. Text = -1 PI = -2 Comment = -3 CDATA = -4 EntityRef = -5
+   */
   def typeTag$:Int = 0;
 
   /** QName (the label of this node). I.e. "foo" for &lt;foo/&gt;) */
