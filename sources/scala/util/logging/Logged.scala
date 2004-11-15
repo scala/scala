7457a6092e49775542c22089aa91129fbd19f722
buraq <buraq@epfl.ch>
2004-11-15 17:26:48 +0000

    comment

--- sources/scala/util/logging/Logged.scala
+++ sources/scala/util/logging/Logged.scala
@@ -2,8 +2,17 @@ package scala.util.logging;
 
 /**
  *  Mixing in the trait Logged indicates that a class provides support
- *  for logging.
- */
+ *  for logging. For instance, a developer of a library writes
+ *  <code>
+   class MyClass with Logged { ... do stuff, call log }
+   </code>
+   *
+   * The user of the library instantiates:
+   <code>
+   val x = new MyClass() with ConsoleLogger;
+   </code>
+   * and the logging will be sent to the Console.
+   */
 trait Logged {
   /** this method should log the message given as argument somewhere
    * as a side-effect
