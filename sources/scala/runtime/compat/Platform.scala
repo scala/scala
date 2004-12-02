9eb2d3fa77e582aca1098836633903fc7e53be81
mihaylov <mihaylov@epfl.ch>
2004-12-02 16:25:20 +0000

    - Generalized exceptions from java.lang.Excepti...
    - Generalized exceptions from java.lang.Exception to java.lang.Throwable

--- sources/scala/runtime/compat/Platform.scala
+++ sources/scala/runtime/compat/Platform.scala
@@ -14,8 +14,8 @@ package scala.runtime.compat;
 object Platform {
   def getClass(obj: AnyRef) = obj.getClass();
   def getClassName(obj: AnyRef) = obj.getClass().getName();
-  def printStackTrace(exc: java.lang.Exception) = exc.printStackTrace();
-  def getMessage(exc: java.lang.Exception) = exc.getMessage();
+  def printStackTrace(exc: java.lang.Throwable) = exc.printStackTrace();
+  def getMessage(exc: java.lang.Throwable) = exc.getMessage();
   def split(str: String, separator: Char): Array[String] = {
     str.split(separator.toString());
   }
