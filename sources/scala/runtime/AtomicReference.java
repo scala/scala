594fd5991600e6b50393d18c22b3d2351f862418
michelou <michelou@epfl.ch>
2005-02-09 15:39:23 +0000

    - added missing header.

--- sources/scala/runtime/AtomicReference.java
+++ sources/scala/runtime/AtomicReference.java
@@ -1,8 +1,18 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2005, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id$
+
 package scala.runtime;
 
 /**
  * Blocking (i.e. non-atomic) placeholder for Java 1.5's
- * java.util.concurrent.atomic.AtomicReference class.
+ * <code>java.util.concurrent.atomic.AtomicReference</code> class.
  */
 
 public class AtomicReference implements java.io.Serializable {
