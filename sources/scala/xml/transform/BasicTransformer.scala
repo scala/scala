2dfa8272da4b1fba6a09199f8730aab18c27c273
buraq <buraq@epfl.ch>
2005-04-19 14:09:38 +0000

    bugfix

--- sources/scala/xml/transform/BasicTransformer.scala
+++ sources/scala/xml/transform/BasicTransformer.scala
@@ -77,7 +77,7 @@ trait BasicTransformer with Function1[Node,Node] {
       if(ch.eq(nch))
 	n
       else
-	Elem(n.namespace, n.label, n.attributes, n.scope, nch:_*)
+	Elem(n.prefix, n.label, n.attributes, n.scope, nch:_*)
     }
   }
 
