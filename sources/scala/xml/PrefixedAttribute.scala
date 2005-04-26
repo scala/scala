31b680f267bc76cd6e338e22ec885574181a7d23
buraq <buraq@epfl.ch>
2005-04-26 11:03:53 +0000

    resurrected

---
+++ sources/scala/xml/PrefixedAttribute.scala
@@ -0,0 +1,63 @@
+package scala.xml;
+
+/** prefixed attributes always have a non-null namespace
+ */
+class PrefixedAttribute(val pre: String, val key: String, val value: String, val next: MetaData) extends MetaData {
+
+  /** returns a copy of this unprefixed attribute with the given next field*/
+  def copy(next: MetaData) =
+    new PrefixedAttribute(pre, key, value, next);
+
+  //** duplicates the MetaData (deep copy), not preserving order */
+  //def deepCopy: MetaData = deepCopy(null);
+
+  //** duplicates the MetaData (deep copy), prepending it to tail */
+  /*
+  def deepCopy(tail:MetaData): MetaData = {
+    val md = copy(tail);
+    if(null == next)
+      md
+    else
+      next.deepCopy(md)
+  }
+  */
+
+  def equals1(m:MetaData) = m.isPrefixed && (m.asInstanceOf[PrefixedAttribute].pre == pre) && (m.key == key) && (m.value == value);
+
+  def getNamespace(owner: Node) =
+    owner.getNamespace(pre);
+
+  /** forwards the call to next */
+  def getValue(key: String): String = next.getValue(key);
+
+  /** gets attribute value of qualified (prefixed) attribute with given key
+   */
+  def getValue(namespace: String, scope: NamespaceBinding, key: String): String = {
+    if(key == this.key && scope.getURI(pre) == namespace)
+      value
+    else
+      next.getValue(namespace, scope, key);
+  }
+
+  /** returns true */
+  final def isPrefixed = true;
+
+  override def hashCode() =
+    pre.hashCode() * 41 + key.hashCode() * 7 + value.hashCode() * 3 + next.hashCode();
+
+
+  def toString1(sb:StringBuffer): Unit = {
+    sb.append(pre);
+    sb.append(':');
+    sb.append(key);
+    sb.append('=');
+    Utility.appendQuoted(value, sb);
+  }
+
+  def wellformed(scope: NamespaceBinding): Boolean = {
+    (null == next.getValue(scope.getURI(pre), scope, key)
+     && next.wellformed(scope));
+  }
+
+}
+
---
+++ sources/scala/xml/UnprefixedAttribute.scala
@@ -0,0 +1,44 @@
+package scala.xml;
+
+/** unprefixed attributes have the null namespace
+ */
+class UnprefixedAttribute(val key: String, val value: String, val next: MetaData) extends MetaData {
+
+  /** returns a copy of this unprefixed attribute with the given next field*/
+  def copy(next: MetaData) =
+    new UnprefixedAttribute(key, value, next);
+
+  def equals1(m:MetaData) = !m.isPrefixed && (m.key == key) && (m.value == value);
+
+  /** returns null */
+  final def getNamespace(owner: Node): String =
+    null;
+
+  /** gets value of unqualified (unprefixed) attribute with given key */
+  def getValue(key: String): String =
+    if(key == this.key)
+      value
+    else
+      next.getValue(key);
+
+  /** forwards the call to next */
+  def getValue(namespace: String, scope: NamespaceBinding, key: String): String =
+    next.getValue(namespace, scope, key);
+
+  override def hashCode() =
+    key.hashCode() * 7 + value.hashCode() * 53 + next.hashCode();
+
+  /** returns false */
+  final def isPrefixed = false;
+
+  def toString1(sb:StringBuffer): Unit = {
+    sb.append(key);
+    sb.append('=');
+    Utility.appendQuoted(value, sb);
+  }
+
+  def wellformed(scope: NamespaceBinding): Boolean =
+    (null == next.getValue(null, scope, key)) && next.wellformed(scope);
+
+}
+
