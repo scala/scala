ece0d0ed89a97f1c5d98eaf9f5a926680f5dbc0f
schinz <schinz@epfl.ch>
2005-05-11 18:32:25 +0000

    - bug fix: made the first type argument covariant

--- sources/scala/$colon$colon.scala
+++ sources/scala/$colon$colon.scala
@@ -18,7 +18,7 @@ import Predef._;
  *  @author  Martin Odersky
  *  @version 1.0, 15/07/2003
  */
-final case class ::[b](hd: b, tl: List[b]) extends List[b] with Serializable {
+final case class ::[+b](hd: b, tl: List[b]) extends List[b] with Serializable {
     private val serialVersionUID = 0 - 8476791151983527571L;
     def isEmpty: boolean = false;
     def head: b = hd;
