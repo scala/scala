3577a16ffe5dfc650843d4fc5634f8530bd52144
mihaylov <mihaylov@epfl.ch>
2005-06-13 08:39:28 +0000

    *** empty log message ***

--- sources/scala/SerialVersionUID.scala
+++ sources/scala/SerialVersionUID.scala
@@ -4,9 +4,9 @@
 **  __\ \/ /__/ __ |/ /__/ __ |                                         **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
-** $Id: Attribute.scala
+** $Id$
 */
+
 package scala;
 
 case class SerialVersionUID(uid: Long) extends Attribute {}
-
