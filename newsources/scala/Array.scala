f683124427597d67f7eb7781d38453310870eaa5
Martin Odersky <odersky@gmail.com>
2005-06-24 16:10:32 +0000

    *** empty log message ***

---
+++ newsources/scala/Array.scala
@@ -0,0 +1,24 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002, LAMP/EPFL                  **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id$
+
+package scala;
+
+/** @meta class [?T] extends scala.AnyRef with java.lang.Cloneable with java.io.Serializable; */
+abstract class Array[T](val length: Int) extends Cloneable with java.io.Serializable with Seq[T] {
+
+  def apply(i: Int): T;
+  def update(i: Int, x: T): Unit;
+
+  def elements = new Iterator[T] {
+    var index = 0;
+    def hasNext: Boolean = index < length;
+    def next: T = { val i = index; index = i + 1; apply(i) }
+  }
+}
