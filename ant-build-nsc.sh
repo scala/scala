35b8aa223750ca82e6bc7d3aee825ac6bcdefdf2
Burak Emir <emir@epfl.ch>
2005-06-29 10:13:02 +0000

    separated the paths to properties file

--- ant-build-nsc.sh
+++ ant-build-nsc.sh
@@ -2,16 +2,28 @@
 
 export ANT_OPTS=-Xmx256m
 
-CLASSPATH=/home/linuxsoft/apps/fjbg/jars/fjbg.jar
-CLASSPATH=$CLASSPATH:/home/linuxsoft/apps/jaco/lib/jaco.jar
-CLASSPATH=$CLASSPATH:/home/linuxsoft/apps/fjbg/jars/fjbg.jar
-CLASSPATH=$CLASSPATH:/localhome/buraq/scala/objects/main/lib/scala.jar
-CLASSPATH=$CLASSPATH:/localhome/buraq/scala/objects/main/lib/tools.jar
-CLASSPATH=$CLASSPATH:/home/linuxsoft/apps/jaco/lib/jaco.jar
-CLASSPATH=$CLASSPATH:/tmp/hackedPico
-CLASSPATH=$CLASSPATH:/tmp/jars/nsc4ant.jar:/tmp/jars/nsc.jar
+awk '/^$/ {next;} /^#/ {next;} {print "export " $1 $2 $3}' build-nsc.properties > env.sh
 
+. env.sh
+
+# ------- don't change these, change build-nsc.properties instead
+
+# jars for `scalac' task
+CLASSPATH=${fjbg_jar}
+CLASSPATH=$CLASSPATH:${scala_jar}
+CLASSPATH=$CLASSPATH:${tools_jar}
+
+# jars for `pico' task including the `-scala-hacks' enableda
+CLASSPATH=$CLASSPATH:${jaco_jar}
+CLASSPATH=$CLASSPATH:${hacked_pico_dir}
+
+# jars for `nsc' task (once its compiled)
+CLASSPATH=$CLASSPATH:${jars_dir}/nsc4ant.jar:${jars_dir}/nsc.jar
 
 export CLASSPATH
+
+# for debugging your classpath
+
+#echo $CLASSPATH
 
 ant -f build-nsc.xml $*
