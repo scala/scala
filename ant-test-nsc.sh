adde8def57e2478a5bc6f17abd4ba10fd0ca4085
Burak Emir <emir@epfl.ch>
2005-07-30 21:40:09 +0000

    testing the result of ant-build-nsc

---
+++ ant-test-nsc.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+. ant-common.sh
+
+# jar with fjbg, scala runtime
+if ! addJar $fjbg_jar fjbg_jar;  then exit -1; fi
+if ! addJar $tools_jar tools_jar;  then exit -1; fi
+if ! addJar $scala_jar scala_jar; then echo "try: make jar target=LIBRARY" && exit -1; fi
+
+# jars for `nsc' task (once its compiled)
+if ! addJar $jars_dir/nsc4ant.jar;   then exit -1; fi
+if ! addJar $jars_dir/nsc.jar;       then exit -1; fi
+
+export CLASSPATH
+
+# for debugging your classpath
+#echo $CLASSPATH
+
+ant -f test-nsc.xml $*
