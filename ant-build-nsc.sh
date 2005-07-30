f505a2d5a2fd2114f3704ecf02a0ad29ebe0923d
Burak Emir <emir@epfl.ch>
2005-07-30 21:42:47 +0000

    clean up

--- ant-build-nsc.sh
+++ ant-build-nsc.sh
@@ -10,14 +10,9 @@ if ! addJar $tools_jar tools_jar; then echo "try: make jar target=TOOLS" && exit
 # jars for `pico' task MUST EXIST including the `-scala-hacks' enabled
 if ! addJar $jaco_jar jaco_jar;  then exit -1; fi 
 
-# jars for `nsc' task (once its compiled)
-if ! addJar $jars_dir/nsc4ant.jar;   then echo "{ *** WARNING: $jars_dir/nsc4ant.jar doesn't exist}"; fi
-if ! addJar $jars_dir/nsc.jar;       then echo "{ *** WARNING: $jars_dir/nsc.jar doesn't exist}"; fi
-
 export CLASSPATH
 
 # for debugging your classpath
-
 #echo $CLASSPATH
 
 ant -f build-nsc.xml $*
