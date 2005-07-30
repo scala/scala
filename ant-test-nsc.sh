6113fda69769064827137586a01e082f1eaf4d2d
Burak Emir <emir@epfl.ch>
2005-07-30 21:43:09 +0000

    cleanup

--- ant-test-nsc.sh
+++ ant-test-nsc.sh
@@ -8,8 +8,8 @@ if ! addJar $tools_jar tools_jar;  then exit -1; fi
 if ! addJar $scala_jar scala_jar; then echo "try: make jar target=LIBRARY" && exit -1; fi
 
 # jars for `nsc' task (once its compiled)
-if ! addJar $jars_dir/nsc4ant.jar;   then exit -1; fi
-if ! addJar $jars_dir/nsc.jar;       then exit -1; fi
+if ! addJar $jars_dir/nsc4ant.jar "jars_dir containing nsc4ant";   then echo "try 'sh ant-build-nsc.sh build.nsc4'" && exit -1; fi
+if ! addJar $jars_dir/nsc.jar "jars_dir containing nsc.jar";       then echo "try 'sh ant-build-nsc.sh'" && exit -1; fi
 
 export CLASSPATH
 
