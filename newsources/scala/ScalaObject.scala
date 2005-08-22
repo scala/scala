d5fec7cd487bab5d46dff8b918e355b518142fc4
michelou <michelou@epfl.ch>
2005-08-22 11:38:57 +0000

    - updated file header.

--- newsources/scala/ScalaObject.scala
+++ newsources/scala/ScalaObject.scala
@@ -1,6 +1,6 @@
 /*                     __                                               *\
 **     ________ ___   / /  ___     Scala API                            **
-**    / __/ __// _ | / /  / _ |    (c) 2002, LAMP/EPFL                  **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2005, LAMP/EPFL             **
 **  __\ \/ /__/ __ |/ /__/ __ |                                         **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
@@ -12,8 +12,9 @@ package scala;
 
 trait ScalaObject extends AnyRef {
 
-    /** This method is needed for optimizing pattern matching expressions
-     *  which match on constructors of case classes.
-     */
-    def $tag(): Int = 0;
+  /** This method is needed for optimizing pattern matching expressions
+   *  which match on constructors of case classes.
+   */
+  def $tag(): Int = 0;
+
 }
