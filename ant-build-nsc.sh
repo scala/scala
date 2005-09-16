92c43defc4add3a23353b8a26cf12330741fee5a
michelou <michelou@epfl.ch>
2005-09-16 11:23:10 +0000

    - classpath must be handled differently on Cygwin.

--- ant-build-nsc.sh
+++ ant-build-nsc.sh
@@ -10,11 +10,23 @@
 # ant build
 
 CLASSPATH=$nsc_fjbg_jar:$nsc_scala_jar:$nsc_tools_jar:$nsc_jaco_jar
+PLATFORM=unix
+
+# For Cygwin, switch paths to appropriate format before running ant
+if $cygwin; then
+    if [ "$OS" = "Windows_NT" ] && cygpath -m .>/dev/null 2>/dev/null ; then
+        format=mixed
+    else
+        format=windows
+    fi
+    CLASSPATH=`cygpath --path --$format "$CLASSPATH"`
+    PLATFORM=win
+fi
 
 # for debugging your classpath
 #echo CLASSPATH=$CLASSPATH
 
-CLASSPATH="$CLASSPATH" $ANT_CMD -Dplatform=unix -f "$ANT_BUILDFILE" $*
+CLASSPATH="$CLASSPATH" $ANT_CMD -Dplatform=$PLATFORM -f "$ANT_BUILDFILE" $*
 $RM "$ANT_BUILDFILE"
 
 ##############################################################################
