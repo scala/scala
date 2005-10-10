e093d72b2f4eb1bf65beafbd6aff1023e3dd1511
Martin Odersky <odersky@gmail.com>
2005-10-10 12:09:07 +0000

    *** empty log message ***

---
+++ sources/scala/reflect/Code.scala
@@ -0,0 +1,18 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2004, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+** $Id$
+\*                                                                      */
+package scala.reflect;
+
+abstract class Code;
+
+case class Ident(sym: Symbol) extends Code;
+case class Apply(fun: Code, args: List[Code]) extends Code;
+case class TypeApply(fun: Code, args: List[Type]) extends Code;
+case class Select(qual: Code, sym: Symbol) extends Code;
+case class Function(params: List[Symbol], body: Code) extends Code;
+
---
+++ sources/scala/reflect/Type.scala
@@ -0,0 +1,22 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2004, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+** $Id$
+\*                                                                      */
+package scala.reflect;
+
+abstract class Type;
+
+case object NoPrefix extends Type;
+case object NoType extends Type;
+
+case class TypeIdent(pre: Type, sym: Symbol) extends Type;
+case class SingleType(pre: Type, sym: Symbol) extends Type;
+case class ThisType(clazz: Symbol) extends Type;
+case class AppliedType(tpe: Type, args: List[Type]) extends Type;
+case class TypeBounds(lo: Type, hi: Type) extends Type;
+case class MethodType(formals: List[Type], restpe: Type) extends Type;
+class ImplicitMethodType(formals: List[Type], restpe: Type) extends MethodType(formals, restpe);
