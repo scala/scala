5a5a3eb0e1179b718687851b5d5eae505192b9a1
michelou <michelou@epfl.ch>
2005-10-17 11:21:59 +0000

    - updated lists of Scala commands.

--- config/izpack/create_wrappers.sh
+++ config/izpack/create_wrappers.sh
@@ -3,11 +3,9 @@
 DIRNAME="dirname"
 LN="/bin/ln -sf"
 
-COMMANDS="scala scalac scaladoc scala-info scalaint scalansc scalanstest scalap scalarun scalatest"
-COMMANDS_DEBUG="scala-debug scalac-debug scaladoc-debug scalaint-debug scalansc-debug scalarun-debug"
+COMMANDS="scala scalac scaladoc scalap"
 
 cd `$DIRNAME $0` && \
-for cmd in $COMMANDS; do $LN .scala_wrapper $cmd; done && \
-for cmd in $COMMANDS_DEBUG; do $LN .scala_wrapper $cmd; done
+for cmd in $COMMANDS; do $LN .nsc_wrapper $cmd; done
 
 # $Id$
