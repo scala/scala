a34d33eecbbc80ef5d93b6a09f9438459533d846
michelou <michelou@epfl.ch>
2005-11-16 09:26:37 +0000

    - removed leading tabs.

--- sources/scala/dbc/Syntax.scala
+++ sources/scala/dbc/Syntax.scala
@@ -8,32 +8,37 @@
 
 package scala.dbc;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
+import java.math.{BigDecimal, BigInteger};
 
+
+/** This class ..
+ *
+ */
 object Syntax {
 
   import syntax.DataTypeUtil;
 
-	/* Data types */
-	def boolean = DataTypeUtil.boolean;
-	def tinyint = DataTypeUtil.tinyint;
-	def smallint = DataTypeUtil.smallint;
-	def integer = DataTypeUtil.integer;
-	def bigint = DataTypeUtil.bigint;
-	def numeric (precision:Int) = DataTypeUtil.numeric(precision);
-	def numeric (precision:Int, scale:Int) = DataTypeUtil.numeric(precision, scale);
-	def real = DataTypeUtil.real;
-	def doublePrecision = DataTypeUtil.doublePrecision;
-	def character (length: Int) = DataTypeUtil.character(length);
-	def characterVarying (length: Int) = DataTypeUtil.characterVarying(length);
-	def characterLargeObject = DataTypeUtil.characterLargeObject;
+  /* Data types */
+  def boolean  = DataTypeUtil.boolean;
+  def tinyint  = DataTypeUtil.tinyint;
+  def smallint = DataTypeUtil.smallint;
+  def integer  = DataTypeUtil.integer;
+  def bigint   = DataTypeUtil.bigint;
+  def real     = DataTypeUtil.real;
 
-	/* Statements */
-	//def select
+  def numeric(precision: Int) = DataTypeUtil.numeric(precision);
+  def numeric(precision: Int, scale: Int) = DataTypeUtil.numeric(precision, scale);
 
-	/* Other stuff */
-	def database (server:String, username:String, password:String): dbc.Database =
-		syntax.Database.database(server, username, password);
+  def doublePrecision = DataTypeUtil.doublePrecision;
+  def character(length: Int) = DataTypeUtil.character(length);
+  def characterVarying(length: Int) = DataTypeUtil.characterVarying(length);
+  def characterLargeObject = DataTypeUtil.characterLargeObject;
+
+  /* Statements */
+  //def select
+
+  /* Other stuff */
+  def database (server: String, username: String, password: String): dbc.Database =
+    syntax.Database.database(server, username, password);
 
 }
