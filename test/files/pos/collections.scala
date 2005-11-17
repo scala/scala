cbd29e36277770bd6cc50c1d3aca478118e6361c
Sean McDirmid <sean.mcdirmid@gmail.com>
2005-11-17 18:21:33 +0000

    Test case for abstract override bug in NSC.

---
+++ test/files/pos/collections.scala
@@ -0,0 +1,15 @@
+package mixins;
+
+import scala.collection.mutable._;
+
+class Collections extends HashSet[Int] with ObservableSet[Int,Collections] {
+  override def +=(elem: Int): Unit = super.+=(elem);
+  override def -=(elem: Int): Unit = super.-=(elem);
+  override def clear: Unit = super.clear;
+
+}
+
+object collections extends Collections;
+
+class Collections1 extends HashSet[Int] with ObservableSet[Int,Collections1];
+object collections1 extends Collections1;
