9bcec1fcbd498aa28a9db73fb95e5a0bad95d60f
michelou <michelou@epfl.ch>
2005-11-29 12:03:39 +0000

    - updated file header

--- sources/scala/reflect/Print.scala
+++ sources/scala/reflect/Print.scala
@@ -1,11 +1,13 @@
 /*                     __                                               *\
 **     ________ ___   / /  ___     Scala API                            **
-**    / __/ __// _ | / /  / _ |    (c) 2002-2004, LAMP/EPFL             **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2005, LAMP/EPFL             **
 **  __\ \/ /__/ __ |/ /__/ __ |                                         **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
-** $Id$
 \*                                                                      */
+
+// $Id$
+
 package scala.reflect;
 
 object Print extends Function1[Any, String] {
--- sources/scala/reflect/Type.scala
+++ sources/scala/reflect/Type.scala
@@ -1,11 +1,13 @@
 /*                     __                                               *\
 **     ________ ___   / /  ___     Scala API                            **
-**    / __/ __// _ | / /  / _ |    (c) 2002-2004, LAMP/EPFL             **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2005, LAMP/EPFL             **
 **  __\ \/ /__/ __ |/ /__/ __ |                                         **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
-** $Id$
 \*                                                                      */
+
+// $Id$
+
 package scala.reflect;
 
 abstract class Type;
@@ -15,20 +17,28 @@ case object NoType extends Type;
 
 /** fullname */
 case class NamedType(fullname: String) extends Type;
+
 /** pre # sym */
 case class PrefixedType(pre: Type, sym: Symbol) extends Type;
+
 /** pre.type # sym == pre.sym */
 case class SingleType(pre: Type, sym: Symbol) extends Type;
+
 /** clazz.this */
 case class ThisType(clazz: Symbol) extends Type;
+
 /** clazz.super[superClazz] */
 /** tpe[args1, ..., argsn] */
 case class AppliedType(tpe: Type, args: List[Type]) extends Type;
+
 /** [a &lt;: lo &gt;: hi] */
 case class TypeBounds(lo: Type, hi: Type) extends Type;
+
 /** (formals1 ... formalsn) restpe */
 case class MethodType(formals: List[Type], restpe: Type) extends Type;
+
 /**  */
 case class PolyType(typeParams: List[Symbol], typeBounds: List[Pair[Type, Type]], resultType: Type) extends Type;
+
 /**  */
 class ImplicitMethodType(formals: List[Type], restpe: Type) extends MethodType(formals, restpe);
