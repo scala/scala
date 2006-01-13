191e3d5e23c634d4730bae817073dcb6b6f3472f
michelou <michelou@epfl.ch>
2006-01-13 15:16:05 +0000

    - prepended "_" to variable names in batch command

--- support/windows/scala_wrapper-footer.bat
+++ support/windows/scala_wrapper-footer.bat
@@ -1,29 +1,29 @@
 
-if "%SCALA_HOME%" == "" goto error1
-if not exist "%SCALA_HOME%\VERSION-%VERSION%" goto error2
+if "%_PREFIX%"=="" goto error1
+if not exist "%_PREFIX%\VERSION-%_VERSION%" goto error2
 
-set ARGS=
+set _ARGS=
 
 :loop
-if '%1' == '' goto exec
-set ARGS=%ARGS% %1
+if "%1"=="" goto exec
+set _ARGS=%_ARGS% %1
 shift
 goto loop
 
 :exec
-%COMMAND% %ARGS%
+%_COMMAND% %_ARGS%
 goto end
 
 :error1
-echo ERROR: environment variable SCALA_HOME is undefined. It should point to the directory containing the file "VERSION-%VERSION%".
+echo ERROR: environment variable SCALA_HOME is undefined. It should point to the directory containing the file "VERSION-%_VERSION%".
 goto end
 
 :error2
-echo ERROR: environment variable SCALA_HOME points to the wrong directory "%SCALA_HOME%". It should point to the directory containing the file "VERSION-%VERSION%".
+echo ERROR: environment variable SCALA_HOME points to the wrong directory "%_PREFIX%". It should point to the directory containing the file "VERSION-%_VERSION%".
 goto end
 
 :end
-set VERSION=
-set COMMAND=
+set _VERSION=
+set _COMMAND=
 
 if "%OS%"=="Windows_NT" @endlocal
