0a1598f285e9fe3107c143bba0475cbd201f2fa8
michelou <michelou@epfl.ch>
2006-03-18 21:33:39 +0000

    added missing file scala/runtime/NonLocalReturn...
    added missing file scala/runtime/NonLocalReturnException.scala

---
+++ src/library/scala/runtime/NonLocalReturnException.scala
@@ -0,0 +1,13 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2006, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id: $
+
+package scala.runtime
+
+class NonLocalReturnException[T](val key: Object, val value: T) extends RuntimeException
