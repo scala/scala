1e93a4694fdda4bf464d2b1dfe2f1c0570ba24a2
michelou <michelou@epfl.ch>
2006-03-24 11:06:56 +0000

    added header in file scala/tools/nsc/EvalLoop.s...
    added header in file scala/tools/nsc/EvalLoop.scala

--- src/compiler/scala/tools/nsc/EvalLoop.scala
+++ src/compiler/scala/tools/nsc/EvalLoop.scala
@@ -1,15 +1,22 @@
-package scala.tools.nsc;
+/* NSC -- new Scala compiler
+ * Copyright 2005-2006 LAMP/EPFL
+ * @author  Martin Odersky
+ */
+// $Id$
+
+package scala.tools.nsc
 
 trait EvalLoop {
 
-  def prompt: String;
+  def prompt: String
 
   def loop(action: (String) => Unit): Unit = {
-    Console.print(prompt);
-    val line = Console.readLine;
+    Console.print(prompt)
+    val line = Console.readLine
     if (line != null && line.length() > 0) {
-      action(line);
-      loop(action);
+      action(line)
+      loop(action)
     }
   }
+
 }
