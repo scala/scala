0b47ca3e5bb8d724240b49b970f4017d6a8798a9
michelou <michelou@epfl.ch>
2006-04-27 15:40:29 +0000

    updated copyright notice and removed semicolons

--- src/compiler/scala/tools/nsc/CompilerRun.scala
+++ src/compiler/scala/tools/nsc/CompilerRun.scala
@@ -1,21 +1,22 @@
-/* NSC -- new scala compiler
- * Copyright 2005 LAMP/EPFL
+/* NSC -- new Scala compiler
+ * Copyright 2005-2006 LAMP/EPFL
  * @author  Martin Odersky
  */
 // $Id$
-package scala.tools.nsc;
+
+package scala.tools.nsc
 
 class CompilerRun {
-  def firstPhase: Phase = NoPhase;
-  def terminalPhase: Phase = NoPhase;
-  def namerPhase: Phase = NoPhase;
-  def typerPhase: Phase = NoPhase;
-  def refchecksPhase: Phase = NoPhase;
-  def explicitOuterPhase: Phase = NoPhase;
-  def erasurePhase: Phase = NoPhase;
-  def flattenPhase: Phase = NoPhase;
-  def mixinPhase: Phase = NoPhase;
-  def icodePhase: Phase = NoPhase;
-  def phaseNamed(name: String): Phase = NoPhase;
+  def firstPhase: Phase = NoPhase
+  def terminalPhase: Phase = NoPhase
+  def namerPhase: Phase = NoPhase
+  def typerPhase: Phase = NoPhase
+  def refchecksPhase: Phase = NoPhase
+  def explicitOuterPhase: Phase = NoPhase
+  def erasurePhase: Phase = NoPhase
+  def flattenPhase: Phase = NoPhase
+  def mixinPhase: Phase = NoPhase
+  def icodePhase: Phase = NoPhase
+  def phaseNamed(name: String): Phase = NoPhase
 }
 
