7199ea34abfcc9c45147e6c5f6a18afa38cd3f46
michelou <michelou@epfl.ch>
2006-05-12 16:27:50 +0000

    fixed error in file build.xml
    fixed IBM J9 related error in file test/clitest

---
+++ test/files/cli/test1/Main.check.java
@@ -0,0 +1,6 @@
+Unrecognized option: -cpp
+Could not create the Java virtual machine.
+1: test 1 passed (1)
+1: test 2 passed (1)
+1: test 3 passed (1)
+1: test 4 passed (2)
---
+++ test/files/cli/test3/Main.check.java
@@ -0,0 +1,10 @@
+Unrecognized option: -cpp
+Could not create the Java virtual machine.
+3: 1: test 1 passed (1)
+3: 2: 1: test 1 passed (1)
+3: 1: test 2 passed (1)
+3: 2: 1: test 2 passed (1)
+3: 1: test 3 passed (1)
+3: 2: 1: test 3 passed (1)
+3: 1: test 4 passed (2)
+3: 2: 1: test 4 passed (2)
