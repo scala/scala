1918e7230b7da9ca9b45fd465a29ee1609feed04
michelou <michelou@epfl.ch>
2006-05-27 07:55:02 +0000

    adapted script fact.scala to Cygwin

--- test/files/script/fact.scala
+++ test/files/script/fact.scala
@@ -2,7 +2,22 @@
 # fact - A simple Scala script that prints out the factorial of
 #        the argument specified on the command line.
 
-exec scalascript "$0" "$@"
+cygwin=false;
+case "`uname`" in
+    CYGWIN*) cygwin=true ;;
+esac
+
+SOURCE=$0;
+if $cygwin; then
+    if [ "$OS" = "Windows_NT" ] && cygpath -m .>/dev/null 2>/dev/null ; then
+        format=mixed
+    else
+        format=windows
+    fi
+    SOURCE=`cygpath --$format "$SOURCE"`;
+fi
+
+exec scalascript $SOURCE "$@"
 !#
 
 
