e5556bbbe0821456afbe9c0a55f4de53ed05d4ad
michelou <michelou@epfl.ch>
2006-06-13 09:25:08 +0000

    removed leading/trailing tabs/spaces

--- src/compiler/scala/tools/nsc/MainTokenMetric.scala
+++ src/compiler/scala/tools/nsc/MainTokenMetric.scala
@@ -1,61 +1,62 @@
-/* NSC -- new scala compiler
- * Copyright 2005 LAMP/EPFL
+/* NSC -- new Scala compiler
+ * Copyright 2005-2006 LAMP/EPFL
  * @author  Martin Odersky
  */
 // $Id$
-package scala.tools.nsc;
 
-//import java.io._;
-import scala.tools.nsc.reporters.{Reporter, ConsoleReporter};
+package scala.tools.nsc
+
+//import java.io._
+import scala.tools.nsc.reporters.{Reporter, ConsoleReporter}
 
 /** The main class for NSC, a compiler for the programming
  *  language Scala.
  */
 object MainTokenMetric {
 
-  private var reporter: ConsoleReporter = _;
+  private var reporter: ConsoleReporter = _
 
   def tokenMetric(compiler: Global, fnames: List[String]): unit = {
-    import compiler.CompilationUnit;
-    import compiler.syntaxAnalyzer.Scanner;
-    import ast.parser.Tokens.EOF;
-    var totale = 0;
+    import compiler.CompilationUnit
+    import compiler.syntaxAnalyzer.Scanner
+    import ast.parser.Tokens.EOF
+    var totale = 0
     for (val source <- fnames) {
-      val s = new Scanner(new CompilationUnit(compiler.getSourceFile(source)));
-      var i = 0;
+      val s = new Scanner(new CompilationUnit(compiler.getSourceFile(source)))
+      var i = 0
       while(s.token != EOF) {
-        i = i + 1;
+        i = i + 1
         s.nextToken()
       }
       var j = 0 ; while(j + Math.log(i)/ Math.log(10) < 7) {
-        j = j+1;
-        Console.print(' ');
+        j = j+1
+        Console.print(' ')
       }
-      Console.print(i.toString());
-      Console.print(" ");
-      Console.println(source.toString());
-      totale = totale + i;
+      Console.print(i.toString())
+      Console.print(" ")
+      Console.println(source.toString())
+      totale = totale + i
     }
-    Console.println(totale.toString()+" total");
+    Console.println(totale.toString()+" total")
   }
 
   def process(args: Array[String]): unit = {
-    val command = new CompilerCommand(List.fromArray(args), error, false);
-    reporter = new ConsoleReporter();
+    val command = new CompilerCommand(List.fromArray(args), error, false)
+    reporter = new ConsoleReporter()
     try {
-      val compiler = new Global(command.settings, reporter);
-      tokenMetric(compiler, command.files);
+      val compiler = new Global(command.settings, reporter)
+      tokenMetric(compiler, command.files)
     } catch {
       case ex @ FatalError(msg) =>
         if (command.settings.debug.value)
           ex.printStackTrace();
-      reporter.error(null, "fatal error: " + msg);
+      reporter.error(null, "fatal error: " + msg)
     }
   }
 
   def main(args: Array[String]): unit = {
-    process(args);
-    System.exit(if (reporter.errors > 0) 1 else 0);
+    process(args)
+    System.exit(if (reporter.errors > 0) 1 else 0)
   }
 
 }
