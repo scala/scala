8599693b3c95d0d6687398a3f8d5e7203cc74864
Lex Spoon <lex@lexspoon.org>
2006-06-16 12:11:04 +0000

    use getMethod("main",blahblah) instead of getMe...
    use getMethod("main",blahblah) instead of getMethods().find(blahblah)

--- src/compiler/scala/tools/nsc/ObjectRunner.scala
+++ src/compiler/scala/tools/nsc/ObjectRunner.scala
@@ -13,16 +13,6 @@ import java.net.URLClassLoader
 
 /** An object that runs another object specified by name. */
 object ObjectRunner {
-
-  def isMainMethod(meth: Method): Boolean = {
-    def paramsOK(params: Array[Class]): Boolean =
-      params.length == 1 &&
-      params(0) == classOf[Array[String]]
-    meth.getName == "main" &&
-    Modifier.isStatic(meth.getModifiers) &&
-    paramsOK(meth.getParameterTypes)
-  }
-
   def run(
       classpath: List[String],
       objectName: String,
@@ -32,17 +22,13 @@ object ObjectRunner {
       val mainLoader = new URLClassLoader(classpathURLs, null)
       val clsToRun = Class.forName(objectName, true, mainLoader)
 
-      val method = clsToRun.getMethods.find(isMainMethod) match {
-        case Some(meth) =>
-          meth
-        case None =>
-          throw new Error("no main method in object " + objectName)
-      }
+      val method = clsToRun.getMethod("main", List(classOf[Array[String]]).toArray)
+
       val res = method.invoke(null, List(arguments.toArray).toArray)
       ()
     } catch {
       case e: Exception =>
-        // ClassNotFoundException, InvocationTargetException, ..
+        // ClassNotFoundException, InvocationTargetException, NoSuchMethod ..
         Console.println(e)
         exit(1)
     }
