4ac8886e43cf5969b4201d0dd6d32ca215f93011
michelou <michelou@epfl.ch>
2006-07-04 12:45:17 +0000

    added test for bug #648

---
+++ test/files/neg/bug648.scala
@@ -0,0 +1,14 @@
+import scala.tools.nsc.transform.Transform
+
+abstract class Detach extends Transform {
+  import global._
+  class DetachTransformer extends Transformer {
+    def foo: Scope = new Scope(
+      List[Symbol]() map { sym =>
+        val newAcc = sym.cloneSymbol
+        // def setFlag(mask: long): this.type
+        newAcc.setFlag(0)
+      }
+    )
+  }
+}
