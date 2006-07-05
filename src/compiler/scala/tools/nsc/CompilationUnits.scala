ce9000fb3a7ee2d0bc833d28321253c3e4417372
michelou <michelou@epfl.ch>
2006-07-05 14:54:51 +0000

    removed leading/trailing tabs/blanks in nsc/Com...
    removed leading/trailing tabs/blanks in nsc/CompilationUnits.scala

--- src/compiler/scala/tools/nsc/CompilationUnits.scala
+++ src/compiler/scala/tools/nsc/CompilationUnits.scala
@@ -1,40 +1,40 @@
-/* NSC -- new scala compiler
- * Copyright 2005 LAMP/EPFL
+/* NSC -- new Scala compiler
+ * Copyright 2005-2006 LAMP/EPFL
  * @author  Martin Odersky
  */
 // $Id$
 
-package scala.tools.nsc;
+package scala.tools.nsc
 
-import scala.tools.nsc.util.{SourceFile, Position};
-import scala.tools.nsc.util.FreshNameCreator;
-import scala.tools.nsc.io.AbstractFile;
-import scala.collection.mutable.HashSet;
+import scala.tools.nsc.util.{FreshNameCreator,Position,SourceFile}
+import scala.tools.nsc.io.AbstractFile
+import scala.collection.mutable.HashSet
 
 trait CompilationUnits requires Global {
 
   class CompilationUnit(val source: SourceFile) {
 
     /** the fresh name creator */
-    var fresh = new FreshNameCreator;
+    var fresh = new FreshNameCreator
 
     /** the content of the compilation unit in tree form */
-    var body: Tree = EmptyTree;
+    var body: Tree = EmptyTree
 
-    val depends = new HashSet[AbstractFile];
+    val depends = new HashSet[AbstractFile]
 
-    def position(pos: int) = new Position(source, pos);
+    def position(pos: int) = new Position(source, pos)
 
     val errorPositions = new HashSet[int]
 
-    def error(pos: int, msg: String) = {
+    def error(pos: int, msg: String) =
       if (!(errorPositions contains pos)) {
-        errorPositions += pos;
-        reporter.error(position(pos), msg);
+        errorPositions += pos
+        reporter.error(position(pos), msg)
       }
-    }
-    def warning(pos: int, msg: String) = reporter.warning(position(pos), msg);
-    override def toString() = source.toString();
+
+    def warning(pos: int, msg: String) = reporter.warning(position(pos), msg)
+
+    override def toString() = source.toString()
 
     def clear() = {
       fresh = null
