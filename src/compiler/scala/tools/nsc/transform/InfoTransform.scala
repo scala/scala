3e41f705d18ea6ef41c6e9c380db422fdc9bf105
michelou <michelou@epfl.ch>
2006-07-17 16:49:39 +0000

    removed leading/trailing tabs/blanks in transfo...
    removed leading/trailing tabs/blanks in transform/InfoTransform.scala

--- src/compiler/scala/tools/nsc/transform/InfoTransform.scala
+++ src/compiler/scala/tools/nsc/transform/InfoTransform.scala
@@ -1,28 +1,30 @@
-/* NSC -- new scala compiler
- * Copyright 2005 LAMP/EPFL
+/* NSC -- new Scala compiler
+ * Copyright 2005-2006 LAMP/EPFL
  * @author
  */
 // $Id$
-package scala.tools.nsc.transform;
+
+package scala.tools.nsc.transform
 
 /** A base class for transforms.
  *  A transform contains a compiler phase which applies a tree transformer.
  */
 abstract class InfoTransform extends Transform {
-  import global.{Symbol, Type, InfoTransformer, infoTransformers};
+  import global.{Symbol, Type, InfoTransformer, infoTransformers}
 
-  def transformInfo(sym: Symbol, tpe: Type): Type;
+  def transformInfo(sym: Symbol, tpe: Type): Type
 
-  override def newPhase(prev: scala.tools.nsc.Phase): StdPhase = new Phase(prev);
+  override def newPhase(prev: scala.tools.nsc.Phase): StdPhase =
+    new Phase(prev)
 
-  protected def changesBaseClasses = true;
+  protected def changesBaseClasses = true
 
   class Phase(prev: scala.tools.nsc.Phase) extends super.Phase(prev) {
     if (infoTransformers.nextFrom(id).pid != id) {
       val infoTransformer = new InfoTransformer {
-	val pid = id;
-	val changesBaseClasses = InfoTransform.this.changesBaseClasses;
-	def transform(sym: Symbol, tpe: Type): Type = transformInfo(sym, tpe);
+        val pid = id
+        val changesBaseClasses = InfoTransform.this.changesBaseClasses
+        def transform(sym: Symbol, tpe: Type): Type = transformInfo(sym, tpe)
       }
       infoTransformers.insert(infoTransformer)
     }
