e3efde8ea0a9e5d884bf20f6d3390565478e5767
Burak Emir <emir@epfl.ch>
2006-09-22 15:44:47 +0000

    revert for 1.4 compatibility

--- src/compiler/scala/tools/nsc/Settings.scala
+++ src/compiler/scala/tools/nsc/Settings.scala
@@ -67,9 +67,9 @@ class Settings(error: String => unit) {
   }
 
   private val encodingDefault =
-    java.nio.charset.Charset.defaultCharset.name()
-    //new java.io.OutputStreamWriter(
-    //  new java.io.ByteArrayOutputStream()).getEncoding
+    //java.nio.charset.Charset.defaultCharset.name() // bq: this exists only in Java1.5 :-(
+    new java.io.OutputStreamWriter(
+      new java.io.ByteArrayOutputStream()).getEncoding
 
   private val windowtitleDefault = "Scala Library Documentation"
   private val documenttitleDefault = "Scala 2"
