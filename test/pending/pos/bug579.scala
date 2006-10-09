bff27eb916c2cbd9030b1fc91cd99115702de337
Burak Emir <emir@epfl.ch>
2006-10-09 12:58:10 +0000

    updated

--- test/pending/pos/bug579.scala
+++ test/pending/pos/bug579.scala
@@ -12,4 +12,10 @@ object Test extends Application {
   val x = new MyBean;
   x.frombulizer = "hello"
 
+  x.setFrombulizer ("hola") // synthetic methods comes too late for typechecking this code
+
+  val z:String = x.frombulizer
+
+  val zz:String = x.getFrombulizer  // synthetic methods comes too late for typechecking this code
+
 }
