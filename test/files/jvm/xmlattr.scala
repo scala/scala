543c22858f0249425a64a53811c5fc1bcd21320b
Burak Emir <emir@epfl.ch>
2006-10-11 09:19:40 +0000

    attribute nillable

---
+++ test/files/jvm/xmlattr.scala
@@ -0,0 +1,23 @@
+import scala.xml._
+object foo extends Application {
+  {
+  var z:NodeSeq = null
+
+  Console.println(z)
+  Console.println(z==null)
+  var x = new UnprefixedAttribute("foo","bar", Null)
+  //x = new UnprefixedAttribute("foo", null:Seq[Node], x)
+  Console.println(x.toString)
+  Console.println(x.get("foo")) // Some(bar)
+  Console.println(x("foo")) // bar
+
+  val y = x.remove("foo")
+  Console.println(y.toString)
+  Console.println(y == Null) // true
+
+  x = new UnprefixedAttribute("foo", z, x)
+  Console.println(x.toString)
+  Console.println(x.get("foo")) // None
+  Console.println(x("foo")) // null
+  }
+}
