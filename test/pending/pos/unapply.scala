31e93255cbbc4dc99d4227aade3d2d1956830d1a
Burak Emir <emir@epfl.ch>
2006-10-27 13:51:37 +0000

    testcase 4 unapply

---
+++ test/pending/pos/unapply.scala
@@ -0,0 +1,28 @@
+case class MyTuple2[A,B](val _1:A, val _2:B)
+
+object Foo {
+  def unapply(x:Any): Option[Product2[Int,String]] = {
+    if(x.isInstanceOf[Bar]) {
+      val y = x.asInstanceOf[Bar]
+      Some(MyTuple2(y.size, y.name))
+    } else None
+  }
+
+  def main(args:Array[String]) = {
+    val b = new Bar
+    b match {
+      case Foo(s:Int,n:String) => Console.println("size "+s+" name "+n)
+    }
+    b.size = 54
+    b.name = "large"
+    b match {
+      case Foo(s:Int,n:String) => Console.println("size "+s+" name "+n)
+    }
+  }
+}
+
+class Bar {
+  var size: Int    = 50
+  var name: String = "medium"
+}
+
