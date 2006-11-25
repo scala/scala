9eb3282d5e910fda7d20638b4aab587ae945670f
Sean McDirmid <sean.mcdirmid@gmail.com>
2006-11-25 07:21:01 +0000

    Blah, SVN is evil.

---
+++ test/files/resident/bug597/Test.scala
@@ -0,0 +1,27 @@
+package test;
+
+abstract class Base {
+  type A <: Ax;
+
+  abstract class Ax {
+    def a = null;
+    def string = "A";
+  }
+}
+trait ExtB extends Base {
+  type A <: Ax;
+  trait Ax extends super.Ax {
+    def c = null;
+    override def string = super.string + "C";
+  }
+}
+
+trait ExtC extends /*ExtA with*/ ExtB {
+  type A <: Ax;
+  trait Ax extends super.Ax {
+    a
+    c
+    def d = null;
+    override def string = super.string + "D";
+  }
+}
