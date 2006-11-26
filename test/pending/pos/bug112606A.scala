e150c1dd7e8d0d312101bce65d0499500235d02d
Sean McDirmid <sean.mcdirmid@gmail.com>
2006-11-26 15:46:06 +0000

    Files related to bug reports

---
+++ test/pending/pos/bug112606A.scala
@@ -0,0 +1,25 @@
+package test;
+trait Test {
+  trait Global {
+    type Tree;
+    def get : Tree;
+  }
+  trait TreeBuilder {
+    val global : Global;
+    def set(tree : global.Tree) = {}
+  }
+  val nsc : Global;
+  trait FileImpl {
+    object treeBuilder extends TreeBuilder {
+      val global : nsc.type = nsc;
+    }
+    // ok
+    treeBuilder.set(nsc.get);
+  }
+  val file0 : FileImpl;
+  // ok
+  file0.treeBuilder.set(nsc.get);
+  def file : FileImpl;
+  // type mismatch
+  file.treeBuilder.set(nsc.get);
+}
