6474945c60b85f874f7f130d4b8b9637ce0de489
Martin Odersky <odersky@gmail.com>
2006-11-30 16:03:10 +0000

    added test files for bug835/bug836

---
+++ test/files/neg/bug835.scala
@@ -0,0 +1,3 @@
+object Test extends Application {
+  Console.println(List(List(1, 2, 3) : _*, List(4, 5, 6) : _*))
+}
---
+++ test/files/neg/bug836.scala
@@ -0,0 +1,16 @@
+abstract class Obj { type S }
+class ObjImpl extends Obj { type S = String }
+
+abstract class A {
+  type MyObj <: Obj
+  type S = MyObj#S
+
+  val any:  Any = 0
+  val some: S = any   // compiles => type X is set to scala.Any
+}
+
+class B extends A {
+  type MyObj = ObjImpl
+  val myString:   S = "hello"
+  val realString: String = myString   // error: type missmatch
+}
