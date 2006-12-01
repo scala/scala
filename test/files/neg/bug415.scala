4f6a14b33d664dede92b8d7063d7cd437023d987
Martin Odersky <odersky@gmail.com>
2006-12-01 16:32:05 +0000

    updated tests.

--- test/files/neg/bug415.scala
+++ test/files/neg/bug415.scala
@@ -1,21 +1,9 @@
-class Foo[T]
-
 abstract class A {
-  type T <: Foo[T]
+  type T <: String;
   def x: T;
 }
 
 abstract class B {
   def a: A;
-  val y  = a.x;
-}
-
-abstract class A2 {
-  type T <: String
-  def x: Array[T]
-}
-
-abstract class B2 {
-  def a: A2;
-  val y: Array[String]  = a.x;
+  val y: String  = a.x;
 }
--- test/files/pos/bug415.scala
+++
@@ -1,9 +0,0 @@
-abstract class A {
-  type T <: String;
-  def x: T;
-}
-
-abstract class B {
-  def a: A;
-  val y: String  = a.x;
-}
