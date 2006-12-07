a0f6d27d543a97b18d31a0a0c053233e1ba182a9
michelou <michelou@epfl.ch>
2006-12-07 11:46:27 +0000

    updated check files for cli tests

--- test/files/cli/test2/Main.check.scala
+++ test/files/cli/test2/Main.check.scala
@@ -1,13 +1,22 @@
 unknown option: '-cpp'
-scala [ <compiler-option> | -howtorun:how ]... [<torun> <arguments>]
+scala [ <option> ]... [<torun> <arguments>]
 
-<compiler-option>'s are as for scalac; see scalac -help.
+All options to scalac are allowed.  See scalac -help.
+
 <torun>, if present, is an object or script file to run.
 If no <torun> is present, run an interactive interpreter.
--howtorun allows explicitly specifying how to run <torun>:
+
+Option -howtorun allows explicitly specifying how to run <torun>:
     script: it is a script file
     object: it is an object name
     guess: (the default) try to guess
+
+Option -savecompiled requests that the compiled script be saved
+for future use.
+
+Option -nocompdaemon requests that the fsc offline compiler not be used.
+
+Option -Dproperty=value sets a Java system property.
 
 2: 1: test 1 passed (1)
 2: 1: test 2 passed (1)
