a2fced5b2c137a447f918f2d9b23a7b3b719150d
michelou <michelou@epfl.ch>
2007-02-28 15:29:51 +0000

    updated example in scaladoc comment

--- src/library/scala/throws.scala
+++ src/library/scala/throws.scala
@@ -1,7 +1,7 @@
 /*                     __                                               *\
 **     ________ ___   / /  ___     Scala API                            **
-**    / __/ __// _ | / /  / _ |    (c) 2002-2006, LAMP/EPFL             **
-**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2007, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
 */
@@ -11,18 +11,16 @@
 
 package scala
 
-/**
- * Annotation for specifying the exceptions thrown by a method.
- * <p>
- *  Example:
- * </p>
- * <pre>
- * <b>class</b> Reader(fname: String) {
- *   <b>private val</b> in =
- *     <b>new</b> BufferedReader(<b>new</b> FileReader(fname))
- *   [throws(classOf[IOException])]
- *   <b>def</b> read() = in.read()
- * }</pre>
+/** <p>
+ *    Annotation for specifying the exceptions thrown by a method.
+ *    For example:
+ *  </p><pre>
+ *    <b>class</b> Reader(fname: String) {
+ *      <b>private val</b> in =
+ *        <b>new</b> BufferedReader(<b>new</b> <a class="java_io_FileReader" href="" target="_top">FileReader</a>(fname))
+ *      @throws(classOf[<a class="java_io_IOException" href="" target="_top">IOException</a>])
+ *      <b>def</b> read() = in.read()
+ *    }</pre>
  *
  * @author  Nikolay Mihaylov
  * @version 1.0, 19/05/2006
