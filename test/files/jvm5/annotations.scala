12a3b4c5ffe61ad646c33c59a666386e11429316
michelou <michelou@epfl.ch>
2007-03-02 13:35:59 +0000

    added test for @BeanProperty annotation

--- test/files/jvm5/annotations.scala
+++ test/files/jvm5/annotations.scala
@@ -5,7 +5,7 @@ object Test1 {
     @remote
     def foo: Unit = ()
   }
-  def run: Unit = {
+  def run {
     val method = classOf[Foo].getMethod("foo", Array())
     method.getExceptionTypes foreach Console.println
   }
@@ -19,7 +19,7 @@ object Test2 {
     @throws(classOf[IOException])
     def read() = in.read()
   }
-  def run: Unit = {
+  def run {
     val method = classOf[Reader].getMethod("read", Array())
     method.getExceptionTypes foreach Console.println
   }
@@ -41,7 +41,7 @@ object Test3 {
     @Deprecated
     def foo: Unit = ()
   }
-  def run: Unit = {
+  def run {
     val method = classOf[Foo].getMethod("foo", Array())
     val annotation = method.getAnnotation(classOf[Deprecated])
     Console.println(annotation)
@@ -74,10 +74,33 @@ object Test4 {
   class Foo2
   @SourceAnnotation("http://bloodsuckers.com")
   class Foo3
-  def run: Unit = {
+  def run {
     classOf[Foo1].getAnnotations foreach Console.println
     classOf[Foo2].getAnnotations foreach Console.println
     classOf[Foo3].getAnnotations foreach Console.println
+  }
+}
+
+object Test5 {
+  import scala.reflect.BeanProperty
+  class Count {
+    // we use "Integer" instead of "Int" because of Java reflection
+    @BeanProperty
+    private var count: Integer = 0
+
+    private val getter =
+      getClass().getMethod("getCount", Array[java.lang.Class]())
+    private val setter =
+      getClass().getMethod("setCount", Array[java.lang.Class](classOf[Integer]))
+
+    def get = getter.invoke(this, Array()).asInstanceOf[Integer].intValue
+    def set(n: Int) = setter.invoke(this, Array(new Integer(n)))
+  }
+  def run {
+    val count = new Count
+    Console.println(count.get)
+    count.set(99)
+    Console.println(count.get)
   }
 }
 
@@ -87,5 +110,6 @@ object Test {
     Test2.run
     Test3.run     // requires the use of -target:jvm-1.5
     Test4.run
+    Test5.run
   }
 }
