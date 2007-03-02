d3d697a9c5c51e4a78d8a0392cc37723bbe6e3e5
Burak Emir <emir@epfl.ch>
2007-03-02 19:25:20 +0000

    fixed xml02

--- test/files/jvm/xml02.scala
+++ test/files/jvm/xml02.scala
@@ -2,21 +2,25 @@ import testing.SUnit._
 
 object Test extends TestConsoleMain {
 
-  import scala.xml.NodeSeq
+  import scala.xml.{NodeSeq, Utility}
   import NodeSeq.view
 
   val ax = <hello foo="bar">
              <world/>
            </hello>
 
+  val cx = <z:hello foo="bar" xmlns:z="z">
+             crazy text world
+           </z:hello>
+
   val bx = <hello foo="bar&amp;x"></hello>
 
   class XmlEx extends TestCase("attributes") with Assert {
 
-    override def run = {
-      assertEquals("@one",   "bar",           ax \ "@foo")
-      assertEquals("@two",   xml.Text("bar"), ax \ "@foo")
-      assertEquals("@three", "bar&x",         bx \ "@foo")
+    override def runTest = {
+      assertTrue("@one",       ax \ "@foo" == "bar")              // uses NodeSeq.view!
+      assertTrue("@two",       ax \ "@foo" == xml.Text("bar"))    // dto.
+      assertTrue("@three",     bx \ "@foo" == "bar&x")            // dto.
       assertTrue  ("@four", (bx \ "@foo") sameElements List(xml.Text("bar&x")))
       //assertTrue("@four", (bx \ "@foo") sameElements List(xml.Text("bar"),xml.EntityRef("amp"),xml.Text("x")))
       assertEquals("@five",  "<hello foo=\"bar&amp;x\"></hello>", bx.toString)
@@ -24,28 +28,13 @@ object Test extends TestConsoleMain {
   }
 
   class XmlPat extends TestCase("patterns") with Assert {
-    override def run = {
+    override def runTest = {
       assertTrue(<hello/> match { case <hello/> => true; case _ => false; })
       assertTrue(<x:ga xmlns:x="z"/> match { case <x:ga/> => true; case _ => false; });
-
-  /*
-  assertEquals(ax match { case x @ <hello>
-                               <world/>
-                           </hello> if x \ "@foo" == "bar" => true;
-                      case _ => false; },
-               true);
-
-  assertEquals(
-     <hello foo="bar">
-       crazy text world
-     </hello> match { case <hello>
-                               crazy   text  world
-                           </hello> => true;
-                      case _ => false; },
-               true);
-  */
+      assertTrue(Utility.trim(cx) match { case n @ <hello>crazy text world</hello> if n \ "@foo" == "bar" => true; })
+    }
   }
-  }
+
   def suite = new TestSuite(
     new XmlEx,
 	new XmlPat
