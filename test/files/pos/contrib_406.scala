e0e03261824f9d42a6dbb290b0936eaf0e22143d
mihaylov <mihaylov@epfl.ch>
2007-03-26 10:44:11 +0000

    Moved pos/contribution_406.scala in pending bec...
    Moved pos/contribution_406.scala in pending because it does actually
    crash the compiler

--- test/files/pos/contrib_406.scala
+++
@@ -1,5 +0,0 @@
-package bug.contrib_406
-
-object Test {
-  val (a: List[Int], b) = (List(1 ,2 ,3 ), 4)
-}
---
+++ test/pending/pos/contrib_406.scala
@@ -0,0 +1,5 @@
+package bug.contrib_406
+
+object Test {
+  val (a: List[_], b) = (List(1 ,2 ,3 ), 4)
+}
