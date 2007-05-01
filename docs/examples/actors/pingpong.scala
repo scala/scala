2aa6c12894ad5391e908a1e168138180b60c0c76
Philipp Haller <hallerp@gmail.com>
2007-05-01 10:00:11 +0000

    Small changes

--- docs/examples/actors/pingpong.scala
+++ docs/examples/actors/pingpong.scala
@@ -7,6 +7,20 @@ case object Ping
 case object Pong
 case object Stop
 
+/**
+ * Ping pong example.
+ *
+ * @author  Philipp Haller
+ * @version 1.1
+ */
+object pingpong extends Application {
+  val pong = new Pong
+  val ping = new Ping(100000, pong)
+  ping.start
+  pong.start
+  ping ! SendPing
+}
+
 class Ping(count: int, pong: Actor) extends Actor {
   def act() {
     var pingsLeft = count - 1
@@ -45,11 +59,4 @@ class Pong extends Actor {
       }
     }
   }
-}
-
-object pingpong extends Application {
-  val pong = new Pong
-  val ping = new Ping(100000, pong)
-  ping.start
-  pong.start
 }
