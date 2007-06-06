6739cacb9dbc1cbc3c459b87b8ba97923d687fbe
Martin Odersky <odersky@gmail.com>
2007-06-06 21:26:38 +0000

    fixed test file

--- test/files/run/bug949.scala
+++ test/files/run/bug949.scala
@@ -1,6 +1,6 @@
 object Test extends Application {
 
-    def f = new T { val state = State.A }
+    private def f = new T { val state = State.A }
 
     private object State extends Enumeration {
         val A, B = Value
