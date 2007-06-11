1cf6fcfbfaedd1b1659554a0ecc9dd89a43d0a07
michelou <michelou@epfl.ch>
2007-06-11 15:34:04 +0000

    added 64-bit library

--- test/files/jvm/natives.scala
+++ test/files/jvm/natives.scala
@@ -1,6 +1,7 @@
 object Test {
   //println("java.library.path=" + System.getProperty("java.library.path"))
-  System.loadLibrary("natives")
+  val model = System.getProperty("sun.arch.data.model", "32")
+  System.loadLibrary("natives-" + model)
 
   @native
   def sayHello(s: String): String = null
