09dd9eb7efc36a7610f43b3ad31c82aa40e56544
Lex Spoon <lex@lexspoon.org>
2007-06-19 17:57:19 +0000

    Decrease the size of array used for the stress ...
    Decrease the size of array used for the stress test

--- test/files/run/sort.scala
+++ test/files/run/sort.scala
@@ -1,5 +1,5 @@
 object Test extends Application {
-  println((1 to 1000000).toList.sort(_<_).length)
+  println((1 to 100000).toList.sort(_<_).length)
   println(List(1, 5, 10, 3, 2).toList.sort(_<_))
   println(List(1, 5, 10, 3, 2).toList.sort(_>_))
   println(List(10).toList.sort(_<_))
