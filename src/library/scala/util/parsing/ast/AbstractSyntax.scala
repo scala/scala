00c12b4d00e45a9af61652d37355460ddfeb03c6
Adriaan Moors <adriaan.moors@epfl.ch>
2007-07-07 11:44:18 +0000

    integrating combinators into trunk

---
+++ src/library/scala/util/parsing/ast/AbstractSyntax.scala
@@ -0,0 +1,31 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2006-2007, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |                                         **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+package scala.util.parsing.ast
+
+import scala.util.parsing.input.Positional
+
+/** This component provides the core abstractions for representing an Abstract Syntax Tree
+ *
+ * @author Adriaan Moors
+ */
+trait AbstractSyntax {
+  /** The base class for elements of the abstract syntax tree.
+   */
+  trait Element extends Positional
+
+  /** The base class for elements in the AST that represent names {@see Binders}.
+   */
+  trait NameElement extends Element {
+    def name: String
+    override def equals(that: Any): Boolean = that match {
+      case n: NameElement => n.name == name
+      case _ => false
+    }
+  }
+}
