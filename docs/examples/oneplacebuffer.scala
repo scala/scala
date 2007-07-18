14ea3ab51e7a7712afd4aa94ecbe0d6799f5e4d9
michelou <michelou@epfl.ch>
2007-07-18 15:14:14 +0000

    updated example

--- docs/examples/oneplacebuffer.scala
+++ docs/examples/oneplacebuffer.scala
@@ -2,7 +2,7 @@ package examples
 
 object oneplacebuffer {
 
-  import scala.concurrent._
+  import scala.concurrent.{MailBox, ops}
 
   class OnePlaceBuffer {
     private val m = new MailBox() {}          // An internal mailbox
@@ -11,15 +11,17 @@ object oneplacebuffer {
 
     m send Empty()                            // Initialization
 
-    def write(x: Int): Unit = m receive {
-      case Empty() =>
-        Console.println("put " + x)
-        m send Full(x)
+    def write(x: Int) {
+      m receive {
+        case Empty() =>
+          println("put " + x)
+          m send Full(x)
+      }
     }
 
     def read: Int = m receive {
       case Full(x) =>
-        Console.println("get " + x)
+        println("get " + x)
         m send Empty(); x
     }
   }
@@ -27,8 +29,8 @@ object oneplacebuffer {
   def kill(delay: Int) = new java.util.Timer().schedule(
     new java.util.TimerTask {
       override def run() = {
-        Console.println("[killed]")
-        System.exit(0)
+        println("[killed]")
+        exit(0)
       }
     },
     delay) // in milliseconds
@@ -37,7 +39,7 @@ object oneplacebuffer {
     val buf = new OnePlaceBuffer
     val random = new java.util.Random()
 
-    def producer(n: int) {
+    def producer(n: Int) {
       Thread.sleep(random.nextInt(1000))
       buf.write(n)
       producer(n + 1)
