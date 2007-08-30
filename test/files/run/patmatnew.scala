7896c668273de297728519afb6fb7d4afadb29e4
Burak Emir <emir@epfl.ch>
2007-08-30 00:13:48 +0000

    moved test cases in bug457.scala in to patmatne...
    moved test cases in bug457.scala in to patmatnew.scala

--- test/files/run/patmatnew.scala
+++ test/files/run/patmatnew.scala
@@ -36,7 +36,8 @@ object Test extends TestConsoleMain {
       new Test1093,
       new Test1163_Order,
       new TestUnbox,
-      ClassDefInGuard
+      ClassDefInGuard,
+      Bug457
     )
 
   class Foo(j:Int) {
@@ -449,6 +450,49 @@ object Test extends TestConsoleMain {
       }
     }
   }
+
+
+
+  object Bug457 extends TestCase("Bug457") {
+
+    def method1() = {
+      val x = "Hello, world";
+      val y = 100;
+
+      y match {
+        case _: Int if (x match { case t => t.trim().length() > 0 }) => false;
+        case _ => true;
+      }
+    }
+
+    def method2(): scala.Boolean = {
+      val x: String = "Hello, world";
+      val y: scala.Int = 100;
+      {
+        var temp1: scala.Int = y;
+        var result: scala.Boolean = false;
+        if (
+          {
+            var result1: scala.Boolean = true;
+            if (y == 100)
+              result1
+            else
+              throw new MatchError("crazybox.scala, line 11")
+          } && (y > 90)
+        )
+          result
+            else
+              throw new MatchError("crazybox.scala, line 9")
+      }
+    }
+
+    override def runTest {
+      method1();
+      method2();
+    }
+
+  }
+
 
 }
 
