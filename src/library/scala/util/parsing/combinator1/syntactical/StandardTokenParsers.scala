6b801413fdb089d5084c64485867b9de7187a869
Martin Odersky <odersky@gmail.com>
2007-09-14 15:51:48 +0000

    new version of combinator parsers

---
+++ src/library/scala/util/parsing/combinator1/syntactical/StandardTokenParsers.scala
@@ -0,0 +1,24 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2006-2007, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id: StdTokenParsers.scala 12242 2007-07-09 09:43:09Z michelou $
+
+
+package scala.util.parsing.combinator1.syntactical
+
+import scala.util.parsing.syntax._
+import scala.util.parsing.combinator1.lexical.StdLexical
+
+/** This component provides primitive parsers for the standard tokens defined in `StdTokens'.
+*
+* @author Martin Odersky, Adriaan Moors
+ */
+class StandardTokenParsers extends StdTokenParsers {
+  type Tokens = StdTokens
+  val lexical = new StdLexical
+}
---
+++ src/library/scala/util/parsing/combinator1/syntactical/StdTokenParsers.scala
@@ -0,0 +1,44 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2006-2007, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id: StdTokenParsers.scala 12242 2007-07-09 09:43:09Z michelou $
+
+
+package scala.util.parsing.combinator1.syntactical
+
+import scala.util.parsing.syntax._
+
+/** This component provides primitive parsers for the standard tokens defined in `StdTokens'.
+*
+* @author Martin Odersky, Adriaan Moors
+ */
+trait StdTokenParsers extends TokenParsers {
+  type Tokens <: StdTokens
+  import lexical.{Keyword, NumericLit, StringLit, Identifier}
+
+  /** A parser which matches a single keyword token.
+   *
+   * @param chars    The character string making up the matched keyword.
+   * @return a `Parser' that matches the given string
+   */
+  implicit def keyword(chars: String): Parser[String] = accept(Keyword(chars)) ^^ (_.chars)
+
+  /** A parser which matches a numeric literal */
+  def numericLit: Parser[String] =
+    elem("number", _.isInstanceOf[NumericLit]) ^^ (_.chars)
+
+  /** A parser which matches a string literal */
+  def stringLit: Parser[String] =
+    elem("string literal", _.isInstanceOf[StringLit]) ^^ (_.chars)
+
+  /** A parser which matches an identifier */
+  def ident: Parser[String] =
+    elem("identifier", _.isInstanceOf[Identifier]) ^^ (_.chars)
+}
+
+
