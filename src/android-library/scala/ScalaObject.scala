aab21c2dc81e5ba7bce33f87ff96fd5b398f1bf9
Iulian Dragos <jaguarul@gmail.com>
2007-11-22 17:16:07 +0000

    Added android-library target in the build file,...
    Added android-library target in the build file, and special ScalaObect
    which has no @remote attribute (no RMI on android).

---
+++ src/android-library/scala/ScalaObject.scala
@@ -0,0 +1,22 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2007, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id: ScalaObject.scala 10059 2007-02-20 16:37:44Z michelou $
+
+
+package scala
+
+
+trait ScalaObject extends AnyRef {
+
+  /** This method is needed for optimizing pattern matching expressions
+   *  which match on constructors of case classes.
+   */
+  def $tag(): Int = 0
+
+}
