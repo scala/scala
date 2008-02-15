787b0264db2b8f197d1688318a2790fcd037d9c8
michelou <michelou@epfl.ch>
2008-02-15 14:49:53 +0000

    fixed #485 (IdentityHashMap)

--- test/files/run/t0485.scala
+++ test/files/run/t0485.scala
@@ -21,11 +21,28 @@ object testMap {
         println(e); println()
     }
   }
+  def test1(m1: Map[Int, Int]) {
+    try {
+      m1.put(10, 20)
+      val m2 = m1.clone()
+      m1.put(20, 30)
+      println("m1.size > m2.size is "+ (m1.size > m2.size))
+      println()
+    }
+    catch {
+      case e: Exception =>
+        println(e); println()
+    }
+  }
   test(new HashMap[Int, Int])
-  test(new IdentityHashMap[Int, Int])
   test(new LinkedHashMap[Int, Int])
+  // NB. class IdentityHashMap makes no guarantees as to the order
+  // of the map; in particular, it does not guarantee that the order
+  // will remain constant over time (see Java API docs).
+  //test1(new IdentityHashMap[Int, Int])
   test(new TreeMap[Int, Int])
   test(new WeakHashMap[Int, Int])
+  test1(new IdentityHashMap[Int, Int])
 }
 
 object testSet {
