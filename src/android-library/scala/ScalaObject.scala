b4916be877bd007cb46c038f4f60fe3ca5bb8354
michelou <michelou@epfl.ch>
2008-02-25 11:15:41 +0000

    fixed android build

--- src/android-library/scala/ScalaObject.scala
+++ src/android-library/scala/ScalaObject.scala
@@ -1,6 +1,6 @@
 /*                     __                                               *\
 **     ________ ___   / /  ___     Scala API                            **
-**    / __/ __// _ | / /  / _ |    (c) 2002-2007, LAMP/EPFL             **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2008, LAMP/EPFL             **
 **  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
@@ -11,6 +11,7 @@
 
 package scala
 
+import Predef._
 
 trait ScalaObject extends AnyRef {
 
