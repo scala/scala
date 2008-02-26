cf5f84719b7e0586f7e971e941bd3d1d77d65f1a
Martin Odersky <odersky@gmail.com>
2008-02-26 10:02:33 +0000

    fixed #537

--- src/compiler/scala/tools/nsc/symtab/classfile/Pickler.scala
+++ src/compiler/scala/tools/nsc/symtab/classfile/Pickler.scala
@@ -73,7 +73,7 @@ abstract class Pickler extends SubComponent {
      *  Such symbols should be treated as if they were local.
      */
     private def isUnrootedExistential(sym: Symbol) =
-      false && sym.isAbstractType && sym.hasFlag(EXISTENTIAL) && sym.owner.isPackageClass
+      sym.isAbstractType && sym.hasFlag(EXISTENTIAL) && sym.owner.isPackageClass
 
     private def normalizedOwner(sym: Symbol) =
       if (isUnrootedExistential(sym)) root else sym.owner
