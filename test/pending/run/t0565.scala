1e01637f897d39719fac79456d274fe8661ce4d6
NAME <USER@epfl.ch>
2008-03-12 11:45:45 +0000

    Pending test for #565

---
+++ test/pending/run/t0565.scala
@@ -0,0 +1,22 @@
+object Test extends Application {
+
+  class MacGuffin
+
+  object A {
+     var x : { type T >: Null <: AnyRef;
+               val y : T;
+               def z (w : T) : T } =
+             new { type T = String;
+                   val y = "foo";
+                   def z (w : String) = w + "bar" }
+     lazy val u = { println("u evaluated"); x }
+     def foo (v : => u.type#T) : u.type#T = {
+       x = new { type T = MacGuffin;
+                 val y = new MacGuffin;
+                 def z (w : MacGuffin) = w }
+       u.z(v)
+     }
+  }
+
+  A.foo(A.u.y)
+}
