e700865476cedc44074fad5d8d40aa1ca23c2dbd
NAME <USER@epfl.ch>
2008-03-31 13:49:42 +0000

    Pending test for #663

---
+++ test/pending/run/t0663.scala
@@ -0,0 +1,6 @@
+object Test extends Application {
+  val src = scala.io.Source.fromString("<?xml version='1.0' encoding='UTF-8'?><feed/>")
+  val parser = xml.parsing.ConstructingParser.fromSource(src, true)
+  println(parser.document)
+}
+
