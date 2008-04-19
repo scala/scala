5fbb8b6f9a1f225da9f55363df877529276519c6
Sean McDirmid <sean.mcdirmid@gmail.com>
2008-04-19 11:41:05 +0000

    Deprecated non-position versions of newName, ad...
    Deprecated non-position versions of newName, added explaining comment.

--- src/compiler/scala/tools/nsc/util/FreshNameCreator.scala
+++ src/compiler/scala/tools/nsc/util/FreshNameCreator.scala
@@ -9,9 +9,12 @@ package scala.tools.nsc.util
 import scala.collection.mutable.HashMap
 
 trait FreshNameCreator {
+  /** do not call before after type checking ends. Use newName(Position,String) instead. */
   @deprecated def newName(prefix : String) : String
-  def newName() : String
+  /** do not call before after type checking ends. Use newName(Position) instead. */
+  @deprecated def newName() : String
   def newName(pos : util.Position, prefix : String) : String
+  def newName(pos : util.Position) : String
 }
 object FreshNameCreator {
   class Default extends FreshNameCreator {
@@ -31,6 +34,7 @@ object FreshNameCreator {
     safePrefix + count
   }
   def newName(pos : util.Position, prefix : String) = newName(prefix)
+  def newName(pos : util.Position) = newName()
 
   def newName(): String = {
     counter = counter + 1
