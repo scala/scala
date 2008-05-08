0f09ba97e71daedf80a508d0af3d09893d9d172f
pradel <pradel@epfl.ch>
2008-05-08 15:49:23 +0000

    removed some needless lines

--- src/compiler/scala/tools/nsc/symtab/SymbolLoaders.scala
+++ src/compiler/scala/tools/nsc/symtab/SymbolLoaders.scala
@@ -278,11 +278,6 @@ abstract class SymbolLoaders {
   protected def completeClassfile(root : Symbol, loader : ClassfileLoader)(f : => Unit) : Unit = f
 
   class ClassfileLoader(val classFile: AbstractFile, override val sourceFile: AbstractFile, sourcePath0: AbstractFile) extends SymbolLoader {
-    if (sourcePath0 == null) {
-      assert(true)
-      assert(true)
-    }
-
     private object classfileParser extends ClassfileParser {
       val global: SymbolLoaders.this.global.type = SymbolLoaders.this.global
       override def sourcePath = sourcePath0 /* could be null */
