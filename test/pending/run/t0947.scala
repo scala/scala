6209dbe66ef4511464ff1c47715c72b8fe147baf
Geoffrey Washburn <geoffrey.washburn@epfl.ch>
2008-05-25 08:56:53 +0000

    Tried adding a test for t0947, but I can't seem...
    Tried adding a test for t0947, but I can't seem to make the program
    cooperate with scalatest.

---
+++ test/pending/run/t0947.scala
@@ -0,0 +1,8 @@
+import scala.tools.nsc._
+
+object Test extends Application {
+  class Foo { override def toString = "Foo" };
+
+  val int = new Interpreter(new Settings());
+  int.bind("foo", "Test.Foo", new Test.Foo());
+}
