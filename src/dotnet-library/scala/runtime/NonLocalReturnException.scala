f6974416057ead15f0903bd4da5b1ccf5f423a84
Lukas Rytz <lukas.rytz@epfl.ch>
2008-07-28 22:18:15 +0000

    Fixes the msil build.
    On .NET, the method fillInStackTrace does not exist (in
    System.Exception, which is aliased as RuntimeException). So
    NonLocalReturnException cannot override it.

---
+++ src/dotnet-library/scala/runtime/NonLocalReturnException.scala
@@ -0,0 +1,17 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2007, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id: NonLocalReturnException.scala 15604 2008-07-24 09:56:59Z phaller $
+
+
+package scala.runtime
+
+
+import Predef.RuntimeException
+
+class NonLocalReturnException[T](val key: AnyRef, val value: T) extends RuntimeException
