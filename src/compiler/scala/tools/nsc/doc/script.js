81eaea6bc0ffb2382eb9dbf94941e9ba43c01613
Lukas Rytz <lukas.rytz@epfl.ch>
2008-08-11 12:46:21 +0000

    updated version numbers for 2.7.2

--- src/compiler/scala/tools/nsc/doc/script.js
+++ src/compiler/scala/tools/nsc/doc/script.js
@@ -39,7 +39,7 @@ function get_api_root(key) {
   return root;
 }
 
-var scala_src_root = 'http://lampsvn.epfl.ch/trac/scala/browser/scala/trunk/';
+var scala_src_root = 'http://lampsvn.epfl.ch/trac/scala/browser/scala/tags/R_2_7_2-RC1';
 var lib_src_root = scala_src_root + 'src/library/';
 var comp_src_root = scala_src_root + 'src/compiler/';
 var actors_src_root = scala_src_root + 'src/actors/';
