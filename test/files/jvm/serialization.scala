40d7db8596c14a744dcb365beed36dcfdfcd3945
michelou <michelou@epfl.ch>
2008-08-26 14:37:23 +0000

    minor test add-on

--- test/files/jvm/serialization.scala
+++ test/files/jvm/serialization.scala
@@ -130,6 +130,8 @@ object Test1_scala {
     println()
     println("x9 eq x10: " + (x9 eq x10) + " - x10 eq x9: " + (x10 eq x9))
     println("x9 equals x10: " + (x9 equals x10) + " - x10 equals x9: " + (x10 equals x9))
+    println("x9 eq y10: " + (x9 eq y10) + " - y10 eq x9: " + (y10 eq x9))
+    println("x9 equals y10: " + (x9 equals y10) + " - y10 equals x9: " + (y10 equals x9))
     println()
   }
   catch {
