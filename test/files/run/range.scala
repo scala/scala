f0ab2e175e6b64f3356e2472db81e2caad6684a5
Philipp Haller <hallerp@gmail.com>
2008-08-28 16:46:26 +0000

    Added test files for #1297

---
+++ test/files/run/range.scala
@@ -0,0 +1,14 @@
+object Test extends Application{
+  def rangeForeach(range : Range) = {
+    val buffer = new scala.collection.mutable.ListBuffer[Int];
+    range.foreach(buffer += _);
+    assert(buffer.toList == range.elements.toList);
+  }
+
+  rangeForeach(1 to 10);
+  rangeForeach(1 until 10);
+  rangeForeach(10 to 1 by -1);
+  rangeForeach(10 until 1 by -1);
+  rangeForeach(10 to 1 by -3);
+  rangeForeach(10 until 1 by -3);
+}
