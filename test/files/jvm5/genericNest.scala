4b6277f8511bab6d4d0fc01f398bd9e967b94bdb
Iulian Dragos <jaguarul@gmail.com>
2008-09-01 13:56:12 +0000

    Fixed #1315, outer instances are passed implici...
    Fixed #1315, outer instances are passed implicitly. Reorganized nested
    classes tests.

---
+++ test/files/jvm5/genericNest.scala
@@ -0,0 +1,13 @@
+/** found in genericNest.jar, compiled from OuterTParams.java */
+import nestpkg._;
+
+// bug #695
+object ForceParse extends OuterTParams[AnyRef] {
+  // Force import of HarderToParse<A>.InnerClass,
+  // which has confusing method signature.
+  var field: InnerClass = null
+}
+
+object Test extends Application {
+  ForceParse
+}
