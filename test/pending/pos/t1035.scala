1ed25d9dd0c9914b1a90baf178efd19bc0f0775d
David MacIver <david.maciver@gmail.com>
2008-09-03 20:38:01 +0000

    More bugs from tickets added to pending.

---
+++ test/pending/pos/t1035.scala
@@ -0,0 +1,32 @@
+//A fatal error or Scala compiler
+// Scala compiler version 2.7.1-final -- (c) 2002-2007 LAMP/EPFL
+// Carlos Loria cloria@artinsoft.com
+// 7/10/2008
+
+class A  {
+  var name:String = _
+  def getName() = name
+  def this(name:String, age:Int){this();this.name=name}
+
+}
+
+class B(name:String) extends A(name,0){
+}
+
+class D {
+
+   object A {
+     def unapply(p:A) = Some(p.getName)
+   }
+
+   object B {
+     def unapply(p:B) = Some(p.getName)
+   }
+   def foo(p:Any) = p match {
+      case B(n)    => println("B")
+      case A(n)    => println("A")
+
+
+   }
+
+}
