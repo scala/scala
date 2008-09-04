cd5754edd13fa91497994ed34bb4db016f98d978
David MacIver <david.maciver@gmail.com>
2008-09-04 15:08:57 +0000

    Fix for ticket #0508 added to the 2.7.2 branch.

---
+++ test/files/run/t0508.scala
@@ -0,0 +1,13 @@
+object Test extends Application {
+
+  case class Foo(s: String, n: Int)
+
+  def foo[A, B, C](unapply1: A => Option[(B, C)], v: A) = {
+    unapply1(v) match {
+      case Some((fst, snd)) => println("first: " + fst, " second: " + snd)
+      case _ => println(":(")
+    }
+  }
+
+  foo(Foo.unapply, Foo("this might be fun", 10))
+}
