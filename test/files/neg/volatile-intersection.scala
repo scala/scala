118a288beecc155c7c84ecbebf6f00bc87594607
Geoffrey Washburn <geoffrey.washburn@epfl.ch>
2008-09-09 10:03:31 +0000

    New test concerning volatile intersections.

---
+++ test/files/neg/volatile-intersection.scala
@@ -0,0 +1,21 @@
+object Test extends Application {
+ trait A
+ trait B extends A
+
+  trait C {
+    type U
+    trait D { type T >: B <: A }
+    val y: (D with U)#T = new B { }
+  }
+
+  class D extends C {
+    trait E
+    trait F { type T = E }
+    type U = F
+    def frob(arg : E) : E = arg
+    frob(y)
+  }
+
+  new D
+}
+
