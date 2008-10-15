ddda42af0a441b57cfbfdb7b29d87c769661aa3c
Antonio Cunei <antonio.cunei@epfl.ch>
2008-10-15 15:05:21 +0000

    Removed unnecessary "getHostName()", see ticket...
    Removed unnecessary "getHostName()", see ticket #1366

--- src/compiler/scala/tools/nsc/CompileSocket.scala
+++ src/compiler/scala/tools/nsc/CompileSocket.scala
@@ -205,9 +205,9 @@ class CompileSocket {
       } else {
         val port = if(create) getPort(vmArgs) else pollPort()
         if(port < 0) return null
-        val hostName = InetAddress.getLocalHost().getHostName()
+        val hostAdr = InetAddress.getLocalHost()
         try {
-          val result = new Socket(hostName, port)
+          val result = new Socket(hostAdr, port)
           info("[Connected to compilation daemon at port " + port + "]")
           result
         } catch {
