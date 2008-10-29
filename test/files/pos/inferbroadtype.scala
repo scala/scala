3128d1e0e540eb914b8ca0fe22b97a1bee56580a
David MacIver <david.maciver@gmail.com>
2008-10-29 15:42:38 +0000

    Test case extracted from Paul's broken patch fo...
    Test case extracted from Paul's broken patch for #1458
    
    Essentially one needs to make sure Base { val changesBaseClasses = true } doesn't infer the refinement type Base { val changesBaseClasses : Boolean(true) = true }

---
+++ test/files/pos/inferbroadtype.scala
@@ -0,0 +1,8 @@
+object Test {
+  abstract class Base { val changesBaseClasses: Boolean }
+  class Concrete extends Base { val changesBaseClasses = true }
+  def getBase : Base = new Concrete
+
+  var c = new Base { val changesBaseClasses = true }
+  c = getBase
+}
