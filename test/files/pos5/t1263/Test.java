2dbbad7ebcbe7022620191c414fa2ef1e961e188
David MacIver <david.maciver@gmail.com>
2008-10-31 16:27:14 +0000

    Tests from ismael.

---
+++ test/files/pos5/t1263/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.rmi.RemoteException;
+
+import test.Map;
+
+public class Test implements Map<String, String> {
+  public Map<String, String>.MapTo plus(String o) {
+    return null;
+  }
+
+  public int $tag() throws RemoteException {
+    return 0;
+  }
+}
+
---
+++ test/files/pos5/t1263/test.scala
@@ -0,0 +1,10 @@
+package test
+
+trait Map[A, +B] {
+  def plus(key: A): MapTo = new MapTo(key)
+
+  class MapTo(key: A) {
+    def arrow [B1 >: B](value: B1) = null
+  }
+}
+
