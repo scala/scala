535d514b233c3556af79a4a481b9ad560989fe17
David MacIver <david.maciver@gmail.com>
2008-11-10 21:03:16 +0000

    Tests for ticket #1503.

---
+++ test/pending/run/castsingleton.scala
@@ -0,0 +1,10 @@
+object Test extends Application {
+  case class L();
+  object N extends L();
+
+  def empty(xs : L) : Unit = xs match {
+    case x@N => println(x); println(x);
+  }
+
+  empty(L())
+}
