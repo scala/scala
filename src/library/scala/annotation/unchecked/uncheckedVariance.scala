c92b30307cfa1c03aa2540fc8f551a3c54705326
Martin Odersky <odersky@gmail.com>
2008-11-24 14:20:24 +0000

    added uncheckedVariance annotation

---
+++ src/library/scala/annotation/unchecked/uncheckedVariance.scala
@@ -0,0 +1,13 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2007, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+package scala.annotation.unchecked
+
+/** An annotation for type arguments for which one wants to suppress variance checking
+ *  types are volatile.
+ */
+final class uncheckedVariance extends StaticAnnotation {}
