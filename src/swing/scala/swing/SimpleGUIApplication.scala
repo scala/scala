d215c743751a45004e6018f0c2272cb1fcaae725
Ingo Maier <ingo.maier@epfl.ch>
2009-01-11 13:03:57 +0000

    reuse run method from superclass

--- src/swing/scala/swing/SimpleGUIApplication.scala
+++ src/swing/scala/swing/SimpleGUIApplication.scala
@@ -9,10 +9,9 @@ import javax.swing._
 abstract class SimpleGUIApplication extends GUIApplication {
   def top: Frame
 
-  def main(args: Array[String]) = {
-    SwingUtilities.invokeLater {
-      new Runnable { def run() { init(); top.pack(); top.visible = true } }
-    }
+  def main(args: Array[String]) = run {
+    top.pack()
+    top.visible = true
   }
 
   def resourceFromClassloader(path: String): java.net.URL =
