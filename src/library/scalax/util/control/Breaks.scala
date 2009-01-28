0606aa4755cc5261645e075176946fc16945561d
Martin Odersky <odersky@gmail.com>
2009-01-28 19:12:43 +0000

    break and continue now both go to a breakable b...
    break and continue now both go to a breakable block.

--- src/library/scalax/util/control/Breaks.scala
+++ src/library/scalax/util/control/Breaks.scala
@@ -5,21 +5,20 @@ object Breaks {
   private val breakException = new BreakException
   private class ContinueException extends RuntimeException
   private val continueException = new BreakException
-  def break { throw breakException }
+
+  /** A block from which one can exit with a `break' and which can be resumed with a `continue'. */
   def breakable(op: => Unit) {
     try {
       op
     } catch {
       case ex: BreakException =>
+      case ex: ContinueException => breakable(op)
     }
   }
+
+  /* Break from closest enclosing breakable block */
+  def break { throw breakException }
+
+  /* Continue with start of closest enclosing breakable block */
   def continue { throw continueException }
-  def continuable(op: => Unit) {
-    try {
-      op
-    } catch {
-      case ex: ContinueException =>
-	continuable(op)
-    }
-  }
 }
