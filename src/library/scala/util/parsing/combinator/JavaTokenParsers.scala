a35f1315940e444325b373255956a91d80e3405a
Miles Sabin <miles@milessabin.com>
2009-02-03 17:38:58 +0000

    Backport of fix for #1689 to 2.7.x.

--- src/library/scala/util/parsing/combinator/JavaTokenParsers.scala
+++ src/library/scala/util/parsing/combinator/JavaTokenParsers.scala
@@ -16,7 +16,7 @@ trait JavaTokenParsers extends RegexParsers {
   def wholeNumber: Parser[String] =
     """-?\d+""".r
   def decimalNumber: Parser[String] =
-    """(\d+(\.\d*)?|\d*.\d+)""".r
+    """(\d+(\.\d*)?|\d*\.\d+)""".r
   def stringLiteral: Parser[String] =
     ("\""+"""([^"\p{Cntrl}\\]|\\[\\/bfnrt]|\\u[a-fA-F0-9]{4})*"""+"\"").r
   def floatingPointNumber: Parser[String] =
