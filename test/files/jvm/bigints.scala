00c9c7e85cf3bcc6aabe367cf56054fef367a3db
michelou <michelou@epfl.ch>
2009-03-23 09:35:55 +0000

    fixed ticket #1813 (BigDecimal implicits)

--- test/files/jvm/bigints.scala
+++ test/files/jvm/bigints.scala
@@ -1,38 +1,65 @@
-object Test extends Application {
-  Test1
-  Test2
+//############################################################################
+// BigInt, BigDecimal
+//############################################################################
+// $Id$
+
+//############################################################################
+
+import testing.SUnit._
+
+/** Test the Scala implementation of class <code>scala.BigDecimal</code>.
+*
+*  @author Stephane Micheloud
+*/
+object Test extends TestConsoleMain {
+  def suite = new TestSuite(
+    Test_BigInt,
+    Test_BigDecimal
+  )
 }
 
-object Test1 {
-  import BigInt._
+object Test_BigInt extends TestCase("BigInt") with Assert {
+  override def enableStackTrace = false
+  override def runTest {
+    import BigInt._
 
-  val x: BigInt = 1
-  val y = x + 1
-  val z = 1 + y
-  println(z)
-  println(z <= 3)
-  println(3 < z)
-  println(z == 3)
-  println(3 == z)
-  println()
+    val x: BigInt = 1
+    val y = x + 1
+    val z = 1 + y
+    assertEquals("int_add_bigint", 1+y, y+1)
+    assertEquals("int_sub_bigint", 1-y, -(y-1))
+    assertEquals("int_mul_bigint", 2*x*y, y*x*2)
+    assertTrue("z_<=_3", z <= 3)
+    assertFalse("3_<_z", 3 < z)
+    assertTrue("z_==_3", z == 3)
+    assertTrue("3_==_z", 3 == z)
+  }
 }
 
-object Test2 {
-  import BigDecimal._
+object Test_BigDecimal extends TestCase("BigDecimal") with Assert {
+  override def enableStackTrace = false
+  override def runTest {
+    import scala.BigDecimal, BigDecimal._
 
-  val x: BigDecimal = 1
-  val y = x + 1
-  val z = 1 + y
-  println(z)
-  println(z <= 3)
-  println(3 < z)
-  println(z == 3)
-  println(3 == z)
+    val xi: BigDecimal = 1
+    val xd: BigDecimal = 1.0
+    val xf: BigDecimal = 1.0f
+    val xs: BigDecimal = "1.0"
+    val xbi: BigDecimal = scala.BigInt(1)
 
-  val a = BigDecimal(Math.MAX_LONG)
-  val b = BigDecimal(Test1.x)
-  val c = a - b
-  println(c)
-  println(c > Math.MAX_LONG)
-  println(c <= Math.MAX_LONG)
+    val x: BigDecimal = 1
+    val y = x + 1
+    val z = 1 + y
+    assertTrue("z_<=_3", z <= 3)
+    assertFalse("3_<_z", 3 < z)
+    assertTrue("z_==_3", z == 3)
+    assertTrue("3_==_z", 3 == z)
+
+    val a: BigDecimal= Math.MAX_LONG
+    val b: BigDecimal = 1
+    val c = a - b
+    assertFalse("c_>_MAX_LONG", c > Math.MAX_LONG)
+    assertTrue("c_<=_MAX_LONG", c <= Math.MAX_LONG)
+  }
 }
+
