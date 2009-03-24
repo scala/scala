1d439e0bd0bf099bdcf8bf5891d60f55d57de000
Iulian Dragos <jaguarul@gmail.com>
2009-03-24 17:47:42 +0000

    Generate java signatures for private members as...
    Generate java signatures for private members as well (#1395)

--- src/compiler/scala/tools/nsc/backend/jvm/GenJVM.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenJVM.scala
@@ -405,7 +405,7 @@ abstract class GenJVM extends SubComponent {
     }
 
     def addGenericSignature(jmember: JMember, sym: Symbol) {
-      if (!sym.hasFlag(Flags.LIFTED | Flags.PRIVATE |
+      if (!sym.hasFlag(Flags.LIFTED |
                        Flags.EXPANDEDNAME | Flags.SYNTHETIC) && settings.target.value == "jvm-1.5") {
         erasure.javaSig(sym) match {
           case Some(sig) =>
