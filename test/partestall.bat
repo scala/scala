8137c1492f49a291ebaf2843e6d01240a21ae96d
Gilles Dubochet <gilles.dubochet@epfl.ch>
2009-04-29 11:35:28 +0000

    Removed outdated test infastructure.

--- test/partestall.bat
+++
@@ -1,7 +0,0 @@
-scala -cp C:\scala\build\quick\lib\partest;C:\scala\build\quick\lib\actors scala.tools.partest.nest.NestRunner --run
-scala -cp C:\scala\build\quick\lib\partest;C:\scala\build\quick\lib\actors scala.tools.partest.nest.NestRunner --jvm
-scala -cp C:\scala\build\quick\lib\partest;C:\scala\build\quick\lib\actors scala.tools.partest.nest.NestRunner --jvm5
-scala -cp C:\scala\build\quick\lib\partest;C:\scala\build\quick\lib\actors scala.tools.partest.nest.NestRunner --pos
-scala -cp C:\scala\build\quick\lib\partest;C:\scala\build\quick\lib\actors scala.tools.partest.nest.NestRunner --neg
-scala -cp C:\scala\build\quick\lib\partest;C:\scala\build\quick\lib\actors scala.tools.partest.nest.NestRunner --shootout
-
