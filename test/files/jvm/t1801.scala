07eef107992887d539a23c4f34a4d6b8c6a4a8f7
Philipp Haller <hallerp@gmail.com>
2009-05-27 15:08:36 +0000

    Added test case for #1801.

---
+++ test/files/jvm/t1801.scala
@@ -0,0 +1,31 @@
+import scala.actors.Actor._
+
+object Test {
+  val rt = Runtime.getRuntime()
+  val sender = actor {
+    var cnt = 0
+    while(cnt < 1000) {
+      if ((cnt % 100) == 0) println(cnt)
+      receiver ! new Array[Int] (1048576)
+      cnt += 1
+      //println ("Used Mem: " + (((rt.totalMemory() - rt.freeMemory()) / 1048576.) formatted "%.2f") + " Mb")
+    }
+    receiver ! 'exit
+  }
+
+  val receiver = actor {
+    loop {
+      react {
+        case x: Array[Int] => ()//println ("received " + x.length)
+	case 'exit => {
+          println("done!")
+	  exit()
+	}
+      }
+    }
+  }
+
+  def main (args: Array[String]) {
+    sender
+  }
+}
