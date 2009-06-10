b9e451ce6e51f0c5f00384d37ab6d3fe335b40f8
Paul Phillips <paulp@improving.org>
2009-06-10 18:56:54 +0000

    Test case for #252.

---
+++ test/files/pos/bug252.scala
@@ -0,0 +1,17 @@
+abstract class Module {}
+
+abstract class T {
+  type moduleType <: Module
+  val module: moduleType
+}
+
+abstract class Base {
+  type mType = Module
+  type tType = T { type moduleType <: mType }
+}
+
+abstract class Derived extends Base {
+  def f(inputs: List[tType]): Unit = {
+    for (t <- inputs; val m = t.module) { }
+  }
+}
