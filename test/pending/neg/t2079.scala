e1b88d7758c2f042169fe9a85d8f642692176cf7
Philipp Haller <hallerp@gmail.com>
2009-06-22 13:50:49 +0000

    Added test for #2079.

---
+++ test/pending/neg/t2079.scala
@@ -0,0 +1,25 @@
+trait A {
+  type T
+  var v : T
+}
+
+object B {
+  def f(x : { val y : A }) { x.y.v = x.y.v }
+
+  var a : A = _
+  var b : Boolean = false
+  def y : A = {
+    if(b) {
+      a = new A { type T = Int; var v = 1 }
+      a
+    } else {
+      a = new A { type T = String; var v = "" }
+      b = true
+      a
+    }
+  }
+}
+
+object Test extends Application {
+  B.f(B)
+}
