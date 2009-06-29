1ecef3bcd368527fe355bb7943c9e5983fdb9c1a
Philipp Haller <hallerp@gmail.com>
2009-06-29 16:38:49 +0000

    Added test for #2031.

---
+++ test/files/neg/t2031.scala
@@ -0,0 +1,9 @@
+import scala.collection.immutable._
+
+object Test extends Application {
+ val res0 = TreeSet(1, 2, 3)
+
+ //res0.map(x => x)(TreeSet.newBuilder[Int])
+
+ res0.map(x => x)(TreeSet.newBuilder)
+}
