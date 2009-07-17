d6798ac2ab92a8414678fc075c4381e0bc51f438
Paul Phillips <paulp@improving.org>
2009-07-17 19:07:55 +0000

    Removed Vector's indexOfSeq method in favor of ...
    Removed Vector's indexOfSeq method in favor of the superclass
    implementation, which already discriminates based on hasDefiniteSize.

--- src/library/scala/collection/generic/VectorTemplate.scala
+++ src/library/scala/collection/generic/VectorTemplate.scala
@@ -257,13 +257,6 @@ trait VectorTemplate[+A, +This <: VectorTemplate[A, This] with Vector[A]] extend
       super.endsWith(that)
   }
 
-  override def indexOfSeq[B >: A](that: Sequence[B]): Int = {
-    var i = 0
-    val last = length - that.length
-    while (i <= last && !startsWith(that, i)) i += 1
-    negLength(i)
-  }
-
   override def equals(that: Any): Boolean = that match {
     case that1: Vector[a] =>
       val len = this.length
