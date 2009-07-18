fd1c68f004f9cec9fc2aef430b0d28e845207987
Paul Phillips <paulp@improving.org>
2009-07-18 18:10:30 +0000

    Added test case for #1764 to the test case for ...
    Added test case for #1764 to the test case for #1878 (as #1764 was fixed
    along with it in r18343.)

--- test/files/neg/bug1878.scala
+++ test/files/neg/bug1878.scala
@@ -7,4 +7,9 @@ object Test extends Application {
 
   // illegal
   val List(List(_*, arg2), _) = List(List(1,2,3), List(4,5,6))
+
+  // illegal - bug #1764
+  null match {
+    case <p> { _* } </p> =>
+  }
 }
