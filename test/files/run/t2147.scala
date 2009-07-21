ff4552038d6e0f8940ddac2f10e307d17507d1b1
Philipp Haller <hallerp@gmail.com>
2009-07-21 09:12:51 +0000

    Added regression test for #2147.

---
+++ test/files/run/t2147.scala
@@ -0,0 +1,8 @@
+object Test extends Application {
+  val s: Seq[Int] = Stream.from(1)
+  val res0 = s.map(a => 2).head
+  val res1 = Stream.from(1).flatMap(a => List(1)).head
+
+  println((for{a <- Stream.from(1); b <- 1 to 5; if a > 10} yield a).head)
+  println((for{a <- Stream.from(1); b <- 1 to a; if a > 10} yield a).head)
+}
