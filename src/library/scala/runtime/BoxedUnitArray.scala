90bdc95f5ad6ec6be185a86df6de5c5f5c0e4dba
Martin Odersky <odersky@gmail.com>
2009-08-18 19:25:22 +0000

    Added file; I forgot before and it broke the build

---
+++ src/library/scala/runtime/BoxedUnitArray.scala
@@ -0,0 +1,25 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2009, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+// $Id: BoxedDoubleArray.scala 17680 2009-05-08 16:33:15Z odersky $
+
+
+package scala.runtime
+
+@serializable
+final class BoxedUnitArray(val value: Array[Unit]) extends BoxedArray[Unit] {
+
+  def length: Int = value.length
+
+  def apply(index: Int): Unit = value(index)
+
+  def update(index: Int, elem: Unit) {
+    value(index) = elem
+  }
+  def unbox(elemClass: Class[_]): AnyRef = value
+}
