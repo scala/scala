e8a46e6459d582dcfe88116bb10c5c60871ce172
Martin Odersky <odersky@gmail.com>
2009-08-20 09:56:36 +0000

    Added #:: extractor for streams to package.scala

--- src/library/scala/package.scala
+++ src/library/scala/package.scala
@@ -26,6 +26,7 @@ package object scala {
 
   type Stream[+A] = scala.collection.immutable.Stream[A]
   val Stream = scala.collection.immutable.Stream
+  val #:: = scala.collection.immutable.#::
 
   type StringBuilder = scala.collection.mutable.StringBuilder
   val StringBuilder = scala.collection.mutable.StringBuilder
