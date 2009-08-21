d0024b6e996e294f790972a67b3dd47e2b02dff2
Paul Phillips <paulp@improving.org>
2009-08-21 18:18:39 +0000

    Fix for the discovery that .get on a non-existe...
    Fix for the discovery that .get on a non-existent Map key returns None,
    even if a default value has been declared.

--- src/library/scala/collection/immutable/Map.scala
+++ src/library/scala/collection/immutable/Map.scala
@@ -47,7 +47,7 @@ object Map extends ImmutableMapFactory[Map] {
 
   class WithDefault[A, +B](underlying: Map[A, B], d: A => B) extends Map[A, B] {
     override def size = underlying.size
-    def get(key: A) = underlying.get(key)
+    def get(key: A) = underlying.get(key) orElse Some(default(key))
     def iterator = underlying.iterator
     override def empty = new WithDefault(underlying.empty, d)
     override def updated[B1 >: B](key: A, value: B1): WithDefault[A, B1] = new WithDefault[A, B1](underlying.updated[B1](key, value), d)
