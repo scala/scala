d60430186bd285d543bd8179c7257caf1d5f352c
Mark Harrah <dmharrah@gmail.com>
2009-08-23 22:21:15 -0400

    Compilation with analysis independent of Scala version of sbt. Basic test for this.
    Rewritten from sbt/zinc@7302eac1f2fe5bb1f71e899adcacd135f89e668c

--- src/test/scala/TestCompile.scala
+++ src/test/scala/TestCompile.scala
@@ -40,23 +40,3 @@ object CallbackTest
 		}
 	}
 }
-object WithFiles
-{
-	/** Takes the relative path -> content pairs and writes the content to a file in a temporary directory.  The written file
-	* path is the relative path resolved against the temporary directory path.  The provided function is called with the resolved file paths
-	* in the same order as the inputs. */
-	def apply[T](sources: (File, String)*)(f: Seq[File] => T): T =
-	{
-		withTemporaryDirectory { dir =>
-			val sourceFiles =
-				for((file, content) <- sources) yield
-				{
-					assert(!file.isAbsolute)
-					val to = new File(dir, file.getPath)
-					write(to, content)
-					to
-				}
-			f(sourceFiles)
-		}
-	}
-}
