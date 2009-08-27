d19685e7a52bd421934cfe389886f1a1cc938df1
Paul Phillips <paulp@improving.org>
2009-08-27 20:07:02 +0000

    Test case for #2250, which Martin fixed in r18589.

---
+++ test/files/run/bug2250.scala
@@ -0,0 +1,12 @@
+object Test {
+  def main(args: Array[String]): Unit = {
+    val a: Array[String] = "goobledy bing, goobledy bling, wikka wokka wup.".split("")
+    val b = java.util.Arrays.asList(a: _*)
+    java.util.Collections.shuffle(b)
+
+    // we'll say rather unlikely a.sameElements(b) unless
+    // they are pointing to the same array
+    import scala.collection.JavaConversions._
+    assert(a sameElements b)
+  }
+}
