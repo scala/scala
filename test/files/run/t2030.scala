d0442a8636e268f051052e4e4ff04225bab4f67e
Paul Phillips <paulp@improving.org>
2009-10-01 16:44:56 +0000

    Test case for #2030 and #2056.

--- test/files/run/t2030.scala
+++ test/files/run/t2030.scala
@@ -1,7 +1,8 @@
 import scala.collection.immutable._
 
 object Test extends Application {
-  val res0 = TreeSet(1, 2, 3)
+  val res0 = TreeSet(1, 2, 3, 4, 5, 6)
   val res1 = res0.map(x => x)
+  println(res0.toList == res1.toList)
   println(res1.getClass)
 }
