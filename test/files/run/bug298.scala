fad438ec01992e9aed93df85fda1d9225cceef2b
Paul Phillips <paulp@improving.org>
2009-10-05 05:44:58 +0000

    Test case for #298.

---
+++ test/files/run/bug298.scala
@@ -0,0 +1,17 @@
+object Test extends Application {
+  implicit def anyList[T]: List[T] = Nil
+
+  implicit def intList: List[Int] = 42::24::Nil
+
+  def foo[T](implicit x: T) = x
+
+  val s = foo[List[Int]]
+
+  println(s)  // correct - prints "List(42, 24)"
+
+  implicit def tupleList[T](implicit t: List[T]): List[(T,T)] = t.map(x => (x,x))
+
+  val t = foo[List[Tuple2[Int,Int]]]
+
+  println(t)  // incorrect - prints "List()"
+}
