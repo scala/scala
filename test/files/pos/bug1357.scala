2d5390fd9925c39f18110c1d74d8dbefd3b16f11
Paul Phillips <paulp@improving.org>
2009-10-06 13:36:01 +0000

    Test case for #1357.

---
+++ test/files/pos/bug1357.scala
@@ -0,0 +1,21 @@
+object NonEmptyCons {
+  def unapply[H, T](c: (H, T)): Option[(H, T)] = Some(c)
+}
+
+
+object Main {
+
+  type BT[+H, +T <: Tuple2[Tuple2[H, T], Tuple2[H, T]]] = Tuple2[H, T]
+
+  // type T = Tuple2[String,String]
+  type BinaryTree[+E] = BT[E, T forSome { type T <: Tuple2[BT[E, T], BT[E, T]] }]
+
+  def foo[E](tree: BinaryTree[E]): Unit = tree match {
+    case NonEmptyCons(_, tail) => {
+      tail match {
+        case NonEmptyCons(_, _) => {
+        }
+      }
+    }
+  }
+}
