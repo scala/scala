a0cf7a48c8680a4af5d703b33574579739025b86
Philipp Haller <hallerp@gmail.com>
2009-10-12 13:34:27 +0000

    Fix for disabled test t2359.

--- src/actors/scala/actors/scheduler/DaemonScheduler.scala
+++ src/actors/scala/actors/scheduler/DaemonScheduler.scala
@@ -16,6 +16,7 @@ package scheduler
  */
 object DaemonScheduler extends DelegatingScheduler {
 
-  def makeNewScheduler(): IScheduler = new DefaultThreadPoolScheduler(true)
+  def makeNewScheduler(): IScheduler =
+    new ForkJoinScheduler(true)
 
 }
