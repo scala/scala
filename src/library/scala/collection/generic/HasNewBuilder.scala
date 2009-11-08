f8187cb5199c4a3f464f46ebdbce31d41351dfcc
Martin Odersky <odersky@gmail.com>
2009-11-08 19:21:07 +0000

    Added missing class

---
+++ src/library/scala/collection/generic/HasNewBuilder.scala
@@ -0,0 +1,16 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2003-2009, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+package scala.collection
+package generic
+
+import mutable.Builder
+
+trait HasNewBuilder[+A, +Repr] {
+  /** The builder that builds instances of Repr */
+  protected[this] def newBuilder: Builder[A, Repr]
+}
