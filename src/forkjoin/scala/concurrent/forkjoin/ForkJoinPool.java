38625cc96c837908a129cb2c378a93f8e31eface
Philipp Haller <hallerp@gmail.com>
2009-12-03 11:26:58 +0000

    Replaced RunnableFuture with placeholder interf...
    Replaced RunnableFuture with placeholder interface. ForkJoinPool no
    longer inherits from AbstractExecutorService.

--- src/forkjoin/scala/concurrent/forkjoin/ForkJoinPool.java
+++ src/forkjoin/scala/concurrent/forkjoin/ForkJoinPool.java
@@ -57,7 +57,7 @@ import java.lang.reflect.*;
  * pools with greater than the maximum result in
  * IllegalArgumentExceptions.
  */
-public class ForkJoinPool extends AbstractExecutorService {
+public class ForkJoinPool /*extends AbstractExecutorService*/ {
 
     /*
      * See the extended comments interspersed below for design,
@@ -69,6 +69,10 @@ public class ForkJoinPool extends AbstractExecutorService {
 
     /** Max pool size -- must be a power of two minus 1 */
     private static final int MAX_THREADS =  0x7FFF;
+
+    // placeholder for java.util.concurrent.RunnableFuture
+    interface RunnableFuture<T> extends Runnable {
+    }
 
     /**
      * Factory for creating new ForkJoinWorkerThreads.  A
