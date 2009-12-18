70ed6680a571b0ca0cae338b4af1fe965116ab61
Gilles Dubochet <gilles.dubochet@epfl.ch>
2009-12-18 15:21:44 +0000

    [scaladoc] Added option "-doc-version" to Scala...
    [scaladoc] Added option "-doc-version" to Scaladoc tool (and
    "docversion" attribute to ant task). Both "-doc-version" and
    "-doc-title" can be set, but are not yet used in the output. No review.

--- src/compiler/scala/tools/nsc/doc/Settings.scala
+++ src/compiler/scala/tools/nsc/doc/Settings.scala
@@ -10,16 +10,23 @@ package doc
 import java.io.File
 import java.lang.System
 
+/** An extended version of compiler settings, with additional Scaladoc-specific options.
+  * @param error A function that prints a string to the appropriate error stream. */
 class Settings(error: String => Unit) extends scala.tools.nsc.Settings(error) {
 
-  /** scaladoc specific options */
-  val docformat      = ChoiceSetting    ("-doc-format", "Selects to which format documentation is rendered", List("html"), "html")
-  val doctitle       = StringSetting    ("-doc-title", "doc-title", "Include title for the overview page", "Scala 2 API")
+  /** A setting that defines in which format the documentation is output. ''Note:'' this setting is currently always
+    * `html`. */
+  val docformat      = ChoiceSetting    ("-doc-format", "Selects in which format documentation is rendered", List("html"), "html")
+
+  /** A setting that defines the overall title of the documentation, typically the name of the library being
+    * documented. 'Note:'' This setting is currently not used. */
+  val doctitle       = StringSetting    ("-doc-title", "doc-title", "The overall name of the Scaladoc site", "")
+
+  /** A setting that defines the overall version number of the documentation, typically the version of the library being
+    * documented. 'Note:'' This setting is currently not used. */
+  val docversion     = StringSetting    ("-doc-version", "doc-version", "An optional version number, to be appended to the title", "")
 
   // working around issue described in r18708.
   suppressVTWarn.value = true
 
-
-  // working around issue described in r18708.
-  suppressVTWarn.value = true
 }
