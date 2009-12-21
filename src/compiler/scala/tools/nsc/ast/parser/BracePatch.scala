a326f40dbf25e50976d17a3a90bdd2dc5e37a87e
Martin Odersky <odersky@gmail.com>
2009-12-21 17:30:23 +0000

    (1) Added some classes to allow arbitrary patch...
    (1) Added some classes to allow arbitrary patches to source buffers.
    (These are not yet complete so do not need a review yet I think.
    (2) Avoided reflexive array operations in ScalaRunTime. review by
    (community.

--- src/compiler/scala/tools/nsc/ast/parser/BracePatch.scala
+++ src/compiler/scala/tools/nsc/ast/parser/BracePatch.scala
@@ -10,3 +10,4 @@ package ast.parser
  *  @param inserted  If true, brace needs to be inserted, otherwise brace needs to be deleted.
  */
 case class BracePatch(off: Int, inserted: Boolean)
+extends Patch(off, if (inserted) Insertion("{") else Deletion(1))
---
+++ src/compiler/scala/tools/nsc/ast/parser/Change.scala
@@ -0,0 +1,10 @@
+/* NSC -- new Scala compiler
+ * Copyright 2005-2010 LAMP/EPFL
+ * @author  Martin Odersky
+ */
+package scala.tools.nsc.ast.parser
+
+abstract class Change
+case class Insertion(text: String) extends Change
+case class Deletion(nchars: Int) extends Change
+
