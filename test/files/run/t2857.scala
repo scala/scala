d66a76c1214cb0162931c32b9300f4c1a38b1099
Hubert Plociniczak <hubert.plociniczak@epfl.ch>
2010-01-06 12:33:28 +0000

    Fixes #2857. No review.

---
+++ test/files/run/t2857.scala
@@ -0,0 +1,9 @@
+object Test extends Application {
+  import collection.mutable._
+  val m = new HashMap[Int, Set[String]] with MultiMap[Int, String]
+  m.addBinding(6, "Foo")
+  m.removeBinding(6, "Foo")
+  println(m.contains(6))
+}
+
+
