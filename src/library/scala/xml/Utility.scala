6cb01719eb7e406b7238fd742c358872c0666ff3
David Pollak <feeder.of.the.bears@gmail.com>
2010-02-03 18:04:50 +0000

    Fixed XML Utility.escape method to conform to X...
    Fixed XML Utility.escape method to conform to XML spec. Closes #3014

--- src/library/scala/xml/Utility.scala
+++ src/library/scala/xml/Utility.scala
@@ -106,11 +106,29 @@ object Utility extends AnyRef with parsing.TokenTests
    * @param s    ...
    * @return     ...
    */
-  final def escape(text: String, s: StringBuilder): StringBuilder =
-    text.foldLeft(s)((s, c) => escMap.get(c) match {
-      case Some(str)  => s append str
-      case None       => s append c
-    })
+  final def escape(text: String, s: StringBuilder): StringBuilder = {
+    // Implemented per XML spec:
+    // http://www.w3.org/International/questions/qa-controls
+    // imperative code 3x-4x faster than current implementation
+    // dpp (David Pollak) 2010/02/03
+    val len = text.length
+    var pos = 0
+    while (pos < len) {
+      text.charAt(pos) match {
+        case '<' => s.append("&lt;")
+        case '>' => s.append("&gt;")
+        case '&' => s.append("&amp;")
+        case '"' => s.append("&quot;")
+        case '\n' => s.append('\n')
+        case '\r' => s.append('\r')
+        case '\t' => s.append('\t')
+        case c => if (c >= ' ') s.append(c)
+      }
+
+      pos += 1
+    }
+    s
+  }
 
   /**
    * Appends unescaped string to <code>s</code>, amp becomes &amp;
