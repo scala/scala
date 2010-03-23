5b394541a253a5bfbc4cab52f7547c63cac47ebf
Philipp Haller <hallerp@gmail.com>
2010-03-23 15:23:39 +0000

    Added test case for #3186. Closes #3186.

---
+++ test/files/run/t3186.scala
@@ -0,0 +1,7 @@
+object Dist1 extends Enumeration { val Mile, Foot, Inch = Value }
+
+object Dist2 extends Enumeration { val Kilometer, Millimeter, Parsec = Value }
+
+object Test extends Application {
+  println(Dist1.Mile == Dist2.Kilometer)
+}
