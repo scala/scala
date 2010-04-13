248ae6753e94429d5a5c14f9039ecf588b19b5bc
Derek Chen-Beker <dchenbecker@gmail.com>
2010-04-13 20:51:44 +0000

    Fix for #3284.
    but in the interest of not breaking backwards compatibility, the
    JSON.parse method has been marked deprecated for now.

--- src/library/scala/util/parsing/json/Parser.scala
+++ src/library/scala/util/parsing/json/Parser.scala
@@ -16,6 +16,31 @@ import scala.util.parsing.combinator.syntactical._
 import scala.util.parsing.combinator.lexical._
 
 /**
+ *  A marker class for the JSON result types.
+ *
+ *  @author Derek Chen-Becker <"java"+@+"chen-becker"+"."+"org">
+ */
+sealed abstract class JSONType
+
+/**
+ *  Represents a JSON Object (map).
+ *  @author Derek Chen-Becker <"java"+@+"chen-becker"+"."+"org">
+ */
+case class JSONObject (obj : Map[Any,Any]) extends JSONType {
+  override def toString = "JSONObject(" + obj.map({ case (k,v) => k + " -> " + v }).mkString(", ") + ")"
+}
+
+/**
+ *  Represents a JSON Array (list).
+ *  @author Derek Chen-Becker <"java"+@+"chen-becker"+"."+"org">
+ */
+case class JSONArray (list : List[Any]) extends JSONType {
+  override def toString = "JSONArray(" + list.mkString(", ") + ")"
+}
+
+/**
+ *  The main JSON Parser.
+ *
  *  @author Derek Chen-Becker <"java"+@+"chen-becker"+"."+"org">
  */
 class Parser extends StdTokenParsers with ImplicitConversions {
@@ -40,8 +65,8 @@ class Parser extends StdTokenParsers with ImplicitConversions {
 
   // Define the grammar
   def root       = jsonObj | jsonArray
-  def jsonObj    = "{" ~> repsep(objEntry, ",") <~ "}"
-  def jsonArray  = "[" ~> repsep(value, ",") <~ "]"
+  def jsonObj    = "{" ~> repsep(objEntry, ",") <~ "}" ^^ { case vals : List[_] => JSONObject(Map(vals : _*)) }
+  def jsonArray  = "[" ~> repsep(value, ",") <~ "]" ^^ { case vals : List[_] => JSONArray(vals) }
   def objEntry   = stringVal ~ (":" ~> value) ^^ { case x ~ y => (x, y) }
   def value: Parser[Any] = (jsonObj | jsonArray | number | "true" ^^^ true | "false" ^^^ false | "null" ^^^ null | stringVal)
   def stringVal  = accept("string", { case lexical.StringLit(n) => n} )
