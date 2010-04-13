ee10da727b07edd36ffa6f2fd36c38ac43a97d7a
Iulian Dragos <jaguarul@gmail.com>
2010-04-13 22:52:56 +0000

    Revert "Fix for #3284.
    test/files/run/json.scala, and we need at least one good nightly before
    the RC.

--- src/library/scala/util/parsing/json/JSON.scala
+++ src/library/scala/util/parsing/json/JSON.scala
@@ -42,33 +42,8 @@ object JSON extends Parser {
    *
    * @param input the given JSON string.
    * @return      an optional list of of elements.
-   *
-   * @deprecated Use parseFull or parseRaw as needed.
    */
-  def parse(input: String): Option[List[Any]] = parseRaw(input).map(unRaw).flatMap({
-    case l : List[_] => Some(l)
-    case _ => None
-  })
-
-  /**
-   * This method converts "raw" results back into the original, deprecated
-   * form.
-   */
-  private def unRaw (in : Any) : Any = in match {
-    case JSONObject(obj) => obj.map({ case (k,v) => (k,unRaw(v))}).toList
-    case JSONArray(list) => list.map(unRaw)
-    case x => x
-  }
-
-  /**
-   * Parse the given JSON string and return a list of elements. If the
-   * string is a JSON object it will be a JSONObject. If it's a JSON
-   * array it will be be a JSONArray.
-   *
-   * @param input the given JSON string.
-   * @return      an optional JSONType element.
-   */
-  def parseRaw(input : String) : Option[JSONType] =
+  def parse(input: String): Option[List[Any]] =
     phrase(root)(new lexical.Scanner(input)) match {
       case Success(result, _) => Some(result)
       case _ => None
@@ -83,7 +58,7 @@ object JSON extends Parser {
    * @return      an optional list or map.
    */
   def parseFull(input: String): Option[Any] =
-    parseRaw(input) match {
+    parse(input) match {
       case Some(data) => Some(resolveType(data))
       case None => None
     }
@@ -92,12 +67,23 @@ object JSON extends Parser {
    * A utility method to resolve a parsed JSON list into objects or
    * arrays. See the parse method for details.
    */
-  def resolveType(input: Any): Any = input match {
-    case JSONObject(data) => data.transform {
-      case (k,v) => resolveType(v)
-    }
-    case JSONArray(data) => data.map(resolveType)
-    case x => x
+  def resolveType(input: List[_]): Any = {
+    var objMap = Map[String, Any]()
+
+    if (input.forall {
+      case (key: String, value: List[_]) =>
+        objMap = objMap.+[Any](key -> resolveType(value))
+        true
+      case (key : String, value) =>
+        objMap += key -> value
+        true
+      case _ => false
+    }) objMap
+    else
+      input.map {
+          case l : List[_] => resolveType(l)
+          case x => x
+      }
   }
 
   /**
