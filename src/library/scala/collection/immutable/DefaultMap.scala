a0e5e165c97c01cf0854aeda72e4e9d1d037bb8c
Paul Phillips <paulp@improving.org>
2010-04-24 02:49:34 +0000

    Took another of universal equality's victims to...
    Took another of universal equality's victims to the hospital. Closes
    #3348, no review.

--- src/library/scala/collection/immutable/DefaultMap.scala
+++ src/library/scala/collection/immutable/DefaultMap.scala
@@ -50,7 +50,7 @@ trait DefaultMap[A, +B] extends Map[A, B] { self =>
    */
   override def - (key: A): Map[A, B] = {
     val b = newBuilder
-    b ++= this filter (key !=)
+    for (kv <- this ; if kv._1 != key) b += kv
     b.result
   }
 }
