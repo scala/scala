d76868fc01d27ce44e6e7c925ca20088a5c14e8a
Antonio Cunei <antonio.cunei@epfl.ch>
2010-05-07 13:34:27 +0000

    Merged revisions 21845,21847 via svnmerge from
    https://lampsvn.epfl.ch/svn-repos/scala/scala/trunk
    
    ........
      r21845 | moors | 2010-05-07 11:28:25 +0200 (Fri, 07 May 2010) | 1 line
    
      docs for breakout and no-arg apply in canbuildfrom ........ r21847 |
    plocinic | 2010-05-07 12:38:47 +0200 (Fri, 07 May 2010) | 1 line
    
      Fixed partest for windows nightly. No review.
    ........

--- src/library/scala/collection/package.scala
+++ src/library/scala/collection/package.scala
@@ -3,8 +3,10 @@ package scala
 package object collection {
   import scala.collection.generic.CanBuildFrom // can't refer to CanBuild here
 
+  /** Provides a CanBuildFrom instance that builds a specific target collection (`To') irrespective of the original collection (`From').
+   */
   def breakOut[From, T, To](implicit b : CanBuildFrom[Nothing, T, To]) =
-    new CanBuildFrom[From, T, To] {
+    new CanBuildFrom[From, T, To] { // TODO: could we just return b instead?
       def apply(from: From) = b.apply() ; def apply() = b.apply()
     }
 }
