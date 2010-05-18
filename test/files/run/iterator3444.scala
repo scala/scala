da8253c2e07a8e461c1ece23e68fb2b72a638dd2
Aleksandar Pokopec <aleksandar.prokopec@epfl.ch>
2010-05-18 21:16:31 +0000

    Fixes #3444. Review by phaller.

---
+++ test/files/run/iterator3444.scala
@@ -0,0 +1,23 @@
+
+
+// ticked #3444
+object Test {
+
+  def main(args: Array[String]) {
+    val it = (1 to 12).toSeq.iterator
+
+    assert(it.next == 1)
+    assert(it.take(2).toList == List(2, 3))
+
+    val jt = (4 to 12).toSeq.iterator
+    assert(jt.next == 4)
+    assert(jt.drop(5).toList == List(10, 11, 12))
+
+    val kt = (1 until 10).toSeq.iterator
+    assert(kt.drop(50).toList == Nil)
+
+    val mt = (1 until 5).toSeq.iterator
+    assert(mt.take(50).toList == List(1, 2, 3, 4))
+  }
+
+}
