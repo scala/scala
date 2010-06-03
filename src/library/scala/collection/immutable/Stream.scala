60354bdda2978ecbadbf29db0ec0532b0cc6c133
Aleksandar Pokopec <aleksandar.prokopec@epfl.ch>
2010-06-03 08:52:01 +0000

    Fixes #3511 by adding a custom StreamView.

--- src/library/scala/collection/immutable/Stream.scala
+++ src/library/scala/collection/immutable/Stream.scala
@@ -460,9 +460,17 @@ self =>
       flatten1(asTraversable(head))
   }
 
+  override def view = new StreamView[A, Stream[A]] {
+    protected lazy val underlying = self.repr
+    override def iterator = self.iterator
+    override def length = self.length
+    override def apply(idx: Int) = self.apply(idx)
+  }
+
   /** Defines the prefix of this object's <code>toString</code> representation as ``Stream''.
    */
   override def stringPrefix = "Stream"
+
 }
 
 /**
