da5910c7c622fc248fe525f5c03b1dee6be384b5
Paul Phillips <paulp@improving.org>
2010-06-04 18:34:02 +0000

    Fix for init-order caused NPE in NumericRange.
    ran across some tortured logic trying to accomodate the long abandoned
    idea of having 5 != 5L, so simplified the contains method. Closes #3518,
    no review.

---
+++ test/files/run/bug3518.scala
@@ -0,0 +1,16 @@
+object Test {
+  val r1 = 1.0 to 10.0 by 0.5
+  val r2 = 1.0 to 1.0 by 1.0
+  val r3 = 10.0 to 1.0 by -0.5
+  val r4 = 1.0 until 1.0 by 1.0
+  val r5 = 1 to 100 by 2
+
+  def main(args: Array[String]): Unit = {
+    assert(r3 forall (r1 contains _))
+    assert(r1 forall (r3 contains _))
+    assert(r2.size == 1)
+    assert(r4.isEmpty)
+    assert(List(1,3,5,97,99) forall (r5 contains _))
+    assert(List(2,4,6,98,100) forall (x => !r5.contains(x)))
+  }
+}
