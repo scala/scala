f492b003234d3c6e03fb0c1e71ac1eb5269f66a4
Iulian Dragos <jaguarul@gmail.com>
2010-06-14 11:27:45 +0000

    Closes #3558.

---
+++ test/files/run/spec-constr.scala
@@ -0,0 +1,14 @@
+object Test {
+  class E[@specialized(Int) A](var f: A => Boolean) {
+    def this() = this(null)
+
+    println("hello?")
+    if (f == null) f = { _ => false }
+  }
+
+  def main(args: Array[String]) {
+    new E[Int]
+    println("goodbye")
+  }
+}
+
