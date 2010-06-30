574f77446b87425744b20b3d43c0cc05fae2e03e
Paul Phillips <paulp@improving.org>
2010-06-30 23:56:32 +0000

    Test case closes #3440.  No review.

---
+++ test/files/pos/bug3440.scala
@@ -0,0 +1,18 @@
+object test {
+  abstract class SampleFormat1 {
+    def readerFactory: Any
+  }
+
+  case object Int8 extends SampleFormat1 {
+    def readerFactory = error("")
+  }
+  case object Int16 extends SampleFormat1 {
+    def readerFactory = error("")
+  }
+
+  (new {}: Any) match {
+   case 8   => Int8
+   case 16  => Int16
+   case _   => error("")
+  }
+}
