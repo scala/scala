322e856f132c4ee8ac651848a041b693f830371b
Paul Phillips <paulp@improving.org>
2010-07-02 20:50:29 +0000

    Routed all places in trunk which look for jar f...
    Routed all places in trunk which look for jar files through the same
    function, and then changed the default behavior to recognize jars even
    if they don't have a *.jar extension by squinting at the first few bytes
    of the file.
    
    Closes #3000 (THE AMAZING TICKET 3000!), review by phaller.

--- src/compiler/scala/tools/nsc/plugins/Plugin.scala
+++ src/compiler/scala/tools/nsc/plugins/Plugin.scala
@@ -133,7 +133,7 @@ object Plugin {
     val alljars = (jars ::: (for {
       dir <- dirs if dir.isDirectory
       entry <- dir.toDirectory.files.toList sortBy (_.name)
-      if entry.extension == "jar"
+      if Path.isJarOrZip(entry)
       pdesc <- loadDescription(entry)
       if !(ignoring contains pdesc.name)
     } yield entry)).distinct
