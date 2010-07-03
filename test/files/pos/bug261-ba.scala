7a8a37e5f154da49cca1ea7e36ab94d16751ec66
Paul Phillips <paulp@improving.org>
2010-07-03 05:19:47 +0000

    Test case closes #261, no review.

---
+++ test/files/pos/bug261-ba.scala
@@ -0,0 +1,9 @@
+trait B {
+  private val foo: String = "B"
+  def f = println(foo)
+}
+trait A { val foo: String = "A" }
+object Test extends Application with B with A {
+  println(foo) // prints "A", as expected
+  f            // prints "B", as expected
+}
