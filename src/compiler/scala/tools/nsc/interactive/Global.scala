9c365348fd4e0e40b6499f2abf1b1057735296f6
Martin Odersky <odersky@gmail.com>
2010-07-19 13:56:03 +0000

    Added `ask` method to compiler control to do fa...
    Added `ask` method to compiler control to do fast trunaround
    computations on presentation compiler thread.

--- src/compiler/scala/tools/nsc/interactive/Global.scala
+++ src/compiler/scala/tools/nsc/interactive/Global.scala
@@ -110,10 +110,16 @@ self =>
   // ----------------- Polling ---------------------------------------
 
   /** Called from runner thread and signalDone:
-   *  Poll for exceptions.
-   *  Poll for work reload/typedTreeAt/doFirst commands during background checking.
+   *  Poll for interrupts and execute them immediately.
+   *  Then, poll for exceptions and execute them.
+   *  Then, poll for work reload/typedTreeAt/doFirst commands during background checking.
    */
   def pollForWork() {
+    scheduler.pollInterrupt() match {
+      case Some(ir) =>
+        ir.execute(); pollForWork()
+      case _ =>
+    }
     scheduler.pollThrowable() match {
       case Some(ex @ CancelActionReq) => if (acting) throw ex
       case Some(ex @ FreshRunReq) =>
