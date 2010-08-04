8a8172da3cda740586f86a3b4b276e7d9235bca6
Gilles Dubochet <gilles.dubochet@epfl.ch>
2010-08-04 14:54:10 +0000

    Keyword of method symbol signature is correctly...
    Keyword of method symbol signature is correctly printed as 'def' instead
    of 'val'. No review.

--- src/compiler/scala/tools/nsc/symtab/Symbols.scala
+++ src/compiler/scala/tools/nsc/symtab/Symbols.scala
@@ -1506,7 +1506,7 @@ trait Symbols extends reflect.generic.Symbols { self: SymbolTable =>
       else if (isVariable) "var"
       else if (isPackage) "package"
       else if (isModule) "object"
-      else if (isMethod) "def"
+      else if (isSourceMethod) "def"
       else if (isTerm && (!hasFlag(PARAM) || hasFlag(PARAMACCESSOR))) "val"
       else ""
 
