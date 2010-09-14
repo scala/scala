c0f03d837c9af04f404e800ce6fbeb65d1eac38a
Adriaan Moors <adriaan.moors@epfl.ch>
2010-09-14 12:15:35 +0000

    closes #3692: make instantiateTypeVar more care...
    closes #3692: make instantiateTypeVar more careful so it does not change
    T's info to >: T <: T.
    
    review by odersky

---
+++ test/files/neg/t3692.scala
@@ -0,0 +1,17 @@
+object ManifestTester {
+  def main(args: Array[String]) = {
+    val map = Map("John" -> 1, "Josh" -> 2)
+    new ManifestTester().toJavaMap(map)
+  }
+}
+
+class ManifestTester {
+  private final def toJavaMap[T, V](map: Map[T, V])(implicit m1: Manifest[T], m2: Manifest[V]): java.util.Map[_, _] = {
+    map match {
+      case m0: Map[Int, Int] => new java.util.HashMap[Integer, Integer]
+      case m1: Map[Int, V] => new java.util.HashMap[Integer, V]
+      case m2: Map[T, Int] => new java.util.HashMap[T, Integer]
+      case _ => new java.util.HashMap[T, V]
+    }
+  }
+}
