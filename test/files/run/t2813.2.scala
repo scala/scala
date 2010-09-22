d5baff39ed0f25a39818591dbbd069551352b91d
Paul Phillips <paulp@improving.org>
2010-09-22 21:05:40 +0000

    Test case submitted by mark harrah closes #2813...
    Test case submitted by mark harrah closes #2813, no review.

---
+++ test/files/run/t2813.2.scala
@@ -0,0 +1,39 @@
+import java.util.LinkedList
+import collection.JavaConversions._
+
+object Test extends Application {
+  def assertListEquals[A](expected: List[A], actual: Seq[A]) {
+    assert(expected.sameElements(actual),
+           "Expected list to contain " + expected.mkString("[", ", ", "]") +
+           ", but was " + actual.mkString("[", ", ", "]"))
+  }
+
+  def addAllOfNonCollectionWrapperAtZeroOnEmptyLinkedList() {
+    val l = new LinkedList[Int]
+    l.addAll(0, List(1, 2))
+    assertListEquals(List(1, 2), l)
+  }
+
+  def addAllOfNonCollectionWrapperAtZeroOnLinkedList() {
+    val l = new LinkedList[Int] + 1 + 2
+    l.addAll(0, List(10, 11))
+    assertListEquals((List(10, 11, 1, 2)), l)
+  }
+
+  def addAllOfCollectionWrapperAtZeroOnLinkedList() {
+    val l = new LinkedList[Int] + 1 + 2
+    l.addAll(0, new LinkedList[Int] + 10 + 11)
+    assertListEquals((List(10, 11, 1, 2)), l)
+  }
+
+  def addAllOfCollectionWrapperAtZeroOnEmptyLinkedList() {
+    val l = new LinkedList[Int]
+    l.addAll(0, new LinkedList[Int] + 10 + 11)
+    assertListEquals((List(10, 11)), l)
+  }
+
+  addAllOfNonCollectionWrapperAtZeroOnEmptyLinkedList
+  addAllOfNonCollectionWrapperAtZeroOnLinkedList
+  addAllOfCollectionWrapperAtZeroOnEmptyLinkedList
+  addAllOfCollectionWrapperAtZeroOnLinkedList
+}
