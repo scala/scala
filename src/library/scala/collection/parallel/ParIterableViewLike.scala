e4afb129495bd39d260502829cfbaef6e7df3897
Aleksandar Pokopec <aleksandar.prokopec@epfl.ch>
2010-09-24 15:21:18 +0000

    A small typo. No review

--- src/library/scala/collection/parallel/ParIterableViewLike.scala
+++ src/library/scala/collection/parallel/ParIterableViewLike.scala
@@ -43,7 +43,7 @@ self =>
 
   override protected[this] def newCombiner: Combiner[T, This] = throw new UnsupportedOperationException(this + ".newCombiner");
 
-  type CPI = SignalContextPassingIterator[ParIterator]
+  type SCPI = SignalContextPassingIterator[ParIterator]
 
   /* wrappers */
 
