40cd42b7f53965424fd3c7cc655466e1c0861784
Paul Phillips <paulp@improving.org>
2010-10-09 16:29:11 +0000

    Settings help tweak.  Closes #3910, no review.

--- src/compiler/scala/tools/nsc/settings/ScalaSettings.scala
+++ src/compiler/scala/tools/nsc/settings/ScalaSettings.scala
@@ -124,7 +124,7 @@ trait ScalaSettings extends AbsScalaSettings with StandardScalaSettings {
   val nopredefs     = BooleanSetting    ("-Yno-predefs", "Compile without any implicit predefined values")
   val Yrecursion    = IntSetting        ("-Yrecursion", "Recursion depth used when locking symbols", 0, Some(0, Int.MaxValue), (_: String) => None)
   val selfInAnnots  = BooleanSetting    ("-Yself-in-annots", "Include a \"self\" identifier inside of annotations")
-  val Xshowtrees    = BooleanSetting    ("-Yshow-trees", "Show detailed trees when used in connection with -print:phase")
+  val Xshowtrees    = BooleanSetting    ("-Yshow-trees", "Show detailed trees when used in connection with -Xprint:<phase>")
   val skip          = PhasesSetting     ("-Yskip", "Skip")
   val Xsqueeze      = ChoiceSetting     ("-Ysqueeze", "if on, creates compact code in matching", List("on","off"), "on") .
                                           withHelpSyntax("-Ysqueeze:<enabled>")
