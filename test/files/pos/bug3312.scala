6de5505cd98b8d852b1b63e3f0ec1e48e9deb701
Paul Phillips <paulp@improving.org>
2010-10-26 05:59:48 +0000

    Added test for #3312 which it looks like was fi...
    Added test for #3312 which it looks like was fixed long ago but not
    closed. Closes #3312, no review.

---
+++ test/files/pos/bug3312.scala
@@ -0,0 +1,17 @@
+trait Root {
+  def say: String
+}
+
+trait A extends Root {
+  override def say: String = "bow"
+}
+
+trait B extends Root {
+  override def say: String = "hi"
+}
+
+object Foo extends A with B  {
+  override def say: String = foo(super[A].say)
+
+  def foo(p: => String): String = p
+}
