16b3e8c1d70a0ddf5307c6564fd34a4854e4bf77
Paul Phillips <paulp@improving.org>
2010-11-28 02:38:38 +0000

    Fleshed out the mock code a little further so i...
    Fleshed out the mock code a little further so it's easy for closures to
    become SAMs.
    
    // implicit not necessary, but improves fun factor
    scala> implicit def mkUFn(x: AnyRef) = scala.tools.reflect.UniversalFn(x)
    mkUFn: (x: AnyRef)scala.tools.reflect.UniversalFn
    
    scala> (() => 5*5*5).as[java.util.concurrent.Callable[Int]]
    
    res1: java.util.concurrent.Callable[Int] = <function1>
    
    scala> res1.call
    res2: Int = 125
    
    No review.

--- src/compiler/scala/tools/reflect/package.scala
+++ src/compiler/scala/tools/reflect/package.scala
@@ -14,10 +14,23 @@ package object reflect {
     if (cl == null) Nil
     else cl.getInterfaces.toList ++ allInterfaces(cl.getSuperclass) distinct
 
+  def methodsNamed(target: AnyRef, name: String): List[Method] =
+    target.getClass.getMethods filter (x => x.getName == name) toList
+
+  /** If there is a single non-bridge apply method in the given instance,
+   *  return it: otherwise None.
+   */
+  def uniqueApply(target: AnyRef) = {
+    methodsNamed(target, "apply") filterNot (_.isBridge) match {
+      case List(x)  => Some(x)
+      case _        => None
+    }
+  }
+
   def zeroOfClass(clazz: Class[_]) = zeroOf(Manifest.classType(clazz))
   def zeroOf[T](implicit m: Manifest[T]): AnyRef = {
     if (m == manifest[Boolean] || m == manifest[jl.Boolean]) false: jl.Boolean
-    else if (m == manifest[Unit] || m == manifest[jl.Void]) scala.runtime.BoxedUnit.UNIT
+    else if (m == manifest[Unit] || m == manifest[jl.Void] || m == manifest[scala.runtime.BoxedUnit]) scala.runtime.BoxedUnit.UNIT
     else if (m == manifest[Char] || m == manifest[jl.Character]) 0.toChar: jl.Character
     else if (m == manifest[Byte] || m == manifest[jl.Byte]) 0.toByte: jl.Byte
     else if (m == manifest[Short] || m == manifest[jl.Short]) 0.toShort: jl.Short
