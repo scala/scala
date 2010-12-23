7418d531f0309aa43c7002386b35e73742f063a3
Paul Phillips <paulp@improving.org>
2010-12-23 02:57:42 +0000

    Fix for stability failure, I think.  No review.

--- src/compiler/scala/tools/nsc/backend/jvm/GenJVM.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenJVM.scala
@@ -1727,11 +1727,12 @@ abstract class GenJVM extends SubComponent with GenJVMUtil with GenAndroid {
     ////////////////////// Utilities ////////////////////////
 
     private val javaNameCache = mutable.HashMap[Symbol, String]()
-    override def javaName(sym: Symbol): String = javaNameCache.getOrElseUpdate(sym, {
+    override def javaName(sym: Symbol): String = {
       if (sym.isClass && !sym.rawowner.isPackageClass && !sym.isModuleClass)
         innerClasses = innerClasses + sym
-      super.javaName(sym)
-    })
+
+      javaNameCache.getOrElseUpdate(sym, super.javaName(sym))
+    }
 
     private val javaTypeCache = mutable.HashMap[Symbol, JType]()
     override def javaType(sym: Symbol): JType = javaTypeCache.getOrElseUpdate(sym, super.javaType(sym))
