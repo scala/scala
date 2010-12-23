8b567935cf21eb5179cbbf9aa5e4bffd6ab32d6b
Paul Phillips <paulp@improving.org>
2010-12-23 04:47:25 +0000

    Looks like there is some side effecting I can't...
    Looks like there is some side effecting I can't pin down. Reverts the
    last two patches until my bring gets bigger. No review.

--- src/compiler/scala/tools/nsc/backend/jvm/GenJVM.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenJVM.scala
@@ -1726,16 +1726,11 @@ abstract class GenJVM extends SubComponent with GenJVMUtil with GenAndroid {
 
     ////////////////////// Utilities ////////////////////////
 
-    private val javaNameCache = mutable.HashMap[Symbol, String]()
     override def javaName(sym: Symbol): String = {
       if (sym.isClass && !sym.rawowner.isPackageClass && !sym.isModuleClass)
         innerClasses = innerClasses + sym
-
-      javaNameCache.getOrElseUpdate(sym, super.javaName(sym))
+      super.javaName(sym)
     }
-
-    private val javaTypeCache = mutable.HashMap[Symbol, JType]()
-    override def javaType(sym: Symbol): JType = javaTypeCache.getOrElseUpdate(sym, super.javaType(sym))
 
     /** Calls to methods in 'sym' need invokeinterface? */
     def needsInterfaceCall(sym: Symbol): Boolean = {
