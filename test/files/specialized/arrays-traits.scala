47bea31877a637c3c0510b6fc714a273ee8d9b64
Aleksandar Pokopec <aleksandar.prokopec@epfl.ch>
2011-02-09 16:34:38 +0000

    A missing test.
    No review

---
+++ test/files/specialized/arrays-traits.scala
@@ -0,0 +1,46 @@
+
+
+
+import runtime.ScalaRunTime._
+
+
+
+trait SuperS[@specialized(AnyRef) T] {
+  def arr: Array[T]
+  def foo() = arr(0)
+  def bar(b: Array[T]) = b(0) = arr(0)
+}
+
+
+class BaseS[@specialized(AnyRef) T](val arr: Array[T]) extends SuperS[T] {
+}
+
+
+trait SuperG[T] {
+  def arr: Array[T]
+  def foo() = arr(0)
+  def bar(b: Array[T]) = b(0) = arr(0)
+}
+
+
+class BaseG[T](val arr: Array[T]) extends SuperG[T] {
+}
+
+
+object Test {
+
+  def main(args: Array[String]) {
+    (new BaseS(new Array[String](1)): SuperS[String]).foo
+    println(arrayApplyCount)
+    (new BaseS(new Array[String](1)): SuperS[String]).bar(new Array[String](1))
+    println(arrayApplyCount)
+    println(arrayUpdateCount)
+
+    (new BaseG(new Array[String](1)): SuperG[String]).foo
+    println(arrayApplyCount)
+    (new BaseG(new Array[String](1)): SuperG[String]).bar(new Array[String](1))
+    println(arrayApplyCount)
+    println(arrayUpdateCount)
+  }
+
+}
