804a16122790442ba7eb21d3b07e0239376666de
Hubert Plociniczak <hubert.plociniczak@epfl.ch>
2011-02-10 14:01:16 +0000

    Reverting failed attempt to add more debugging ...
    Reverting failed attempt to add more debugging to build manager. no
    review

--- src/compiler/scala/tools/nsc/interactive/RefinedBuildManager.scala
+++ src/compiler/scala/tools/nsc/interactive/RefinedBuildManager.scala
@@ -116,10 +116,8 @@ class RefinedBuildManager(val settings: Settings) extends Changes with BuildMana
     def update0(files: Set[AbstractFile]): Unit = if (!files.isEmpty) {
       deleteClassfiles(files)
       val run = compiler.newRun()
-      if (settings.Ybuildmanagerdebug.value) {
-        compiler.inform("classpath used: " + compiler.settings.classpath)
+      if (settings.Ybuildmanagerdebug.value)
         compiler.inform("compiling " + toStringSet(files))
-      }
       buildingFiles(files)
 
       run.compileFiles(files.toList)
