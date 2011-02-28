34719ee9cb51650550cfcf849178cf5da5a93dd5
Paul Phillips <paulp@improving.org>
2011-02-28 20:52:52 +0000

    Test case closes #3613, no review.

---
+++ test/files/run/bug3613.scala
@@ -0,0 +1,22 @@
+class Boopy {
+    private val s = new Schnuck
+    def observer : PartialFunction[ Any, Unit ] = s.observer
+
+    private class Schnuck extends javax.swing.AbstractListModel {
+        model =>
+        val observer : PartialFunction[ Any, Unit ] = {
+            case "Boopy" => fireIntervalAdded( model, 0, 1 )
+        }
+        def getSize = 0
+        def getElementAt( idx: Int ) : AnyRef = "egal"
+    }
+
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val x = new Boopy
+    val o = x.observer
+    o( "Boopy" ) // --> throws runtime error
+  }
+}
