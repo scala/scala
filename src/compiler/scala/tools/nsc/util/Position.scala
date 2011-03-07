fe94d625630e36a97f9e7845b94b9619e3707382
Donna Malayeri <lindydonna@gmail.com>
2011-03-07 16:51:12 +0000

    Clarify documentation of Position.includes, clo...
    Clarify documentation of Position.includes, closes #2874. No review.

--- src/compiler/scala/tools/nsc/util/Position.scala
+++ src/compiler/scala/tools/nsc/util/Position.scala
@@ -132,8 +132,8 @@ trait Position {
   def focusEnd = this
 
   /** Does this position include the given position `pos`.
-   *  This holds if this is a range position and its range [start..end]
-   *  is the same or covers the range of the given position.
+   *  This holds if `this` is a range position and its range [start..end]
+   *  is the same or covers the range of the given position, which may or may not be a range position.
    */
   def includes(pos: Position) = false
 
