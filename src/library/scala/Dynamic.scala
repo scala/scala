764072ffcbadde8b296eaa2f963f4792af2d70c4
Martin Odersky <odersky@gmail.com>
2011-03-14 14:36:11 +0000

    Removing everything in trait Dynamic.

--- src/library/scala/Dynamic.scala
+++ src/library/scala/Dynamic.scala
@@ -1,22 +1,12 @@
 package scala
 
-/** A trait that supports dynamic invocations. Instances `x` of this trait
+/** A marker trait that enables dynamic invocations. Instances `x` of this trait
  *  allow calls `x.meth(args)` for arbitrary method names `meth` and argument lists
  *  `args`. If a call is not natively supported by `x`, it is rewritten to
- *  `x.invokeDynamic("meth", args)`.
+ *  `x.applyDynamic("meth", args)`.
  */
 trait Dynamic {
 
-  /** The dynamic invocation operation
-   *  @param name  The name of the invoked method
-   *  @param args  The arguments to the method
-   */
-  def applyDynamic(name: String)(args: Any*): Any
-
-  /** Returns the underlying value typed as an instance of type T
-   *  @param T  The target type
-   */
-  def typed[T]: T
 }
 
 
