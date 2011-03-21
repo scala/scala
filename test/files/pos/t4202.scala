0b0a927a607f8884651168c2361c2d91ebcd04b0
Hubert Plociniczak <hubert.plociniczak@epfl.ch>
2011-03-21 13:37:53 +0000

    Closes #4202 again, closes #4363.

--- test/files/pos/t4202.scala
+++ test/files/pos/t4202.scala
@@ -1,7 +1,18 @@
-object t4202 {
+object t4202_1 {
   () => {
     trait T {
       def t = ()
+    }
+  }
+}
+
+object t4202_2 {
+  () => {
+    trait T {
+      def t = ()
+    }
+    object T2 extends T {
+      t
     }
   }
 }
