9e0b5eb6c4ff9ca165c8e8fd629e23ae061d77f9
Paul Phillips <paulp@improving.org>
2011-03-26 19:42:44 +0000

    Cleaning up util.Tracer and its usage in Erasure.

--- src/compiler/scala/tools/nsc/util/Tracer.scala
+++ src/compiler/scala/tools/nsc/util/Tracer.scala
@@ -6,31 +6,56 @@
 package scala.tools.nsc
 package util
 
+import java.io.PrintStream
+
 class Tracer(enabled: () => Boolean) {
+  def out: PrintStream = System.out
+  def intoString(x: Any): String = "" + x
+  def stringify(x: Any): String = x match {
+    case null                   => "null"
+    case x: TraversableOnce[_]  => x map stringify mkString ", "
+    case x: Product             => stringify(x.productIterator)
+    case x: AnyRef              => intoString(x)
+  }
+
+  private val LBRACE = "{"
+  private val RBRACE = "}"
   private var indentLevel = 0
-  private def ind(s: String) = (" " * (indentLevel*2)) + s
+  private def ind(s: String) = (" " * (indentLevel * 2)) + s
   private def indented[T](body: => T): T = {
     indentLevel += 1
     try body
     finally indentLevel -= 1
   }
   private def p(s: String) = {
-    System.out.print(s)
-    System.out.flush()
+    out.print(s)
+    out.flush()
   }
   private def pin[T](x: T): T = {
     p(ind("" + x))
     x
   }
-  def seq[T](name: String, args: => Seq[Any])(body: => T): T = {
+  def apply[T](name: String, args: => Any)(body: => T): T = {
+    val result = body
     if (enabled()) {
-      p(ind("%s(%s) = {\n".format(name, args mkString ", ")))
-      try indented(pin(body))
-      finally println("\n" + ind("}"))
+      // concise output optimization
+      val boolResult = result match {
+        case x: Boolean => Some(x)
+        case _          => None
+      }
+      p(ind("%s(%s) = %s\n".format(
+        name,
+        stringify(args),
+        boolResult getOrElse LBRACE))
+      )
+      if (boolResult.isEmpty) {
+        indented(pin(result))
+        p("\n" + ind(RBRACE))
+      }
+      result
     }
-    else body
+    else result
   }
-  def apply[T](name: String, args: Any*)(body: => T): T = seq(name, args.toSeq)(body)
 }
 
 object Tracer {
