21f3cf0e80286a12c267b9e68c8df8a37f4e0c04
Donna Malayeri <lindydonna@gmail.com>
2011-04-07 14:03:49 +0000

    [scaladoc] Don't generate an attributes block i...
    [scaladoc] Don't generate an attributes block if its contents are empty.
    No review.

--- src/compiler/scala/tools/nsc/doc/html/page/Template.scala
+++ src/compiler/scala/tools/nsc/doc/html/page/Template.scala
@@ -383,10 +383,12 @@ class Template(tpl: DocTemplateEntity) extends HtmlPage {
     }
     // end attributes block vals ---
 
+    val attributesInfo = attributes ++ definitionClasses ++ selfType ++ annotations ++ sourceLink ++ deprecation ++ mainComment
     val attributesBlock =
-      <dl class="attributes block"> {
-        attributes ++ definitionClasses ++ selfType ++ annotations ++ sourceLink ++ deprecation ++ mainComment
-      }</dl>
+      if (attributesInfo.isEmpty)
+        NodeSeq.Empty
+      else
+        <dl class="attributes block"> { attributesInfo }</dl>
 
     val linearization =
       mbr match {
