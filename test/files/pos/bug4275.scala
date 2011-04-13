703bbdae73800e49d737e133e3a05929005294fc
Paul Phillips <paulp@improving.org>
2011-04-13 03:26:27 +0000

    Test cases for #1071 and #4275 since I don't se...
    Test cases for #1071 and #4275 since I don't see a lot of test cases,
    hint hint, no review.

---
+++ test/files/pos/bug4275.scala
@@ -0,0 +1,13 @@
+object Test {
+  def f = "abc".count(_ > 'a')
+
+  class A {
+    private val count: Int = 0
+  }
+  class B extends A { }
+  object B {
+    implicit def b2seq(x: B): Seq[Int] = Nil
+
+    def f = (new B) count (_ > 0)
+  }
+}
