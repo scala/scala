70f18a67e5cc55e534a504b71ee26a93488b40f8
Paul Phillips <paulp@improving.org>
2011-05-01 19:58:44 +0000

    Added a @bridge'd $scope back to scala package ...
    Added a @bridge'd $scope back to scala package object for binary compat.
    No review.

--- src/library/scala/package.scala
+++ src/library/scala/package.scala
@@ -6,7 +6,7 @@
 **                          |/                                          **
 \*                                                                      */
 
-
+import annotation.bridge
 
 /**
  * Core Scala types. They are always available without an explicit import.
@@ -74,6 +74,11 @@ package object scala {
   // Migrated from Predef
   @deprecated("Use Thread.currentThread instead", "2.9.0")
   def currentThread = java.lang.Thread.currentThread()
+
+  // Moved back into Predef to avoid unnecessary indirection by
+  // way of the scala package object within the standard library,
+  // but bridged for compatibility.
+  @bridge def $scope = scala.xml.TopScope
 
   // Numeric types which were moved into scala.math.*
 
