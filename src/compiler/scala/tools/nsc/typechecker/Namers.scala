83c0929417261c900100b57c19d5172f4d5bf665
Paul Phillips <paulp@improving.org>
2011-06-12 02:24:51 +0000

    Reverts r25051 due to witnessed movement of sca...
    Reverts r25051 due to witnessed movement of scalaz in the noncompiling
    direction. Postmortem help I hope in the review by moors.

--- src/compiler/scala/tools/nsc/typechecker/Namers.scala
+++ src/compiler/scala/tools/nsc/typechecker/Namers.scala
@@ -637,8 +637,14 @@ trait Namers { self: Analyzer =>
 
       // This infers Foo.type instead of "object Foo"
       // See Infer#adjustTypeArgs for the polymorphic case.
-      if (tpe.typeSymbolDirect.isModuleClass) tpe1
-      else if (sym.isVariable || sym.isMethod && !sym.hasAccessorFlag)
+      //
+      // XXX - disabled for now due to impact on implicit search, visible
+      // when trying to compile scalaz.  Example:
+      // scalaz/Each.scala:27: value toList is not a member of scalaz.Scalaz.IndSeq[A]
+      //          def each[A](e: IndSeq[A], f: A => Unit) = e.toList foreach f
+      // if (tpe.typeSymbolDirect.isModuleClass) tpe1
+      // else
+      if (sym.isVariable || sym.isMethod && !sym.hasAccessorFlag)
         if (tpe2 <:< pt) tpe2 else tpe1
       else if (isHidden(tpe)) tpe2
       // In an attempt to make pattern matches involving method local vals
