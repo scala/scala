270c0cb80df85b613cfa3e5f85f70cf1f35e2388
Paul Phillips <paulp@improving.org>
2011-06-16 16:01:10 +0000

    Suppressing the ability to suppress stack trace...
    Suppressing the ability to suppress stack trace suppression in
    NonLocalReturnControl for performance reasons, no review.

--- src/library/scala/runtime/NonLocalReturnControl.scala
+++ src/library/scala/runtime/NonLocalReturnControl.scala
@@ -12,4 +12,6 @@ package scala.runtime
 
 import scala.util.control.ControlThrowable
 
-class NonLocalReturnControl[T](val key: AnyRef, val value: T) extends ControlThrowable
+class NonLocalReturnControl[T](val key: AnyRef, val value: T) extends ControlThrowable {
+  final override def fillInStackTrace(): Throwable = this
+}
