dbd2a2a626be10c5cb656e97c120258112f9c71a
Paul Phillips <paulp@improving.org>
2011-07-06 15:44:15 +0000

    Test case for #4737, no review.

---
+++ test/files/pos/bug4737/J_1.java
@@ -0,0 +1,9 @@
+package j;
+
+public class J_1 {
+    protected class JavaInnerClass {
+    }
+    public void method(JavaInnerClass javaInnerclass) {
+        System.out.println("hello");
+    }
+}
---
+++ test/files/pos/bug4737/S_2.scala
@@ -0,0 +1,10 @@
+package s
+
+import j.J_1
+
+class ScalaSubClass extends J_1 {
+  override def method(javaInnerClass: J_1#JavaInnerClass) {
+    println("world")
+  }
+}
+
