29e501db0b70abb6e6791b03c7af6fc11cadfc20
Paul Phillips <paulp@improving.org>
2011-07-16 05:51:35 +0000

    Added Vector class documentation.

--- src/library/scala/collection/immutable/Vector.scala
+++ src/library/scala/collection/immutable/Vector.scala
@@ -15,6 +15,13 @@ import scala.collection.generic._
 import scala.collection.mutable.Builder
 import scala.collection.parallel.immutable.ParVector
 
+/** Vector is a general-purpose, immutable data structure.  It provides random access and updates
+ * in effectively constant time, as well as very fast append and prepend.  Because vectors strike
+ * a good balance between fast random selections and fast random functional updates, they are
+ * currently the default implementation of immutable indexed sequences.  It is backed by a little
+ * endian bit-mapped vector trie with a branching factor of 32.  Locality is very good, but not
+ * contiguous, which is good for very large sequences.
+ */
 object Vector extends SeqFactory[Vector] {
   @inline implicit def canBuildFrom[A]: CanBuildFrom[Coll, A, Vector[A]] =
     ReusableCBF.asInstanceOf[CanBuildFrom[Coll, A, Vector[A]]]
