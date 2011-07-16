0152bb89e6d75f77f791814e135cae9e486257b8
Paul Phillips <paulp@improving.org>
2011-07-16 05:51:26 +0000

    Added documentation for RichInt.
    Contributed by Marc Weil.

--- src/library/scala/runtime/RichInt.scala
+++ src/library/scala/runtime/RichInt.scala
@@ -16,23 +16,58 @@ import annotation.bridge
 // the Int-specific Range class from until/to.
 final class RichInt(val self: Int) extends ScalaNumberProxy[Int] with RangedProxy[Int] {
   type ResultWithoutStep = Range
+
+  /**
+    * @return `true` if this number has no decimal component. Always returns true for `RichInt`.
+    */
   def isWhole() = true
 
+  /**
+    * @param end The final bound of the range to make.
+    * @return A [[Range]] from `this` up to but not including `end`.
+    */
   def until(end: Int): Range = Range(self, end)
+
+  /**
+    * @param end The final bound of the range to make.
+    * @param step The number to increase by for each step of the range.
+    * @return A [[Range]] from `this` up to but not including `end`.
+    */
   def until(end: Int, step: Int): Range = Range(self, end, step)
 
 //  @bridge
 //  def until(end: Int): Range with Range.ByOne = new Range(self, end, 1) with Range.ByOne
 
   /** like `until`, but includes the last index */
+  /**
+    * @param end The final bound of the range to make.
+    * @return A [[Range]] from `this` up to and including `end`.
+    */
   def to(end: Int): Range.Inclusive = Range.inclusive(self, end)
+
+  /**
+    * @param end The final bound of the range to make.
+    * @param step The number to increase by for each step of the range.
+    * @return A [[Range]] from `this` up to and including `end`.
+    */
   def to(end: Int, step: Int): Range.Inclusive = Range.inclusive(self, end, step)
 
 //  @bridge
 //  def to(end: Int): Range with Range.ByOne = new Range.Inclusive(self, end, 1) with Range.ByOne
 
+  /**
+    * @return `this` if `this < that` or `that` otherwise
+    */
   override def min(that: Int): Int = if (self < that) self else that
+
+  /**
+    * @return `this` if `this > that` or `that` otherwise
+    */
   override def max(that: Int): Int = if (self > that) self else that
+
+  /**
+    * Computes the absolute value of `this`.
+    */
   override def abs: Int = if (self < 0) -self else self
 
   def toBinaryString: String = java.lang.Integer.toBinaryString(self)
