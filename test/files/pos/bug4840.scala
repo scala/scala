a3b36f0ddb8412a636ef5a3a9dcb1a14e1af58f3
Paul Phillips <paulp@improving.org>
2011-07-27 01:02:42 +0000

    Backport of r25383:
    Fix/workaround for inliner bug uncovered by finalizing Option methods.
    Something in the backend is leaving open but empty blocks in the
    worklist. Rather than freaking out at the merest mention of an empty
    block, I quietly remove the empty ones. A proper fix will involve not
    leaving empty blocks lying around but we're on a schedule here people.
    Review by dragos.

---
+++ test/files/pos/bug4840.scala
@@ -0,0 +1,13 @@
+class Crashy {
+  def g(): Option[Any] = None
+
+  def crashy() = {
+    for (_ <- g()) {
+      (null: Any) match {
+        case Some(_) => 5
+        case None    => sys.error("")
+      }
+    }
+  }
+}
+
