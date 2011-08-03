f155eaaa00a37786cafed5ade2ae9652a44880ea
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2011-08-03 16:12:01 +0000

    Adds version information from @migration and @d...
    Adds version information from @migration and @deprecated to ScalaDoc.
    Adds support for @migration in Symbols and MemberEntity. Contributed by
    Simon Ochsenreither. Review by heathermiller.

--- src/compiler/scala/tools/nsc/doc/html/page/Template.scala
+++ src/compiler/scala/tools/nsc/doc/html/page/Template.scala
@@ -375,15 +375,12 @@ class Template(tpl: DocTemplateEntity) extends HtmlPage {
         <dd class="cmt">{ bodyToHtml(mbr.deprecation.get) }</dd>
       }
 
-    val migration: Seq[scala.xml.Node] = {
-      mbr.annotations.find(_.qualifiedName == "migration") match {
-        case None => NodeSeq.Empty
-        case Some(mig) => {
+    val migration: Seq[scala.xml.Node] =
+      if(mbr.migration.isEmpty || isReduced) NodeSeq.Empty
+      else {
           <dt>Migration</dt>
-          <dd class="cmt"><p>{mig.arguments.view.map(_.value).drop(2).mkString(" ")}</p></dd>
-        }
+          <dd class="cmt">{ bodyToHtml(mbr.migration.get) }</dd>
       }
-    }
 
     val mainComment: Seq[scala.xml.Node] = mbr.comment match {
       case Some(comment) if (! isReduced) =>
