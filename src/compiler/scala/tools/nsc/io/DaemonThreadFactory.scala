4f4b85e62229f701b06e8e6a65f50360806c6d22
Paul Phillips <paulp@improving.org>
2011-08-15 17:02:05 +0000

    (For SI-4705) Some tweaks to repl thread creati...
    (For SI-4705) Some tweaks to repl thread creation based on speculation
    from mark harrah, no review.

---
+++ src/compiler/scala/tools/nsc/io/DaemonThreadFactory.scala
@@ -0,0 +1,21 @@
+/* NSC -- new Scala compiler
+ * Copyright 2005-2011 LAMP/EPFL
+ * @author Paul Phillips
+ */
+
+package scala.tools.nsc
+package io
+
+import java.util.concurrent._
+
+class DaemonThreadFactory extends ThreadFactory {
+  def newThread(r: Runnable): Thread = {
+    val thread = new Thread(r)
+    thread setDaemon true
+    thread
+  }
+}
+
+object DaemonThreadFactory {
+  def newPool() = Executors.newCachedThreadPool(new DaemonThreadFactory)
+}
