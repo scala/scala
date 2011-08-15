5bae04edd7dd96070622d5801bf444f694f2063f
Paul Phillips <paulp@improving.org>
2011-08-15 17:01:49 +0000

    Revert previous commit.

--- src/forkjoin/scala/concurrent/forkjoin/RecursiveTask.java
+++ src/forkjoin/scala/concurrent/forkjoin/RecursiveTask.java
@@ -1,29 +1,29 @@
 /*
  * Written by Doug Lea with assistance from members of JCP JSR-166
  * Expert Group and released to the public domain, as explained at
- * http://creativecommons.org/publicdomain/zero/1.0/
+ * http://creativecommons.org/licenses/publicdomain
  */
 
 package scala.concurrent.forkjoin;
 
 /**
- * A recursive result-bearing {@link ForkJoinTask}.
+ * Recursive result-bearing ForkJoinTasks.
+ * <p> For a classic example, here is a task computing Fibonacci numbers:
  *
- * <p>For a classic example, here is a task computing Fibonacci numbers:
- *
- *  <pre> {@code
- * class Fibonacci extends RecursiveTask<Integer> {
+ * <pre>
+ * class Fibonacci extends RecursiveTask&lt;Integer&gt; {
  *   final int n;
- *   Fibonacci(int n) { this.n = n; }
+ *   Fibonnaci(int n) { this.n = n; }
  *   Integer compute() {
- *     if (n <= 1)
+ *     if (n &lt;= 1)
  *        return n;
  *     Fibonacci f1 = new Fibonacci(n - 1);
  *     f1.fork();
  *     Fibonacci f2 = new Fibonacci(n - 2);
  *     return f2.compute() + f1.join();
  *   }
- * }}</pre>
+ * }
+ * </pre>
  *
  * However, besides being a dumb way to compute Fibonacci functions
  * (there is a simple fast linear algorithm that you'd use in
@@ -33,14 +33,17 @@ package scala.concurrent.forkjoin;
  * minimum granularity size (for example 10 here) for which you always
  * sequentially solve rather than subdividing.
  *
- * @since 1.7
- * @author Doug Lea
  */
 public abstract class RecursiveTask<V> extends ForkJoinTask<V> {
-    private static final long serialVersionUID = 5232453952276485270L;
 
     /**
-     * The result of the computation.
+     * Empty constructor for use by subclasses.
+     */
+    protected RecursiveTask() {
+    }
+
+    /**
+     * The result returned by compute method.
      */
     V result;
 
@@ -58,7 +61,7 @@ public abstract class RecursiveTask<V> extends ForkJoinTask<V> {
     }
 
     /**
-     * Implements execution conventions for RecursiveTask.
+     * Implements execution conventions for RecursiveTask
      */
     protected final boolean exec() {
         result = compute();
