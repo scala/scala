0ef5e8a64556974f52b5aa2a121687862429d1fd
Martin Odersky <odersky@gmail.com>
2011-10-11 14:53:21 +0000

    Simple test case for string interpolation.

---
+++ test/files/run/stringInterpolation.scala
@@ -0,0 +1,9 @@
+package test
+
+object stringInterpolation {
+  def main(args : Array[String]) : Unit = {
+    println("\{1} plus \{1} is \{1 + 1}")
+    val x = 1.1
+    println("We have a \{ x ;2.2f}% chance of success")
+  }
+}
