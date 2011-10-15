5690452698acafe6e4843bb08a14f6ca1cc55f8a
Paul Phillips <paulp@improving.org>
2011-10-15 12:40:59 +0000

    Changed a power mode import.
    Applying a bandaid since I'll never get the globals lined up. No review.

--- src/compiler/scala/tools/nsc/interpreter/Power.scala
+++ src/compiler/scala/tools/nsc/interpreter/Power.scala
@@ -140,7 +140,7 @@ abstract class Power(
   private def initImports = List(
     "scala.tools.nsc._",
     "scala.collection.JavaConverters._",
-    "global.{ error => _, _ }",
+    "intp.global.{ error => _, _ }",
     "definitions.{ getClass => _, _ }",
     "power.Implicits._",
     "power.rutil._"
