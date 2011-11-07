15b9785d30e0358fc5d58e874421ffd396886b9a
Paul Phillips <paulp@improving.org>
2011-11-07 14:37:59 +0000

    A couple bits of r25957 lost under time pressure.
    No review.

--- src/partest/scala/tools/partest/ReplTest.scala
+++ src/partest/scala/tools/partest/ReplTest.scala
@@ -18,7 +18,7 @@ abstract class ReplTest extends DirectTest {
   // final because we need to enforce the existence of a couple settings.
   final override def settings: Settings = {
     val s = super.settings
-    s.Yreplsync.value = true
+    // s.Yreplsync.value = true
     s.Xnojline.value = true
     transformSettings(s)
   }
--- test/files/run/repl-exceptions.scala
+++ test/files/run/repl-exceptions.scala
@@ -1,8 +1,9 @@
 import scala.tools.partest.ReplTest
 import scala.tools.util.Javap
 
+// This test blows up under -Yrepl-sync.
 object Test extends ReplTest {
-  override def extraSettings = "-Yrich-exceptions -Yrepl-sync"
+  override def extraSettings = "-Yrich-exceptions"
   def code = """
     |sys.SystemProperties.traceSourcePath setValue ""
     |def f = sys.error("hi mom")
