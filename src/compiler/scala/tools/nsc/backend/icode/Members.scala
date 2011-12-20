66cc6ee5736facc0cb7670d0f334aba2274630ee
Paul Phillips <paulp@improving.org>
2011-12-20 10:47:02 -0800

    Optimization in backend.
    Eliminated one source of the thousands of copies of the same
    Strings we had/have (strongly reachable) on the heap.  It is
    another good example of how constructor parameters unintentionally
    become fields and how that in turn brings the pain.

--- src/compiler/scala/tools/nsc/backend/icode/Members.scala
+++ src/compiler/scala/tools/nsc/backend/icode/Members.scala
@@ -24,9 +24,7 @@ trait Members { self: ICodes =>
    * This class represents the intermediate code of a method or
    * other multi-block piece of code, like exception handlers.
    */
-  class Code(label: String, method: IMethod) {
-    def this(method: IMethod) = this(method.symbol.simpleName.toString, method)
-
+  class Code(method: IMethod) {
     /** The set of all blocks */
     val blocks = mutable.ListBuffer[BasicBlock]()
 
@@ -73,7 +71,7 @@ trait Members { self: ICodes =>
     }
 
     /** This methods returns a string representation of the ICode */
-    override def toString() : String = "ICode '" + label + "'";
+    override def toString() : String = "ICode '" + method.symbol.decodedName + "'";
 
     /* Compute a unique new label */
     def nextLabel: Int = {
