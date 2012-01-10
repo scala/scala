126469f9224430f09af31afb50bf4f6fa0b4e189
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2012-01-10 23:33:07 +0100

    Preserve attribute order in inline XML

--- test/files/run/xml-attribute.scala
+++ test/files/run/xml-attribute.scala
@@ -12,12 +12,11 @@ object Test {
     assert(noAttr == preAttrNull)
     assert(noAttr == preAttrNone)
 
-    val noAttrStr = "<t></t>"
-    assert(noAttr.toString() == noAttrStr)
-    assert(attrNull.toString() == noAttrStr)
-    assert(attrNone.toString() == noAttrStr)
-    assert(preAttrNull.toString() == noAttrStr)
-    assert(preAttrNone.toString() == noAttrStr)
+    println(noAttr)
+    println(attrNull)
+    println(attrNone)
+    println(preAttrNull)
+    println(preAttrNone)
 
     val xml1 = <t b="1" d="2"/>
     val xml2 = <t a={ null: String } p:a={ null: String } b="1" c={ null: String } d="2"/>
@@ -25,9 +24,14 @@ object Test {
     assert(xml1 == xml2)
     assert(xml1 == xml3)
 
-    val xml1Str = "<t d=\"2\" b=\"1\"></t>"
-    assert(xml1.toString() == xml1Str)
-    assert(xml2.toString() == xml1Str)
-    assert(xml3.toString() == xml1Str)
+    println(xml1)
+    println(xml2)
+    println(xml3)
+
+    // Check if attribute order is retained
+    println(<t a="1" d="2"/>)
+    println(<t b="1" d="2"/>)
+    println(<t a="1" b="2" c="3"/>)
+    println(<t g="1" e="2" p:a="3" f:e="4" mgruhu:ji="5"/>)
   }
 }
