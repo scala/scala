556dc8c5406f9ab9c9470ff22d430693f00d2807
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2012-02-01 17:32:48 +0100

    Added a test-case for switches with Yvirtpatmat.
    Added a bunch of tests that cover changes related
    to switches that were applied to Yvirtpatmat
    implementation.
    
    Note: I didn't add those tests progressively because
    my changes fix trees after typer phase but do not
    affect resulting bytecode. How come? It's because
    -Yvirtpatmat will emit pattern for switches and then
    the old pattern matcher implementation would transform
    them in the old fashion in explicitouter. We cannot
    disable the old pattern matcher in explicitouter yet
    because it doesn't handle patterns used for catching
    exceptions.
    
    Thus, consider this as a sign of the fact that Yvirtpatmat
    is still work in progress.

---
+++ test/files/run/virtpatmat_switch.scala
@@ -0,0 +1,32 @@
+object Test extends App {
+  def intSwitch(x: Int) = x match {
+    case 0 => "zero"
+    case 1 => "one"
+    case _ => "many"
+  }
+  
+  println(intSwitch(0))
+  println(intSwitch(1))
+  println(intSwitch(10))
+  
+  def charSwitch(x: Char) = x match {
+    case 'a' => "got a"
+    case 'b' => "got b"
+    case _ => "got some letter"
+  }
+  
+  println(charSwitch('a'))
+  println(charSwitch('b'))
+  println(charSwitch('z'))
+  
+  def implicitDefault(x: Int) = x match {
+    case 0 => 0
+  }
+  
+  try {
+    implicitDefault(5)
+  } catch {
+    case e: MatchError => println(e)
+  }
+
+}
