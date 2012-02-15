0d315d652ae34e1c4cf1098cc753a43d4862e362
Paul Phillips <paulp@improving.org>
2012-02-15 10:24:55 -0800

    Restored scala.reflect.Code.
    There's a deprecation process to follow before removing stuff.
    scala.reflect.Code isn't even deprecated in 2.9.1.

---
+++ src/library/scala/reflect/Code.scala
@@ -0,0 +1,24 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala API                            **
+**    / __/ __// _ | / /  / _ |    (c) 2002-2011, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+
+package scala.reflect
+
+/** This type is required by the compiler and <b>should not be used in client code</b>. */
+@deprecated("Replaced with scala.reflect.macro.Context#reify, will be completely removed soon", "2.10.0")
+class Code[T: Manifest](val tree: scala.reflect.mirror.Tree) {
+  val manifest = implicitly[Manifest[T]]
+  override def toString = "Code(tree = "+tree+", manifest = "+manifest+")"
+}
+
+/** This type is required by the compiler and <b>should not be used in client code</b>. */
+@deprecated("Replaced with scala.reflect.macro.Context#reify, will be completely removed soon", "2.10.0")
+object Code {
+  def lift[A](tree: A): Code[A] =
+    throw new Error("Code was not lifted by compiler")
+}
