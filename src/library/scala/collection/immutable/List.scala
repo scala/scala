ac7376ed5306cac06c62213ddf358e26101453b3
Heather Miller <heather.miller@epfl.ch>
2012-02-25 13:25:17 +0100

    Improvement to API docs: GenTraversableLike and List. Fixes SI-5522.

--- src/library/scala/collection/immutable/List.scala
+++ src/library/scala/collection/immutable/List.scala
@@ -205,6 +205,16 @@ sealed abstract class List[+A] extends AbstractSeq[A]
     these
   }
 
+  /**
+   *  @example {{{
+   *  // Given a list
+   *  val letters = List('a','b','c','d','e')
+   *
+   *  // `slice` returns all elements beginning at index `from` and afterwards,
+   *  // up until index `until` (excluding index `until`.)
+   *  letters.slice(1,3) // Returns List('b','c')
+   *  }}}
+   */
   override def slice(from: Int, until: Int): List[A] = {
     val lo = math.max(from, 0)
     if (until <= lo || isEmpty) Nil
