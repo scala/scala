6ec24f161105b1cb54fd1048c5757255a5530098
Josh Suereth <joshua.suereth@gmail.com>
2012-03-14 01:04:45 -0400

    Fixes to build numbering system for 2.9.x release

--- tools/get-scala-revision.bat
+++ tools/get-scala-revision.bat
@@ -14,9 +14,14 @@ if "%*"=="" (
 )
 cd %_DIR%
 
+rem  TODO - Look up bat scripting example and fix the darn string.
 if exist .git\NUL (
   git describe HEAD --abbrev=7 --match dev
 )
+
+rem  Implement something like the following
+rem for /f "tokens=1,2,3 delims=- " %%a in ("%gitdescribe%") do set version=%%a&set commits=%%b&set sha=%%c
+rem echo %date?%-%commits%-%sha%
 
 :end
 @endlocal
