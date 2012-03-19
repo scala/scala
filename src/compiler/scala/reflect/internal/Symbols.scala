50bf852c9a48312e60b441d052150fdd78b52a1a
Paul Phillips <paulp@improving.org>
2012-03-19 15:07:57 -0700

    Tree adjustments.
    I noticed that if we gave Ident a "qualifier" method which is
    always EmptyTree, a whole bunch of code could be simplified.
    
    A transparent subclass of Apply which preserves the source
    level "new" a little longer than never.
    
    Since productPrefix is tied up in reification - and oh I
    detest this "String"-based programming, we shouldn't have to
    wait until everyone learns this independently to change it -
    I added "printingPrefix" to Tree, which defaults to
    productPrefix but can be changed without breaking reify.

--- src/compiler/scala/reflect/internal/Symbols.scala
+++ src/compiler/scala/reflect/internal/Symbols.scala
@@ -1985,6 +1985,7 @@ trait Symbols extends api.Symbols { self: SymbolTable =>
         else if (isTrait) ("trait", "trait", "TRT")
         else if (isClass) ("class", "class", "CLS")
         else if (isType) ("type", "type", "TPE")
+        else if (isClassConstructor && isPrimaryConstructor) ("primary constructor", "constructor", "PCTOR")
         else if (isClassConstructor) ("constructor", "constructor", "CTOR")
         else if (isSourceMethod) ("method", "method", "METH")
         else if (isTerm) ("value", "value", "VAL")
