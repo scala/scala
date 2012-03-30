c9e254ec27b8153c9b9921abb308b2e3a2c07599
Paul Phillips <paulp@improving.org>
2012-03-30 11:45:27 -0700

    Backport fix for SI-4545, SI-5633.

--- src/compiler/scala/tools/nsc/symtab/Definitions.scala
+++ src/compiler/scala/tools/nsc/symtab/Definitions.scala
@@ -471,8 +471,8 @@ trait Definitions extends reflect.generic.StandardDefinitions {
 
     def isFunctionType(tp: Type): Boolean = tp.normalize match {
       case TypeRef(_, sym, args) if args.nonEmpty =>
-        val len = args.length
-        len < MaxFunctionArity && sym == FunctionClass(len - 1)
+        val arity = args.length - 1   // -1 is the return type
+        arity <= MaxFunctionArity && sym == FunctionClass(arity)
       case _ =>
         false
     }
