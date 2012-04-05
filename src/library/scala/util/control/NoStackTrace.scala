scala/scala

commit 413a076381f9a5b1e9dc5fd5066412700bfccf67

Speeding up stack trace suppression.

src/library/scala/util/control/NoStackTrace.scala
@@ -18,6 +18,10 @@ package scala.util.control
  */
 trait NoStackTrace extends Throwable {
   override def fillInStackTrace(): Throwable =
-    if (sys.SystemProperties.noTraceSupression) super.fillInStackTrace()
+    if (noSuppression) super.fillInStackTrace()
     else this
+}
+
+object NoStackTrace {
+  final val noSuppression = sys.SystemProperties.noTraceSupression.value
 }

commit db06eecd9fd38d7884ec5dcc63c6d6beafd5369d

make noSuppression a def to avoid field init prob
fixes the supposed checkinit failure introduced 10 months ago by 413a076381

src/library/scala/util/control/NoStackTrace.scala
@@ -23,5 +23,5 @@ trait NoStackTrace extends Throwable {
 }
 
 object NoStackTrace {
-  final val noSuppression = sys.SystemProperties.noTraceSupression.value
+  final def noSuppression = sys.SystemProperties.noTraceSupression.value
 }