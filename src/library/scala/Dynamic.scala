d4d8f15ad121edfc0b924deaeb9c30a9d9b71e84
Dominik Gruntz <dominik.gruntz@fhnw.ch>
2012-04-11 18:03:30 +0200

    changed applyDynamic explanation in trait Dynamic

--- src/library/scala/Dynamic.scala
+++ src/library/scala/Dynamic.scala
@@ -11,7 +11,7 @@ package scala
 /** A marker trait that enables dynamic invocations. Instances `x` of this
  *  trait allow calls `x.meth(args)` for arbitrary method names `meth` and
  *  argument lists `args`.  If a call is not natively supported by `x`, it
- *  is rewritten to `x.applyDynamic("meth", args)`.
+ *  is rewritten to `x.applyDynamic("meth")(args)`.
  *
  *  As of scala 2.9, `scalac` must receive the `-Xexperimental` option for
  *  `Dynamic` to receive this treatment.
