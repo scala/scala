53a0e803f03fe715c214c01c03f82baa19d7f2c2
Christopher Vogt <github.com.nsp@cvogt.org>
2012-04-14 00:57:09 +0200

    implemented DynamicProxy as portrayed by the Scala reflection team

--- src/library/scala/reflect/api/Names.scala
+++ src/library/scala/reflect/api/Names.scala
@@ -34,12 +34,12 @@ trait Names {
     def toTypeName: TypeName
 
     /** Replaces all occurrences of $op_names in this name by corresponding operator symbols.
-     *  Example: `foo_+=` becomes `foo_$plus$eq`.
+     *  Example: `foo_$plus$eq` becomes `foo_+=`
      */
     def decoded: String
 
     /** Replaces all occurrences of operator symbols in this name by corresponding $op_names.
-     *  Example: `foo_$plus$eq` becomes `foo_+=`
+     *  Example: `foo_+=` becomes `foo_$plus$eq`.
      */
     def encoded: String
 
