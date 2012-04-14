f2e529700fef17824de7445042368629c37cbc58
Adriaan Moors <adriaan.moors@epfl.ch>
2012-04-14 15:43:33 +0200

    workaround for inliner bug in run/t3835

--- test/files/run/t3835.scala
+++ test/files/run/t3835.scala
@@ -1,4 +1,7 @@
-object Test extends App {
-  println((1, 2, 3) match { case (r, \u03b8, \u03c6) => r + \u03b8 + \u03c6 })
-  println(1 match { case \u00e9 => \u00e9 })
+object Test {
+  def main(args: Array[String]): Unit = {
+    println((1, 2, 3) match { case (r, \u03b8, \u03c6) => r + \u03b8 + \u03c6 })
+    val x = 1 match { case \u00e9 => \u00e9 } // store in local variable due to problem with inliner + virtpatmat
+    println(x)
+  }
 }
