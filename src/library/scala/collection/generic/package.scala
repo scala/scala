73f7001d963a80950751b4e13b95b05d2526edc8
Miles Sabin <miles@milessabin.com>
2012-04-29 14:23:38 +0100

    Added infrastructure to enable easy enrichment of GenTraversables.

--- src/library/scala/collection/generic/package.scala
+++ src/library/scala/collection/generic/package.scala
@@ -4,6 +4,12 @@ import generic.CanBuildFrom
 package object generic {
   type CanBuild[-Elem, +To] = CanBuildFrom[Nothing, Elem, To]
 
+  /** The type of conversions from a collection representation type
+   *  `Repr` to its corresponding GenTraversableLike.
+   *  @see [[scala.collection.generic.FromRepr]]
+   */
+  type HasElem[Repr, A] = Repr => GenTraversableLike[A, Repr]
+
   @deprecated("use ArrayTagTraversableFactory instead", "2.10.0")
   type ClassManifestTraversableFactory[CC[X] <: Traversable[X] with GenericClassManifestTraversableTemplate[X, CC]] = ArrayTagTraversableFactory[CC]
 
