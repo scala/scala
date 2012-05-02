d0fd3b3a3423cf07ed90d699b41f3a4822773107
Aleksandar Prokopec <axel22@gmail.com>
2012-05-02 15:05:24 +0200

    Add pending test for SI-5018.

---
+++ test/pending/run/t5018.scala
@@ -0,0 +1,34 @@
+
+
+
+import java.io._
+import collection._
+
+
+
+object Test {
+  
+  def serializeDeserialize[T <: AnyRef](obj: T) = {
+    val buffer = new ByteArrayOutputStream
+    val out = new ObjectOutputStream(buffer)
+    out.writeObject(obj)
+    val in = new ObjectInputStream(new ByteArrayInputStream(buffer.toByteArray))
+    in.readObject.asInstanceOf[T]
+  }
+  
+  def main(args: Array[String]) {
+    val values = mutable.Map(1 -> 1).values
+    assert(serializeDeserialize(values) == values)
+    
+    val keyset = mutable.Map(1 -> 1).keySet
+    assert(serializeDeserialize(keyset) == keyset)
+    
+    val imkeyset = immutable.Map(1 -> 1).keySet
+    assert(serializeDeserialize(imkeyset) == imkeyset)
+    
+    val defaultmap = immutable.Map(1 -> 1).withDefaultValue(1)
+    assert(serializeDeserialize(defaultmap) == defaultmap)
+  }
+  
+}
+
