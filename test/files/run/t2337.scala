8bc8b83f0bd7daef62b41b4a0c87b4e9b7344284
Paul Phillips <paulp@improving.org>
2012-05-03 17:11:30 -0700

    Moved passing tests from pending to files.
    Most are pattern matcher bugs fixed by virtpatmat. A few are reifier,
    package object, or miscellaneous. I threw in an original test for
    SI-2337, to go with those for SI-1697, SI-3705, SI-4415, and SI-1357,
    all of which (in the interests of making sure this basket has all the
    eggs) I am closing.

---
+++ test/files/run/t2337.scala
@@ -0,0 +1,21 @@
+
+object Test {
+
+  def compare(first: Any, second: Any): Any = {
+      (first, second) match {
+        case (k: Int, o: Int) => k compare o
+        //why the next case matches (Float, Int) but does not match (Int, Float) ???
+        case (k: Number, o: Number) => k.doubleValue() compare o.doubleValue()
+        case _ => "BOGON"
+        // throw new Exception("Unsupported compare " + first + "; " + second)
+    }
+  }
+    
+  def main(args: Array[String]): Unit = {
+    println("Both Int", -1, compare(0, 1))
+    println("Both Float", 1, compare(1.0, 0.0))
+    println("Float then Int", 0, compare(10.0, 10))
+    println("Int then Float", 0, compare(10, 10.0)) //this fails with an exception
+  }
+}
+
