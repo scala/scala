b5919100e785df58bde35bb24abe9d60b4da08a2
Dominik Gruntz <dominik.gruntz@fhnw.ch>
2012-05-09 15:01:25 +0200

    removes redundant hash implementation from BoxesRunTime.java

--- test/files/run/hashCodeScalaRunTime.scala
+++ test/files/run/hashCodeScalaRunTime.scala
@@ -1,23 +1,23 @@
-// This only tests direct access to the methods in BoxesRunTime,
+// This only tests direct access to the methods in ScalaRunTime,
 // not the whole scheme.
 object Test
 {
   import java.{ lang => jl }
-  import scala.runtime.BoxesRunTime.{ hashFromNumber, hashFromObject }
+  import scala.runtime.ScalaRunTime.{ hash }
   
   def allSame[T](xs: List[T]) = assert(xs.distinct.size == 1, "failed: " + xs)
   
   def mkNumbers(x: Int): List[Number] =
     List(x.toByte, x.toShort, x, x.toLong, x.toFloat, x.toDouble)
   
-  def testLDF(x: Long) = allSame(List[Number](x, x.toDouble, x.toFloat) map hashFromNumber)
+  def testLDF(x: Long) = allSame(List[Number](x, x.toDouble, x.toFloat) map hash)
    
   def main(args: Array[String]): Unit = {
     List(Byte.MinValue, -1, 0, 1, Byte.MaxValue) foreach { n => 
-      val hashes = mkNumbers(n) map hashFromNumber
+      val hashes = mkNumbers(n) map hash
       allSame(hashes)
       if (n >= 0) {
-        val charCode = hashFromObject(n.toChar: Character)
+        val charCode = hash(n.toChar: Character)
         assert(charCode == hashes.head)
       }
     }
