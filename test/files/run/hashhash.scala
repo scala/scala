b0e85333a286075882352f66e59e5aa3f287e62e
Paul Phillips <paulp@improving.org>
2012-05-10 06:37:20 -0700

    Revert "Fix for ## inconsistency."
    This reverts commit 58bb2d1bd2000ac3aa2c64b6c5dc56c91e911860.
    
    I guess this must be what's failing the nightly.

--- test/files/run/hashhash.scala
+++ test/files/run/hashhash.scala
@@ -6,10 +6,5 @@ object Test {
     /** Just a little sanity check, not to be confused with a unit test. */
     List(5, 5.5f, "abc", new AnyRef, ()) foreach confirmSame
     List(5.0f, 1.0d, -(5.0f), (-1.0d)) foreach confirmDifferent
-
-    val x = (BigInt(1) << 64).toDouble
-    val y: Any = x
-
-    assert(x.## == y.##, ((x, y)))
   }
 }
