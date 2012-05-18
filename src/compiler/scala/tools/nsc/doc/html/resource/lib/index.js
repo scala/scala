86da9aaa05549d18524d9fe5b00a37ffe1d99c68
Heather Miller <heather.miller@epfl.ch>
2012-05-18 22:33:41 +0200

    Minor fix to Tab-switch keyboard shortcut

--- src/compiler/scala/tools/nsc/doc/html/resource/lib/index.js
+++ src/compiler/scala/tools/nsc/doc/html/resource/lib/index.js
@@ -270,13 +270,13 @@ function configureTextFilter() {
         input.bind("keydown", function(event) {
             if (event.keyCode == 27) { // escape
                 input.attr("value", "");
-                textFilter();
             }
             if (event.keyCode == 9) { // tab
                 $("#template").contents().find("#mbrsel-input").focus();
                 input.attr("value", "");
                 return false;
-            }              
+            } 
+            textFilter();
         });
         input.focus(function(event) { input.select(); });
     });
