820897b978ee6837ca463b186bab0f6349807c18
Jason Zaugg <jzaugg@gmail.com>
2012-05-22 00:34:06 +0200

    SI-2405 Confer implicit privileges to renamed imports.
    Yin and yang would be pleased: A fix in two parts.
    
    1. Use the name of the imported symbol, rather than the alias, in the generated `Select(qual, name)` tree.
    2. Do the opposite in `isQualifyingImplicit`, which performs one part of the shadowing check.
    
    But there is still work to do. The second part of the shadowing check, `nonImplicitSynonymInScope`,
    fails to notice this case (irrespective of aliased imports).
    
      // Expecting shadowing #2. Alas, none is cast!
      object Test1 {
        object A { implicit val x: Int = 1 }
        import A.x
        def x: Int = 0
        implicitly[Int]
      }
    
    I'm hitching the residual problem to SI-4270's wagon.

---
+++ test/files/pos/t2405.scala
@@ -0,0 +1,23 @@
+object A { implicit val x: Int = 1 }
+
+// Problem as stated in the ticket.
+object Test1 {
+	import A.{x => y}
+	implicitly[Int]
+}
+
+// Testing for the absense of shadowing #1.
+object Test2 {
+	import A.{x => y}
+	val x = 2
+	implicitly[Int]
+}
+
+// Testing for the absense of shadowing #2.
+object Test3 {
+	{
+		import A.{x => y}
+		def x: Int = 0
+		implicitly[Int]
+	}
+}
