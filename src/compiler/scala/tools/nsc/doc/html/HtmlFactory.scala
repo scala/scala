fba4098ca592bdd7d6609017daa4a08f274f35ec
Damien <damien.obrist@gmail.com>
2012-06-13 21:41:06 +0200

    Scaladoc class diagrams part 2
    This commit contains the svg diagram generation using the graphviz
    package, the template changes, the css styling and javascript code
    that enables displaying and interacting with the diagrams.
    
    The full history is located at:
    https://github.com/damienobrist/scala/tree/feature/diagrams-dev
    
    The diagrams are included as svg markup inside the html code. This
    enables interacting with the image beyond what would be possible
    with a static image (highlighting, scaling, tooltips, links to
    nodes, etc).
    
    The svg generation has four main phases: model => dot,
    dot => svg (using the graphviz package), svg postprocessing,
    inclusion in the html page.
    
    This commit also fixes SI-5212 - links to individual pages
    automatically load the left navigation panel of the website.
    
    Commit summary:
    
     - diagram generation
       - model => dot (DotDiagramGenerator.scala, DiagramGenerator.scala)
       - dot => svg (DotRunner.scala)
       - svg post-processing (DotDiagramGenerator.scala)
       - svg inclusion in the html (Template.scala)
    
     - diagram interaction
       - css, js and image files
    
    Review by @heathermiller, @kzys.

--- src/compiler/scala/tools/nsc/doc/html/HtmlFactory.scala
+++ src/compiler/scala/tools/nsc/doc/html/HtmlFactory.scala
@@ -12,6 +12,8 @@ import java.io.{ File => JFile }
 import io.{ Streamable, Directory }
 import scala.collection._
 
+import html.page.diagram.DiagramGenerator
+
 /** A class that can generate Scaladoc sites to some fixed root folder.
   * @author David Bernard
   * @author Gilles Dubochet */
@@ -29,21 +31,27 @@ class HtmlFactory(val universe: doc.Universe, index: doc.Index) {
     "jquery.js",
     "jquery.layout.js",
     "scheduler.js",
+    "diagrams.js",
     "template.js",
     "tools.tooltip.js",
+    "modernizr.custom.js",
 
     "index.css",
     "ref-index.css",
     "template.css",
+    "diagrams.css",
 
     "class.png",
     "class_big.png",
+    "class_diagram.png",
     "object.png",
     "object_big.png",
+    "object_diagram.png",
     "package.png",
     "package_big.png",
     "trait.png",
     "trait_big.png",
+    "trait_diagram.png",
 
     "class_to_object_big.png",
     "object_to_class_big.png",
@@ -105,6 +113,8 @@ class HtmlFactory(val universe: doc.Universe, index: doc.Index) {
       finally out.close()
     }
 
+    DiagramGenerator.initialize(universe.settings)
+
     libResources foreach (s => copyResource("lib/" + s))
 
     new page.Index(universe, index) writeFor this
@@ -115,6 +125,8 @@ class HtmlFactory(val universe: doc.Universe, index: doc.Index) {
     for (letter <- index.firstLetterIndex) {
       new html.page.ReferenceIndex(letter._1, index, universe) writeFor this
     }
+
+    DiagramGenerator.cleanup()
   }
 
   def writeTemplates(writeForThis: HtmlPage => Unit) {
