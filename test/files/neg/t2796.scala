ac71812170547acdce74fc224bfa9f3a776b4cd1
Jason Zaugg <jzaugg@gmail.com>
2012-06-26 23:21:38 +0200

    SI-2796 Warn if early definitions are used with a trait.
    For which they (currently) have no special meaning.

---
+++ test/files/neg/t2796.scala
@@ -0,0 +1,28 @@
+trait Base {
+  val abstractVal: String
+  final val useAbstractVal = abstractVal
+}
+
+trait T1 extends {
+  val abstractVal = "T1.abstractVal" // warn
+} with Base
+
+trait T2 extends {
+  type X = Int                       // okay
+} with Base
+
+
+class C1 extends {
+  val abstractVal = "C1.abstractVal" // okay
+} with Base
+
+object Test {
+  def main(args: Array[String]) {
+    assert(new C1 ().useAbstractVal == "C1.abstractVal")
+    // This currently fails. a more ambitious approach to this ticket would add $earlyinit$
+    // to traits and call it from the right places in the right order.
+    //
+    // For now, we'll just issue a warning.
+    assert(new T1 {}.useAbstractVal == "T1.abstractVal")
+  }
+}
