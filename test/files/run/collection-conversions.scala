674f4db1cd9323cb4e637dca6f4641d76c09fc84
Aleksandar Prokopec <axel22@gmail.com>
2012-06-27 13:59:20 +0200

    Parallelize convertTo in parallel collection.

--- test/files/run/collection-conversions.scala
+++ test/files/run/collection-conversions.scala
@@ -1,6 +1,7 @@
 import collection._
 import mutable.Buffer
 import parallel.immutable.ParVector
+import parallel.mutable.ParArray
 import reflect.ClassTag
 
 object Test {
@@ -29,6 +30,7 @@ object Test {
   val testStream = Stream(1,2,3)
   val testArray = Array(1,2,3)
   val testParVector = ParVector(1,2,3)
+  val testParArray = ParArray(1,2,3)
 
   def testConversion[A: ClassTag](name: String, col: => GenTraversableOnce[A]): Unit = {
     val tmp = col
@@ -45,14 +47,16 @@ object Test {
     printResult("[Direct] Array    ", col.toArray, testArray)
     printResult("[Copy]   Array    ", col.convertTo[Array], testArray)
     printResult("[Copy]   ParVector", col.convertTo[ParVector], testParVector)
+    printResult("[Copy]   ParArray ", col.convertTo[ParArray], testParArray)
   }
   
   def main(args: Array[String]): Unit = {
-	  testConversion("iterator", (1 to 3).iterator)
+    testConversion("iterator", (1 to 3).iterator)
     testConversion("Vector", Vector(1,2,3))
     testConversion("List", List(1,2,3))
     testConversion("Buffer", Buffer(1,2,3))
     testConversion("ParVector", ParVector(1,2,3))
+    testConversion("ParArray", ParArray(1,2,3))
     testConversion("Set", Set(1,2,3))
     testConversion("SetView", Set(1,2,3).view)
     testConversion("BufferView", Buffer(1,2,3).view)
