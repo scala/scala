64acb46ab796a01bb5186385b7f8568748a857be
Adriaan Moors <adriaan.moors@epfl.ch>
2012-07-02 10:55:40 +0200

    SI-5830 switches: support guards, unreachability
    turn switches with guards into guard-free switches by collecting all cases
    that are (possibly) guarded by different guards but that switch on the same constant,
    and pushing the implied if-then-else into the collapsed case body
    
    ```
    case C if G1 => B1
    case C if Gi => Bi
    case C if GN => BN
    ```
    
    becomes
    
    ```
    case C => if (G1) B1
         else if (Gi) Bi
         else if (GN) BN
         else default() // not necessary if GN == EmptyTree
    ```
    
    default() is a jump to the default case; to enable this, we wrap a default() { } labeldef
    around the last case's body (the user-defined default or the synthetic case that throws the matcherror)
    so we can jump to the default case after the last guard is checked
    (assuming unreachability is checked, once we ended up in a non-default case,
    one of the guards either matches or we go to the default case)
    
    the unreachability analysis is minimal -- we simply check (after rewriting to guard-free form)
    that:
      - there are no duplicate cases
      - the default case comes last
    
    misc notes:
      - can't jump in exception handlers (TODO: a more fine-grained analysis on when we need to jump)
      - work around SI-6015 (test file run/t5830.scala crashed the inliner)
      - propagate type of case body to label def of default case
        (needed for existentials, see e.g., t2683)
      - the default(){} LabelDef breaks SelectiveANFTransform -- workaround: don't write guarded switches in CPS code
        (do the above transformation manually)

---
+++ test/files/run/t5830.scala
@@ -0,0 +1,56 @@
+import scala.annotation.switch
+
+object Test extends App {
+  // TODO: should not emit a switch
+  // def noSwitch(ch: Char, eof: Boolean) = (ch: @switch) match {
+  //   case 'a' if eof => println("a with oef") // then branch
+  // }
+
+  def onlyThen(ch: Char, eof: Boolean) = (ch: @switch) match {
+    case 'a' if eof => println("a with oef") // then branch
+    case 'c' =>
+  }
+
+  def ifThenElse(ch: Char, eof: Boolean) = (ch: @switch) match {
+    case 'a' if eof => println("a with oef") // then branch
+    case 'a' if eof => println("a with oef2") // unreachable, but the analysis is not that sophisticated
+    case 'a' => println("a") // else-branch
+    case 'c' =>
+  }
+
+  def defaultUnguarded(ch: Char, eof: Boolean) = (ch: @switch) match {
+    case ' ' if eof => println("spacey oef")
+    case _ => println("default")
+  }
+
+  def defaults(ch: Char, eof: Boolean) = (ch: @switch) match {
+    case _ if eof => println("def with oef") // then branch
+    case _ if eof => println("def with oef2") // unreachable, but the analysis is not that sophisticated
+    case _ => println("def") // else-branch
+  }
+
+  // test binders in collapsed cases (no need to run, it's "enough" to know it doesn't crash the compiler)
+  def guard(x: Any): Boolean = true
+  def testBinders =
+    try { println("") } // work around SI-6015
+    catch {
+      case _ if guard(null) =>
+      case x if guard(x) => throw x
+    }
+
+  // def unreachable(ch: Char) = (ch: @switch) match {
+  //   case 'a' => println("b") // ok
+  //   case 'a' => println("b") // unreachable
+  //   case 'c' =>
+  // }
+
+  // noSwitch('a', true)
+  onlyThen('a', true)       // 'a with oef'
+  ifThenElse('a', true)     // 'a with oef'
+  ifThenElse('a', false)    // 'a'
+  defaults('a', true)       // 'def with oef'
+  defaults('a', false)      // 'def'
+
+  // test that it jumps to default case, no match error
+  defaultUnguarded(' ', false) // default
+}
