489a8e200d4766808a042ef19d427c82868993fe
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2012-07-16 11:22:40 +0200

    Partest: add `instrumented` test category.
    ---
    (taken from README)
    
    Tests in `instrumented/` directory are executed the same way as in `run/` but
    they have additional byte-code instrumentation performed for profiling. You
    should put your tests in `instrumented/` directory if you are interested in
    method call counts. Examples include tests for specialization (you want to
    count boxing and unboxing method calls) or high-level tests for optimizer
    where you are interested if methods are successfuly inlined (so they should
    not be called at runtime) or closures are eliminated (so no constructors
    of closures are called).
    
    Check `scala.tools.partest.instrumented.Instrumentation` to learn how to
    use the instrumentation infrastructure.
    
    The instrumentation itself is achieved by attaching a Java agent to the
    forked VM process that injects calls to profiler. Check
    `scala.tools.partest.javaagent.ProfilingAgent` for details.
    
    ---
    
    A few notes on low-level details of this change:
    
      * Partest now depends on asm library for byte-code instrumentation
        (`build.xml`)
      * Build additional jar called `scala-partest-javaagent.jar` that is
        used with `-javaagent:` option. (`build.xml`)
      * Set `-javaagent:` option for all tests in `instrumented/` directory.
        (`RunnerManger.scala`)
      * Introduce a new category of tests called `instrumented`.
      * Add one instrumented test to demonstrate usage and test new
        infrastructure itself. (`InstrumentationTest.scala`)
    
    Review by @phaller.

---
+++ src/partest/scala/tools/partest/javaagent/ASMTransformer.java
@@ -0,0 +1,38 @@
+/* NEST (New Scala Test)
+ * Copyright 2007-2012 LAMP/EPFL
+ * @author Grzegorz Kossakowski
+ */
+
+package scala.tools.partest.javaagent;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.security.ProtectionDomain;
+
+import scala.tools.asm.ClassReader;
+import scala.tools.asm.ClassWriter;
+
+public class ASMTransformer implements ClassFileTransformer {
+  
+  private boolean shouldTransform(String className) {
+    return 
+        // do not instrument instrumentation logic (in order to avoid infinite recursion)
+        !className.startsWith("scala/tools/partest/instrumented/") &&
+        !className.startsWith("scala/tools/partest/javaagent/") &&
+        // we instrument all classes from empty package
+        (!className.contains("/") ||
+        // we instrument all classes from scala package
+        className.startsWith("scala/"));
+  }
+	
+	public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+	  if (shouldTransform(className)) {
+  		ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+  		ProfilerVisitor visitor = new ProfilerVisitor(writer);
+  		ClassReader reader = new ClassReader(classfileBuffer);
+  		reader.accept(visitor, 0);
+  		return writer.toByteArray();
+	  } else {
+	    return classfileBuffer;
+	  }
+	}
+}
