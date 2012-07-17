476204b03aa333cc91ed9ee31a69967f22c08650
Eugene Burmako <xeno.by@gmail.com>
2012-07-17 21:35:24 +0200

    SI-5949 updates documentation of staticClass

--- src/library/scala/reflect/base/MirrorOf.scala
+++ src/library/scala/reflect/base/MirrorOf.scala
@@ -15,6 +15,24 @@ abstract class MirrorOf[U <: base.Universe with Singleton] {
 
   /** The symbol corresponding to the globally accessible class with the
    *  given fully qualified name `fullName`.
+   *
+   *  If the name points to a type alias, it's recursively dealiased and its target is returned.
+   *  If you need a symbol that corresponds to the type alias itself, load it directly from the package class:
+   *
+   *    scala> cm.staticClass("scala.List")
+   *    res0: reflect.runtime.universe.ClassSymbol = class List
+   *
+   *    scala> res0.fullName
+   *    res1: String = scala.collection.immutable.List
+   *
+   *    scala> cm.staticModule("scala")
+   *    res2: reflect.runtime.universe.ModuleSymbol = package scala
+   *
+   *    scala> res2.moduleClass.typeSignature member newTypeName("List")
+   *    res3: reflect.runtime.universe.Symbol = type List
+   *
+   *    scala> res3.fullName
+   *    res4: String = scala.List
    */
   def staticClass(fullName: String): U#ClassSymbol
 
