f97476e010038f8ca6b711b9d479b671a63e45fe
Paul Phillips <paulp@improving.org>
2012-07-21 19:49:55 -0700

    Improve unchecked warnings a little.
    Don't warn on case _: CC[Any] if Any corresponds to a
    covariant type parameter.

---
+++ test/files/neg/unchecked.scala
@@ -0,0 +1,50 @@
+object Test {
+  class Def[T]
+  class Exp[T]
+
+  case class ArrayApply[T](x: Exp[Array[T]], i: Exp[Int], j: Exp[_]) extends Def[T]
+
+  val IntArrayApply = ArrayApply[Int](new Exp[Array[Int]], new Exp[Int], new Exp[Int])
+
+  def f(x: Any) = x match {
+    case xs: Iterable[Any]    => xs.head // okay
+    case _                    => 0
+  }
+  def f2(x: Any) = x match {
+    case xs: Iterable[String] => xs.head // unchecked
+    case _                    => 0
+  }
+  def f3(x: Any) = x match {
+    case xs: Set[Any]    => xs.head // unchecked
+    case _                    => 0
+  }
+  def f4(x: Any) = x match {
+    case xs: Map[Any, Any] => xs.head // unchecked
+    case _                 => 0
+  }
+
+  def g[T](x: Def[T]) = x match {
+    case ArrayApply(x: Exp[Array[T]], i: Exp[Int], _) => x // okay - shouldn't warn, TODO
+    case _                                            => 0
+  }
+
+  def g2[T](x: Def[T]) = x match {
+    case ArrayApply(x: Exp[Array[T]], _, j: Exp[String]) => x // unchecked
+    case _                                               => 0
+  }
+
+  def g3[T](x: Any) = x match {
+    case ArrayApply(x: Exp[Array[T]], _, _) => x // unchecked
+    case _                                  => 0
+  }
+
+  def g4 = IntArrayApply match {
+    case ArrayApply(x: Exp[Array[Int]], _, _) => x // okay
+    case _                                    => ()
+  }
+  // 
+  // def g5 = IntArrayApply match {
+  //   case ArrayApply(x: Exp[Array[String]], _, _) => x // nope
+  //   case _                                       => ()
+  // }
+}
