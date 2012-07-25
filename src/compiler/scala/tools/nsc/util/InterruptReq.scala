6c547830a40f8fd6a4c9a7c24ff0ca37f4190ea1
Martin Odersky <odersky@gmail.com>
2012-07-25 15:58:45 +0200

    Backport.
    Adds method askForResponse
    
    Adds method askForResponse which returns a response immediately instead of waiting for a result. That way, one can wait for an ask's result using a timeout.
    (cherry picked from commit e896c0ab2d36740207f91086262e9feb4fc1c32d)
    
    Conflicts:
    
    	src/compiler/scala/tools/nsc/util/InterruptReq.scala

--- src/compiler/scala/tools/nsc/util/InterruptReq.scala
+++ src/compiler/scala/tools/nsc/util/InterruptReq.scala
@@ -2,6 +2,7 @@ package scala.tools.nsc
 package util
 
 /** A class of work items to be used in interrupt requests.
+ *  Todo: we should replace the Eithers by Futures or Try's.
  */
 abstract class InterruptReq {
   /** The result type of the operation
@@ -11,15 +12,23 @@ abstract class InterruptReq {
   /** The operation to be performed */
   protected val todo: () => R
 
+  type Continuation = Either[R, Throwable] => Unit
+
   /** The result provided */
   private var result: Option[Either[R, Throwable]] = None
+
+  /** The continuations waiting asynchronously on a provided result */
+  private var waiting: List[Continuation] = Nil
 
   /** To be called from interrupted server to execute demanded task */
   def execute(): Unit = synchronized {
     try {
       result = Some(Left(todo()))
     } catch {
-      case t => result = Some(Right(t))
+      case t: Throwable => result = Some(Right(t))
+    } finally {
+      notify()
+      for (k <- waiting.reverse) k(result.get)
     }
     notify()
   }
@@ -36,6 +45,10 @@ abstract class InterruptReq {
       case Left(res) => res
       case Right(t) => throw new FailedInterrupt(t)
     }
+  }
+
+  def onComplete(k: Continuation) = synchronized {
+    waiting = k :: waiting
   }
 }
 
