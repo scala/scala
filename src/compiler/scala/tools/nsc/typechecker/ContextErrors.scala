243cedecda536d2d49fd64a19a9862fd26fe07f6
Paul Phillips <paulp@improving.org>
2012-07-26 10:16:29 -0700

    [backport] Removed restriction on final vars, SI-2418.
    Backport of b79c7600544db9964c228b94a2f70f3ed854f89b
    
    The original fix for SI-2418 excluded final vars entirely, but
    the problem was not final vars per se, but the emission of ACC_FINAL
    in combination with ACC_VOLATILE.  Since vars never get ACC_FINAL
    now, this is no longer an issue.

--- src/compiler/scala/tools/nsc/typechecker/ContextErrors.scala
+++ src/compiler/scala/tools/nsc/typechecker/ContextErrors.scala
@@ -273,9 +273,6 @@ trait ContextErrors {
       def VolatileValueError(vdef: Tree) =
         issueNormalTypeError(vdef, "values cannot be volatile")
 
-      def FinalVolatileVarError(vdef: Tree) =
-        issueNormalTypeError(vdef, "final vars cannot be volatile")
-
       def LocalVarUninitializedError(vdef: Tree) =
         issueNormalTypeError(vdef, "local variables must be initialized")
 
