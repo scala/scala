scala/scala

commit b7b81ca286b1f426cd19befd50fbc513bb82282f

Fixed type unsoundness problem in t5120 and als...
Fixed type unsoundness problem in t5120 and also discovered by
roman.kalukiewicz@gmail.com. Fix should be refined further, as I am not
convinced we are quite done yet. Review by moors.

src/compiler/scala/reflect/internal/Types.scala
@@ -560,11 +560,19 @@ trait Types extends api.Types { self: SymbolTable =>
     def asSeenFrom(pre: Type, clazz: Symbol): Type =
       if (isTrivial || phase.erasedTypes && pre.typeSymbol != ArrayClass) this
       else {
+//        scala.tools.nsc.util.trace.when(pre.isInstanceOf[ExistentialType])("X "+this+".asSeenfrom("+pre+","+clazz+" = ") {
         incCounter(asSeenFromCount)
         val start = startTimer(asSeenFromNanos)
         val m = new AsSeenFromMap(pre.normalize, clazz)
         val tp = m apply this
-        val result = existentialAbstraction(m.capturedParams, tp)
+        val tp1 = existentialAbstraction(m.capturedParams, tp)
+        val result: Type =
+          if (m.capturedSkolems.isEmpty) tp1
+          else {
+            val captured = cloneSymbols(m.capturedSkolems)
+            captured foreach (_ setFlag CAPTURED)
+            tp1.substSym(m.capturedSkolems, captured)
+          }
         stopTimer(asSeenFromNanos, start)
         result
       }
@@ -3606,6 +3614,7 @@ A type's typeSymbol should never be inspected directly.
 
   /** A map to compute the asSeenFrom method  */
   class AsSeenFromMap(pre: Type, clazz: Symbol) extends TypeMap with KeepOnlyTypeConstraints {
+    var capturedSkolems: List[Symbol] = List()
     var capturedParams: List[Symbol] = List()
 
     override def mapOver(tree: Tree, giveup: ()=>Nothing): Tree = {
@@ -3679,7 +3688,7 @@ A type's typeSymbol should never be inspected directly.
                 pre1
               }
             } else {
-              toPrefix(base(pre, clazz).prefix, clazz.owner);
+              toPrefix(base(pre, clazz).prefix, clazz.owner)
             }
           toPrefix(pre, clazz)
         case SingleType(pre, sym) =>
@@ -3752,7 +3761,7 @@ A type's typeSymbol should never be inspected directly.
                         " gets applied to arguments "+baseargs.mkString("[",",","]")+", phase = "+phase)
                     }
                   case ExistentialType(tparams, qtpe) =>
-                    capturedParams = capturedParams union tparams
+                    capturedSkolems = capturedSkolems union tparams
                     toInstance(qtpe, clazz)
                   case t =>
                     throwError

src/compiler/scala/tools/nsc/typechecker/Typers.scala
@@ -2841,7 +2841,7 @@ trait Typers extends Modes with Adaptations with PatMatVirtualiser {
       def isLocal(sym: Symbol): Boolean =
         if (sym == NoSymbol || sym.isRefinementClass || sym.isLocalDummy) false
         else if (owner == NoSymbol) tree exists (defines(_, sym))
-        else containsDef(owner, sym) || isRawParameter(sym)
+        else containsDef(owner, sym) || isRawParameter(sym) || ((sym hasFlag EXISTENTIAL) && (sym hasFlag CAPTURED)) // todo refine this
       def containsLocal(tp: Type): Boolean =
         tp exists (t => isLocal(t.typeSymbol) || isLocal(t.termSymbol))
       val normalizeLocals = new TypeMap {

test/files/neg/t3015.check
@@ -1,11 +1,11 @@
 t3015.scala:7: error: scrutinee is incompatible with pattern type;
- found   : _$1 where type _$1
+ found   : _$1 where type +_$1
  required: String
   val b(foo) = "foo" 
        ^
 t3015.scala:7: error: type mismatch;
- found   : _$1(in value foo) where type _$1(in value foo) <: String
- required: (some other)_$1(in value foo) where type (some other)_$1(in value foo)
+ found   : String with _$1(in object Test) where type +_$1(in object Test)
+ required: (some other)_$1(in object Test) where type +(some other)_$1(in object Test)
   val b(foo) = "foo" 
       ^
 two errors found

test/files/neg/t5120.check
@@ -1,0 +1,12 @@
+t5120.scala:11: error: type mismatch;
+ found   : Object
+ required: _1
+    List(str, other) foreach (_.x1 = new AnyRef)
+                                     ^
+t5120.scala:25: error: type mismatch;
+ found   : Thread
+ required: h.T
+    (which expands to)  _2
+  List(str, num).foreach(h => h.f1 = new Thread())
+                                     ^
+two errors found

test/files/neg/t5120.scala
@@ -1,0 +1,29 @@
+class Cell[T](x0: T) {
+  type U = T
+  var x1: U = x0
+}
+
+object Test {
+  val str: Cell[String] = new Cell("a")
+  val other: Cell[Int]  = new Cell(0)
+
+  def main(args: Array[String]): Unit = {
+    List(str, other) foreach (_.x1 = new AnyRef)
+    str.x1.length
+  }
+}
+// another way demonstrating the same underlying problem, as reported by roman kalukiewicz
+
+class Holder[_T](_f1 : _T, _f2 : _T) {
+  type T = _T
+  var f1 : T = _f1
+  var f2 : T = _f2
+}
+object Test2 {
+  val str = new Holder("t1", "t2")
+  val num = new Holder(1, 2)
+  List(str, num).foreach(h => h.f1 = new Thread())
+  def main(args: Array[String]) {
+    println(str.f1)
+  }
+}

commit 26c70ab549840516f26fa4daf73bd214f4123468

SI-5330, SI-6014 deal with existential self-type
This has been broken since https://github.com/scala/scala/commit/b7b81ca2#L0L567.
The existential rash is treated in a similar manner as in fc24db4c.

Conceptually, the fix would be `def selfTypeSkolemized = widen.skolemizeExistential.narrow`,
but simply widening before narrowing achieves the same thing. Since we're in existential voodoo territory,
let's go for the minimal fix: replacing `this.narrow` by `widen.narrow`.

src/reflect/scala/reflect/internal/Types.scala
@@ -1116,6 +1116,11 @@ trait Types extends api.Types { self: SymbolTable =>
       var required = requiredFlags
       var excluded = excludedFlags | DEFERRED
       var continue = true
+
+      // SI-5330. I we don't widen before narrowing, subsequent calls to asSeenFrom have different symbols for existentials, e.g. pos/t5330.scala.
+      //          If we don't re-narrow the result, pos/t5330b.scala fails.
+      // See also SI-6014 and SI-5120 (whose fix caused the regressions SI-5330 and SI-6014)
+      def selfTypeSkolemized = widen.narrow
       var self: Type = null
       val fingerPrint: Long = name.fingerPrint
 
@@ -1147,7 +1152,7 @@ trait Types extends api.Types { self: SymbolTable =>
                     if ((member ne sym) &&
                         ((member.owner eq sym.owner) ||
                          (flags & PRIVATE) != 0 || {
-                           if (self eq null) self = this.narrow
+                           if (self eq null) self = selfTypeSkolemized
                            if (membertpe eq null) membertpe = self.memberType(member)
                            !(membertpe matches self.memberType(sym))
                          })) {
@@ -1162,7 +1167,7 @@ trait Types extends api.Types { self: SymbolTable =>
                              (other ne sym) &&
                              ((other.owner eq sym.owner) ||
                               (flags & PRIVATE) != 0 || {
-                                if (self eq null) self = this.narrow
+                                if (self eq null) self = selfTypeSkolemized
                                 if (symtpe eq null) symtpe = self.memberType(sym)
                                 !(self.memberType(other) matches symtpe)
                              })}) {

test/files/pos/t5330.scala
@@ -1,0 +1,24 @@
+trait FM[A] {
+  def map(f: A => Any)
+}
+
+trait M[A] extends FM[A] {
+  def map(f: A => Any)
+}
+
+trait N[A] extends FM[A]
+
+object test {
+  def kaboom(xs: M[_]) = xs map (x => ()) // missing parameter type.
+
+  def okay1[A](xs: M[A]) = xs map (x => ())
+  def okay2(xs: FM[_]) = xs map (x => ())
+  def okay3(xs: N[_]) = xs map (x => ())
+}
+
+
+// does not compile
+class CC2(xs: List[_]) {
+  def f(x1: Any, x2: Any) = null
+  def g = xs map (x => f(x, x))
+}

test/files/pos/t5330b.scala
@@ -1,0 +1,6 @@
+abstract trait Base {
+  def foo: this.type
+};
+class Derived[T] extends Base {
+  def foo: Nothing = sys.error("!!!")
+}

test/files/pos/t6014.scala
@@ -1,0 +1,13 @@
+object Test {
+  case class CC[T](key: T)
+  type Alias[T] = Seq[CC[T]]
+
+  def f(xs: Seq[CC[_]]) = xs map { case CC(x) => CC(x) }    // ok
+  def g(xs: Alias[_])   = xs map { case CC(x) => CC(x) }    // fails
+  // ./a.scala:11: error: missing parameter type for expanded function
+  // The argument types of an anonymous function must be fully known. (SLS 8.5)
+  // Expected type was: ?
+  //   def g(xs: Alias[_])   = xs map { case CC(x) => CC(x) }    // fails
+  //                                  ^
+  // one error found
+}