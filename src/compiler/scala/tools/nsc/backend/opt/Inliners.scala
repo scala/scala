9537ca0be7687f0c10a5759db2f588350858ef46
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2012-08-14 09:31:58 +0200

    new approach to field publicizing in Inliner

--- src/compiler/scala/tools/nsc/backend/opt/Inliners.scala
+++ src/compiler/scala/tools/nsc/backend/opt/Inliners.scala
@@ -645,31 +645,40 @@ abstract class Inliners extends SubComponent {
           else if (sym.isProtected) Protected
           else Public
 
-        def canMakePublic(f: Symbol): Boolean =
-          (m.sourceFile ne NoSourceFile) &&
-          (f.isSynthetic || f.isParamAccessor) &&
-          { toBecomePublic = f :: toBecomePublic; true }
+        /*
+         * (1) Everything accessed by @inline methods had access-level widened to public in ExplicitOuter.
+         *     We rely on that irrespective of whether the callee is being compiled in this run, or was separately compiled.
+         *
+         * (2) We also inline here *a few* other fields not publicized before,
+         *     as long as they're compiled in this run and are (synthetic, param accessors, or owned by an anon-closure).
+         *
+         * In all cases, `toBecomePublic` fields are made public at bytecode-level only, not in pickles.
+         *
+         **/
+        def canRegardAsPublic(f: Symbol): Boolean = {
 
-        /* A safety check to consider as private, for the purposes of inlining, a public field that:
-         *   (1) is defined in an external library, and
-         *   (2) can be presumed synthetic (due to a dollar sign in its name).
-         * Such field was made public by `doMakePublic()` and we don't want to rely on that,
-         * because under other compilation conditions (ie no -optimize) that won't be the case anymore.
-         *
-         * This allows aggressive intra-library inlining (making public if needed)
-         * that does not break inter-library scenarios (see comment for `Inliners`).
-         *
-         * TODO handle more robustly the case of a trait var changed at the source-level from public to private[this]
-         *      (eg by having ICodeReader use unpickler, see SI-5442).
-         
-         DISABLED
-         
-        def potentiallyPublicized(f: Symbol): Boolean = {
-          (m.sourceFile eq NoSourceFile) && f.name.containsChar('$')
+          val calleeSym = m.symbol
+          if(hasInline(calleeSym)) {
+            if(currentRun.compiles(f)) {
+              toBecomePublic ::= f;
+            }
+            return true
+          }
+          val canForcePublic = {
+            currentRun.compiles(f) && (
+              f.isSynthetic     ||
+              f.isParamAccessor ||
+              (isClosureClass(f.owner) && f.isOuterField /* ie is an anon-closure-class field */ )
+            )
+          }
+          if(canForcePublic) {
+            toBecomePublic ::= f;
+            return true
+          }
+          false
         }
-        */
 
-        def checkField(f: Symbol)   = check(f, f.isPrivate && !canMakePublic(f))
+        def checkField(f: Symbol)   = check(f, f.isPrivate && !canRegardAsPublic(f))
         def checkSuper(n: Symbol)   = check(n, n.isPrivate || !n.isClassConstructor)
         def checkMethod(n: Symbol)  = check(n, n.isPrivate)
 
