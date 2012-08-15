c14543863b81634721cf4841107f203299289e7b
Martin Odersky <odersky@gmail.com>
2012-08-15 19:21:37 +0200

    Optimization to atOwner
    Moved costly hashMap updates into SuperAccessors from general tree transformers and replaced by immutable map.

--- src/compiler/scala/tools/nsc/typechecker/SuperAccessors.scala
+++ src/compiler/scala/tools/nsc/typechecker/SuperAccessors.scala
@@ -357,9 +357,22 @@ abstract class SuperAccessors extends transform.Transform with transform.TypingT
       }
     }
 
-    override def atOwner[A](owner: Symbol)(trans: => A): A = {
+    /** a typer for each enclosing class */
+    private var typers = immutable.Map[Symbol, analyzer.Typer]()
+
+    /** Specialized here for performance; the previous blanked
+     *  introduction of typers in TypingTransformer caused a >5%
+     *  performance hit for the compiler as a whole.
+     */
+    override def atOwner[A](tree: Tree, owner: Symbol)(trans: => A): A = {
       if (owner.isClass) validCurrentOwner = true
-      super.atOwner(owner)(trans)
+      val savedLocalTyper = localTyper
+      localTyper = localTyper.atOwner(tree, if (owner.isModule) owner.moduleClass else owner)
+      typers = typers updated (owner, localTyper)
+      val result = super.atOwner(tree, owner)(trans)
+      localTyper = savedLocalTyper
+      typers -= owner
+      result
     }
 
     private def withInvalidOwner[A](trans: => A): A = {
