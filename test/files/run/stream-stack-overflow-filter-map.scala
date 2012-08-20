baca952d6a3953e1c3537bc1f13fa733211ec040
Paolo Giarrusso <p.giarrusso@gmail.com>
2012-08-20 22:44:49 +0200

    Cleanup testcase
    No need to check the output - checking programmatically that the
    produced streams are empty is enough.

--- test/files/run/stream-stack-overflow-filter-map.scala
+++ test/files/run/stream-stack-overflow-filter-map.scala
@@ -1,12 +1,10 @@
 object Test extends App {
-  //This runs fine.
   val resFMap1 = (1 to 10000).toStream filter (_ => false) flatMap (Seq(_))
   val resMap1 = (1 to 10000).toStream filter (_ => false) map (_ + 1)
   assert(resMap1.isEmpty)
   assert(resFMap1.isEmpty)
-  println(resMap1)
-  println(resFMap1)
-  //This will cause a stack overflow 
+
+  //This risks causing a stack overflow 
   val resFMap2 = (1 to 10000).toStream withFilter (_ => false) flatMap (Seq(_))
   val resMap2 = (1 to 10000).toStream withFilter (_ => false) map (_ + 1)
   assert(resMap1 == resMap2)
