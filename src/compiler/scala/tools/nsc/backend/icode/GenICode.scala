f7494156c4dc545167455579ac85bc3382e09fb9
Aleksandar Prokopec <axel22@gmail.com>
2012-08-30 11:04:58 +0200

    Fix SI-6294.

--- src/compiler/scala/tools/nsc/backend/icode/GenICode.scala
+++ src/compiler/scala/tools/nsc/backend/icode/GenICode.scala
@@ -121,13 +121,16 @@ abstract class GenICode extends SubComponent  {
         m.native = m.symbol.hasAnnotation(definitions.NativeAttr)
 
         if (!m.isAbstractMethod && !m.native) {
-          if (m.symbol.isAccessor && m.symbol.accessed.hasStaticAnnotation) {
+          val compClass = m.symbol.owner.companionClass
+          val staticfield =
+            if (m.symbol.isAccessor && m.symbol.accessed.hasStaticAnnotation)
+              compClass.info.findMember(m.symbol.accessed.name, NoFlags, NoFlags, false)
+            else NoSymbol
+          if (staticfield != NoSymbol) {
             // in companion object accessors to @static fields, we access the static field directly
-            val hostClass = m.symbol.owner.companionClass
-            val staticfield = hostClass.info.findMember(m.symbol.accessed.name, NoFlags, NoFlags, false)
             
             if (m.symbol.isGetter) {
-              ctx1.bb.emit(LOAD_FIELD(staticfield, true) setHostClass hostClass, tree.pos)
+              ctx1.bb.emit(LOAD_FIELD(staticfield, true) setHostClass compClass, tree.pos)
               ctx1.bb.closeWith(RETURN(m.returnType))
             } else if (m.symbol.isSetter) {
               ctx1.bb.emit(LOAD_LOCAL(m.locals.head), tree.pos)
---
+++ test/files/pos/t6294.scala
@@ -0,0 +1,14 @@
+
+
+
+object A {
+  @annotation.static final val x = 123
+}
+
+
+object B {
+  println(A.x)
+}
+
+
+
