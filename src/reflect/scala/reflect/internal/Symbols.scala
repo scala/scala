04e257d3ce9e6ce44c3cce16d3e28046e04986ce
Eugene Burmako <xeno.by@gmail.com>
2012-09-06 18:19:42 +0200

    removes isLocatable from the public API
    Hard to come up with a good name, would need extensive documentation
    to justify its purpose => no go. If necessary we can always reintroduce
    it later in 2.10.1.

--- src/reflect/scala/reflect/internal/Symbols.scala
+++ src/reflect/scala/reflect/internal/Symbols.scala
@@ -853,7 +853,16 @@ trait Symbols extends api.Symbols { self: SymbolTable =>
     final def isInitialized: Boolean =
       validTo != NoPeriod
 
-    /** Determines whether this symbol can be loaded by subsequent reflective compilation */
+    /** Can this symbol be loaded by a reflective mirror?
+     *
+     *  Scalac relies on `ScalaSignature' annotation to retain symbols across compilation runs.
+     *  Such annotations (also called "pickles") are applied on top-level classes and include information
+     *  about all symbols reachable from the annotee. However, local symbols (e.g. classes or definitions local to a block)
+     *  are typically unreachable and information about them gets lost.
+     *
+     *  This method is useful for macro writers who wish to save certain ASTs to be used at runtime.
+     *  With `isLocatable' it's possible to check whether a tree can be retained as is, or it needs special treatment.
+     */
     final def isLocatable: Boolean = {
       if (this == NoSymbol) return false
       if (isRoot || isRootPackage) return true
