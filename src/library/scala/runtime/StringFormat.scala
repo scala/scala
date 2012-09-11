ddb3b889b5fa55760167d9995d2f1549e2542b71
Jason Zaugg <jzaugg@gmail.com>
2012-09-11 23:40:11 +0200

    A little cleanup along the Any to AnyRef trail.
    Followup to 35316be and d3f879a.
    
     - Remove obsolete comments and replace them with a test.
     - Don't emit error addendum unless we know we're dealing
       with a value class.

--- src/library/scala/runtime/StringFormat.scala
+++ src/library/scala/runtime/StringFormat.scala
@@ -11,12 +11,6 @@ package scala.runtime
 /** A wrapper class that adds a `formatted` operation to any value
  */
 final class StringFormat(val self: Any) extends AnyVal {
-  // Note: The implicit conversion from Any to StringFormat is one of two
-  // implicit conversions from Any to AnyRef in Predef. It is important to have at least
-  // two such conversions, so that silent conversions from value types to AnyRef
-  // are avoided. If StringFormat should become a value class, another
-  // implicit conversion from Any to AnyRef has to be introduced in Predef
-
   /** Returns string formatted according to given `format` string.
    *  Format strings are as for `String.format`
    *  (@see java.lang.String.format).
