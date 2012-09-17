121f3a92ea86258ab8507c6a7759cd2242bebab6
Paul Phillips <paulp@improving.org>
2012-09-16 20:55:34 -0700

    Revert "Added constant empty array to the companion objects."
    This reverts most of commit 9d84e89d2 .

--- test/files/run/empty-array.scala
+++ test/files/run/empty-array.scala
@@ -1,8 +1,8 @@
 object Test {
   def main(args: Array[String]): Unit = {
-    println(Byte.emptyArray.length)
-    println(Double.emptyArray.length)
-    println(Boolean.emptyArray.length)
+    println(Array.emptyByteArray.length)
+    println(Array.emptyDoubleArray.length)
+    println(Array.emptyBooleanArray.length)
     // okay okay okay
   }
 }
