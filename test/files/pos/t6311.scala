scala/scala

commit 5c5e8d4dcd151a6e2bf9e7c259c618b9b4eff00f

SI-4881 infer variance from formals, then result
Changed behavior so that when determining the target variance of a method parameter,
the variance in formals comes first. If variance is still undecided by that,
the variance in the result type is used as a secondary criterion.

(This is also done when determining prototype type params.)

src/compiler/scala/tools/nsc/typechecker/Infer.scala
@@ -445,7 +445,7 @@ trait Infer {
       val tvars = tparams map freshVar
       if (isConservativelyCompatible(restpe.instantiateTypeParams(tparams, tvars), pt))
         map2(tparams, tvars)((tparam, tvar) =>
-          instantiateToBound(tvar, varianceInTypes(formals)(tparam)))
+          instantiateToBound(tvar, inferVariance(formals, restpe)(tparam)))
       else
         tvars map (tvar => WildcardType)
     }
@@ -575,11 +575,22 @@ trait Infer {
             "argument expression's type is not compatible with formal parameter type" + foundReqMsg(tp1, pt1))
         }
       }
+
       val targs = solvedTypes(
-        tvars, tparams, tparams map varianceInTypes(formals),
+        tvars, tparams, tparams map inferVariance(formals, restpe),
         false, lubDepth(formals) max lubDepth(argtpes)
       )
       adjustTypeArgs(tparams, tvars, targs, restpe)
+    }
+
+    /** Determine which variance to assume for the type paraneter. We first chose the variance
+     *  that minimizes any formal parameters. If that is still undetermined, because the type parameter
+     *  does not appear as a formal parameter type, then we pick the variance so that it minimizes the
+     *  method's result type instead.
+     */
+    private def inferVariance(formals: List[Type], restpe: Type)(tparam: Symbol): Int = {
+      val v = varianceInTypes(formals)(tparam)
+      if (v != VarianceFlags) v else varianceInType(restpe)(tparam)
     }
 
     private[typechecker] def followApply(tp: Type): Type = tp match {

test/files/neg/t5845.check
@@ -1,7 +1,4 @@
-t5845.scala:9: error: value +++ is not a member of Int
-  println(5 +++ 5)
-            ^
 t5845.scala:15: error: value +++ is not a member of Int
   println(5 +++ 5)
             ^
-two errors found
+one error found

test/files/pos/t4881.scala
@@ -1,0 +1,31 @@
+class Contra[-T]
+trait A
+trait B extends A
+trait C extends B
+
+// test improved variance inference: first try formals to see in which variance positions the type param appears;
+// only when that fails to determine variance, look at result type
+object Test {
+  def contraLBUB[a >: C <: A](): Contra[a] = null
+  def contraLB[a >: C](): Contra[a] = null
+
+{
+  val x = contraLBUB() //inferred Contra[C] instead of Contra[A]
+  val x1: Contra[A] = x
+}
+
+{
+  val x = contraLB() //inferred Contra[C] instead of Contra[Any]
+  val x1: Contra[Any] = x
+}
+
+{
+  val x = contraLBUB // make sure it does the same thing as its ()-less counterpart
+  val x1: Contra[A] = x
+}
+
+{
+  val x = contraLB
+  val x1: Contra[Any] = x
+}
+}

commit 6ea54efa60b54016daa339b1e13c5241a7d5feec

Test case for SI-6311.
This covers the situation which broke in 5c5e8d4dcd,
reverted in the previous commit.

test/files/pos/t6311.scala
@@ -1,0 +1,5 @@
+class A {
+  def fooMinimal[T, Coll <: Traversable[T]](msg: String)(param1: Traversable[T])(param2: Coll): Traversable[T] = throw new Exception()
+
+  fooMinimal("")(List(1))(List(2))
+}