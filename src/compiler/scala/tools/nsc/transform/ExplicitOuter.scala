516fe526f40e900e13319298f472db3e63525204
Paul Phillips <paulp@improving.org>
2012-10-02 07:53:07 -0700

    Removed obsolete migration test.
    Arrays are not Seqs: a fact known by all and sundry.

--- src/compiler/scala/tools/nsc/transform/ExplicitOuter.scala
+++ src/compiler/scala/tools/nsc/transform/ExplicitOuter.scala
@@ -83,12 +83,6 @@ abstract class ExplicitOuter extends InfoTransform
     }
   }
 
-  /** Issue a migration warning for instance checks which might be on an Array and
-   *  for which the type parameter conforms to Seq, because these answers changed in 2.8.
-   */
-  def isArraySeqTest(lhs: Type, rhs: Type) =
-    (ArrayClass.tpe <:< lhs.widen) && (rhs.widen matchesPattern SeqClass.tpe)
-
   def outerAccessor(clazz: Symbol): Symbol = {
     val firstTry = clazz.info.decl(nme.expandedName(nme.OUTER, clazz))
     if (firstTry != NoSymbol && firstTry.outerSource == clazz) firstTry
@@ -550,13 +544,6 @@ abstract class ExplicitOuter extends InfoTransform
           }
 
         case _ =>
-          if (settings.Xmigration28.value) tree match {
-            case TypeApply(fn @ Select(qual, _), args) if fn.symbol == Object_isInstanceOf || fn.symbol == Any_isInstanceOf =>
-              if (isArraySeqTest(qual.tpe, args.head.tpe))
-                unit.warning(tree.pos, "An Array will no longer match as Seq[_].")
-            case _ => ()
-          }
-
           val x = super.transform(tree)
           if (x.tpe eq null) x
           else x setType transformInfo(currentOwner, x.tpe)
