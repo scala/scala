e8525254314627bd145d2093f0a1a0f3ca6a8067
Vlad Ureche <vlad.ureche@gmail.com>
2012-10-10 09:59:12 +0200

    scala.reflect.api.Constants documentation

--- src/reflect/scala/reflect/api/package.scala
+++ src/reflect/scala/reflect/api/package.scala
@@ -9,6 +9,19 @@ import scala.reflect.api.{Universe => ApiUniverse}
  *
  *  In Scala 2.10.0, reflection API and its implementation have experimental status. This means that the API and the docs are not complete and can be changed
  *  in binary- and source-incompatible manner in 2.10.1. This also means that the implementation has known issues
+ *
+ *  @groupprio API        9
+ *  @groupprio Extractors 10
+ *  @groupprio Tags       11
+ *  @groupdesc API        The methods available for each reflection entity, without the implementation. Since the
+ *                        reflection entities are abstract types that are later overridden, their API counterparts
+ *                        guarantee a minimum set of methods implemented in both runtime reflection and macros.
+ *  @groupdesc Extractors Extractors provide the machinery necessary to allow pattern matching and construction of
+ *                        reflection entities that is similar to case classes, although the entities are only abstract
+ *                        types that are later overridden.
+ *  @groupdesc Tags       Implicit values that provide [[scala.reflect.ClassTag `ClassTags`]] for the reflection
+ *                        classes. These are abstract in the interface but are later filled in to provide ClassTags
+ *                        for the either the runtime reflection or macros entities, depending on the use.
  */
 package object api {
 
