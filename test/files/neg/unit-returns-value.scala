02909f2be30db5c0f79f961cad17e2dc2f026ff4
Paul Phillips <paulp@improving.org>
2012-10-12 22:24:32 -0700

    Warn about more misplaced expressions.
    An identifier being used in statement position is not likely
    what was meant when it is a non-lazy getter.

--- test/files/neg/unit-returns-value.scala
+++ test/files/neg/unit-returns-value.scala
@@ -3,9 +3,30 @@ object Test {
     var b = false
     if (b) return 5
   }
-  
+
   // no warning
   def g {
     return println("hello")
   }
 }
+
+class UnusedValues {
+  var i1 = 2
+  val i2 = 2
+  lazy val i3 = 2
+  object i4 { }
+  def i5 = 2
+  final def i6 = 2
+
+  def x = {
+    i1 // warn
+    i2 // warn
+    i3 // no warn
+    i4 // no warn
+    i5 // no warn
+    i6 // could warn someday, if i6 returned 2.type instead of Int
+
+    5
+  }
+}
+
