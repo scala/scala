1841114e6cbb1ab4e3ad6abcd17b0bc9ebef0481
Paul Phillips <paulp@improving.org>
2012-10-29 12:36:33 -0700

    An option for real repl output.
    There have been many requests to expose the products of
    the repl in some way outside in-memory. This does that.
    
      scala                        # usual in-memory behavior
      scala -Yrepl-outdir ""       # make up a temp dir
      scala -Yrepl-outdir /foo/bar # use /foo/bar

--- src/compiler/scala/tools/nsc/settings/ScalaSettings.scala
+++ src/compiler/scala/tools/nsc/settings/ScalaSettings.scala
@@ -170,6 +170,7 @@ trait ScalaSettings extends AbsScalaSettings
   val Ybuilderdebug   = ChoiceSetting     ("-Ybuilder-debug", "manager", "Compile using the specified build manager.", List("none", "refined", "simple"), "none")
   val Yreifycopypaste = BooleanSetting    ("-Yreify-copypaste", "Dump the reified trees in copypasteable representation.")
   val Yreplsync       = BooleanSetting    ("-Yrepl-sync", "Do not use asynchronous code for repl startup")
+  val Yreploutdir     = StringSetting     ("-Yrepl-outdir", "path", "Write repl-generated classfiles to given output directory (use \"\" to generate a temporary dir)" , "")
   val Ynotnull        = BooleanSetting    ("-Ynotnull", "Enable (experimental and incomplete) scala.NotNull.")
   val YmethodInfer    = BooleanSetting    ("-Yinfer-argument-types", "Infer types for arguments of overriden methods.")
   val etaExpandKeepsStar = BooleanSetting ("-Yeta-expand-keeps-star", "Eta-expand varargs methods to T* rather than Seq[T].  This is a temporary option to ease transition.")
