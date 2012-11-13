0b0c5756d22302f93fe9c62571a930504b819594
Paul Phillips <paulp@improving.org>
2012-11-13 08:27:02 -0800

    Members removed from scala.reflect.io.
    Mostly hailing from a long-ago day when I imagined I was
    writing a general purpose library. We dodged that bullet.

--- src/reflect/scala/reflect/io/VirtualDirectory.scala
+++ src/reflect/scala/reflect/io/VirtualDirectory.scala
@@ -11,7 +11,7 @@ import scala.collection.mutable
  * An in-memory directory.
  *
  * @author Lex Spoon
- * 
+ *
  * ''Note:  This library is considered experimental and should not be used unless you know what you are doing.''
  */
 class VirtualDirectory(val name: String, maybeContainer: Option[VirtualDirectory])
@@ -26,7 +26,7 @@ extends AbstractFile {
 
   def container = maybeContainer.get
   def isDirectory = true
-  var lastModified: Long = System.currentTimeMillis
+  val lastModified: Long = System.currentTimeMillis
 
   override def file = null
   override def input = sys.error("directories cannot be read")
