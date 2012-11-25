0b92073a38f9d1823f051ac18173078bfcfafc8a
Jean-Remi Desjardins <jeanremi.desjardins@gmail.com>
2012-11-25 18:59:03 -0500

    SI-4664 Make scala.util.Random Serializable

--- src/library/scala/util/Random.scala
+++ src/library/scala/util/Random.scala
@@ -17,7 +17,7 @@ import scala.language.{implicitConversions, higherKinds}
  *  @author Stephane Micheloud
  *
  */
-class Random(val self: java.util.Random) {
+class Random(val self: java.util.Random) extends AnyRef with Serializable {
   /** Creates a new random number generator using a single long seed. */
   def this(seed: Long) = this(new java.util.Random(seed))
 
