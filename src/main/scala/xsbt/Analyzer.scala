902150828b603845187bacab4cf9f821e2228831
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2012-11-27 00:14:04 -0800

    Fix for dependency on class file corresponding to a package. (#620)
    While trying to determine binary dependencies sbt lookups class files
    corresponding to symbols. It tried to do that for packages and most of the
    time would fail because packages don't have corresponding class file
    generated. However, in case of case insensitive file system, combined
    with special nesting structure you could get spurious dependency.
    See added test case for an example of such structure.
    
    The remedy is to never even try to locate class files corresponding to
    packages.
    
    Fixes #620.
    
    Rewritten from sbt/zinc@768a72066d71d03bc45f1b8792d8b124db63a836

--- src/main/scala/xsbt/Analyzer.scala
+++ src/main/scala/xsbt/Analyzer.scala
@@ -81,6 +81,9 @@ final class Analyzer(val global: CallbackGlobal) extends Compat
 
 	private[this] final val classSeparator = '.'
 	private[this] def classFile(sym: Symbol): Option[(AbstractFile, String, Boolean)] =
+	// package can never have a corresponding class file; this test does not
+	// catch package objects (that do not have this flag set)
+	if (sym hasFlag scala.tools.nsc.symtab.Flags.PACKAGE) None else
 	{
 		import scala.tools.nsc.symtab.Flags
 		val name = flatname(sym, classSeparator) + moduleSuffix(sym)
