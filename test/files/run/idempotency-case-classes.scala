502818157671ff1d62264091b0102d05a42ff352
Eugene Burmako <xeno.by@gmail.com>
2012-12-04 12:35:13 +0100

    tests for idempotency issues in the typechecker
    For more details see https://issues.scala-lang.org/browse/SI-5464.
    
    Check files are intentionally very precise, so that we can monitor
    how the situation changes over time.

---
+++ test/files/run/idempotency-case-classes.scala
@@ -0,0 +1,22 @@
+import scala.reflect.runtime.universe._
+import scala.reflect.runtime.{currentMirror => cm}
+import scala.tools.reflect.{ToolBox, ToolBoxError}
+import scala.tools.reflect.Eval
+
+object Test extends App {
+  val casee = reify {
+    case class C(x: Int, y: Int)
+    println(C(2, 3))
+  }
+  println(casee.eval)
+  val tb = cm.mkToolBox()
+  val tcasee = tb.typeCheck(casee.tree)
+  println(tcasee)
+  val rtcasee = tb.resetAllAttrs(tcasee)
+  try {
+    println(tb.eval(rtcasee))
+  } catch {
+    // this is the current behaviour, rather than the desired behavior; see SI-5467
+    case _: ToolBoxError => println("error!")
+  }
+}
