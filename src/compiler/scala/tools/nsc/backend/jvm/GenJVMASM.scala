a23cc20ed5d3795584cb9ed74c0cd4bda25c2df1
Jason Zaugg <jzaugg@gmail.com>
2012-12-05 15:19:19 +0100

    SI-5894 Don't emit static forwarders for macros.

--- src/compiler/scala/tools/nsc/backend/jvm/GenJVMASM.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenJVMASM.scala
@@ -36,7 +36,7 @@ trait GenJVMASM {
   protected val ExcludedForwarderFlags = {
     import Flags._
     // Should include DEFERRED but this breaks findMember.
-    ( CASE | SPECIALIZED | LIFTED | PROTECTED | STATIC | EXPANDEDNAME | BridgeAndPrivateFlags )
+    ( CASE | SPECIALIZED | LIFTED | PROTECTED | STATIC | EXPANDEDNAME | BridgeAndPrivateFlags | MACRO )
   }
 
   protected def isJavaEntryPoint(icls: IClass) = {
