31a0aa75cf9739b2ae565d634670f6c0ce89bbc1
Jason Zaugg <jzaugg@gmail.com>
2012-12-05 14:16:27 +0100

    SI-1672 Catches are in tail position without finally.
    So we can eliminate tail calls within.

---
+++ test/files/neg/t1672b.scala
@@ -0,0 +1,41 @@
+object Test {
+  @annotation.tailrec
+  def bar : Nothing = {
+    try {
+      throw new RuntimeException
+    } catch {
+      case _: Throwable => bar
+    } finally {
+      bar
+    }
+  }
+
+  @annotation.tailrec
+  def baz : Nothing = {
+    try {
+      throw new RuntimeException
+    } catch {
+      case _: Throwable => baz
+    } finally {
+      ???
+    }
+  }
+
+  @annotation.tailrec
+  def boz : Nothing = {
+    try {
+      throw new RuntimeException
+    } catch {
+      case _: Throwable => boz; ???
+    }
+  }
+
+  @annotation.tailrec
+  def bez : Nothing = {
+    try {
+      bez
+    } finally {
+      ???
+    }
+  }
+}
