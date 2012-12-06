xeno-by/backupler-2012-12-26

introduces global.pendingSuperCall
Similarly to global.emptyValDef, which is a dummy that stands for an
empty self-type, this commit introduces global.pendingSuperCall, which
stands for a yet-to-be-filled-in call to a superclass constructor.

pendingSuperCall is emitted by Parsers.template, treated specially by
Typers.typedParentType and replaced with a real superclass ctor call
by Typers.typedTemplate.

To avoid copy/paste, this commit also factors out and unifies dumminess
of EmptyTree, emptyValDef and pendingSuperCall - they all don't have a
position and actively refuse to gain one, same story for tpe.

src/compiler/scala/tools/nsc/typechecker/StdAttachments.scala
@@ -21,12 +21,14 @@ trait StdAttachments {
    */
   case class SuperCallArgsAttachment(argss: List[List[Tree]])
 
-  /** Extractor for `SuperCallArgsAttachment`.
+  /** Convenience method for `SuperCallArgsAttachment`.
    *  Compared with `MacroRuntimeAttachment` this attachment has different a usage pattern,
    *  so it really benefits from a dedicated extractor.
    */
-  object CarriesSuperCallArgs {
-    def unapply(tree: Tree): Option[List[List[Tree]]] =
-      tree.attachments.get[SuperCallArgsAttachment] collect { case SuperCallArgsAttachment(argss) => argss }
-  }
+  def superCallArgs(tree: Tree): Option[List[List[Tree]]] =
+    tree.attachments.get[SuperCallArgsAttachment] collect { case SuperCallArgsAttachment(argss) => argss }
+
+  /** Determines whether the given tree has an associated SuperCallArgsAttachment.
+   */
+  def carriesSuperCallArgs(tree: Tree): Boolean = superCallArgs(tree).nonEmpty
 }

scala/scala

src/compiler/scala/tools/nsc/typechecker/StdAttachments.scala
@@ -21,12 +21,14 @@ trait StdAttachments {
    */
   case class SuperCallArgsAttachment(argss: List[List[Tree]])
 
-  /** Extractor for `SuperCallArgsAttachment`.
+  /** Convenience method for `SuperCallArgsAttachment`.
    *  Compared with `MacroRuntimeAttachment` this attachment has different a usage pattern,
    *  so it really benefits from a dedicated extractor.
    */
-  object CarriesSuperCallArgs {
-    def unapply(tree: Tree): Option[List[List[Tree]]] =
-      tree.attachments.get[SuperCallArgsAttachment] collect { case SuperCallArgsAttachment(argss) => argss }
-  }
+  def superCallArgs(tree: Tree): Option[List[List[Tree]]] =
+    tree.attachments.get[SuperCallArgsAttachment] collect { case SuperCallArgsAttachment(argss) => argss }
+
+  /** Determines whether the given tree has an associated SuperCallArgsAttachment.
+   */
+  def hasSuperArgs(tree: Tree): Boolean = superCallArgs(tree).nonEmpty
 }