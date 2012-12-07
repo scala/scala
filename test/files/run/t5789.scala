5f2b7c4c36f96f955573c4ac322b8ca6d632abc1
James Iry <james.iry@typesafe.com>
2012-12-07 15:46:04 -0800

    SI-5789 Use the ReplTest framework in the test
    Don't check for the crashed message, just dump the output
    from the REPL. Use the ReplTest framework to the make
    the test clean

--- test/files/run/t5789.scala
+++ test/files/run/t5789.scala
@@ -1,27 +1,14 @@
 
 import scala.tools.nsc._
 import interpreter.ILoop
-
-object Test {
-
-  def main(args : Array[String]) {
-
-    val code = """
-  val n = 2
-  () => n
-    """
-
-    val s = new Settings()
-    s.optimise.value = true
-    val lines = ILoop.runForTranscript(code + "\n" + code, s).lines.toList
+import scala.tools.partest.ReplTest
 
 
-    if (lines exists (_ contains "Abandoning crashed session")) {
-     lines foreach println
-    	println("crashed!")
-    } else {
-      println("completed successfully")
-    }
-  }
+object Test extends ReplTest {
+  override def extraSettings = "-Yinline"
+  def code = """
+    val n = 2
+    () => n
+  """
 }
 
