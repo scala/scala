4c45c2a619e088a20ef40c2f858649a922c35703
Den Shabalin <den.shabalin@gmail.com>
2012-12-08 19:43:06 +0100

    Changes tree pretty printer to use shorter name constructor

--- src/compiler/scala/reflect/reify/utils/NodePrinters.scala
+++ src/compiler/scala/reflect/reify/utils/NodePrinters.scala
@@ -36,8 +36,8 @@ trait NodePrinters {
           flagsAreUsed = true
           show(m.group(1).toLong)
         })
-        s = s.replace("Modifiers(0L, newTypeName(\"\"), List())", "Modifiers()")
-        s = """Modifiers\((\d+)[lL], newTypeName\("(.*?)"\), List\((.*?)\)\)""".r.replaceAllIn(s, m => {
+        s = s.replace("Modifiers(0L, TypeName(\"\"), List())", "Modifiers()")
+        s = """Modifiers\((\d+)[lL], TypeName\("(.*?)"\), List\((.*?)\)\)""".r.replaceAllIn(s, m => {
           val buf = new scala.collection.mutable.ListBuffer[String]
 
           val annotations = m.group(3)
@@ -46,7 +46,7 @@ trait NodePrinters {
 
           val privateWithin = "" + m.group(2)
           if (buf.nonEmpty || privateWithin != "")
-            buf.append("newTypeName(\"" + privateWithin + "\")")
+            buf.append("TypeName(\"" + privateWithin + "\")")
 
           val bits = m.group(1)
           if (buf.nonEmpty || bits != "0L") {
