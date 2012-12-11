601536136e6300cb8fef8f20b1f1e74cec033621
Jason Zaugg <jzaugg@gmail.com>
2012-12-12 00:01:58 +0100

    Expand pattern match position tests.
     - Adds tests for unapplySeq and unapply: Boolean.
       Both seem to be well positioned after the previous
       changes.

--- test/files/run/t6288.scala
+++ test/files/run/t6288.scala
@@ -6,14 +6,30 @@ object Test extends DirectTest {
   override def extraSettings: String = "-usejavacp -Xprint:patmat -Xprint-pos -d " + testOutput.path
 
   override def code =
-  """
-  |object Case3 {
-  |  def unapply(z: Any): Option[Int] = Some(-1)
-  |
-  |  "" match {
-  |    case Case3(nr) => ()
-  |  }
-  |}""".stripMargin.trim
+    """
+      |object Case3 {
+      |  def unapply(z: Any): Option[Int] = Some(-1)
+      |
+      |  "" match {
+      |    case Case3(nr) => ()
+      |  }
+      |}
+      |object Case4 {
+      |  def unapplySeq(z: Any): Option[List[Int]] = None
+      |
+      |  "" match {
+      |    case Case4(nr) => ()
+      |  }
+      |}
+      |object Case5 {
+      |  def unapply(z: Any): Boolean = true
+      |
+      |  "" match {
+      |    case Case4() => ()
+      |  }
+      |}
+      |
+      |""".stripMargin.trim
 
   override def show(): Unit = {
     // Now: [84][84]Case3.unapply([84]x1);
