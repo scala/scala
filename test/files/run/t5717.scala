4afcfac232376f25df02c471a02089e2e2a50b04
Paul Phillips <paulp@improving.org>
2012-12-10 16:13:26 -0800

    Test case for SI-5717.
    Thanks to reviewer for illuminating that this test was
    closer at hand than it had appeared.

---
+++ test/files/run/t5717.scala
@@ -0,0 +1,16 @@
+import scala.tools.partest._
+import java.io.File
+
+object Test extends StoreReporterDirectTest {
+  def code = ???
+
+  def compileCode(code: String) = {
+    val classpath = List(sys.props("partest.lib"), testOutput.path) mkString sys.props("path.separator")
+    compileString(newCompiler("-cp", classpath, "-d", testOutput.path))(code)
+  }
+  def show(): Unit = {
+    // Don't crash when we find a file 'a' where package 'a' should go.
+    scala.reflect.io.File(testOutput.path + "/a").writeAll("a")
+    compileCode("package a { class B }")
+  }
+}
