9f5a021aa718a4c87b87610ef475982760140767
Eugene Burmako <xeno.by@gmail.com>
2012-12-22 17:41:33 +0100

    renames c.fresh to c.freshName

--- src/reflect/scala/reflect/macros/Names.scala
+++ src/reflect/scala/reflect/macros/Names.scala
@@ -11,13 +11,27 @@ trait Names {
   self: Context =>
 
   /** Creates a unique string. */
+  @deprecated("Use freshName instead", "2.11.0")
   def fresh(): String
 
   /** Creates a unique string having a given prefix. */
+  @deprecated("Use freshName instead", "2.11.0")
   def fresh(name: String): String
 
   /** Creates a unique name having a given name as a prefix and
    *  having the same flavor (term name or type name) as the given name.
    */
+  @deprecated("Use freshName instead", "2.11.0")
   def fresh[NameType <: Name](name: NameType): NameType
+
+  /** Creates a unique string. */
+  def freshName(): String
+
+  /** Creates a unique string having a given prefix. */
+  def freshName(name: String): String
+
+  /** Creates a unique name having a given name as a prefix and
+   *  having the same flavor (term name or type name) as the given name.
+   */
+  def freshName[NameType <: Name](name: NameType): NameType
 }
