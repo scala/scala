557caa3d757f236c835482f90c199801b4f073e3
Jason Zaugg <jzaugg@gmail.com>
2013-01-08 14:01:21 +0100

    SI-6641 Deprecate SwingWorker
    See discussion: https://github.com/scala/scala/pull/1862

--- src/swing/scala/swing/SwingActor.scala
+++ src/swing/scala/swing/SwingActor.scala
@@ -10,26 +10,6 @@
 
 package scala.swing
 
-import scala.actors._
-
 // Dummy to keep ant from recompiling on every run.
+@deprecated("Will be removed in 2.11.0", "2.10.1")
 trait SwingActor { }
-
-/*object SwingActor {
-  /**
-   * Similar to Actor.actor, but creates an instance of a SwingActor.
-   */
-  def apply(body: => Unit): Actor =
-    new SwingActor { def act() = body }.start()
-}
-
-/**
- * An actor that runs on the Swing event dispatching thread (EDT).
- */
-abstract class SwingActor extends Actor {
-  override val scheduler = new SchedulerAdapter {
-    def execute(op: =>Unit) = Swing onEDT op
-    def onTerminate(a: Actor)(op: => Unit) {}
-    def terminated(a: Actor) {}
-  }
-}*/
