ed6bcf1826fb0b3d3ac9c66f67e5069bbd217eb8
Jason Zaugg <jzaugg@gmail.com>
2013-01-28 22:29:48 +0100

    Stop using Predef.error
    We'd like to remove this from 2.11.0.
    
    This patch should be backported to 0.12.3.
    
    
    Rewritten from sbt/zinc@f78d3db6317a49c40116bd706af9b543dbd1b917

--- src/main/scala/xsbt/Command.scala
+++ src/main/scala/xsbt/Command.scala
@@ -8,16 +8,16 @@ package xsbt
 object Command
 {
 	/**
-		* Construct a CompilerCommand using reflection, to be compatible with Scalac before and after
-		* <a href="https://lampsvn.epfl.ch/trac/scala/changeset/21274">r21274</a>
-		*/
+	 * Construct a CompilerCommand using reflection, to be compatible with Scalac before and after
+	 * <a href="https://lampsvn.epfl.ch/trac/scala/changeset/21274">r21274</a>
+	 */
 	def apply(arguments: List[String], settings: Settings): CompilerCommand = {
 		def constr(params: Class[_]*) = classOf[CompilerCommand].getConstructor(params: _*)
 		try {
 			constr(classOf[List[_]], classOf[Settings]).newInstance(arguments, settings)
 		} catch {
 		case e: NoSuchMethodException =>
-			constr(classOf[List[_]], classOf[Settings], classOf[Function1[_, _]], classOf[Boolean]).newInstance(arguments, settings, error _, false.asInstanceOf[AnyRef])
+			constr(classOf[List[_]], classOf[Settings], classOf[Function1[_, _]], classOf[Boolean]).newInstance(arguments, settings, (s: String) => throw new RuntimeException(s), false.asInstanceOf[AnyRef])
 		}
 	}
 	
