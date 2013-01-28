4c34280e5bb3e48db35d97d890e4f5f1c5fb3a26
Jason Zaugg <jzaugg@gmail.com>
2013-01-28 09:26:31 +0100

    Add a test case from the comments of SI-6666.
    This one lands in the new implementation restriction
    which beats the VerifyError.

---
+++ test/files/neg/t6666b.scala
@@ -0,0 +1,17 @@
+import scala.collection.immutable.TreeMap
+import scala.math.Ordering
+
+class Test[K](param:TreeMap[K,Int]){
+  def this() = this({
+    implicit object TreeOrd extends Ordering[K](){
+      def compare(a: K, b: K) = {
+        -1
+      }
+    }
+    new TreeMap[K, Int]()
+  })
+}
+
+object Test extends App {
+  new Test()
+}
