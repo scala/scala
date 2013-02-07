614bc724f8b12523bc2713464ea175de782ae92b
Eugene Burmako <xeno.by@gmail.com>
2013-02-07 19:16:44 +0100

    accommodates pull request feedback
    https://github.com/scala/scala/pull/2074

---
+++ test/files/run/t5744/Macros_Impls_1.scala
@@ -0,0 +1,22 @@
+import scala.language.experimental.macros
+import scala.reflect.macros.Context
+
+object Macros {
+  def foo[U: Numeric](x: U) = macro foo_impl[U]
+  def bar[U: Numeric : Equiv, Y <% String](x: U)(implicit s: String) = macro bar_impl[U, Y]
+
+  def foo_impl[U](c: Context)(x: c.Expr[U])(numeric: c.Expr[Numeric[U]]) = {
+    import c.universe._
+    val plusOne = Apply(Select(numeric.tree, newTermName("plus")), List(x.tree, Literal(Constant(1))))
+    val body = Apply(Select(Ident(definitions.PredefModule), newTermName("println")), List(plusOne))
+    c.Expr[Unit](body)
+  }
+
+  def bar_impl[U, Y](c: Context)(x: c.Expr[U])(numeric: c.Expr[Numeric[U]], equiv: c.Expr[Equiv[U]], viewAsString: c.Expr[Y => String], s: c.Expr[String]) = {
+    import c.universe._
+    val plusOne = Apply(Select(numeric.tree, newTermName("plus")), List(x.tree, Literal(Constant(1))))
+    val plusLen = Apply(Select(numeric.tree, newTermName("plus")), List(plusOne, Select(s.tree, newTermName("length"))))
+    val body = Apply(Select(Ident(definitions.PredefModule), newTermName("println")), List(plusLen))
+    c.Expr[Unit](body)
+  }
+}
