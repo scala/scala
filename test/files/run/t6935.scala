c049d66c54c4f255bc0de70b2fd4ec559df7ebcd
Vinicius Miana <vinicius@miana.com.br>
2013-02-08 19:00:21 -0200

    SI-6935 Added readResolve in BoxedUnit When deserializing Unit, it would return an instance of Object, but not a Scala Unit. By adding readResolve, the deserialization of Unit will work.

---
+++ test/files/run/t6935.scala
@@ -0,0 +1,14 @@
+object Test {
+
+  def main(args: Array[String]): Unit = {
+		  import java.io._
+		  val bytes = new ByteArrayOutputStream()
+		  val out = new ObjectOutputStream(bytes)
+		  out.writeObject(())
+		  out.close()
+		  val buf = bytes.toByteArray
+		  val in = new ObjectInputStream(new ByteArrayInputStream(buf))
+		  val unit = in.readObject()
+		  assert(unit == ())
+  }
+}
