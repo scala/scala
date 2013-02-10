376ed06307edbfd0402d5bfc5dcea30f41222156
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2013-02-10 17:09:32 +0400

    SI-7109: test interactive documentation for more syntactic categories.

--- test/files/presentation/doc/doc.scala
+++ test/files/presentation/doc/doc.scala
@@ -4,7 +4,6 @@ import scala.tools.nsc.doc.base.comment._
 import scala.tools.nsc.interactive._
 import scala.tools.nsc.interactive.tests._
 import scala.tools.nsc.util._
-import scala.tools.nsc.io._
 
 object Test extends InteractiveTest {
   override val settings: doc.Settings = docSettings
@@ -13,77 +12,96 @@ object Test extends InteractiveTest {
     "@example  `\"abb\".permutations = Iterator(abb, bab, bba)`",
     "@version 1.0, 09/07/2012",
     "@since 2.10",
-    "@todo this method is unsafe",
+    "@todo this is unsafe!",
     "@note Don't inherit!",
-    "@see some other method"
+    "@see something else"
   )
 
-  val comment = "This is a test comment."
-  val caret = "<caret>"
+  val names = Seq("Class", "Def", "Val", "Var", "AbstracType", "TypeAlias", "Trait", "InnerClass")
+  val bareText =
+    """abstract class %s {
+    |  def %s = ""
+    |  val %s = ""
+    |  var %s: String = _
+    |  type %s
+    |  type %s = String
+    |  class %s
+    |}
+    |trait %s""".stripMargin.format(names: _*)
 
-  def text(nTags: Int) = 
-    """|/** %s
-       |
-       | *  %s */
-       |trait Commented {}
-       |class User(c: %sCommented)""".stripMargin.format(comment, tags take nTags mkString "\n", caret)
+  def docComment(nTags: Int) = "/**\n%s*/".format(tags.take(nTags).mkString("\n"))
 
-  override lazy val compiler = {
-    new {
+  def text(name: String, nTags: Int) = {
+    val nameIndex = bareText.indexOf(name)
+    val (pre, post) = bareText.splitAt(nameIndex)
+    val crIndex = pre.lastIndexOf("\n")
+    val (prepre, prepost) = pre.splitAt(crIndex)
+    prepre + docComment(nTags) + prepost + post
+  }
+
+  lazy override val compiler = new {
       override val settings = {
-        prepareSettings(Test.this.settings)
-        Test.this.settings
+        val s = Test.this.settings
+        prepareSettings(s)
+        s
       }
-    } with Global(settings, compilerReporter) with MemberLookupBase with CommentFactoryBase {
-      val global: this.type = this
-      def chooseLink(links: List[LinkTo]): LinkTo = links.head
-      def internalLink(sym: Symbol, site: Symbol) = None
-      def toString(link: LinkTo) = link.toString
+    } with Global(settings, compilerReporter) with CommentFactoryBase with MemberLookupBase {
+   def chooseLink(links: List[LinkTo]): LinkTo = links.head
+   def toString(link: LinkTo) = link.toString
+   def internalLink(sym: Symbol, site: Symbol) = None
+   override val global: this.type = this
 
-      def getComment(sym: Symbol, source: SourceFile) = {
-        val docResponse = new Response[(String, String, Position)]
-        askDocComment(sym, sym.owner, source, docResponse)
-        docResponse.get.left.toOption flatMap {
-          case (expanded, raw, pos) =>
-            if (expanded.isEmpty)
-              None
-            else
-              Some(ask { () => parseAtSymbol(expanded, raw, pos, Some(sym.owner)) })
-        }
+    def getComment(sym: Symbol, source: SourceFile): Option[Comment] = {
+      val docResponse = new Response[(String, String, Position)]
+      askDocComment(sym, sym.owner, source, docResponse)
+      docResponse.get.left.toOption flatMap {
+        case (expanded, raw, pos) =>
+          if (expanded.isEmpty)
+            None
+          else
+            Some(ask { () => parseAtSymbol(expanded, raw, pos, Some(sym.owner)) })
       }
     }
   }
 
   override def runDefaultTests() {
-    for (i <- 1 to tags.length) {
-      val markedText = text(i)
-      val idx = markedText.indexOf(caret)
-      val fileText = markedText.substring(0, idx) + markedText.substring(idx + caret.length)
+    import compiler._
+    val clazz = compiler.ask { () => definitions.EmptyPackage.info.decl(newTypeName(names.head)) }
+    for (name <- names;
+         i <- 1 to tags.length) {
+      val newText = text(name, i)
       val source = sourceFiles(0) 
-      val batch = new BatchSourceFile(source.file, fileText.toCharArray)
+      val batch = new BatchSourceFile(source.file, newText.toCharArray)
       val reloadResponse = new Response[Unit]
       compiler.askReload(List(batch), reloadResponse)
       reloadResponse.get.left.toOption match {
         case None =>
-          reporter.println("Couldn't reload")
+          println("Couldn't reload")
         case Some(_) =>
-          val treeResponse = new compiler.Response[compiler.Tree]
-          val pos = compiler.rangePos(batch, idx, idx, idx)
-          compiler.askTypeAt(pos, treeResponse)
-          treeResponse.get.left.toOption match {
-            case Some(tree) =>
-              val sym = tree.tpe.typeSymbol
-              compiler.getComment(sym, batch) match {
-                case None => println("Got no doc comment")
+          val parseResponse = new Response[Tree]
+          askParsedEntered(batch, true, parseResponse)
+          parseResponse.get.left.toOption match {
+            case None =>
+              println("Couldn't parse")
+            case Some(_) =>
+              val sym = compiler.ask { () =>
+                val toplevel = definitions.EmptyPackage.info.decl(newTypeName(name))
+                if (toplevel eq NoSymbol) {
+                  val term = clazz.info.decl(newTermName(name))
+                  if (term eq NoSymbol) clazz.info.decl(newTypeName(name)) else
+                    if (term.isAccessor) term.accessed else term
+                } else toplevel
+              }
+
+              getComment(sym, batch) match {
+                case None => println(s"Got no doc comment for $name")
                 case Some(comment) =>
                   import comment._
-                  val tags: List[(String, Iterable[Body])] =
-                    List(("@example", example), ("@version", version), ("@since", since.toList), ("@todo", todo), ("@note",  note), ("@see", see))
-                  val str = ("body:" + body + "\n") + 
-                    tags.map{ case (name, bodies) => name + ":" + bodies.mkString("\n") }.mkString("\n")
-                  println(str)
+                  def cnt(bodies: Iterable[Body]) = bodies.size
+                  val actual = cnt(example) + cnt(version) + cnt(since) + cnt(todo) + cnt(note) + cnt(see)
+                  if (actual != i)
+                    println(s"Got docComment with $actual tags instead of $i, file text:\n$newText")
               }
-            case None => println("Couldn't find a typedTree")
           }
       }
     }
