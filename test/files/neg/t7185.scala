387fbf459efe5bb89f3d7a76c819f72b8f8fd58f
Jason Zaugg <jzaugg@gmail.com>
2013-02-26 13:20:03 +0100

    SI-7185 Avoid NPE in TreeInfo.isExprSafeToInline
    We got there typechecking code with a redundant
    layer of Block.
    
    We can't express that in source code, so we test
    this with manual tree construction and with XML
    literals, which as reported produce such trees.

---
+++ test/files/neg/t7185.scala
@@ -0,0 +1,3 @@
+object Test {
+  <e></e>()
+}
---
+++ test/files/run/t7185.scala
@@ -0,0 +1,12 @@
+import scala.tools.partest.ReplTest
+
+object Test extends ReplTest {
+  override def code = """
+import scala.tools.reflect.ToolBox
+import scala.reflect.runtime.universe._
+object O { def apply() = 0 }
+val ORef = reify { O }.tree
+val tree = Apply(Block(Nil, Block(Nil, ORef)), Nil)
+{val tb = reflect.runtime.currentMirror.mkToolBox(); tb.typeCheck(tree): Any}
+"""
+}
