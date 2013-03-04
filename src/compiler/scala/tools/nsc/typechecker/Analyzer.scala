b0401b003954479afd30c6816e63dd4c85770486
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2013-03-04 19:10:49 +0400

    Since the problem in SI-6758 is fixed, it's ok to move checking for unused imports to Analyzer. This allows the check to be used in the IDE.

--- src/compiler/scala/tools/nsc/typechecker/Analyzer.scala
+++ src/compiler/scala/tools/nsc/typechecker/Analyzer.scala
@@ -95,12 +95,16 @@ trait Analyzer extends AnyRef
       }
       def apply(unit: CompilationUnit) {
         try {
+          if (settings.lint.value)
+            clearUnusedImports()
           val typer = newTyper(rootContext(unit))
           unit.body = typer.typed(unit.body)
           if (global.settings.Yrangepos.value && !global.reporter.hasErrors) global.validatePositions(unit.body)
           for (workItem <- unit.toCheck) workItem()
-          if (settings.lint.value)
+          if (settings.lint.value) {
             typer checkUnused unit
+            warnUnusedImports(unit)
+          }
         }
         finally {
           unit.toCheck.clear()
