19eac843d228a240a6bc6f9fb0cea5634aacb7bf
Raphael Jolly <rjolly@users.sourceforge.net>
2013-03-05 19:10:07 +0100

    SI-874 JSR-223 compliance for the interpreter
    Part 2 : jarlister (but not used in build yet)

---
+++ src/compiler/scala/tools/ant/JarLister.scala
@@ -0,0 +1,58 @@
+/*                     __                                               *\
+**     ________ ___   / /  ___     Scala Ant Tasks                      **
+**    / __/ __// _ | / /  / _ |    (c) 2005-2013, LAMP/EPFL             **
+**  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
+** /____/\___/_/ |_/____/_/ | |                                         **
+**                          |/                                          **
+\*                                                                      */
+
+package scala.tools.ant
+
+import java.io.File
+
+/** An Ant task that lists a jar's classes into its manifest.
+ *
+ *  This task can take the following parameters as attributes:
+ *  - `file` (mandatory),
+ *  - `output`.
+ *
+ * @author  Raphael Jolly
+ * @version 1.0
+ */
+class JarLister extends ScalaMatchingTask {
+
+/*============================================================================*\
+**                             Ant user-properties                            **
+\*============================================================================*/
+
+  /** The path to the jar file. */
+  private var file: Option[File] = None
+
+  /** The path to the output file. */
+  private var output: Option[File] = None
+
+/*============================================================================*\
+**                             Properties setters                             **
+\*============================================================================*/
+
+  /** Sets the file attribute. */
+  def setFile(input: File) =
+    file = Some(input)
+
+  /** Sets the output attribute. */
+  def setOutput(input: File) =
+    output = Some(input)
+
+/*============================================================================*\
+**                           The big execute method                           **
+\*============================================================================*/
+
+  /** Performs the jar listing. */
+  override def execute() = {
+    // Tests if all mandatory attributes are set and valid.
+    if (file.isEmpty) buildError("Attribute 'file' is not set.")
+
+    scala.tools.nsc.JarLister.process(file.get.getPath, if (output.isEmpty) "" else output.get.getPath)
+  }
+
+}
