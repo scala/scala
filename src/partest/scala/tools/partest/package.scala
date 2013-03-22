c6ce61796f66770ca1b4d63fc0a569f064436433
Som Snytt <som.snytt@gmail.com>
2013-03-22 14:39:19 -0700

    SI-6289 Partest in technicolor and showing javac errors
    Paulptest includes color and simplified test scarfing.
    
    Scalap tests are moved to the conventional name and location.
    
    Testicolor missed out on Josh Suereth's tweak to sort the files in
    a compilation round.  Restore sortiness to test sources.
    
    Testicolor is due to one of Paul's branches on a timeline
    that apparently did not include the destruction of planet
    Earth and its colonies by the Xindi.
    
    Thanks also to Szabolcs Berecz for his merge effort.  Merging
    is thankless work, but not as thankless as merging in a
    timeline that actually does terminate in the destruction of
    your home world and Enterprise.
    
    Archer had a supremely difficult choice: rescue humanity or
    live out his retirement with T'Pol waiting on him hand and
    foot? I'm sure I don't know how I'd choose.

--- src/partest/scala/tools/partest/package.scala
+++ src/partest/scala/tools/partest/package.scala
@@ -4,30 +4,101 @@
 
 package scala.tools
 
-import java.io.{ FileNotFoundException, File => JFile }
-import nsc.io.{ Path, Directory, File => SFile }
-import scala.tools.util.PathResolver
-import nsc.Properties.{ propOrElse, propOrNone, propOrEmpty }
 import scala.sys.process.javaVmArguments
 import java.util.concurrent.Callable
-
-package partest {
-  class TestState { }
-  object TestState {
-    val Ok      = new TestState
-    val Fail    = new TestState
-    val Timeout = new TestState
-  }
-}
+import scala.tools.partest.nest.NestUI
+import scala.tools.nsc.util.{ ScalaClassLoader, Exceptional }
 
 package object partest {
-  import nest.NestUI
+  type File         = java.io.File
+  type SFile        = scala.reflect.io.File
+  type Directory    = scala.reflect.io.Directory
+  type Path         = scala.reflect.io.Path
+  type PathResolver = scala.tools.util.PathResolver
+  type ClassPath[T] = scala.tools.nsc.util.ClassPath[T]
+  type StringWriter = java.io.StringWriter
 
-  implicit private[partest] def temporaryPath2File(x: Path): JFile = x.jfile
-  implicit private[partest] def temporaryFile2Path(x: JFile): Path = Path(x)
+  val SFile        = scala.reflect.io.File
+  val Directory    = scala.reflect.io.Directory
+  val Path         = scala.reflect.io.Path
+  val PathResolver = scala.tools.util.PathResolver
+  val ClassPath    = scala.tools.nsc.util.ClassPath
+
+  val space                       = "\u0020"
+  val EOL                         = scala.compat.Platform.EOL
+  def onull(s: String)            = if (s == null) "" else s
+  def oempty(xs: String*)         = xs filterNot (x => x == null || x == "")
+  def ojoin(xs: String*): String  = oempty(xs: _*) mkString space
+  def nljoin(xs: String*): String = oempty(xs: _*) mkString EOL
+
+  def setUncaughtHandler() = {
+    Thread.setDefaultUncaughtExceptionHandler(
+      new Thread.UncaughtExceptionHandler {
+        def uncaughtException(thread: Thread, t: Throwable) {
+          val t1 = Exceptional unwrap t
+          System.err.println(s"Uncaught exception on thread $thread: $t1")
+          t1.printStackTrace()
+        }
+      }
+    )
+  }
+
+  /** Sources have a numerical group, specified by name_7 and so on. */
+  private val GroupPattern = """.*_(\d+)""".r
+
+  implicit class FileOps(val f: File) {
+    private def sf = SFile(f)
+
+    def testIdent = {
+      f.toString split """[/\\]+""" takeRight 2 mkString "/" // e.g. pos/t1234
+    }
+
+    def mapInPlace(mapFn: String => String): Unit =
+      writeAll(fileLines.map(x => mapFn(x) + "\n"): _*)
+
+    def appendAll(strings: String*): Unit = sf.appendAll(strings: _*)
+    def writeAll(strings: String*): Unit = sf.writeAll(strings: _*)
+    def absolutePathSegments: List[String] = f.getAbsolutePath split """[/\\]+""" toList
+
+    def isJava        = f.isFile && (sf hasExtension "java")
+    def isScala       = f.isFile && (sf hasExtension "scala")
+    def isJavaOrScala = isJava || isScala
+
+    def extension = sf.extension
+    def hasExtension(ext: String) = sf hasExtension ext
+    def changeExtension(ext: String): File = (sf changeExtension ext).jfile
+
+    /** The group number for this source file, or -1 for no group. */
+    def group: Int =
+      sf.stripExtension match {
+        case GroupPattern(g) if g.toInt >= 0 => g.toInt
+        case _                               => -1
+      }
+
+    def fileContents: String    = try sf.slurp() catch { case _: java.io.FileNotFoundException => "" }
+    def fileLines: List[String] = augmentString(fileContents).lines.toList
+  }
+
+  implicit class PathOps(p: Path) extends FileOps(p.jfile) { }
+
+  implicit class Copier(val f: SFile) extends AnyVal {
+    def copyTo(dest: Path): Unit = dest.toFile writeAll f.slurp(scala.io.Codec.UTF8)
+  }
+
+  implicit def temporaryPath2File(x: Path): File = x.jfile
+  implicit def stringPathToJavaFile(path: String): File = new File(path)
 
   implicit lazy val postfixOps = scala.language.postfixOps
   implicit lazy val implicitConversions = scala.language.implicitConversions
+
+  def fileSeparator = java.io.File.separator
+  def pathSeparator = java.io.File.pathSeparator
+
+  def pathToTestIdent(path: Path) = path.jfile.testIdent
+
+  def canonicalizeSlashes(line: String) = line.replaceAll("""[/\\]+""", "/")
+
+  def words(s: String): List[String] = (s.trim split "\\s+").toList
 
   def timed[T](body: => T): (T, Long) = {
     val t1 = System.currentTimeMillis
@@ -39,15 +110,40 @@ package object partest {
 
   def callable[T](body: => T): Callable[T] = new Callable[T] { override def call() = body }
 
-  def file2String(f: JFile) =
-    try SFile(f).slurp(scala.io.Codec.UTF8)
-    catch { case _: FileNotFoundException => "" }
+  def file2String(f: File): String = f.fileContents
 
   def basename(name: String): String = Path(name).stripExtension
 
-  def resultsToStatistics(results: Iterable[(_, TestState)]): (Int, Int) = {
-    val (files, failures) = results map (_._2 == TestState.Ok) partition (_ == true)
-    (files.size, failures.size)
+  /** In order to allow for spaces in flags/options, this
+   *  parses .flags, .javaopts, javacopts etc files as follows:
+   *  If it is exactly one line, it is split (naively) on spaces.
+   *  If it contains more than one line, each line is its own
+   *  token, spaces and all.
+   */
+  def readOptionsFile(file: File): List[String] = {
+    file.fileLines match {
+      case x :: Nil   => words(x)
+      case xs         => xs map (_.trim)
+    }
+  }
+
+  def findProgram(name: String): Option[File] = {
+    val pathDirs = sys.env("PATH") match {
+      case null => List("/usr/local/bin", "/usr/bin", "/bin")
+      case path => path split "[:;]" filterNot (_ == "") toList
+    }
+    pathDirs.iterator map (d => new File(d, name)) find (_.canExecute)
+  }
+
+  def now = (new java.util.Date).toString
+  def elapsedString(millis: Long): String = {
+    val elapsedSecs = millis/1000
+    val elapsedMins = elapsedSecs/60
+    val elapsedHrs  = elapsedMins/60
+    val dispMins    = elapsedMins - elapsedHrs  * 60
+    val dispSecs    = elapsedSecs - elapsedMins * 60
+
+    "%02d:%02d:%02d".format(elapsedHrs, dispMins, dispSecs)
   }
 
   def vmArgString = javaVmArguments.mkString(
@@ -62,12 +158,9 @@ package object partest {
   }
 
   def showAllJVMInfo() {
-    NestUI.verbose(vmArgString)
-    NestUI.verbose(allPropertiesString)
+    vlog(vmArgString)
+    vlog(allPropertiesString)
   }
-
-  def isPartestDebug: Boolean =
-    propOrEmpty("partest.debug") == "true"
 
   import scala.language.experimental.macros
 
@@ -117,4 +210,10 @@ package object partest {
             a.tree))))),
       a.tree))
   }
+
+  def isPartestTerse   = NestUI.isTerse
+  def isPartestDebug   = NestUI.isDebug
+  def isPartestVerbose = NestUI.isVerbose
+
+  def vlog(msg: => String) = if (isPartestVerbose) System.err.println(msg)
 }
