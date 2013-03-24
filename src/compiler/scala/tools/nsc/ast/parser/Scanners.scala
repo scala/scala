36da62238be6efd92f2e59d9df6a4b29c4c7dae7
Simon Ochsenreither <simon@ochsenreither.de>
2013-03-24 03:19:12 +0100

    SI-7292 Deprecate octal escape literals

--- src/compiler/scala/tools/nsc/ast/parser/Scanners.scala
+++ src/compiler/scala/tools/nsc/ast/parser/Scanners.scala
@@ -789,6 +789,7 @@ trait Scanners extends ScannersCommon {
       if (ch == '\\') {
         nextChar()
         if ('0' <= ch && ch <= '7') {
+          val start = charOffset - 2
           val leadch: Char = ch
           var oct: Int = digit2int(ch, 8)
           nextChar()
@@ -800,6 +801,12 @@ trait Scanners extends ScannersCommon {
               nextChar()
             }
           }
+          val alt = if (oct == LF) "\\n" else "\\u%04x" format oct
+          def msg(what: String) = s"Octal escape literals are $what, use $alt instead."
+          if (settings.future)
+            syntaxError(start, msg("unsupported"))
+          else
+            deprecationWarning(start, msg("deprecated"))
           putChar(oct.toChar)
         } else {
           ch match {
---
+++ test/files/neg/t7292-removal.scala
@@ -0,0 +1,5 @@
+object OctalEscapes {
+  val chr1 = '\0'
+  val str1 = "abc\123456"
+  val lf = '\012'
+}
