f657c375706773ca7b6d2f4f554e7414b35c67b0
Paul Phillips <paulp@improving.org>
2013-04-02 10:21:32 -0700

    Reduce duplication in JavaMirrors.
    Applying some of the abstractions from the prior commits,
    though I would like to emphasize that such infrastructure was
    not a prerequisite for de-duplicating this file.

--- src/reflect/scala/reflect/internal/PrivateWithin.scala
+++ src/reflect/scala/reflect/internal/PrivateWithin.scala
@@ -1,27 +1,11 @@
 package scala.reflect
 package internal
 
-import ClassfileConstants._
 import java.lang.{ Class => jClass }
 import java.lang.reflect.{ Member => jMember }
 
 trait PrivateWithin {
   self: SymbolTable =>
-
-  def importPrivateWithinFromJavaFlags(sym: Symbol, jflags: Int): Symbol = {
-    if ((jflags & (JAVA_ACC_PRIVATE | JAVA_ACC_PROTECTED | JAVA_ACC_PUBLIC)) == 0)
-      // See ticket #1687 for an example of when topLevelClass is NoSymbol: it
-      // apparently occurs when processing v45.3 bytecode.
-      if (sym.enclosingTopLevelClass != NoSymbol)
-        sym.privateWithin = sym.enclosingTopLevelClass.owner
-
-    // protected in java means package protected. #3946
-    if ((jflags & JAVA_ACC_PROTECTED) != 0)
-      if (sym.enclosingTopLevelClass != NoSymbol)
-        sym.privateWithin = sym.enclosingTopLevelClass.owner
-
-    sym
-  }
 
   def propagatePackageBoundary(c: jClass[_], syms: Symbol*): Unit =
     propagatePackageBoundary(JavaAccFlags(c), syms: _*)
