ac62f0ca53dc0e4fc4e65a09dce608d91c2dc357
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-04-03 13:05:12 +0200

    for null outer pointer, NPE via throw null
    As per the JVM spec, the athrow instruction throws a NullPointerException upon finding null on stack top.
    This commit takes advantage of that feature, to emit more compact code.
    
    The constructor of an inner class that receives an outer value first checks that value for nullness, depending on which NPE may be thrown.
    The code now emitted to achieve the above looks like:
    
        0: aload_1
        1: ifnonnull     6
        4: aconst_null
        5: athrow
        6: ...

--- src/reflect/scala/reflect/internal/Definitions.scala
+++ src/reflect/scala/reflect/internal/Definitions.scala
@@ -322,7 +322,6 @@ trait Definitions extends api.StandardDefinitions {
     lazy val ThrowableClass                 = getClassByName(sn.Throwable)
     lazy val UninitializedErrorClass        = requiredClass[UninitializedFieldError]
 
-    lazy val NPEConstructor                = getMemberMethod(NullPointerExceptionClass, nme.CONSTRUCTOR) suchThat (_.paramss.flatten.isEmpty)
     lazy val UninitializedFieldConstructor = UninitializedErrorClass.primaryConstructor
 
     // fundamental reference classes
