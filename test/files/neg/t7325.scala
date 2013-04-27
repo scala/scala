cb1a427d8ad4a92690610bfa7d6c79f48d440864
Eugene Burmako <xeno.by@gmail.com>
2013-04-27 15:26:34 +0200

    SI-7325 cleans up corner cases of percent handling in StringContext.f
    See comments in code for the exhaustive list of the cases handled.
    Also note that treatment of non-formatting percents has been changed.
    Embedding literal %'s now requires escaping.
    
    Moreover, this commit also features exact error positions for string
    interpolation, something that has been impossible until the fix for
    SI-7271, also included in this patch.

---
+++ test/files/neg/t7325.scala
@@ -0,0 +1,25 @@
+object Test extends App {
+  println(f"%")
+  println(f"%%")
+  println(f"%%%")
+  println(f"%%%%")
+  println(f"%%%%%")
+  println(f"%%%%%%")
+
+  println(f"%%n")
+  println(f"%%%n")
+  println(f"%%%%n")
+  println(f"%%%%%n")
+  println(f"%%%%%%n")
+  println(f"%%%%%%%n")
+
+  println(f"${0}%")
+  println(f"${0}%d")
+  println(f"${0}%%d")
+  println(f"${0}%%%d")
+  println(f"${0}%%%%d")
+  println(f"${0}%%%%%d")
+
+  println(f"${0}%n")
+  println(f"${0}%d%n")
+}
