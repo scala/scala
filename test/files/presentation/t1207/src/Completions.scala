e6af9bcd0e68859a3bb71140342bb76c136714ee
Paul Phillips <paulp@improving.org>
2013-04-30 05:25:10 -0700

    SI-7362, crash in presentation compiler.
    Code by retronym, test by huitseeker, I just move stuff around.

---
+++ test/files/presentation/t1207/src/Completions.scala
@@ -0,0 +1,20 @@
+package other {
+  package bongo { }
+  package lang { }
+  package util {
+    package boogly
+  }
+}
+
+package ticket_1001207 {
+  import other./*!*/
+  import other.u/*!*/
+  import other.uti /*!*/
+  import other.util/*!*/
+  import other.{lang, u/*!*/}
+  import j/*!*/
+
+  class T1207 {
+
+  }
+}
