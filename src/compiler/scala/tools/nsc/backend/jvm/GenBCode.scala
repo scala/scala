c6680704531906ea8c5981e308c1443f716e00d1
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-05-05 21:35:25 +0200

    GenBCode tightens code before serializing classfiles
    Before writing classfiles with "optimization level zero"
    (ie -neo:GenBCode) the very least we want to do is remove
    dead code beforehand, so as to prevent an artifact of
    stack-frames computation from showing up, the artifact described at
      http://asm.ow2.org/doc/developer-guide.html#deadcode
    
    That artifact results from the requirement by the Java 6 split verifier
    that a stack map frame be available for each basic block,
    even unreachable ones.
    
    Additionally, for dead-code elimination be effective,
    each multi-jump chain should be collapsed to target its
    final destination via a single jump. Details in:
      - scala.tools.asm.optimiz.JumpChainsCollapser
    
    Just removing dead code might leave stale entries
    (ie pointing to LabelNodes removed during dead-code elimination)
    in LocalVariableTable as well as in the exception-handlers table,
    thus `EssentialCleanser.cleanseMethod()` also gets rid of those.
    The classes in charge of that are:
      - scala.tools.asm.optimiz.LabelsCleanup
      - scala.tools.asm.optimiz.DanglingExcHandlers

--- src/compiler/scala/tools/nsc/backend/jvm/GenBCode.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenBCode.scala
@@ -44,7 +44,7 @@ import scala.tools.asm
  *  @version 1.0
  *
  */
-abstract class GenBCode extends BCodeSyncAndTry {
+abstract class GenBCode extends BCodeOptIntra {
   import global._
   import definitions._
 
@@ -222,7 +222,7 @@ abstract class GenBCode extends BCodeSyncAndTry {
             return
           }
           else {
-            try   { addToQ3(item) }
+            try   { visit(item) }
             catch {
               case ex: Throwable =>
                 ex.printStackTrace()
@@ -231,6 +231,21 @@ abstract class GenBCode extends BCodeSyncAndTry {
           }
         }
       }
+
+      /*
+       *  Performs optimizations.
+       *  Afterwards, adds the ClassNode(s) to queue-3.
+       */
+      def visit(item: Item2) {
+
+        val cnode   = item.plain
+
+        val essential = new EssentialCleanser(cnode)
+        essential.codeFixupDCE()    // the very least fixups that must be done, even for unoptimized runs.
+
+        addToQ3(item)
+
+      } // end of method visit(Item2)
 
       private def addToQ3(item: Item2) {
 
