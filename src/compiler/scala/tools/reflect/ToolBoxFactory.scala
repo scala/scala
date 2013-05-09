43249001a566c46d6bb3b515045ab477b42a0c77
Eugene Burmako <xeno.by@gmail.com>
2013-05-09 12:55:15 +0200

    SI-7461 c.typeCheck(silent = true) now suppresses ambiguous errors
    Otherwise use cases like the one shown in the attached test (trying to
    typecheck something, which leads to an ambiguous overload error) will
    mysteriously fail compilation.

--- src/compiler/scala/tools/reflect/ToolBoxFactory.scala
+++ src/compiler/scala/tools/reflect/ToolBoxFactory.scala
@@ -165,7 +165,7 @@ abstract class ToolBoxFactory[U <: JavaUniverse](val u: U) { factorySelf =>
         transformDuringTyper(expr, withImplicitViewsDisabled = withImplicitViewsDisabled, withMacrosDisabled = withMacrosDisabled)(
           (currentTyper, expr) => {
             trace("typing (implicit views = %s, macros = %s): ".format(!withImplicitViewsDisabled, !withMacrosDisabled))(showAttributed(expr, true, true, settings.Yshowsymkinds.value))
-            currentTyper.silent(_.typed(expr, pt)) match {
+            currentTyper.silent(_.typed(expr, pt), reportAmbiguousErrors = false) match {
               case analyzer.SilentResultValue(result) =>
                 trace("success: ")(showAttributed(result, true, true, settings.Yshowsymkinds.value))
                 result
