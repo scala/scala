4250ce11d177a9576ebaf337212b30211b8369a8
Simon Ochsenreither <simon@ochsenreither.de>
2013-05-11 16:58:16 +0200

    SI-7469 Remove deprecated elements in Java{Conversions,Converters}

--- test/files/pos/t3688-redux.scala
+++
@@ -1,8 +0,0 @@
-import collection.JavaConverters._
-import java.{ util => ju }
-import scala.collection.{ mutable, immutable }
-
-object Test {
-  def m[P <% AsJava[ju.List[Int]]](l: P) = 1
-  m(List(1))
-}
