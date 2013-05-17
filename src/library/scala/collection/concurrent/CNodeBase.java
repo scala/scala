9db9df753712fb8459b20bbf6ca4dc8663392a33
Simon Ochsenreither <simon@ochsenreither.de>
2013-05-17 14:26:48 +0200

    SI-7484 Indentation and whitespace fixes

--- src/library/scala/collection/concurrent/CNodeBase.java
+++ src/library/scala/collection/concurrent/CNodeBase.java
@@ -8,29 +8,26 @@
 
 package scala.collection.concurrent;
 
-
-
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
-
-
 abstract class CNodeBase<K, V> extends MainNode<K, V> {
-    
+
     @SuppressWarnings("rawtypes")
-    public static final AtomicIntegerFieldUpdater<CNodeBase> updater = AtomicIntegerFieldUpdater.newUpdater(CNodeBase.class, "csize");
-    
+    public static final AtomicIntegerFieldUpdater<CNodeBase> updater =
+            AtomicIntegerFieldUpdater.newUpdater(CNodeBase.class, "csize");
+
     public volatile int csize = -1;
-    
+
     public boolean CAS_SIZE(int oldval, int nval) {
 	return updater.compareAndSet(this, oldval, nval);
     }
-    
+
     public void WRITE_SIZE(int nval) {
 	updater.set(this, nval);
     }
-    
+
     public int READ_SIZE() {
 	return updater.get(this);
     }
-    
+
 }
--- src/library/scala/collection/concurrent/MainNode.java
+++ src/library/scala/collection/concurrent/MainNode.java
@@ -8,29 +8,26 @@
 
 package scala.collection.concurrent;
 
-
-
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
-
-
 abstract class MainNode<K, V> extends BasicNode {
-    
+
     @SuppressWarnings("rawtypes")
-    public static final AtomicReferenceFieldUpdater<MainNode, MainNode> updater = AtomicReferenceFieldUpdater.newUpdater(MainNode.class, MainNode.class, "prev");
-    
+    public static final AtomicReferenceFieldUpdater<MainNode, MainNode> updater =
+            AtomicReferenceFieldUpdater.newUpdater(MainNode.class, MainNode.class, "prev");
+
     public volatile MainNode<K, V> prev = null;
-    
+
     public abstract int cachedSize(Object ct);
-    
+
     public boolean CAS_PREV(MainNode<K, V> oldval, MainNode<K, V> nval) {
 	return updater.compareAndSet(this, oldval, nval);
     }
-    
+
     public void WRITE_PREV(MainNode<K, V> nval) {
 	updater.set(this, nval);
     }
-    
+
     // do we need this? unclear in the javadocs...
     // apparently not - volatile reads are supposed to be safe
     // irregardless of whether there are concurrent ARFU updates
@@ -38,5 +35,5 @@ abstract class MainNode<K, V> extends BasicNode {
     public MainNode<K, V> READ_PREV() {
 	return updater.get(this);
     }
-    
+
 }
