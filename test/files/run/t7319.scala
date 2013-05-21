d64de5bd1ec6a58e7f7395dc3c9e96613b523d23
Jason Zaugg <jzaugg@gmail.com>
2013-05-21 09:28:42 +0200

    SI-7499 Additional test case for SI-7319
    From a duplicate ticket.

--- test/files/run/t7319.scala
+++ test/files/run/t7319.scala
@@ -9,5 +9,6 @@ implicit def ma0[A](a: A): M[A] = null
 implicit def ma1[A](a: A): M[A] = null
 def convert[F[X <: F[X]]](builder: F[_ <: F[_]]) = 0
 convert(Some[Int](0))
+Range(1,2).toArray: Seq[_]
 0""" // before the fix, this line, and all that followed, re-issued the implicit ambiguity error.
 }
