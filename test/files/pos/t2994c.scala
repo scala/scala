278305a7f60c46515c2133d11f7c561e972ef705
Paul Phillips <paulp@improving.org>
2013-06-03 21:23:30 -0700

    Revert "SI-7517 type constructors too eagerly normalized."
    This reverts commit 14534c693d2eb6acafaf8244c14b5643388fbd67.
    
    It turns out this approach was breaking the working variations
    in the submitted test case even as it was unbreaking the unworking
    one, but I never managed to uncomment them. Fortunately retronym's
    test case was not so lackadaisical.

--- test/files/pos/t2994c.scala
+++
@@ -1,8 +0,0 @@
-object Test {
-  trait Bar[X[_]]
-  trait Qux[S[_] <: Bar[S], T]
-  trait Baz[S[_] <: Bar[S]] {
-    type Apply[T] = Qux[S,T]
-  }
-  trait Foo[/**/V[_] <: Bar[V]/**/] extends Bar[Baz[V]#Apply]
-}
