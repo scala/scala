ee646e9c84290e721c4ee9fe6247d4b95840e871
Eugene Burmako <xeno.by@gmail.com>
2013-06-05 16:51:06 +0200

    fixes a crash on a degenerate macro definition
    Previous version of the MacroImplReference extractor didn't take into
    the account the fact that RefTree.qualifier.symbol can be null (and it can
    be null if RefTree is an Ident, because then qualifier is an EmptyTree).
    
    This led to NPEs for really weird macro defs that refer to local methods
    as their corresponding macro impls. Now I check for this corner case,
    and the stuff now longer crashes.
    
    This was wrong; this is how I fixed it; the world is now a better place.

--- test/files/neg/macro-invalidshape/Macros_Test_2.scala
+++ test/files/neg/macro-invalidshape/Macros_Test_2.scala
@@ -2,6 +2,11 @@ object Macros {
   def foo1(x: Any) = macro 2
   def foo2(x: Any) = macro Impls.foo(null)(null)
   def foo3(x: Any) = macro {2; Impls.foo}
+  {
+    def impl(c: scala.reflect.macros.Context) = c.literalUnit
+    def foo = macro impl
+    foo
+  }
 }
 
 object Test extends App {
