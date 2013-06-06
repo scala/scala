8d74fa024262c1bd2dc7ed64788b95f888396c05
Jason Zaugg <jzaugg@gmail.com>
2013-06-06 14:51:34 +0200

    [backport] SI-7439 Avoid NPE in `isMonomorphicType` with stub symbols.
    `originalInfo` can return null for stub symbols; deal with that
    as we used to before a regression in 016bc3db.
    
    After this change, we can once again delete A_1.class and still compile
    code instantiating B_1. (A_1 is only referred to in a method signature
    of B_1 which is not called from our code.)
    
    	scala> new B_1
    	warning: Class A_1 not found - continuing with a stub.
    	res0: B_1 = B_1@5284b8f9
    
    In practice, this situation arises when someone uses a third
    party class that was compiled against other libraries not avaialable
    on the current compilation classpath.
    
    (cherry picked from commit a95432168204964e4f6c4571e781559c1640f2d8)

---
+++ test/files/run/t7439/B_1.java
@@ -0,0 +1,3 @@
+public class B_1 {
+	public void b(A_1[] a) {}
+}
---
+++ test/files/run/t7439/Test_2.scala
@@ -0,0 +1,31 @@
+import scala.tools.partest._
+import java.io.File
+
+object Test extends StoreReporterDirectTest {
+  def code = ???
+
+  def compileCode(code: String) = {
+    val classpath = List(sys.props("partest.lib"), testOutput.path) mkString sys.props("path.separator")
+    compileString(newCompiler("-cp", classpath, "-d", testOutput.path))(code)
+  }
+
+  def C = """
+    class C {
+      new B_1
+    }
+  """
+
+  def show(): Unit = {
+    //compileCode(C)
+    assert(filteredInfos.isEmpty, filteredInfos)
+
+    // blow away the entire package
+    val a1Class = new File(testOutput.path, "A_1.class")
+    assert(a1Class.exists)
+    assert(a1Class.delete())
+
+    // bad symbolic reference error expected (but no stack trace!)
+    compileCode(C)
+    println(storeReporter.infos.mkString("\n")) // Included a NullPointerException before.
+  }
+}
