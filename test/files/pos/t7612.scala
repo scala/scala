scala/scala

commit a06d31f6a2df021acc18b10ad9cad0f80d7498f2

Keep annotations when computing lubs
Integrates annotationsLub into lub.

Also fixes SubstSymMap when mapping over annotaion trees. I don't
understand what the previous code was supposed to achieve, but it
crashed in some of my examples.

src/reflect/scala/reflect/internal/Types.scala
@@ -4706,23 +4706,13 @@ trait Types extends api.Types { self: SymbolTable =>
           case idx  => Some(to(idx))
         }
 
-        override def transform(tree: Tree) =
-          tree match {
-            case tree@Ident(_) =>
-              termMapsTo(tree.symbol) match {
-                case Some(tosym) =>
-                  if (tosym.info.bounds.hi.typeSymbol isSubClass SingletonClass) {
-                    Ident(tosym.existentialToString)
-                      .setSymbol(tosym)
-                      .setPos(tosym.pos)
-                      .setType(dropSingletonType(tosym.info.bounds.hi))
-                  } else {
-                    giveup()
-                  }
-                case none => super.transform(tree)
-              }
-            case tree => super.transform(tree)
+        override def transform(tree: Tree) = {
+          termMapsTo(tree.symbol) match {
+            case Some(tosym) => tree.symbol = tosym
+            case None => ()
           }
+          super.transform(tree)
+        }
       }
       trans.transform(tree)
     }
@@ -6618,7 +6608,7 @@ trait Types extends api.Types { self: SymbolTable =>
     val ts0 = elimSub0(ts)
     if (ts0.isEmpty || ts0.tail.isEmpty) ts0
     else {
-      val ts1 = ts0 mapConserve (t => elimAnonymousClass(t.underlying))
+      val ts1 = ts0 mapConserve (t => elimAnonymousClass(t.dealiasWiden))
       if (ts1 eq ts0) ts0
       else elimSub(ts1, depth)
     }
@@ -6735,6 +6725,8 @@ trait Types extends api.Types { self: SymbolTable =>
         NullaryMethodType(lub0(matchingRestypes(ts, Nil)))
       case ts @ TypeBounds(_, _) :: rest =>
         TypeBounds(glb(ts map (_.bounds.lo), depth), lub(ts map (_.bounds.hi), depth))
+      case ts @ AnnotatedType(annots, tpe, _) :: rest =>
+        annotationsLub(lub0(ts map (_.withoutAnnotations)), ts)
       case ts =>
         lubResults get (depth, ts) match {
           case Some(lubType) =>

test/files/pos/lubs.scala
@@ -1,0 +1,3 @@
+object Test {
+  List(new { def f = 1; def g = 1}, new { def f = 2}).map(_.f)
+}

commit f1e57899104addacbd68983934c7498ac5de0051

SI-7612 Fix SOE regression in lubs
The enclosed test case (whittled down lovingly from lift-mapper)
triggers the cycle in `elimSub`.

Regressed in a06d31f6a. The change was dicussed
<https://github.com/scala/scala/pull/2022#discussion_r2832997>.

This patch reverts to using `.underlying`.

Below, I've shown the different paths that are taken through
that method with before and after this patch. The key divergence
is when `<refinement>.this.M.dealias` reveals a captured existential
type symbol. Because the enclosed test case has F-bounded generics,
when we later extrapolate that captured `_2` to its upper bound,
we create another call to lub with the same types.

A more detailed look at how that happens:
   <https://gist.github.com/retronym/5874775>

Following .underlying (after)
=============================

    ts0           = List(A, B)
    underlyings   = List(A, B)
    dealiasWidens = List(A, B)

    ts0           = List(Mapper[A], Mapper[B])
    underlyings   = List(Mapper[A], Mapper[B])
    dealiasWidens = List(Mapper[A], Mapper[B])

    ts0           = List(A, B)
    underlyings   = List(A, B)
    dealiasWidens = List(A, B)

    ts0           = List(Mapper[A], Mapper[B])
    underlyings   = List(Mapper[A], Mapper[B])
    dealiasWidens = List(Mapper[A], Mapper[B])

    ts0           = List(A, B)
    underlyings   = List(A, B)
    dealiasWidens = List(A, B)

    ts0           = List(Mapper[A], Mapper[B])
    underlyings   = List(Mapper[A], Mapper[B])
    dealiasWidens = List(Mapper[A], Mapper[B])

    ts0           = List(=> MappedField[A], => this.id.type)
    underlyings   = List(=> MappedField[A], => this.id.type)
    dealiasWidens = List(=> MappedField[A], => this.id.type)

    ts0           = List(MappedField[A], this.id.type)
    underlyings   = List(MappedField[A], IdPK.this.id.type)
    dealiasWidens = List(MappedField[A], IdPK.this.id.type)

    ts0           = List(MappedField[A], IdPK.this.id.type)

src/reflect/scala/reflect/internal/Types.scala
@@ -6696,7 +6696,9 @@ trait Types extends api.Types { self: SymbolTable =>
     val ts0 = elimSub0(ts)
     if (ts0.isEmpty || ts0.tail.isEmpty) ts0
     else {
-      val ts1 = ts0 mapConserve (t => elimAnonymousClass(t.dealiasWiden))
+      // This was changed to `dealiasWiden` after deliberation https://github.com/scala/scala/pull/2022#discussion_r2832997
+      // That triggered a regression SI-7612, so reverting to `.underlying`.
+      val ts1 = ts0 mapConserve (t => elimAnonymousClass(t.underlying))
       if (ts1 eq ts0) ts0
       else elimSub(ts1, depth)
     }

test/files/pos/t7612.scala
@@ -1,0 +1,34 @@
+trait BaseMapper  {
+  type M
+  def primaryKeyField: MappedField[M]
+}
+
+trait Mapper[A] extends BaseMapper {
+  type M = A
+}
+
+trait IdPK {
+  self: BaseMapper =>
+  object id extends MappedField[M]
+}
+
+trait KeyedMapper[A]
+  extends Mapper[A] with BaseMapper
+
+trait MappedField[A]
+
+abstract class A extends KeyedMapper[A] {
+  def primaryKeyField: MappedField[A]
+}
+
+abstract class B extends Mapper[B] with BaseMapper with IdPK {
+  // okay
+  // def primaryKeyField: MappedField[M]
+
+  // SOE
+  def primaryKeyField: id.type
+}
+
+object Test {
+  def foo(a: A, b: B) = if (true) a else b // LUB triggers StackOverflowError
+}