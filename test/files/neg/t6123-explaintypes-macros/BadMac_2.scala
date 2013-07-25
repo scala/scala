b041fdc9cc372cfa9bed17f939747174b8b39542
Som Snytt <som.snytt@gmail.com>
2013-07-24 21:07:54 -0700

    SI-7695 Macro debug output on -explaintypes
    It looks like checkConforms should wait for error before
    using explainTypes, rather than turning on macro debug
    trace if explaintypes is set.
    
    In particular, macro debug output is Console.println
    instead of reporter.echo or similar, so partest can't
    test it.

--- test/files/neg/t6123-explaintypes-macros/BadMac_2.scala
+++ test/files/neg/t6123-explaintypes-macros/BadMac_2.scala
@@ -1,7 +1,8 @@
 import scala.language.experimental.macros
 import scala.reflect.macros.Context
 
-object Macros {
+// explain some macro types to me
+object BadMac {
   def printf(format: String, params: Any*): Unit = macro printf_impl
-  def printf_impl(c: Context)(format: c.Expr[String], params: c.Expr[Any]*): c.Expr[Unit] = ???
+  def printf_impl(c: Context)(format: c.Expr[String], params: c.Expr[String]*): c.Expr[Unit] = ???
 }
