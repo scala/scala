9672a80d08148e9f3223077bf96aaa4ddf17c599
Paul Phillips <paulp@improving.org>
2013-08-15 15:02:18 -0700

    Add checkability condition.
    All parents of an intersection type must be checkable for the
    type to be checkable.

--- src/compiler/scala/tools/nsc/typechecker/Checkable.scala
+++ src/compiler/scala/tools/nsc/typechecker/Checkable.scala
@@ -246,8 +246,8 @@ trait Checkable {
       uncheckedOk(P0) || (P0.widen match {
         case TypeRef(_, NothingClass | NullClass | AnyValClass, _) => false
         case RefinedType(_, decls) if !decls.isEmpty               => false
-        case p                                                     =>
-          new CheckabilityChecker(AnyTpe, p) isCheckable
+        case RefinedType(parents, _)                               => parents forall isCheckable
+        case p                                                     => new CheckabilityChecker(AnyTpe, p) isCheckable
       })
     )
 
@@ -273,6 +273,8 @@ trait Checkable {
         // Matching on types like case _: AnyRef { def bippy: Int } => doesn't work -- yet.
         case RefinedType(_, decls) if !decls.isEmpty =>
           getContext.unit.warning(tree.pos, s"a pattern match on a refinement type is unchecked")
+        case RefinedType(parents, _) =>
+          parents foreach (p => checkCheckable(tree, p, X, inPattern, canRemedy))
         case _ =>
           val checker = new CheckabilityChecker(X, P)
           log(checker.summaryString)
