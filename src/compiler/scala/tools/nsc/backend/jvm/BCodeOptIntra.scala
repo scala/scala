1db21c18e99272386965369fa6c67f4dddf79e66
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-08-27 19:54:45 +0200

    GenBCode tightens code before serializing classfiles
    Before writing classfiles with "optimization level zero"
    (ie -Ybackend:GenBCode) the very least we want to do is remove
    dead code beforehand, so as to prevent an artifact of
    stack-frames computation from showing up, the artifact described at
      http://asm.ow2.org/doc/developer-guide.html#deadcode
    
    That artifact results from the requirement by the Java 6 split verifier
    that a stack map frame be available for each basic block,
    even unreachable ones.
    
    Additionally, for dead-code elimination to be effective,
    each multi-jump chain should be collapsed to target its
    final destination via a single jump. Details in:
      - scala.tools.nsc.backend.bcode.JumpChainsCollapser
    
    Just removing dead code might leave stale entries
    (ie pointing to LabelNodes removed during dead-code elimination)
    in LocalVariableTable as well as in the exception-handlers table,
    thus `EssentialCleanser.cleanseMethod()` also gets rid of those.
    The classes in charge of that are:
      - scala.tools.nsc.backend.bcode.LabelsCleanup
      - scala.tools.nsc.backend.bcode.DanglingExcHandlers

---
+++ src/compiler/scala/tools/nsc/backend/jvm/BCodeOptIntra.scala
@@ -0,0 +1,112 @@
+/* NSC -- new Scala compiler
+ * Copyright 2005-2013 LAMP/EPFL
+ * @author  Martin Odersky
+ */
+
+
+package scala.tools.nsc
+package backend
+package jvm
+
+import scala.tools.asm
+import asm.Opcodes
+import backend.bcode.Util
+import asm.tree._
+
+import scala.collection.{ mutable, immutable }
+
+/*
+ *  Optimize and tidy-up bytecode before it's serialized for good.
+ *  This class focuses on
+ *    - intra-method optimizations,
+ *    - intra-class  optimizations, and
+ *    - utilities for the above and for inter-procedural optimizations as well.
+ *
+ *  @author  Miguel Garcia, http://lamp.epfl.ch/~magarcia/ScalaCompilerCornerReloaded/
+ *  @version 1.0
+ *
+ *  TODO Improving the Precision and Correctness of Exception Analysis in Soot, http://www.sable.mcgill.ca/publications/techreports/#report2003-3
+ *
+ */
+abstract class BCodeOptIntra extends BCodeSyncAndTry {
+
+  import global._
+
+  /*
+   *  All methods in this class can-multi-thread
+   */
+  class EssentialCleanser(cnode: asm.tree.ClassNode) {
+
+    val jumpsCollapser      = new backend.bcode.JumpChainsCollapser()
+    val unreachCodeRemover  = new backend.bcode.UnreachableCode()
+    val labelsCleanup       = new backend.bcode.LabelsCleanup()
+    val danglingExcHandlers = new backend.bcode.DanglingExcHandlers()
+
+    /*
+     *  This method performs a few intra-method optimizations:
+     *    - collapse a multi-jump chain to target its final destination via a single jump
+     *    - remove unreachable code
+     *    - remove those LabelNodes and LineNumbers that aren't in use
+     *
+     *  Some of the above are applied repeatedly until no further reductions occur.
+     *
+     *  Node: what ICode calls reaching-defs is available as asm.tree.analysis.SourceInterpreter, but isn't used here.
+     *
+     */
+    final def cleanseMethod(cName: String, mnode: asm.tree.MethodNode): Boolean = {
+
+      var changed = false
+      var keepGoing = false
+
+      do {
+        keepGoing = false
+
+        jumpsCollapser.transform(mnode)            // collapse a multi-jump chain to target its final destination via a single jump
+        keepGoing |= jumpsCollapser.changed
+
+        unreachCodeRemover.transform(cName, mnode) // remove unreachable code
+        keepGoing |= unreachCodeRemover.changed
+
+        labelsCleanup.transform(mnode)             // remove those LabelNodes and LineNumbers that aren't in use
+        keepGoing |= labelsCleanup.changed
+
+        danglingExcHandlers.transform(mnode)
+        keepGoing |= danglingExcHandlers.changed
+
+        changed |= keepGoing
+
+      } while (keepGoing)
+
+      changed
+
+    }
+
+    /*
+     *  Removes dead code.
+     *
+     *  When writing classfiles with "optimization level zero" (ie -Ybackend:GenBCode)
+     *  the very least we want to do is remove dead code beforehand,
+     *  so as to prevent an artifact of stack-frames computation from showing up,
+     *  the artifact described at http://asm.ow2.org/doc/developer-guide.html#deadcode
+     *  That artifact results from the requirement by the Java 6 split verifier
+     *  that a stack map frame be available for each basic block, even unreachable ones.
+     *
+     *  Just removing dead code might leave stale LocalVariableTable entries
+     *  thus `cleanseMethod()` also gets rid of those.
+     *
+     */
+    final def codeFixupDCE() {
+      val iter = cnode.methods.iterator()
+      while (iter.hasNext) {
+        val mnode = iter.next()
+        if (Util.hasBytecodeInstructions(mnode)) {
+          Util.computeMaxLocalsMaxStack(mnode)
+          cleanseMethod(cnode.name, mnode) // remove unreachable code
+        }
+      }
+    }
+
+  } // end of class EssentialCleanser
+
+} // end of class BCodeOptIntra
+
