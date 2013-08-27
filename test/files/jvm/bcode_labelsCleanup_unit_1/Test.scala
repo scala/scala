e933462e6c9ed1eec2db35e58c440345e2a080d6
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-08-27 19:38:54 +0200

    BCodeOpt buidling block: LabelsCleanup
    This bytecode transformer removes those LabelNodes,
    LocalVarEntries, and LineNumberNodes that are redundant.
    
    Details in the documentation for scala.tools.nsc.backend.bcode.LabelsCleanup

---
+++ test/files/jvm/bcode_labelsCleanup_unit_1/Test.scala
@@ -0,0 +1,60 @@
+import scala.tools.nsc.backend.bcode.LabelsCleanup
+import scala.tools.partest.BytecodeTest
+import scala.tools.asm
+import scala.collection.JavaConverters._
+
+import scala.tools.asm.Opcodes
+
+object Test extends BytecodeTest {
+
+  def show: Unit = {
+    val ma  = transformed(before())
+    val mb  = after()
+    val isa = wrapped(ma)
+    val isb = wrapped(mb)
+    // redundant LineNumberNodes are removed
+    assert(isa == isb)
+  }
+
+  def wrapped(m: asm.tree.MethodNode) = instructions.fromMethod(m)
+
+  def mkMethodNode = {
+    new asm.tree.MethodNode(
+      Opcodes.ACC_PUBLIC,
+      "m",
+      "()V",
+      null, null
+    )
+  }
+
+  def before(): asm.tree.MethodNode = {
+    val m  = mkMethodNode
+    val L = new asm.Label
+    //     return
+    m.visitInsn(Opcodes.RETURN)
+    // line number 1
+    m.visitLineNumber(1, L)
+    // line number 2
+    m.visitLineNumber(2, L)
+    // L:
+    m.visitLabel(L)
+
+    m
+  }
+
+  def after(): asm.tree.MethodNode = {
+    val m  = mkMethodNode
+    //     return
+    m.visitInsn(Opcodes.RETURN)
+
+    m
+  }
+
+  def transformed(input: asm.tree.MethodNode): asm.tree.MethodNode = {
+    val tr = new LabelsCleanup
+    do { tr.transform(input) } while (tr.changed)
+
+    input
+  }
+
+}
