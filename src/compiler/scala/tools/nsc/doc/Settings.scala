scala/scala

commit 2621ee63285808785159a3c24c9e5a5a723b8b9c

scaladoc fixes and improvements
Changes to scaladoc include:

- fixed transformation of Code(text) into HTML tag <code> - added tool
tips for deprecated entities (classes, methods) using the 'title'
attribute - added syntax highlighting of Scala source code in generated
<pre> blocks (CSS colors are defined in lib/template.css)

Here are several examples of highlighted Scala code:

scala.App
scala.Application
scala.Enumeration
scala.Function1
scala.Function2
scala.native
scala.Option
scala.Proxy
scala.specialized
scala.throws
scala.unchecked

scala.actors.Actor
scala.annotation.deprecatedName
scala.annotation.elidable
scala.annotation.switch

scala.collection.DefaultMap
scala.collection.JavaConversions
scala.collection.JavaConverters
scala.collection.LinearSeqLike
scala.collection.MapLike
scala.collection.SetLike
scala.collection.TraversableLike
scala.collection.immutable.NumericRange
scala.collection.immutable.Range
scala.collection.immutable.Stream
scala.collection.mutable.BufferLike

scala.concurrent.pilib
scala.io.Position
scala.reflect.BeanProperty
scala.reflect.Manifest
scala.testing.Benchmark

scala.util.DynamicVariable
scala.util.control.Breaks
scala.util.control.ControlThrowable
scala.util.control.Exception
scala.util.control.TailCalls
scala.util.logging.Logged
scala.util.parsing.combinator.testing.Tester
scala.util.parsing.json.JSON
scala.util.regexp.WordExp

scala.xml.factory.LoggedNodeFactory
scala.xml.parsing.ConstructingParser

src/compiler/scala/tools/nsc/doc/Settings.scala
@@ -47,6 +47,7 @@ class Settings(error: String => Unit) extends scala.tools.nsc.Settings(error) {
     "A directory containing sources which should be parsed, no more (e.g. AnyRef.scala)",
     ""
   )
+
   lazy val uncompilableFiles = docUncompilable.value match {
     case ""     => Nil
     case path   => io.Directory(path).deepFiles filter (_ hasExtension "scala") toList
@@ -57,7 +58,7 @@ class Settings(error: String => Unit) extends scala.tools.nsc.Settings(error) {
   val docsourceurl = StringSetting (
     "-doc-source-url",
     "url",
-    "A URL pattern used to build links to template sources; use variables, for example: €{TPL_NAME} ('Seq'), €{TPL_OWNER} ('scala.collection'), €{FILE_PATH} ('scala/collection/Seq')",
+    "A URL pattern used to build links to template sources; use variables, for example: ?{TPL_NAME} ('Seq'), ?{TPL_OWNER} ('scala.collection'), ?{FILE_PATH} ('scala/collection/Seq')",
     ""
   )
 

src/compiler/scala/reflect/api/Trees.scala
@@ -356,20 +356,16 @@ trait Trees /*extends reflect.generic.Trees*/ { self: Universe =>
   case class If(cond: Tree, thenp: Tree, elsep: Tree)
        extends TermTree
 
-  /** <p>
-   *    Pattern matching expression  (before explicitouter)
-   *    Switch statements            (after explicitouter)
-   *  </p>
-   *  <p>
-   *    After explicitouter, cases will satisfy the following constraints:
-   *  </p>
-   *  <ul>
-   *    <li>all guards are EmptyTree,</li>
-   *    <li>all patterns will be either <code>Literal(Constant(x:Int))</code>
-   *      or <code>Alternative(lit|...|lit)</code></li>
-   *    <li>except for an "otherwise" branch, which has pattern
-   *      <code>Ident(nme.WILDCARD)</code></li>
-   *  </ul>
+  /** - Pattern matching expression  (before explicitouter)
+   *  - Switch statements            (after explicitouter)
+   *
+   *  After explicitouter, cases will satisfy the following constraints:
+   *
+   *  - all guards are `EmptyTree`,
+   *  - all patterns will be either `Literal(Constant(x:Int))`
+   *    or `Alternative(lit|...|lit)`
+   *  - except for an "otherwise" branch, which has pattern
+   *    `Ident(nme.WILDCARD)`
    */
   case class Match(selector: Tree, cases: List[CaseDef])
        extends TermTree

src/compiler/scala/reflect/internal/AnnotationInfos.scala
@@ -13,16 +13,15 @@ trait AnnotationInfos extends api.AnnotationInfos { self: SymbolTable =>
 
   /** Arguments to classfile annotations (which are written to
    *  bytecode as java annotations) are either:
-   *  <ul>
-   *   <li>constants</li>
-   *   <li>arrays of constants</li>
-   *   <li>or nested classfile annotations</li>
-   *  </ul>
+   *
+   *  - constants
+   *  - arrays of constants
+   *  - or nested classfile annotations
    */
   abstract class ClassfileAnnotArg
 
-  /** Represents a compile-time Constant (Boolean, Byte, Short,
-   *  Char, Int, Long, Float, Double, String, java.lang.Class or
+  /** Represents a compile-time Constant (`Boolean`, `Byte`, `Short`,
+   *  `Char`, `Int`, `Long`, `Float`, `Double`, `String`, `java.lang.Class` or
    *  an instance of a Java enumeration value).
    */
   case class LiteralAnnotArg(const: Constant)
@@ -40,9 +39,12 @@ trait AnnotationInfos extends api.AnnotationInfos { self: SymbolTable =>
 
   object ArrayAnnotArg extends ArrayAnnotArgExtractor
 
-  /** A specific annotation argument that encodes an array of bytes as an array of `Long`. The type of the argument
-    * declared in the annotation must be `String`. This specialised class is used to encode scala signatures for
-    * reasons of efficiency, both in term of class-file size and in term of compiler performance. */
+  /** A specific annotation argument that encodes an array of bytes as an
+   *  array of `Long`. The type of the argument declared in the annotation
+   *  must be `String`. This specialised class is used to encode Scala
+   *  signatures for reasons of efficiency, both in term of class-file size
+   *  and in term of compiler performance.
+   */
   case class ScalaSigBytes(bytes: Array[Byte]) extends ClassfileAnnotArg {
     override def toString = (bytes map { byte => (byte & 0xff).toHexString }).mkString("[ ", " ", " ]")
     lazy val encodedBytes = ByteCodecs.encode(bytes)
@@ -66,30 +68,21 @@ trait AnnotationInfos extends api.AnnotationInfos { self: SymbolTable =>
 
   class AnnotationInfoBase
 
-  /** <p>
-   *    Typed information about an annotation. It can be attached to
-   *    either a symbol or an annotated type.
-   *  </p>
-   *  <p>
-   *    Annotations are written to the classfile as java annotations
-   *    if <code>atp</code> conforms to <code>ClassfileAnnotation</code>
-   *    (the classfile parser adds this interface to any Java annotation
-   *    class).
-   *  </p>
-   *  <p>
-   *    Annotations are pickled (written to scala symtab attribute
-   *    in the classfile) if <code>atp</code> inherits form
-   *    <code>StaticAnnotation</code>.
-   *  </p>
-   *  <p>
-   *    <code>args</code> stores arguments to Scala annotations,
-   *    represented as  typed trees. Note that these trees are not
-   *    transformed by any phases following the type-checker.
-   *  </p>
-   *  <p>
-   *    <code>assocs</code> stores arguments to classfile annotations
-   *    as name-value pairs.
-   *  </p>
+  /** Typed information about an annotation. It can be attached to either
+   *  a symbol or an annotated type.
+   *
+   *  Annotations are written to the classfile as Java annotations
+   *  if `atp` conforms to `ClassfileAnnotation` (the classfile parser adds
+   *  this interface to any Java annotation class).
+   *
+   *  Annotations are pickled (written to scala symtab attribute in the
+   *  classfile) if `atp` inherits form `StaticAnnotation`.
+   *
+   *  `args` stores arguments to Scala annotations, represented as typed
+   *  trees. Note that these trees are not transformed by any phases
+   *  following the type-checker.
+   *
+   *  `assocs` stores arguments to classfile annotations as name-value pairs.
    */
   case class AnnotationInfo(atp: Type, args: List[Tree],
                             assocs: List[(Name, ClassfileAnnotArg)])
@@ -142,7 +135,7 @@ trait AnnotationInfos extends api.AnnotationInfos { self: SymbolTable =>
   lazy val classfileAnnotArgManifest: ClassManifest[ClassfileAnnotArg] =
     reflect.ClassManifest.classType(classOf[ClassfileAnnotArg])
 
-  /** Symbol annotations parsed in Namer (typeCompleter of
+  /** Symbol annotations parsed in `Namer` (typeCompleter of
    *  definitions) have to be lazy (#1782)
    */
   case class LazyAnnotationInfo(annot: () => AnnotationInfo)

src/compiler/scala/tools/nsc/backend/WorklistAlgorithm.scala
@@ -3,7 +3,6 @@
  * @author  Martin Odersky
  */
 
-
 package scala.tools.nsc
 package backend
 
@@ -15,14 +14,13 @@ import scala.collection.{ mutable, immutable }
  * function is applied repeatedly to the first element in the
  * worklist, as long as the stack is not empty.
  *
- * The client class should mix-in this class and initialize the
- * worklist field and define the <code>processElement</code> method.
- * Then call the <code>run</code> method providing a function that
- * initializes the worklist.
+ * The client class should mix-in this class and initialize the worklist
+ * field and define the `processElement` method. Then call the `run` method
+ * providing a function that initializes the worklist.
  *
  * @author  Martin Odersky
  * @version 1.0
- * @see     <a href="icode/Linearizers.html" target="contentFrame">scala.tools.nsc.backend.icode.Linearizers</a>
+ * @see     [[scala.tools.nsc.backend.icode.Linearizers]]
  */
 trait WorklistAlgorithm {
   type Elem
@@ -31,9 +29,9 @@ trait WorklistAlgorithm {
   val worklist: WList
 
   /**
-   * Run the iterative algorithm until the worklist
-   * remains empty. The initializer is run once before
-   * the loop starts and should initialize the worklist.
+   * Run the iterative algorithm until the worklist remains empty.
+   * The initializer is run once before the loop starts and should
+   * initialize the worklist.
    *
    * @param initWorklist ...
    */

src/compiler/scala/tools/nsc/backend/icode/analysis/TypeFlowAnalysis.scala
@@ -3,13 +3,12 @@
  * @author  Martin Odersky
  */
 
-
 package scala.tools.nsc
 package backend.icode.analysis
 
 import scala.collection.{mutable, immutable}
 
-/** A data-flow analysis on types, that works on <code>ICode</code>.
+/** A data-flow analysis on types, that works on `ICode`.
  *
  *  @author Iulian Dragos
  */

src/compiler/scala/tools/nsc/doc/DocFactory.scala
@@ -1,4 +1,7 @@
-/* NSC -- new Scala compiler -- Copyright 2007-2011 LAMP/EPFL */
+/* NSC -- new Scala compiler
+ * Copyright 2007-2011 LAMP/EPFL
+ * @author  David Bernard, Manohar Jonnalagedda
+ */
 
 package scala.tools.nsc
 package doc
@@ -9,18 +12,20 @@ import util.NoPosition
 import io.{ File, Directory }
 import DocParser.Parsed
 
-/** A documentation processor controls the process of generating Scala documentation, which is as follows.
+/** A documentation processor controls the process of generating Scala
+  * documentation, which is as follows.
   *
-  * * A simplified compiler instance (with only the front-end phases enabled) is created, and additional
-  *   ''sourceless'' comments are registered.
+  * * A simplified compiler instance (with only the front-end phases enabled)
+  * * is created, and additional ''sourceless'' comments are registered.
   * * Documentable files are compiled, thereby filling the compiler's symbol table.
   * * A documentation model is extracted from the post-compilation symbol table.
   * * A generator is used to transform the model into the correct final format (HTML).
   *
-  * A processor contains a single compiler instantiated from the processor's `settings`. Each call to `document`
-  * uses the same compiler instance with the same symbol table. In particular, this implies that the scaladoc site
-  * obtained from a call to `run` will contain documentation about files compiled during previous calls to the same
-  * processor's `run` method.
+  * A processor contains a single compiler instantiated from the processor's
+  * `settings`. Each call to `document` uses the same compiler instance with
+  * the same symbol table. In particular, this implies that the scaladoc site
+  * obtained from a call to `run` will contain documentation about files compiled
+  * during previous calls to the same processor's `run` method.
   *
   * @param reporter The reporter to which both documentation and compilation errors will be reported.
   * @param settings The settings to be used by the documenter and compiler for generating documentation.
@@ -41,9 +46,10 @@ class DocFactory(val reporter: Reporter, val settings: doc.Settings) { processor
     override def forScaladoc = true
   }
 
-  /** Creates a scaladoc site for all symbols defined in this call's `files`, as well as those defined in `files` of
-    * previous calls to the same processor.
-    * @param files The list of paths (relative to the compiler's source path, or absolute) of files to document. */
+  /** Creates a scaladoc site for all symbols defined in this call's `files`,
+    * as well as those defined in `files` of previous calls to the same processor.
+    * @param files The list of paths (relative to the compiler's source path,
+    *        or absolute) of files to document. */
   def makeUniverse(files: List[String]): Option[Universe] = {
     assert(settings.docformat.value == "html")
     new compiler.Run() compile files
@@ -97,7 +103,7 @@ class DocFactory(val reporter: Reporter, val settings: doc.Settings) { processor
 
   /** Generate document(s) for all `files` containing scaladoc documenataion.
     * @param files The list of paths (relative to the compiler's source path, or absolute) of files to document. */
-  def document(files: List[String]): Unit = {
+  def document(files: List[String]) {
     def generate() = {
       import doclet._
       val docletClass    = Class.forName(settings.docgenerator.value) // default is html.Doclet
@@ -120,6 +126,7 @@ class DocFactory(val reporter: Reporter, val settings: doc.Settings) { processor
     try generate()
     catch documentError
   }
+
   private[doc] def docdbg(msg: String) {
     if (settings.Ydocdebug.value)
       println(msg)

src/compiler/scala/tools/nsc/doc/DocParser.scala
@@ -12,8 +12,8 @@ import util._
 import interactive.RangePositions
 import DocParser.Parsed
 
-/** A very minimal global customized for extracting DocDefs.  It stops
- *  right after parsing so it can read DocDefs from source code which would
+/** A very minimal global customized for extracting `DocDefs`.  It stops
+ *  right after parsing so it can read `DocDefs` from source code which would
  *  otherwise cause the compiler to go haywire.
  */
 class DocParser(settings: nsc.Settings, reporter: Reporter)
@@ -31,8 +31,8 @@ class DocParser(settings: nsc.Settings, reporter: Reporter)
     phasesSet += syntaxAnalyzer
   }
 
-  /** Returns a list of DocParser.Parseds, which hold the DocDefs found in the
-   *  given code along with the surrounding trees.
+  /** Returns a list of `DocParser.Parseds`, which hold the DocDefs found
+   *  in the given code along with the surrounding trees.
    */
   def docDefs(code: String) = {
     def loop(enclosing: List[Tree], tree: Tree): List[Parsed] = tree match {
@@ -54,7 +54,7 @@ class DocParser(settings: nsc.Settings, reporter: Reporter)
 }
 
 /** Since the DocParser's whole reason for existing involves trashing a
- *  global, it is designed to bottle up general Global#Tree types rather
+ *  global, it is designed to bottle up general `Global#Tree` types rather
  *  than path dependent ones.  The recipient will have to deal.
  */
 object DocParser {

src/compiler/scala/tools/nsc/doc/Index.scala
@@ -1,3 +1,8 @@
+/* NSC -- new Scala compiler
+ * Copyright 2005-2011 LAMP/EPFL
+ * @author  Martin Odersky
+ */
+
 package scala.tools.nsc.doc
 
 import scala.collection._

commit 610beb3613fa4f6151249f6b748915ad022d447b

Fix encoding problems in output of scaladoc -help
From the output of `scaladoc -help`:

```
  -doc-source-url <url>                       A URL pattern used to
  build links to template sources; use variables, for example:
  ?{TPL_NAME} ('Seq'), ?{TPL_OWNER} ('scala.collection'),
  ?{FILE_PATH} ('scala/collection/Seq')
```

The ? should be €. Fix that in the source, using the correct
encoding (which is already UTF-8). This requires the *default JVM
encoding* to be UTF-8 when the script is run, that is, passing
`-Dfile.encoding=UTF-8`.

So, after this commit, the euro sign is garbled in the output of
`./qbin/scaladoc -help` and correctly displayed in the output of
`./qbin/scaladoc -Dfile.encoding=UTF-8 -help`.

The next step is to make that option the default, but of course that
change has more potential for breakage.

The encoding problem was introduced in 2011 by commit
2621ee63285808785159a3c24c9e5a5a723b8b9c, and seems to not have been
reported till now.

src/compiler/scala/tools/nsc/doc/Settings.scala
@@ -66,7 +66,7 @@ class Settings(error: String => Unit, val printMsg: String => Unit = println(_))
   val docsourceurl = StringSetting (
     "-doc-source-url",
     "url",
-    "A URL pattern used to build links to template sources; use variables, for example: ?{TPL_NAME} ('Seq'), ?{TPL_OWNER} ('scala.collection'), ?{FILE_PATH} ('scala/collection/Seq')",
+    "A URL pattern used to build links to template sources; use variables, for example: €{TPL_NAME} ('Seq'), €{TPL_OWNER} ('scala.collection'), €{FILE_PATH} ('scala/collection/Seq')",
     ""
   )
 