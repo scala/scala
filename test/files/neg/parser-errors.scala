1c8bbd7ec5b8859f75abfbc4e70482dede2b7007
Paul Phillips <paulp@improving.org>
2013-09-18 07:48:57 -0700

    Position#show prints the point.
    Can't ensure range position points are meaningful when we never
    see them. To limit noise, only print the point when it != start.
    
      [x:y]    // point=x, start=x, end=y
      [p/x:y]  // point=p, start=x, end=y
    
    I'm open to a different syntax.
    
    Also prints NoPosition as [X] rather than [NoPosition] because
    noise is for construction workers and attenders of rock concerts.
    
    Some range position and parser tests are included so we can see
    the checkfile change when the forthcoming fix happens (either an
    error message improvement or a positional one.)

---
+++ test/files/neg/parser-errors.scala
@@ -0,0 +1,5 @@
+abstract class A1 { lazy val foo: Int }
+abstract class A2 { val Some(x)       }
+abstract class A3 { var x = _         }
+abstract class A4 { val x: Int = _    }
+abstract class A5 { var Some(x) = _   }
