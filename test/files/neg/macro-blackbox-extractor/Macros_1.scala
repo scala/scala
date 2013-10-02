f83296552aa80faf8a9350131c0448bc05b34c96
Eugene Burmako <xeno.by@gmail.com>
2013-10-02 17:22:17 +0200

    blackbox restriction #4: can't customize pattern matching
    When an application of a blackbox macro is used as an extractor in a
    pattern match, it triggers an unconditional compiler error,
    preventing customizations of pattern matching implemented with macros.

---
+++ test/files/neg/macro-blackbox-extractor/Macros_1.scala
@@ -0,0 +1,21 @@
+import scala.reflect.macros.BlackboxContext
+import language.experimental.macros
+
+object Extractor {
+  def unapply(x: Int) = macro Macros.unapplyImpl
+}
+
+object Macros {
+  def unapplyImpl(c: BlackboxContext)(x: c.Tree) = {
+    import c.universe._
+    q"""
+      new {
+        class Match(x: Int) {
+          def isEmpty = false
+          def get = x
+        }
+        def unapply(x: Int) = new Match(x)
+      }.unapply($x)
+    """
+  }
+}
