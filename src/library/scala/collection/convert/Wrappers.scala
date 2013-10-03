f38f762fc645ad24f5c9a297a6e62a75e9fa4b94
Ilya Maykov <ivmaykov@gmail.com>
2013-10-03 14:42:54 -0700

    SI-7883: Added a comment per CR feedback from @adriaanm, @Ichoran

--- src/library/scala/collection/convert/Wrappers.scala
+++ src/library/scala/collection/convert/Wrappers.scala
@@ -214,6 +214,9 @@ private[collection] trait Wrappers {
     }
 
     override def containsKey(key: AnyRef): Boolean = try {
+      // Note: Subclass of collection.Map with specific key type may redirect generic
+      // contains to specific contains, which will throw a ClassCastException if the
+      // wrong type is passed. This is why we need a type cast to A inside a try/catch.
       underlying.contains(key.asInstanceOf[A])
     } catch {
       case ex: ClassCastException => false
