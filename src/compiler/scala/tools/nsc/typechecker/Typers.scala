8db838eb7f19662fe453f36bb12b4fcdf6a33a9f
Adriaan Moors <adriaan.moors@typesafe.com>
2013-10-04 10:53:57 -0700

    Simplify partest.task target, fix typo in comment.

--- src/compiler/scala/tools/nsc/typechecker/Typers.scala
+++ src/compiler/scala/tools/nsc/typechecker/Typers.scala
@@ -2484,7 +2484,7 @@ trait Typers extends Adaptations with Tags with TypersTracking with PatternTyper
       assert(pt.typeSymbol == PartialFunctionClass, s"PartialFunction synthesis for match in $tree requires PartialFunction expected type, but got $pt.")
       val targs = pt.dealiasWiden.typeArgs
 
-      // if targs.head isn't fully defined, we can translate --> error
+      // if targs.head isn't fully defined, we can't translate --> error
       targs match {
         case argTp :: _ if isFullyDefined(argTp) => // ok
         case _ => // uh-oh
