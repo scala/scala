3127ec09783572cffe33238f80bcf5cca609b9ec
rjfwhite <stuff@rjfw.co.uk>
2013-11-11 00:49:43 +0000

    SI-7568 Adding Serializable to ResizableArrayAccess inner class of PriorityQueue

--- src/library/scala/collection/mutable/PriorityQueue.scala
+++ src/library/scala/collection/mutable/PriorityQueue.scala
@@ -43,7 +43,7 @@ class PriorityQueue[A](implicit val ord: Ordering[A])
 {
   import ord._
 
-  private class ResizableArrayAccess[A] extends AbstractSeq[A] with ResizableArray[A] {
+  private class ResizableArrayAccess[A] extends AbstractSeq[A] with ResizableArray[A] with Serializable {
     def p_size0 = size0
     def p_size0_=(s: Int) = size0 = s
     def p_array = array
