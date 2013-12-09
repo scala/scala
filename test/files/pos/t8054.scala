scala/scala

commit 80767383fdc52a444295fdc2ab5f4b9fbfa1c352

SI-7928 Favour module accessors symbols in rebind
The Refchecks tree transformer transforms a nested modules that
overrides a method into a pair of symbols: the module itself, and
an module accessor that matches the overridden symbol.

    [[syntax trees at end of                     typer]] // test1.scala
    package <empty> {
      abstract trait C2 extends scala.AnyRef {
        def O1: Any
      };
      class C1 extends AnyRef with C2 {
        object O1 extends scala.AnyRef
      }
    }

    [[syntax trees at end of                 refchecks]] // test1.scala
    package <empty> {
      abstract trait C2 extends scala.AnyRef {
        def O1: Any
      };
      class C1 extends AnyRef with C2 {
        object O1 extends scala.AnyRef
        @volatile <synthetic> private[this] var O1$module: C1.this.O1.type = _;
        <stable> def O1: C1.this.O1.type = {
          C1.this.O1$module = new C1.this.O1.type();
          C1.this.O1$module
        }
      }
    }

When constructing a TypeRef or SingleType with a prefix and and a symbol,
the factory methods internally use `rebind` to see if the provided symbol
should be replaced with an overriding symbol that is available in that prefix.

Trying this out in the REPL is a bit misleading, because even if you change
phase to `refchecks`, you won't get the desired results because the transform
is not done in an InfoTransformer.

   scala> val O1 = typeOf[C1].decl(TermName("O1"))
   O1: $r.intp.global.Symbol = object O1

   scala> typeRef(typeOf[C2], O1, Nil)
   res13: $r.intp.global.Type = C2#O1

   scala> res13.asInstanceOf[TypeRef].sym.owner
   res14: $r.intp.global.Symbol = class C1

But debugging the test case, we get into `rebind` during an AsSeenFrom
which is where we crashed when `suchThat` encountered the overloaded
module and module accessor symbols:

    typeOf[OuterObject.Inner.type].memberType(symbolOf[InnerTrait.Collection])
      ...
      singleTypeAsSeen(OuterTrait.this.Inner.type)

src/reflect/scala/reflect/internal/Types.scala
@@ -3385,7 +3385,11 @@ trait Types
   /** Rebind symbol `sym` to an overriding member in type `pre`. */
   private def rebind(pre: Type, sym: Symbol): Symbol = {
     if (!sym.isOverridableMember || sym.owner == pre.typeSymbol) sym
-    else pre.nonPrivateMember(sym.name).suchThat(sym => sym.isType || (sym.isStable && !sym.hasVolatileType)) orElse sym
+    else pre.nonPrivateMember(sym.name).suchThat(sym =>
+      // SI-7928 `isModuleNotMethod` is here to avoid crashing with overloaded module accessor and module symbols
+      //         after refchecks eliminates a ModuleDef that implements and interface.
+      sym.isType || (!sym.isModuleNotMethod && sym.isStable && !sym.hasVolatileType)
+    ) orElse sym
   }
 
   /** Convert a `super` prefix to a this-type if `sym` is abstract or final. */

test/files/pos/t7928.scala
@@ -1,0 +1,16 @@
+trait OuterTrait {
+  trait InnerTrait {
+    type Element
+    type Collection <: Iterable[Inner.Element]
+  }
+
+  val Inner: InnerTrait
+
+}
+
+object OuterObject extends OuterTrait {
+  object Inner extends InnerTrait {
+    type Element = String
+    override type Collection = Seq[Inner.Element]
+  }
+}

commit 369f370b1e894893d315de3bd861c9292695f71d

SI-8054 Fix regression in TypeRef rebind with val overriding object
Regressed in 80767383fd / SI-7928

The condition added in that commit are necessary after refchecks but
poisonous before.

This still seems rather fragile: I wonder if `eliminateModuleDefs`
in `RefChecks` ought to flag the module symbol as `ARTIFACT` after
creating the accessor, so as to signal that it shouldn't be bound
anymore?

src/reflect/scala/reflect/internal/Types.scala
@@ -3392,11 +3392,14 @@ trait Types
   /** Rebind symbol `sym` to an overriding member in type `pre`. */
   private def rebind(pre: Type, sym: Symbol): Symbol = {
     if (!sym.isOverridableMember || sym.owner == pre.typeSymbol) sym
-    else pre.nonPrivateMember(sym.name).suchThat(sym =>
-      // SI-7928 `isModuleNotMethod` is here to avoid crashing with overloaded module accessor and module symbols
-      //         after refchecks eliminates a ModuleDef that implements and interface.
-      sym.isType || (!sym.isModuleNotMethod && sym.isStable && !sym.hasVolatileType)
-    ) orElse sym
+    else pre.nonPrivateMember(sym.name).suchThat { sym =>
+      // SI-7928 `isModuleNotMethod` is here to avoid crashing with spuriously "overloaded" module accessor and module symbols.
+      //         These appear after refchecks eliminates ModuleDefs that implement an interface.
+      //         Here, we exclude the module symbol, which allows us to bind to the accessor.
+      // SI-8054 We must only do this after refchecks, otherwise we exclude the module symbol which does not yet have an accessor!
+      val isModuleWithAccessor = phase.refChecked && sym.isModuleNotMethod
+      sym.isType || (!isModuleWithAccessor && sym.isStable && !sym.hasVolatileType)
+    } orElse sym
   }
 
   /** Convert a `super` prefix to a this-type if `sym` is abstract or final. */

test/files/pos/t8054.scala
@@ -1,0 +1,31 @@
+trait D {
+  trait Manifest {
+    class Entry
+  }
+
+  val M: Manifest
+
+  def m: M.Entry = ???
+}
+
+object D1 extends D {
+  object M extends Manifest
+}
+
+object D2 extends D {
+  val M: Manifest = ???
+}
+
+object Hello {
+
+  def main(args: Array[String]) {
+    // 2.10.3 - ok
+    // 2.11.0-M7 - type mismatch; found : Seq[DB1.MANIFEST.Entry]
+    // required: Seq[DB1.MANIFEST.Entry]
+    val t1: D1.M.Entry = D1.m
+
+    // 2.10.3 - ok
+    // 2.11.0-M7 - ok
+    val t2: D2.M.Entry = D2.m
+  }
+}