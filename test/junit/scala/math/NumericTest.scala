b46d7aefd6eda36454cfd4cf339642e3c13c2022
clhodapp <clhodapp1@gmail.com>
2014-01-05 21:38:26 -0600

    SI-8102 -0.0.abs must equal 0.0
    SI-8102 points out that -0.0.abs returns -0.0 (in error). The same issue
    exists for -0.0f. This commit fixes the issue for both by delegating to
    math.abs.

---
+++ test/junit/scala/math/NumericTest.scala
@@ -0,0 +1,18 @@
+
+
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(classOf[JUnit4])
+class NumericTest {
+
+  /* Test for SI-8102 */
+  @Test
+  def testAbs {
+    assertTrue(-0.0.abs equals 0.0)
+    assertTrue(-0.0f.abs equals 0.0f)
+  }
+}
+
