d0b82f608ce6d6abfa991e6e6e0dfc92ce9d9d2f
George Leontiev <folone@gmail.com>
2014-01-08 17:57:50 +0100

    Improve comments, move value name to a dedicated val.

--- src/reflect/scala/reflect/api/StandardLiftables.scala
+++ src/reflect/scala/reflect/api/StandardLiftables.scala
@@ -236,6 +236,13 @@ trait StandardLiftables { self: Universe =>
 object LiftableCaseClass {
   import scala.reflect.macros.WhiteboxContext
 
+  /* All the trees (except one that has a special comment) in this macro are hygienic
+   * (and there is a test to check that). That is, all TermNames used
+   * (specifically: Tree, Apply, Liftable, List, implicitly, reify) are fully qualified,
+   * and will not bind to terms with same names, declared in the expansion context.
+   * Each tree is commented with a simplified, unhygienic quasiquote, that explains
+   * what that tree is supposed to do.
+   */
   def impl[T: c.WeakTypeTag](c: WhiteboxContext): c.Tree = {
     import c.universe._
     val Select(prefix, _) = c.prefix.tree
@@ -252,37 +259,51 @@ object LiftableCaseClass {
       val typeSign = tpe.declaration(name).typeSignature
       name → typeSign
     }
+    /* Tree produced by the following quasiquote:
+     * q"reify(${c.mirror.staticModule(symbol.fullName)}).tree"
+     */
     val constructor = Select(Apply(
                                Select(prefix, TermName("reify")),
                                List(Ident(c.mirror.staticModule(symbol.fullName)))),
                         TermName("tree"))
+    val value = "value"
     val arguments = fields(T).map { case (name, typeSign) ⇒
-      // Tree produced by the following quasiquote:
-      // q"implicitly[Liftable[$typeSign]].apply(value.$name)"
+      /* Tree produced by the following quasiquote:
+       * q"implicitly[Liftable[$typeSign]].apply(value.$name)"
+       * Another way to do this would be:
+       * q"""
+       *   val v : $typeSign = value.$name
+       *   q"$$v"
+       * """
+       *
+       * This tree is not hygienic: TermName(value) will bind to value from
+       * enclosing context, which is the tree being returned from the macro.
+       * To ensure that its name does not change in one place, but stays
+       * unchanged in another, it is placed in a dedicated val.
+       */
       Apply(Select(
               TypeApply(Select(Ident(definitions.PredefModule), TermName("implicitly")),
                         List(AppliedTypeTree(Select(prefix, TypeName("Liftable")),
                              List(TypeTree(typeSign))))),
               TermName("apply")),
-           List(Select(Ident(TermName("value")), name)))
-      // Another way to do this would be:
-      /*q"""
-        val v : $typeSign = value.$name
-        q"$$v"
-       """*/
+           List(Select(Ident(TermName(value)), name)))
     }
+    /* Tree produced by the following quasiquote:
+     * q"Apply($constructor, List(..$arguments))"
+     */
     val reflect = Apply(Select(prefix, TermName("Apply")),
                         List(constructor,
                           Apply(Ident(definitions.ListModule),
-                        arguments))) // q"Apply($constructor, List(..$arguments))"
+                        arguments)))
     val implicitName = TermName(symbol.name.encoded ++ "Liftable")
-    // Tree produced by the following quasiquote:
-    /*q"""
-      implicit object $implicitName extends Liftable[$T] {
-        def apply(value: $T): Tree = $reflect
-      }
-      $implicitName
-    """*/
+    /* Tree produced by the following quasiquote:
+     * q"""
+     *   implicit object $implicitName extends Liftable[$T] {
+     *     def apply(value: $T): Tree = $reflect
+     *   }
+     *   $implicitName
+     * """
+     */
     import Flag._
     Block(List(ModuleDef(Modifiers(IMPLICIT),
                 implicitName,
@@ -301,7 +322,7 @@ object LiftableCaseClass {
                                TermName("apply"),
                                List(),
                                List(List(ValDef(Modifiers(PARAM),
-                                         TermName("value"), TypeTree(T), EmptyTree))),
+                                         TermName(value), TypeTree(T), EmptyTree))),
                                Select(prefix, TypeName("Tree")), reflect))))),
           Ident(implicitName))
   }
