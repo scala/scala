4a1eeb7b60e531cccac974393b121d956ff72803
Eugene Burmako <xeno.by@gmail.com>
2014-01-22 13:18:35 +0300

    SI-5920 enables default and named args in macros
    When producing an initial spec for macros two years ago, we sort of glossed
    over named/default arguments in macro applications, leaving them for future work.
    
    Once the aforementioned future has come, I’ve made several attempts at
    making things operational (e.g. last summer), but it’s always been unclear
    how to marry the quite complex desugaring that tryNamesDefaults performs
    with the expectations of macro programmers to see unsugared trees
    in macro impl parameters.
    
    Here’s the list of problems that arise when trying to encode named/default
    arguments of macro applications:
    1) When inside macro impls we don’t really care about synthetic vals
    that are typically introduced to preserve evaluation order in non-positional
    method applications. When we inline those synthetics, we lose information
    about evaluation order, which is something that we wouldn’t like to lose
    in the general case.
    2) More importantly, it’s also not very exciting to see invocations of
    default getters that stand for unspecified default arguments. Ideally,
    we would like to provide macro programmers with right-hand sides of those
    default getters, but that is: a) impossible in the current implementation
    of default parameters, b) would anyway bring scoping problems that we’re
    not ready to deal with just yet.
    
    Being constantly unhappy with potential solutions to the aforementioned
    problems, I’ve been unable to nail this down until the last weekend,
    when I realized that: 1) even though we can’t express potential twists in
    evaluation order within linearly ordered macro impl params, we can use
    c.macroApplication to store all the named arguments we want, 2) even though
    we can’t get exactly what we want for default arguments, we can represent
    them with EmptyTree’s, which is not ideal, but pretty workable. That’s
    what has been put into life in this commit.
    
    As a pleasant side-effect, now the macro engine doesn’t have to reinvent
    the wheel wrt reporting errors about insufficient arg or arglist count.
    Since this logic is intertwined with the tryNamesDefaults desugaring,
    we previously couldn’t make use of it and had to roll our own logic
    that checked that the number of arguments and parameters of macro applications
    correspond to each other. Now it’s all deduplicated and consistent.

---
+++ test/files/neg/macro-expand-varargs-b/Macros_1.scala
@@ -0,0 +1,27 @@
+import scala.reflect.macros.whitebox._
+import scala.language.experimental.macros
+
+object Macros {
+  def foo_impl(c: Context)(xs: c.Tree*) = {
+    import c.universe._
+    q"println(${xs.toString})"
+  }
+
+  def foo(xs: Int*): Unit = macro foo_impl
+
+  def bar_impl(c: Context)(x: c.Tree, xs: c.Tree*) = {
+    import c.universe._
+    q"println((${x.toString}, ${xs.toString}))"
+  }
+
+  def bar(x: Int, xs: Int*): Unit = macro bar_impl
+  // doesn't compile: def bar(x: Int = 1, xs: Int*): Unit = macro bar_impl
+
+  def baz_impl(c: Context)(x: c.Tree, y: c.Tree, xs: c.Tree*) = {
+    import c.universe._
+    q"println((${x.toString}, ${y.toString}, ${xs.toString}))"
+  }
+
+  def baz(x: Int, y: Int, xs: Int*): Unit = macro baz_impl
+  // doesn't compile: def baz(x: Int = 1, y: Int = 2, xs: Int*): Unit = macro baz_impl
+}
