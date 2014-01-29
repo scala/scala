d0afd7ece04e1974093c8c37b0ff6654742ab57b
Jason Zaugg <jzaugg@gmail.com>
2014-01-29 09:15:36 +0100

    SI-7700 @unspecialized, Part Deux: Now Working.
    This annotation was introduced to allow us to mark methods within
    a specialized trait as immune from specialization. In particular,
    this is desirable for `Function1.{andThen, compose}`.
    
    However, it seems we need to check for this in two places in the
    specialization code base. The feature is now backed with a test.

---
+++ test/files/run/t7700.scala
@@ -0,0 +1,17 @@
+import scala.annotation._
+
+trait C[@specialized U] {
+  @unspecialized
+  def foo(u: U): U
+  @unspecialized
+  def bar[A](u: U) = u
+}
+
+object Test extends App {
+  val declared = classOf[C[_]].getDeclaredMethods.sortBy(_.getName)
+  println(declared.mkString("\n"))
+  object CInt extends C[Int] { def foo(i: Int) = i }
+  object CAny extends C[Any] { def foo(a: Any) = a }
+  assert(CInt.foo(1) == 1)
+  assert(CAny.foo("") == "")
+}
