d60b8323dda884653f31410806c269d15140e5e6
Denys Shabalin <denys.shabalin@typesafe.com>
2014-02-06 16:13:36 +0100

    Rename EmptyTypTree into SyntacticEmptyTypeTree
    1. Change the name as Eugene believes previous name was misleading
    
    2. Remove EmptyTree case as it's not needed any longer

--- src/reflect/scala/reflect/internal/BuildUtils.scala
+++ src/reflect/scala/reflect/internal/BuildUtils.scala
@@ -537,13 +537,9 @@ trait BuildUtils { self: SymbolTable =>
       def unapply(tree: Tree): Option[Tree] = gen.Filter.unapply(tree)
     }
 
-    // abstract over possible alternative representations of no type in valdef
-    protected object EmptyTypTree {
-      def unapply(tree: Tree): Boolean = tree match {
-        case EmptyTree => true
-        case tt: TypeTree if (tt.original == null || tt.original.isEmpty) => true
-        case _ => false
-      }
+    // a type tree which wasn't specified by the user but might have been inferred by compiler
+    object SyntacticEmptyTypeTree {
+      def unapply(tt: TypeTree): Boolean = tt.original == null || tt.original.isEmpty
     }
 
     // match a sequence of desugared `val $pat = $value`
@@ -561,8 +557,8 @@ trait BuildUtils { self: SymbolTable =>
         case ValDef(_, name1, _, Match(MaybeUnchecked(value), CaseDef(pat, EmptyTree, Ident(name2)) :: Nil)) :: UnPatSeq(rest)
           if name1 == name2 =>
           Some((pat, value) :: rest)
-        // case q"${_} val $name: ${EmptyTypTree()} = $value" :: UnPatSeq(rest) =>
-        case ValDef(_, name, EmptyTypTree(), value) :: UnPatSeq(rest) =>
+        // case q"${_} val $name: ${SyntacticEmptyTypeTree()} = $value" :: UnPatSeq(rest) =>
+        case ValDef(_, name, SyntacticEmptyTypeTree(), value) :: UnPatSeq(rest) =>
           Some((Bind(name, self.Ident(nme.WILDCARD)), value) :: rest)
         // case q"${_} val $name: $tpt = $value" :: UnPatSeq(rest) =>
         case ValDef(_, name, tpt, value) :: UnPatSeq(rest) =>
@@ -604,8 +600,8 @@ trait BuildUtils { self: SymbolTable =>
       def unapply(tree: Tree): Option[(Tree, Tree)] = tree match {
         case Function(ValDef(Modifiers(PARAM, _, _), name, tpt, EmptyTree) :: Nil, body) =>
           tpt match {
-            case EmptyTypTree() => Some((Bind(name, self.Ident(nme.WILDCARD)), body))
-            case _              => Some((Bind(name, Typed(self.Ident(nme.WILDCARD), tpt)), body))
+            case SyntacticEmptyTypeTree() => Some((Bind(name, self.Ident(nme.WILDCARD)), body))
+            case _                        => Some((Bind(name, Typed(self.Ident(nme.WILDCARD), tpt)), body))
           }
         case UnVisitor(_, CaseDef(pat, EmptyTree, body) :: Nil) =>
           Some((pat, body))
