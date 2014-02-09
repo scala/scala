45cfc7b3a242f0be0397c5eb3946356bf3446dc9
Jason Zaugg <jzaugg@gmail.com>
2014-02-09 22:29:11 +0100

    SI-7374 Test cases for fixed Java interop problem
    Our generic signatures now match the erasure, so no
    more nasty linkage errors.

---
+++ test/files/run/t7374/Some.scala
@@ -0,0 +1,3 @@
+object SomeScala {
+  def list = List(1, 2, 3) 
+}
---
+++ test/files/run/t7374/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(SomeScala.list().tail());
+        System.out.println(SomeScala.list().par());
+        System.out.println(SomeScala.list().init());
+    }
+}
