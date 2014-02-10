5f9315e5dc7c6c7b67268610ac16f79ec2c1f11b
Adriaan Moors <adriaan.moors@typesafe.com>
2014-02-10 13:43:30 -0800

    SI-8177: when fully fixed, neg/t0764 should compile

--- test/files/neg/t0764.scala
+++ test/files/neg/t0764.scala
@@ -12,3 +12,15 @@ class Main[NextType <: Node](value: Node { type T = NextType })
 
 	new Main[AType]( (value: AType).prepend )
 }
+
+/* TODO: this should compile with a proper fix for SI-8177
+
+Behold the equivalent program which already type checks.
+(Expand type alias, convert type member to type param;
+note the covariance to encode subtyping on type members.)
+
+class Node[+T <: Node[_]] { def prepend = new Node[this.type] }
+class Main[NextType <: Node[_]](value: Node[NextType]) {
+  new Main(value.prepend)
+}
+*/
