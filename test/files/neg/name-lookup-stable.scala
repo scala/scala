2d4d2d3f26e66cc1be6dc32ff37ba889bd4c8d0a
Jason Zaugg <jzaugg@gmail.com>
2014-02-12 17:07:05 +0100

    A test case for a name binding progression
    I noticed the change when adapting Slick to work with
    Scala 2.11 in `AbstractSourceCodeGenerator.scala`.
    The behaviour changed in a70c8219.
    
    This commit locks down the new, correct behaviour with a test.

---
+++ test/files/neg/name-lookup-stable.scala
@@ -0,0 +1,20 @@
+// This used to compile under 2.10.3 but the ambiguity is now noticed
+// in 2.11.x (after a70c8219). I think the new behaviour is correct;
+// we shouldn't discard names based on "expected stability" before
+// evaluating ambiguity.
+object ColumnOption {
+  object PrimaryKey
+}
+
+class A {
+  def PrimaryKey: Any = ???
+
+  {
+    import ColumnOption._
+
+    (null: Any) match { case PrimaryKey => }
+
+    PrimaryKey // was already ambigious in 2.10.3
+  }
+}
+
