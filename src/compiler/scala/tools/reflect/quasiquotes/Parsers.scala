51b8e6c020d39393c471dcfee389c5f0f7510afd
Denys Shabalin <denys.shabalin@typesafe.com>
2014-02-24 18:35:59 +0100

    SI-8285 use correct kind of map for quasiquote positions
    Previously mutable.ListMap was used with assumption that it preserved
    order of inserted elements (but it doesn't). Surprisingly logic that
    assumed ordered elements worked mosly fine on unordered ones. I guess
    two bugs can cancel each other out.

--- src/compiler/scala/tools/reflect/quasiquotes/Parsers.scala
+++ src/compiler/scala/tools/reflect/quasiquotes/Parsers.scala
@@ -30,15 +30,15 @@ trait Parsers { self: Quasiquotes =>
 
     def correspondingPosition(offset: Int): Position = {
       val posMapList = posMap.toList
-      def containsOffset(start: Int, end: Int) = start <= offset && offset <= end
+      def containsOffset(start: Int, end: Int) = start <= offset && offset < end
       def fallbackPosition = posMapList match {
         case (pos1, (start1, end1)) :: _   if start1 > offset => pos1
-        case _ :+ ((pos2, (start2, end2))) if offset > end2   => pos2.withPoint(pos2.point + (end2 - start2))
+        case _ :+ ((pos2, (start2, end2))) if end2 <= offset  => pos2.withPoint(pos2.point + (end2 - start2))
       }
       posMapList.sliding(2).collect {
-        case (pos1, (start1, end1)) :: _                if containsOffset(start1, end1) => (pos1, offset - start1)
-        case (pos1, (_, end1)) :: (_, (start2, _)) :: _ if containsOffset(end1, start2) => (pos1, end1)
-        case _ :: (pos2, (start2, end2)) :: _           if containsOffset(start2, end2) => (pos2, offset - start2)
+        case (pos1, (start1, end1)) :: _                        if containsOffset(start1, end1) => (pos1, offset - start1)
+        case (pos1, (start1, end1)) :: (pos2, (start2, _)) :: _ if containsOffset(end1, start2) => (pos1, end1 - start1)
+        case _ :: (pos2, (start2, end2)) :: _                   if containsOffset(start2, end2) => (pos2, offset - start2)
       }.map { case (pos, offset) =>
         pos.withPoint(pos.point + offset)
       }.toList.headOption.getOrElse(fallbackPosition)
