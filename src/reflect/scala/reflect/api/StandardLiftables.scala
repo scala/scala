2e6e13ff25d3909201673b71afdd76e885e15304
George Leontiev <folone@gmail.com>
2014-03-28 13:47:15 +0100

    Materializes Liftable instances for arbitrary case classes.
    Now case classes can be used in quasiquotes.

--- src/reflect/scala/reflect/api/StandardLiftables.scala
+++ src/reflect/scala/reflect/api/StandardLiftables.scala
@@ -1,6 +1,8 @@
 package scala.reflect
 package api
 
+import scala.language.experimental.macros
+
 trait StandardLiftables { self: Universe =>
   import internal._
   import reificationSupport.{SyntacticTuple, ScalaDot}
@@ -54,6 +56,8 @@ trait StandardLiftables { self: Universe =>
       case left: Left[L, R]   => lift(left)
       case right: Right[L, R] => lift(right)
     }
+
+    implicit def liftCaseClass[T]: Liftable[T] = macro LiftableCaseClass.impl[T]
 
     implicit def liftTuple2[T1, T2](implicit liftT1: Liftable[T1], liftT2: Liftable[T2]): Liftable[Tuple2[T1, T2]] = Liftable { t =>
       SyntacticTuple(liftT1(t._1) :: liftT2(t._2) :: Nil)
@@ -231,5 +235,100 @@ trait StandardLiftables { self: Universe =>
     val util       = TermName("util")
     val Vector     = TermName("Vector")
     val WILDCARD   = self.nme.WILDCARD
+  }
+}
+
+object LiftableCaseClass {
+  import scala.reflect.macros.whitebox.Context
+
+  /* All the trees (except one that has a special comment) in this macro are hygienic
+   * (and there is a test to check that). That is, all TermNames used
+   * (specifically: Tree, Apply, Liftable, List, implicitly, reify) are fully qualified,
+   * and will not bind to terms with same names, declared in the expansion context.
+   * Each tree is commented with a simplified, unhygienic quasiquote, that explains
+   * what that tree is supposed to do.
+   */
+  def impl[T: c.WeakTypeTag](c: Context): c.Tree = {
+    import c.universe._
+    val Select(prefix, _) = c.prefix.tree
+    val T = weakTypeOf[T]
+    val symbol = T.typeSymbol
+    if (!symbol.asClass.isCaseClass)
+      c.abort(c.enclosingPosition, s"$symbol is not a case class")
+    if (!symbol.isStatic)
+      c.abort(c.enclosingPosition, s"$symbol is not static")
+    def fields(tpe: Type) = tpe.declarations.collectFirst {
+      case m: MethodSymbol if m.isPrimaryConstructor ⇒ m
+    }.get.paramss.head.map { field ⇒
+      val name = field.name
+      val typeSign = tpe.declaration(name).typeSignature
+      name → typeSign
+    }
+    /* Tree produced by the following quasiquote:
+     * q"reify(${c.mirror.staticModule(symbol.fullName)}).tree"
+     */
+    val constructor = Select(Apply(
+                               Select(prefix, TermName("reify")),
+                               List(Ident(c.mirror.staticModule(symbol.fullName)))),
+                        TermName("tree"))
+    val value = "value"
+    val arguments = fields(T).map { case (name, typeSign) ⇒
+      /* Tree produced by the following quasiquote:
+       * q"implicitly[Liftable[$typeSign]].apply(value.$name)"
+       * Another way to do this would be:
+       * q"""
+       *   val v : $typeSign = value.$name
+       *   q"$$v"
+       * """
+       *
+       * This tree is not hygienic: TermName(value) will bind to value from
+       * enclosing context, which is the tree being returned from the macro.
+       * To ensure that its name does not change in one place, but stays
+       * unchanged in another, it is placed in a dedicated val.
+       */
+      Apply(Select(
+              TypeApply(Select(Ident(definitions.PredefModule), TermName("implicitly")),
+                        List(AppliedTypeTree(Select(prefix, TypeName("Liftable")),
+                             List(TypeTree(typeSign))))),
+              TermName("apply")),
+           List(Select(Ident(TermName(value)), name)))
+    }
+    /* Tree produced by the following quasiquote:
+     * q"Apply($constructor, List(..$arguments))"
+     */
+    val reflect = Apply(Select(prefix, TermName("Apply")),
+                        List(constructor,
+                          Apply(Ident(definitions.ListModule),
+                        arguments)))
+    val implicitName = TermName(symbol.name.encoded ++ "Liftable")
+    /* Tree produced by the following quasiquote:
+     * q"""
+     *   implicit object $implicitName extends Liftable[$T] {
+     *     def apply(value: $T): Tree = $reflect
+     *   }
+     *   $implicitName
+     * """
+     */
+    import Flag._
+    Block(List(ModuleDef(Modifiers(IMPLICIT),
+                implicitName,
+                Template(
+                  List(AppliedTypeTree(Select(prefix, TypeName("Liftable")),
+                       List(TypeTree(T)))),
+                  noSelfType,
+                  List(DefDef(Modifiers(),
+                        nme.CONSTRUCTOR,
+                        List(), List(List()),
+                        TypeTree(),
+                        Block(
+                          List(pendingSuperCall),
+                          Literal(Constant(())))),
+                        DefDef(Modifiers(),
+                               TermName("apply"),
+                               List(),
+                               List(List(ValDef(Modifiers(PARAM),
+                                         TermName(value), TypeTree(T), EmptyTree))),
+                               Select(prefix, TypeName("Tree")), reflect))))),
+          Ident(implicitName))
   }
 }
