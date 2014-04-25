8317266f34dce29078dc7c4447605701ba82e82a
François Garillot <francois@garillot.net>
2014-04-25 15:52:50 +0200

    SI-8537 Puts SI-8157 fix under Xsource

--- src/compiler/scala/tools/nsc/typechecker/RefChecks.scala
+++ src/compiler/scala/tools/nsc/typechecker/RefChecks.scala
@@ -132,7 +132,16 @@ abstract class RefChecks extends InfoTransform with scala.reflect.internal.trans
 
       defaultMethodNames.toList.distinct foreach { name =>
         val methods      = clazz.info.findMember(name, 0L, requiredFlags = METHOD, stableOnly = false).alternatives
-        val haveDefaults = methods filter (sym => mexists(sym.info.paramss)(_.hasDefault) && !nme.isProtectedAccessorName(sym.name))
+        def hasDefaultParam(tpe: Type): Boolean = tpe match {
+          case MethodType(params, restpe) => (params exists (_.hasDefault)) || hasDefaultParam(restpe)
+          case _                          => false
+        }
+        val haveDefaults = methods filter (
+          if (settings.isScala211)
+             (sym => mexists(sym.info.paramss)(_.hasDefault) && !nme.isProtectedAccessorName(sym.name))
+          else
+            (sym => hasDefaultParam(sym.info) && !nme.isProtectedAccessorName(sym.name))
+        )
 
         if (haveDefaults.lengthCompare(1) > 0) {
           val owners = haveDefaults map (_.owner)
