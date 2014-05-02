f93fbef8ecbcf1f09cd18632bd209dbf641f7c96
Lukas Rytz <lukas.rytz@gmail.com>
2014-05-02 10:21:56 +0200

    IntelliJ project files
    Fixes compilation within IDEA. Allows compiling and running JUnit tests directly withing the IDE.

--- src/intellij/setup.sh
+++ src/intellij/setup.sh
@@ -5,19 +5,10 @@
 
 set -e
 export SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-export BASE="$( cd "$( dirname "$0" )"/../.. && pwd )"
 echo "About to delete .ipr and .iml files and replace with the .SAMPLE files. Press enter to continue or CTRL-C to cancel."
 read
 
-(rm -f *.ipr *.iml 2>/dev/null)
-for f in $(ls "$SCRIPT_DIR"/*.SAMPLE); do
-	NEW_FILE=`echo $f | perl -pe 's/.SAMPLE//'`;
-
-	cp $f $NEW_FILE
-
-	# IntelliJ doesn't process the "compilerOptions" setting for variable
-	# replacement. If it did, we would just use "$PROJECT_DIR$". Instead,
-	# we do this replacement ourselves.
-	perl -pi -e 's/\$BASE_DIR\$/$ENV{"BASE"}/g' $NEW_FILE
-	echo "Created $NEW_FILE"
+for f in "$SCRIPT_DIR"/*.SAMPLE; do
+  g=${f%.SAMPLE}
+  cp $f $g
 done
