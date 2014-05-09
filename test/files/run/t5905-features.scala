bdb1258d2ab8ff10d8e9f4924069a227ae82e690
Jason Zaugg <jzaugg@gmail.com>
2014-05-09 16:50:08 +0200

    Revert "SI-5905 Clarify test case"
    This reverts commit 78bd175afcc89878ca1c00cce69d0517909c6ff3.
    
    See discussion:
    
      https://github.com/scala/scala/pull/3597#commitcomment-6270375

--- test/files/run/t5905-features.scala
+++ test/files/run/t5905-features.scala
@@ -3,13 +3,13 @@ import tools.partest.DirectTest
 
 // verify that all languageFeature names are accepted by -language
 object Test extends DirectTest {
-  override def code = "class Code { def f = (1 to 10) size }" // exercise a feature to sanity-check coverage of -language options
+  override def code = "class Code { def f = (1 to 10) size }" // exercise a feature
 
   override def extraSettings = s"-usejavacp -d ${testOutput.path}"
 
   override def show() = {
-    val global = newCompiler("-Ystop-after:typer")
-    compileString(global)("")   // warm me up, scotty
+    val global = newCompiler("-language:postfixOps", "-Ystop-after:typer")
+    compileString(global)(code)
     import global._
     exitingTyper {
       //def isFeature(s: Symbol) = s.annotations.exists((a: AnnotationInfo) => a.tpe <:< typeOf[scala.annotation.meta.languageFeature])
@@ -20,8 +20,6 @@ object Test extends DirectTest {
       val all    = (feats ++ xmen) mkString ","
 
       assert(feats.nonEmpty, "Test must find feature flags.")
-
-      //compile("junk")   // tragically, does not fail the test
 
       //dynamics,postfixOps,reflectiveCalls,implicitConversions,higherKinds,existentials,experimental.macros
       compile(s"-language:$all")
