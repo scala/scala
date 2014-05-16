32196749105c9f98a33712fddc21b1676250d046
Jason Zaugg <jzaugg@gmail.com>
2014-05-16 23:35:43 +0200

    SI-8597 Improved pattern unchecked warnings
    The spec says that `case _: List[Int]` should be always issue
    an unchecked warning:
    
    > Types which are not of one of the forms described above are
    > also accepted as type patterns. However, such type patterns
    > will be translated to their erasure (§3.7). The Scala compiler
    > will issue an “unchecked” warning for these patterns to flag
    > the possible loss of type-safety.
    
    But the implementation goes a little further to omit warnings
    based on the static type of the scrutinee. As a trivial example:
    
       def foo(s: Seq[Int]) = s match { case _: List[Int] => }
    
    need not issue this warning.
    
    These discriminating unchecked warnings are domain of
    `CheckabilityChecker`.
    
    Let's deconstruct the reported bug:
    
      def nowarn[T] = (null: Any) match { case _: Some[T] => }
    
    We used to determine that if the first case matched, the scrutinee
    type would be `Some[Any]` (`Some` is covariant). If this statically
    matches `Some[T]` in a pattern context, we don't need to issue an
    unchecked warning. But, our blanket use of `existentialAbstraction`
    in `matchesPattern` loosened the pattern type to `Some[Any]`, and
    the scrutinee type was deemed compatible.
    
    I've added a new method, `scrutConformsToPatternType` which replaces
    pattern type variables by wildcards, but leaves other abstract
    types intact in the pattern type. We have to use this inside
    `CheckabilityChecker` only. If we were to make `matchesPattern`
    stricter in the same way, tests like `pos/t2486.scala` would fail.
    
    I have introduced a new symbol test to (try to) identify pattern
    type variables introduced by `typedBind`. Its not pretty, and it
    might be cleaner to reserve a new flag for these.
    
    I've also included a test variation exercising with nested matches.
    The pattern type of the inner case can't, syntactically, refer to the
    pattern type variable of the enclosing case. If it could, we would
    have to be more selective in our wildcarding in `ptMatchesPatternType`
    by restricting ourselves to type variables associated with the closest
    enclosing `CaseDef`.
    
    As some further validation of the correctness of this patch,
    four stray warnings have been teased out of
    neg/unchecked-abstract.scala
    
    I also had to changes `typeArgsInTopLevelType` to extract the type
    arguments of `Array[T]` if `T` is an abstract type. This avoids the
    "Checkability checker says 'Uncheckable', but uncheckable type
    cannot be found" warning and consequent overly lenient analysis.
    Without this change, the warning was suppressed for:
    
        def warnArray[T] = (null: Any) match { case _: Array[T] => }

---
+++ test/files/neg/t8597b.scala
@@ -0,0 +1,21 @@
+object Unchecked {
+  (null: Any) match {
+    case _: Some[t] =>
+
+      // t is a fresh pattern type variable, despite our attempts to
+      // backtick our way to the enclosing `t`. Under this interpretation,
+      // the absense of an unchecked warning is expected.
+      (null: Any) match {
+        case _: Some[t] => // no warn
+      }
+      (null: Any) match {
+        case _: Some[`t`] => // no warn
+      }
+
+      // here we correctly issue an unchecked warning
+      type T = t
+      (null: Any) match {
+        case _: Some[T] => // warn
+      }
+  }
+}
