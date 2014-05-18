99b4ef8d8472f154d73160f5fe72daf081abb24e
Jason Zaugg <jzaugg@gmail.com>
2014-05-18 12:11:29 +0200

    Add a test for array load

--- test/files/run/t8601b.scala
+++ test/files/run/t8601b.scala
@@ -1,9 +1,11 @@
 object Test {
   def len(x: Array[String]): Unit = x.length
+  def load(x: Array[String]): Unit = x(0)
 
   def check(x: => Any) = try { x; sys.error("failed to throw NPE!") } catch { case _: NullPointerException => }
 
   def main(args: Array[String]) {
-    check(len(null))
+    check(len(null)) // bug: did not NPE
+    check(load(null))
   }
 }
