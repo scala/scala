0e87b12bcf68d9f52d2dee92ff19d04019641110
Adriaan Moors <adriaan.moors@typesafe.com>
2014-05-19 12:56:07 +0200

    Revert "SI-8601 Test that `null.synchronized` NPEs under -optimize"
    This reverts commit dcade51d751b389fb5137040f7e1006b4bc633c6.

--- test/files/run/t8601d.scala
+++
@@ -1,8 +0,0 @@
-object Test {
-  def monitor(x: AnyRef): Unit = {x.synchronized(()); ()}
-  def check(x: => Any) = try { x; sys.error("failed to throw NPE") } catch { case _: NullPointerException => }
-
-  def main(args: Array[String]) {
-    check(monitor(null))
-  }
-}
