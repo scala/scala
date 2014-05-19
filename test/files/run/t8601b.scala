b11a27e3e655105ec6f7640934072ba7966c88a3
Adriaan Moors <adriaan.moors@typesafe.com>
2014-05-19 12:56:10 +0200

    Revert "Add a test for array load"
    This reverts commit 99b4ef8d8472f154d73160f5fe72daf081abb24e.

--- test/files/run/t8601b.scala
+++ test/files/run/t8601b.scala
@@ -1,11 +1,9 @@
 object Test {
   def len(x: Array[String]): Unit = x.length
-  def load(x: Array[String]): Unit = x(0)
 
   def check(x: => Any) = try { x; sys.error("failed to throw NPE!") } catch { case _: NullPointerException => }
 
   def main(args: Array[String]) {
-    check(len(null)) // bug: did not NPE
-    check(load(null))
+    check(len(null))
   }
 }
