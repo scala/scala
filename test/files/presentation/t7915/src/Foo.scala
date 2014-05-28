d28879042951015f0ef2d5263b7df264a13a8055
Jason Zaugg <jzaugg@gmail.com>
2014-05-28 21:46:53 +0200

    SI-8596 Fix rangepos crasher with defaults, poly methods
    Regressed in SI-7915 / 3009a525b5
    
    We should be deriving the position of the synthetic `Select`
    from `basefun1`, rather than `basefun`. In the new, enclosed
    test, the difference amounts to:
    
        new Container().typeParamAndDefaultArg[Any]()
        `------------ basefun1 --------------'
        `----------------- basefun ---------------'
    
    For monomorphic methods, these are one and the same, which is
    why `presentation/t7915` was working. I've extended that test
    to a polymorphic method to check that hyperlink resolution works.

--- test/files/presentation/t7915/src/Foo.scala
+++ test/files/presentation/t7915/src/Foo.scala
@@ -1,9 +1,11 @@
 class Bar {
-  def bar(b: Int = 2) {}
+  def bar(b: Int = 2) {}; def baz[X](b: Int = 2) {}
 }
 
 class Foo {
   def foo() {
     new Bar/*#*/().bar/*#*/()
+    new Bar/*#*/().baz/*#*/[Any]()
+    new Bar/*#*/().baz/*#*/()
   }
 }
