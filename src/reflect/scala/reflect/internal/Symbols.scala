63207e115a46634d47446a87a7f4bc3c2651b0e7
Lukas Rytz <lukas.rytz@gmail.com>
2014-09-10 13:59:03 +0200

    isAnonymousClass/Function for delambdafy classes is not true
    Ydelambdafy:method lambda classes are not anonymous classes, and not
    anonymous function classes either. They are somethig new, so there's
    a new predicate isDelambdafyFunction.
    
    They are not anonymous classes (or functions) because anonymous
    classes in Java speak are nested. Delambdafy classes are always
    top-level, they are just synthetic.
    
    Before this patch, isAnonymous was sometimes accidentailly true: if
    the lambda is nested in an anonymous class. Now it's always false.

--- src/reflect/scala/reflect/internal/Symbols.scala
+++ src/reflect/scala/reflect/internal/Symbols.scala
@@ -789,6 +789,7 @@ trait Symbols extends api.Symbols { self: SymbolTable =>
       isMethod && owner.isDerivedValueClass && !isParamAccessor && !isConstructor && !hasFlag(SUPERACCESSOR) && !isMacro && !isSpecialized
 
     final def isAnonymousFunction = isSynthetic && (name containsName tpnme.ANON_FUN_NAME)
+    final def isDelambdafyFunction = isSynthetic && (name containsName tpnme.DELAMBDAFY_LAMBDA_CLASS_NAME)
     final def isDefinedInPackage  = effectiveOwner.isPackageClass
     final def needsFlatClasses    = phase.flatClasses && rawowner != NoSymbol && !rawowner.isPackageClass
 
