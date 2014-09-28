dc548947a32f7923fdb5ed4f0a9c8454773b1a05
Jason Zaugg <jzaugg@gmail.com>
2014-09-28 10:24:48 +1000

    Avoid test fragility to changes in Predef
    The check file used to contain a stack trace entry from
    Predef with a line number. I've made the macro fail
    in a different manner that avoids this fragility.

--- test/files/neg/macro-invalidret/Impls_1.scala
+++ test/files/neg/macro-invalidret/Impls_1.scala
@@ -4,7 +4,7 @@ import scala.reflect.runtime.{universe => ru}
 object Impls {
   def foo1(c: Context) = 2
   def foo2(c: Context) = ru.Literal(ru.Constant(42))
-  def foo3(c: Context) = ???
+  def foo3(c: Context) = throw null
   def foo5(c: Context) = c.universe.Literal(c.universe.Constant(42))
   def foo6(c: Context) = c.Expr[Int](c.universe.Literal(c.universe.Constant(42)))
 }
