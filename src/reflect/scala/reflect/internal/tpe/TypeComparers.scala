55edad3bcd0a6eb781d1e75509bdefdb1e3b9c72
Adriaan Moors <adriaan.moors@typesafe.com>
2014-11-04 10:55:56 +0100

    simplify subtyping of SingletonType?

--- src/reflect/scala/reflect/internal/tpe/TypeComparers.scala
+++ src/reflect/scala/reflect/internal/tpe/TypeComparers.scala
@@ -372,8 +372,8 @@ trait TypeComparers {
   private def isSubType2(tp1: Type, tp2: Type, depth: Depth): Boolean = {
     def retry(lhs: Type, rhs: Type) = ((lhs ne tp1) || (rhs ne tp2)) && isSubType(lhs, rhs, depth)
 
-    // TODO SIP-23: should this just be tp1.isInstanceOf[SingletonType] && tp2.isInstanceOf[SingletonType]
-    if (isSingleType(tp1) && isSingleType(tp2) || isConstantType(tp1) && isConstantType(tp2))
+    // TODO SIP-23
+    if (tp1.isInstanceOf[SingletonType] && tp2.isInstanceOf[SingletonType])
       return (tp1 =:= tp2) || isThisAndSuperSubtype(tp1, tp2) || retry(tp1.underlying, tp2)
 
     if (tp1.isHigherKinded || tp2.isHigherKinded)
