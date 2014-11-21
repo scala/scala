20cafc969d1ebc4f2497645b9f1d11b9074bc217
Rex Kerr <ichoran@gmail.com>
2014-11-21 15:22:04 -0800

    SI-8951  SeqView and StreamView stack-overflow on negative index after flatMap
    Checks and throws IndexOutOfBounds now.
    
    No test, caught and checked by scala-collections-laws (line 405 presently).

--- src/library/scala/collection/SeqViewLike.scala
+++ src/library/scala/collection/SeqViewLike.scala
@@ -83,6 +83,7 @@ trait SeqViewLike[+A,
     }
     def length = index(self.length)
     def apply(idx: Int) = {
+      if (idx < 0 || idx >= self.length) throw new IndexOutOfBoundsException(idx.toString)
       val row = findRow(idx, 0, self.length - 1)
       mapping(self(row)).seq.toSeq(idx - index(row))
     }
