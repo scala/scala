f50eca1dc2961141137af879686b9c60b00f2ba1
Som Snytt <som.snytt@gmail.com>
2014-11-23 08:49:25 -0800

    SI-8924 Improve test safety
    Make the test less dangerous: no need to hold allocations;
    stop allocating if the heap is grown, as it probably doesn't
    even matter why it grew, for purposes of the test.

--- test/junit/scala/collection/LinearSeqLikeTest.scala
+++ test/junit/scala/collection/LinearSeqLikeTest.scala
@@ -6,27 +6,31 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 import java.lang.ref._
+import java.lang.management._
+import java.lang.Thread.{ `yield` => share }
 
 @RunWith(classOf[JUnit4])
 class LinearSeqLikeTest {
 
-  // Wait for the list ref to go stale.
-  // A helper thread allocates to goose collection.
-  // The allocation delta must be small enough to create
-  // pressure without failing outright.
-  // In failure mode, the OOME causes the test thread
-  // to fail with InterruptedException.
-  // The test completes quickly.
+  /** Exhaust iterator and wait for ref to underlying list to go stale.
+   *
+   *  A helper thread allocates to goose collection.
+   *  The allocation delta must be small enough to create
+   *  pressure without failing outright.
+   *
+   *  In failure mode, the heap bump (or OOME) causes the test thread
+   *  to fail with InterruptedException.
+   *
+   *  To avoid false negatives on heap bump, the reference queue is checked
+   *  a second time, so in that case the timeout period is always incurred.
+   *  The long timeout accommodates loaded test servers.
+   *
+   *  Otherwise, the test completes quickly.
+   */
   @Test
   def exhaustedIteratorMustNotReferenceUnderlyingSeq(): Unit = {
-    //import java.lang.management._
-    //Console println ManagementFactory.getMemoryMXBean.getHeapMemoryUsage
-
     exhaustedIteratorTestFor(List(1, 2, 3), 2)
-
     exhaustedIteratorTestFor(mutable.MutableList(1, 2, 3), 2)
-
-    //Console println ManagementFactory.getMemoryMXBean.getHeapMemoryUsage
   }
 
   private def exhaustedIteratorTestFor[A, Repr <: LinearSeqLike[A, Repr]](gen: => Repr, two: A): Unit = {
@@ -47,39 +51,76 @@ class LinearSeqLikeTest {
     it.next()                                // exercise
     val rest = it.toList                     // must exhaust iterator
 
-    assertTrue(it.isEmpty)
-    assertEquals(two, rest.head)
+    assertTrue(it.isEmpty)                   // exhausted
+    assertEquals(two, rest.head)             // sanity check
 
-    // apply gentle pressure
-    class Alloc(val parent: Thread) extends Runnable {
-      @volatile var done  = false
-      @volatile var count = 0
-
-      var allocated  = List.empty[Array[Byte]]
-      val allocation = 1 * 1024 * 1024
-
-      override def run(): Unit = {
-        while (!done) {
-          try {
-            allocated = Array.ofDim[Byte](allocation) :: allocated
-          } catch {
-            case _: OutOfMemoryError => parent.interrupt() ; done = true
-          }
-          count += 1
-          Thread.`yield`()
-        }
-      }
-    }
     val alloc   = new Alloc(Thread.currentThread)
     val t       = new Thread(alloc)
     val timeout = 60 * 1000L                 // wait a minute
+
+    // in case alloc failed spuriously (maybe due to other activity in the JVM)
+    // give gc another chance to do the right thing
+    def retry() = {
+      assertTrue(alloc.fault)
+      val retry = refq.remove(timeout)
+      if (retry == null) fail(s"Memory exhaustion (${alloc.cause})")
+      else retry
+    }
     val got =
       try {
         t.start()
         refq.remove(timeout)
-      } finally alloc.done = true
-    assertEquals(ref, got)
-    assertNull(ref.get)
+      } catch {
+        case _: InterruptedException if alloc.fault => retry()
+        case e: InterruptedException                => throw e
+      } finally {
+        try {
+          alloc.done = true
+          t.join(timeout)
+        } catch {
+          // ignore error after refq.remove completes
+          case e: InterruptedException => if (!alloc.fault) throw e
+        }
+      }
+    assertEquals("Timeout remove", ref, got) // null on timeout
+    assertNull("Not collectable", ref.get)   // collection was collectable
     //Console println s"Done after ${alloc.count}"
+  }
+
+  // apply gentle pressure
+  private class Alloc(val parent: Thread) extends Runnable {
+    @volatile var done  = false
+    @volatile var fault = false
+    @volatile var cause = "NONE"
+    @volatile var count = 0
+
+    var allocated  = Array.empty[Byte]
+    val allocation = 1 * 1024 * 1024
+
+    // allocate until heap grows or OOM, then interrupt parent.
+    override def run(): Unit = {
+      val mxb = ManagementFactory.getMemoryMXBean
+      val mem = mxb.getHeapMemoryUsage
+      //Console println mem
+      def fail(what: String) =
+        if (!done) {
+          done  = true
+          fault = true
+          cause = what
+          parent.interrupt()
+          //Console println mxb.getHeapMemoryUsage
+        }
+      while (!done) {
+        try {
+          allocated = Array.ofDim[Byte](allocation)
+          count += 1
+          if (mxb.getHeapMemoryUsage.getCommitted > mem.getCommitted) fail("BUMP")
+        } catch {
+          case _: OutOfMemoryError => fail("OOME")
+        }
+        assertEquals(allocation, allocated.length)
+        share()                              // share the CPU
+      }
+    }
   }
 }
