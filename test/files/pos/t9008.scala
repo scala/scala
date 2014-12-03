scala/scala

commit 84d46719a9ab3caa7ff46af4630d75f8c407c3d2

SI-8869 Prevent ill-kindedness in type lambdas
When type checking an type application, the arguments are allowed
to be of kinds other than *. This leniency is controlled by the
`ContextMode` bit `TypeConstructorAllowed`.

(More fine grained checking of matching arity a bounds of type
constructors is deferred until the refchecks phase to avoid
cycles during typechecking.)

However, this bit is propagated to child contexts, which means
that we fail to report this error in the lexical context marked
here:

    T[({type x = Option}#x)]
        `-------------'

This commit resets this bit to false in any child context
relates to a different tree from its parent.

src/compiler/scala/tools/nsc/typechecker/Contexts.scala
@@ -480,6 +480,8 @@ trait Contexts { self: Analyzer =>
       // SI-8245 `isLazy` need to skip lazy getters to ensure `return` binds to the right place
       c.enclMethod         = if (isDefDef && !owner.isLazy) c else enclMethod
 
+      if (tree != outer.tree) c(TypeConstructorAllowed) = false
+
       registerContext(c.asInstanceOf[analyzer.Context])
       debuglog("[context] ++ " + c.unit + " / " + tree.summaryString)
       c

test/files/neg/t8869.check
@@ -1,0 +1,7 @@
+t8869.scala:5: error: class Option takes type parameters
+  def value: TC[({type l1[x] = Option})#l1] = ??? // error not reported!
+                               ^
+t8869.scala:7: error: class Option takes type parameters
+  type l2[x] = Option // error correctly reported
+               ^
+two errors found

test/files/neg/t8869.scala
@@ -1,0 +1,10 @@
+class TC[T[_]] {
+  def identity[A](a: T[A]): T[A] = a
+}
+object Test {
+  def value: TC[({type l1[x] = Option})#l1] = ??? // error not reported!
+
+  type l2[x] = Option // error correctly reported
+  def value1: TC[l2] = ???
+}
+

commit 64ae23f6438054a08bebf1fa6af0f14d63cfc0ea

SI-9008 Fix regression with higher kinded existentials
Allow a naked type constructor in an existential type if
we are directly within a type application.

Recently, 84d4671 changed nested context creation to avoid passing
down the `TypeConstructorAllowed`, which led to missing kind errors
in code like `type T[({type M = List})#M]`.

However, when typechecking `T forSome { quantifiers }`, we create
a nested context to represent the nested scope introduced for the
quantifiers. But we need to propagate the `TypeConstructorAllowed`
bit to the nested context to allow for higher kinded existentials.

The enclosed tests show:
  - pos/t9008  well kinded application of an hk existential
  - neg/t9008  hk existential forbidden outside of type application
  - neg/t9008b kind error reported for hk existential

Regressed in 84d4671.

src/compiler/scala/tools/nsc/typechecker/Contexts.scala
@@ -480,7 +480,8 @@ trait Contexts { self: Analyzer =>
       // SI-8245 `isLazy` need to skip lazy getters to ensure `return` binds to the right place
       c.enclMethod         = if (isDefDef && !owner.isLazy) c else enclMethod
 
-      if (tree != outer.tree) c(TypeConstructorAllowed) = false
+      if (tree != outer.tree)
+        c(TypeConstructorAllowed) = false
 
       registerContext(c.asInstanceOf[analyzer.Context])
       debuglog("[context] ++ " + c.unit + " / " + tree.summaryString)

src/compiler/scala/tools/nsc/typechecker/Typers.scala
@@ -5207,7 +5207,11 @@ trait Typers extends Adaptations with Tags with TypersTracking with PatternTyper
 
       def typedExistentialTypeTree(tree: ExistentialTypeTree) = {
         val tree1 = typerWithLocalContext(context.makeNewScope(tree, context.owner)){
-          _.typedExistentialTypeTree(tree, mode)
+          typer =>
+            if (context.inTypeConstructorAllowed)
+              typer.context.withinTypeConstructorAllowed(typer.typedExistentialTypeTree(tree, mode))
+            else
+              typer.typedExistentialTypeTree(tree, mode)
         }
         checkExistentialsFeature(tree1.pos, tree1.tpe, "the existential type")
         tree1

test/files/neg/t9008.check
@@ -1,0 +1,4 @@
+t9008.scala:2: error: type M takes type parameters
+  def x: List[M forSome { type M[_] }] = ???
+              ^
+one error found

test/files/neg/t9008.scala
@@ -1,0 +1,3 @@
+object Test {
+  def x: List[M forSome { type M[_] }] = ???
+}

test/files/neg/t9008b.check
@@ -1,0 +1,4 @@
+t9008b.scala:2: error: type M takes type parameters
+  type T = M forSome { type M[_] }
+           ^
+one error found

test/files/neg/t9008b.scala
@@ -1,0 +1,3 @@
+object Test {
+  type T = M forSome { type M[_] }
+}

test/files/pos/t9008.scala
@@ -1,0 +1,5 @@
+trait Monad[M[_]]
+
+object Test {
+  def x: Monad[M forSome { type M[_] }] = ???
+}