30e6a0c1f63344343bcaeb92899f6da95bb6e0a2
Simon Ochsenreither <simon@ochsenreither.de>
2014-12-06 13:56:35 +0100

    SI-7566 Remove use of String=>TermName implicit

--- src/partest/scala/tools/partest/CompilerTest.scala
+++ src/partest/scala/tools/partest/CompilerTest.scala
@@ -49,7 +49,7 @@ abstract class CompilerTest extends DirectTest {
   }
 
   class SymsInPackage(pkgName: String) {
-    def pkg     = rootMirror.getPackage(pkgName)
+    def pkg     = rootMirror.getPackage(TermName(pkgName))
     def classes = allMembers(pkg) filter (_.isClass)
     def modules = allMembers(pkg) filter (_.isModule)
     def symbols = classes ++ terms filterNot (_ eq NoSymbol)
