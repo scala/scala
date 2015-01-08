bd28de002011db515921ddc7614b8dc0d78c7e48
Maks Atygaev <jxcoder@ya.ru>
2015-01-09 01:33:29 +0600

    Trivial refactoring of scala / actors

--- src/actors/scala/actors/Actor.scala
+++ src/actors/scala/actors/Actor.scala
@@ -1,6 +1,6 @@
 /*                     __                                               *\
 **     ________ ___   / /  ___     Scala API                            **
-**    / __/ __// _ | / /  / _ |    (c) 2005-2013, LAMP/EPFL             **
+**    / __/ __// _ | / /  / _ |    (c) 2005-2015, LAMP/EPFL             **
 **  __\ \/ /__/ __ |/ /__/ __ |    http://scala-lang.org/               **
 ** /____/\___/_/ |_/____/_/ | |                                         **
 **                          |/                                          **
@@ -407,5 +407,4 @@ trait Actor extends InternalActor with ReplyReactor {
     this
   }
 
-  }
-
+}
