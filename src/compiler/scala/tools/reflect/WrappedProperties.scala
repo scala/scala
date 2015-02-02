cbc8c596674027580e00c9266cd1ef32c94f7253
Ikumi Shimizu <193s@users.noreply.github.com>
2015-02-02 13:52:11 +0900

    SI-9246 *OrElse should have a by-name parameter
    The `*OrElse` methods in `scala.tools.reflect.WrappedProperties` and
    `scala.util.Properties` should have a by-name parameter
    
    This needs an upgrade to partest 1.0.6, which avoids these methods.

--- src/compiler/scala/tools/reflect/WrappedProperties.scala
+++ src/compiler/scala/tools/reflect/WrappedProperties.scala
@@ -19,13 +19,13 @@ trait WrappedProperties extends PropertiesTrait {
   protected def propCategory   = "wrapped"
   protected def pickJarBasedOn = this.getClass
 
-  override def propIsSet(name: String)               = wrap(super.propIsSet(name)) exists (x => x)
-  override def propOrElse(name: String, alt: String) = wrap(super.propOrElse(name, alt)) getOrElse alt
-  override def setProp(name: String, value: String)  = wrap(super.setProp(name, value)).orNull
-  override def clearProp(name: String)               = wrap(super.clearProp(name)).orNull
-  override def envOrElse(name: String, alt: String)  = wrap(super.envOrElse(name, alt)) getOrElse alt
-  override def envOrNone(name: String)               = wrap(super.envOrNone(name)).flatten
-  override def envOrSome(name: String, alt: Option[String]) = wrap(super.envOrNone(name)).flatten orElse alt
+  override def propIsSet(name: String)                  = wrap(super.propIsSet(name)) exists (x => x)
+  override def propOrElse(name: String, alt: => String) = wrap(super.propOrElse(name, alt)) getOrElse alt
+  override def setProp(name: String, value: String)     = wrap(super.setProp(name, value)).orNull
+  override def clearProp(name: String)                  = wrap(super.clearProp(name)).orNull
+  override def envOrElse(name: String, alt: => String)  = wrap(super.envOrElse(name, alt)) getOrElse alt
+  override def envOrNone(name: String)                  = wrap(super.envOrNone(name)).flatten
+  override def envOrSome(name: String, alt: => Option[String]) = wrap(super.envOrNone(name)).flatten orElse alt
 
   def systemProperties: List[(String, String)] = {
     import scala.collection.JavaConverters._
