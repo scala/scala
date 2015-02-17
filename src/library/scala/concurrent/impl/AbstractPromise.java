30a6b6942b3170c3c380f72f3f72b8653ce4f578
Mariot Chauvin <mariot.chauvin@guardian.co.uk>
2015-02-17 09:14:35 +0000

    SI-8362: Support for Future without sun.misc.Unsafe.
    Instead of using AtomicReferenceFieldUpdater extends directly AtomicReference as AtomicReferenceFieldUpdater.compareAndSet() isn't necessarily even inlined by Hotspot, while AtomicReference.compareAndSet() should be compiled down to the machine's CAS instruction.
    The implementation is similar to the 2.12.x branch’s one.
    
    Add an exclusion rule for mima to not check backward and forward compatibility as the class is package protected and in an imp package.

--- src/library/scala/concurrent/impl/AbstractPromise.java
+++ src/library/scala/concurrent/impl/AbstractPromise.java
@@ -9,51 +9,17 @@
 package scala.concurrent.impl;
 
 
-import scala.concurrent.util.Unsafe;
-import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 
-abstract class AbstractPromise {
-    private volatile Object _ref;
-
-    final static long _refoffset;
-    /*
-     * In a runtime with a security manager installed, or a non-Oracle runtime,
-     * the Unsafe class may be unavailable. In this case, _refoffset will be
-     * set to -1.
-     *
-     * We assume that a valid field offset will be non-negative, and likely a
-     * multiple of 4.
-     */
-    static {
-    	long refoffset = -1;
-	try {
-	    refoffset = Unsafe.instance.objectFieldOffset(AbstractPromise.class.getDeclaredField("_ref"));
-	} catch (Throwable t) {
-	    /* Unsafe object instance access failed */
-	}
-	_refoffset = refoffset;
-    }
+abstract class AbstractPromise extends AtomicReference<Object> {
     
-    /*
-     * The Hotspot compiler can identify that the value of _refoffset never
-     * changes, and compile only the branch below that is used. When the Unsafe
-     * object is available, the compare-and-swap operation will be inlined
-     * directly into *this* method's caller.
-     */
     protected final boolean updateState(Object oldState, Object newState) {
-    	if (_refoffset == -1) {
-	    return updater.compareAndSet(this, oldState, newState);
-	} else {
-	    return Unsafe.instance.compareAndSwapObject(this, _refoffset, oldState, newState);
-	}
+        return compareAndSet(oldState, newState);
     }
 
     protected final Object getState() {
-	return _ref;
+        return get();
     }
-
-    protected final static AtomicReferenceFieldUpdater<AbstractPromise, Object> updater =
-	AtomicReferenceFieldUpdater.newUpdater(AbstractPromise.class, Object.class, "_ref");
 }
