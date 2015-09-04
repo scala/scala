097e25b303f11d70f79c1e109d1e6b3669fe45a5
Janek Bogucki <janekdb@gmail.com>
2015-09-04 21:51:21 +0100

    Update links to docs, codehaus and citeseer
    docs.scala-lang.org
    
     - Align some links to new layout for docs.scala-lang.org
     - Include link to concrete parallel collection performance characteristics
    
    codehaus
    
     - Subsitute a link to a JIRA email for the 404 JRUBY-3576 JIRA link
       in Codec.scala. jira.codehaus.org is not redirecting this.
    
    citeseer
    
     - Replace the citeseer link with a direct link to a PDF which is not
       behind a login challenge.

--- src/reflect/scala/reflect/macros/package.scala
+++ src/reflect/scala/reflect/macros/package.scala
@@ -10,14 +10,14 @@ package reflect
  *  Within these functions the programmer has access to compiler APIs.
  *  For example, it is possible to generate, analyze and typecheck code.
  *
- *  See the [[http://docs.scala-lang.org/overviews/macros.html Macros Guide]] on how to get started with Scala macros.
+ *  See the [[http://docs.scala-lang.org/overviews/macros/overview.html Macros Guide]] on how to get started with Scala macros.
  */
 package object macros {
   /** The Scala macros context.
    *
    *  In Scala 2.11, macros that were once the one are split into blackbox and whitebox macros,
    *  with the former being better supported and the latter being more powerful. You can read about
-   *  the details of the split and the associated trade-offs in the [[http://docs.scala-lang.org/overviews/macros.html Macros Guide]].
+   *  the details of the split and the associated trade-offs in the [[http://docs.scala-lang.org/overviews/macros/overview.html Macros Guide]].
    *
    *  `scala.reflect.macros.Context` follows this tendency and turns into `scala.reflect.macros.blackbox.Context`
    *  and `scala.reflect.macros.whitebox.Context`. The original `Context` is left in place for compatibility reasons,
