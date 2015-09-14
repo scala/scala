3cddeaa525fd6fe9860a27019fdf484297a8d3dd
Som Snytt <som.snytt@gmail.com>
2015-09-13 17:22:11 -0700

    SI-7916: ScriptEngine support
    Refactor the ScriptEngine support to an adaptor atop the
    IMain API.
    
    Allow references to resolve to context attributes. (The
    attributes must be defined at compilation time, though
    they may resolve to updated values at evaluation time.)
    
    This means that attributes are not bound statically in
    REPL history. In particular, we forgo the trick of binding
    attributes named "name: Type" as typed values.
    
    Instead, an `x` bound in dynamic context is injected into
    the script as a dynamic selection `$ctx.x` where `ctx`
    performs the look-up in the script context.
    
    When a compiled script is re-evaluated, a new instance of
    the script class is created and defined symbols are
    rebound.
    
    The context stdout writer is handled with `Console.withOut`,
    with bytes decoded using the default charset.
    
    Compilation errors are thrown as ScriptException with the
    first reported error.
    
    This commit doesn't attempt dynamic selection from objects
    in context. Currently, script must cast.

--- test/files/run/t7843-jsr223-service.scala
+++
@@ -1,8 +0,0 @@
-import scala.tools.nsc.interpreter.IMain
-
-object Test extends App {
-  val engine = new IMain.Factory getScriptEngine()
-  engine.asInstanceOf[IMain].settings.usejavacp.value = true
-  engine put ("n", 10)
-  engine eval "1 to n.asInstanceOf[Int] foreach print"
-}
