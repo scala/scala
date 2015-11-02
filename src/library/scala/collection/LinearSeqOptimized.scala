e83f8f9e14e5b3f272d064e3c4ffcd95d0bc20c4
Dmitry Petrashko <dmitry.petrashko@gmail.com>
2015-11-02 11:25:13 +0100

    Remove procedure syntax in LinearSeqOptimized.

--- src/library/scala/collection/LinearSeqOptimized.scala
+++ src/library/scala/collection/LinearSeqOptimized.scala
@@ -67,7 +67,7 @@ trait LinearSeqOptimized[+A, +Repr <: LinearSeqOptimized[A, Repr]] extends Linea
   }
 
   override /*IterableLike*/
-  def foreach[B](f: A => B) {
+  def foreach[B](f: A => B): Unit = {
     var these = this
     while (!these.isEmpty) {
       f(these.head)
