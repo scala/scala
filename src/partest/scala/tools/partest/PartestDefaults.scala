1f68edb99b63e763894226f8a85609f35e4717ec
Som Snytt <som.snytt@gmail.com>
2015-11-02 22:21:42 -0800

    SI-7786 Partest finds javac
    Since partest is probably running with a JDK,
    use JDK_HOME/bin/javac for compilation, if it
    exists; otherwise, fall back to whatever is on
    the command line.
    
    Javac can still be specified explicitly.

--- src/partest/scala/tools/partest/PartestDefaults.scala
+++ src/partest/scala/tools/partest/PartestDefaults.scala
@@ -3,12 +3,13 @@ package partest
 
 import scala.concurrent.duration.Duration
 import scala.tools.nsc.Properties.{ propOrNone => prop }
+import scala.util.Properties.jdkHome
 import java.lang.Runtime.{ getRuntime => runtime }
 
 object PartestDefaults {
   def sourcePath  = prop("partest.srcdir")      getOrElse "files"
-  def javaCmd     = prop("partest.javacmd")     getOrElse "java"
-  def javacCmd    = prop("partest.javac_cmd")   getOrElse "javac"
+  def javaCmd     = prop("partest.javacmd")     orElse    jdkexec("java")  getOrElse "java"
+  def javacCmd    = prop("partest.javac_cmd")   orElse    jdkexec("javac") getOrElse "javac"
   def javaOpts    = prop("partest.java_opts")   getOrElse  ""     // opts when running java during tests
   def scalacOpts  = prop("partest.scalac_opts") getOrElse  ""
 
@@ -18,4 +19,13 @@ object PartestDefaults {
   def waitTime    = Duration(prop("partest.timeout") getOrElse "4 hours")
 
   //def timeout     = "1200000"   // per-test timeout
+
+  // probe for the named executable
+  private def jdkexec(name: String): Option[String] = {
+    import scala.reflect.io.Path, Path._
+    Some(Path(jdkHome) / "bin") filter (_.isDirectory) flatMap { p =>
+      val candidates = (p walkFilter { e => (e.name == name || e.name.startsWith(s"$name.")) && e.jfile.canExecute }).toList
+      (candidates find (_.name == name) orElse candidates.headOption) map (_.path)
+    }
+  }
 }
