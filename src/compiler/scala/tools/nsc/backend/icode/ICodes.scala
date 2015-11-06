e6b62aa4b4f2d7255c4c92ffa5b9402cb5f73dc2
Lukas Rytz <lukas.rytz@gmail.com>
2015-11-06 11:08:52 +0100

    Remove ICode's TypeKinds, use BTypes in the backend instead

--- src/compiler/scala/tools/nsc/backend/icode/ICodes.scala
+++ src/compiler/scala/tools/nsc/backend/icode/ICodes.scala
@@ -11,19 +11,7 @@ package icode
  *
  *  @author Iulian Dragos
  */
-abstract class ICodes extends AnyRef
-                                 with Members
-                                 with TypeKinds
-                                 with Primitives
-{
+abstract class ICodes extends AnyRef with Members with Primitives {
   val global: Global
-  import global.definitions
-
-  lazy val AnyRefReference: TypeKind    = REFERENCE(definitions.AnyRefClass)
-  lazy val BoxedUnitReference: TypeKind = REFERENCE(definitions.BoxedUnitClass)
-  lazy val NothingReference: TypeKind   = REFERENCE(definitions.NothingClass)
-  lazy val NullReference: TypeKind      = REFERENCE(definitions.NullClass)
-  lazy val ObjectReference: TypeKind    = REFERENCE(definitions.ObjectClass)
-  lazy val StringReference: TypeKind    = REFERENCE(definitions.StringClass)
 }
 
