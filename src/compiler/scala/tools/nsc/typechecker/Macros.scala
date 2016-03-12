05b5a035cd2e9c47885a993f96fb79d498378ee6
Adriaan Moors <adriaan.moors@typesafe.com>
2016-03-11 17:59:21 -0800

    workaround for bug?

--- src/compiler/scala/tools/nsc/typechecker/Macros.scala
+++ src/compiler/scala/tools/nsc/typechecker/Macros.scala
@@ -368,7 +368,10 @@ trait Macros extends MacroRuntimes with Traces with Helpers {
     new {
       val universe: self.global.type = self.global
       val callsiteTyper: universe.analyzer.Typer = typer.asInstanceOf[global.analyzer.Typer]
-      val expandee = universe.analyzer.macroExpanderAttachment(expandeeTree).original orElse duplicateAndKeepPositions(expandeeTree)
+      val expandee = {
+        val orig = universe.analyzer.macroExpanderAttachment(expandeeTree).original
+        if (!orig.isEmpty) orig else duplicateAndKeepPositions(expandeeTree)
+      }
     } with UnaffiliatedMacroContext {
       val prefix = Expr[Nothing](prefixTree)(TypeTag.Nothing)
       override def toString = "MacroContext(%s@%s +%d)".format(expandee.symbol.name, expandee.pos, enclosingMacros.length - 1 /* exclude myself */)
