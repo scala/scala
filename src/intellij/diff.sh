5f5cc186cbedff6dc55993cb3b0dcfe36038efa1
Lukas Rytz <lukas.rytz@gmail.com>
2016-03-16 21:55:08 +0100

    Update IntelliJ build for use with sbt

--- src/intellij/diff.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#
-# Diffs the SAMPLE files against the working project config.
-#
-export SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-for f in "$SCRIPT_DIR"/*.{iml,ipr}; do
-	echo $f; diff -u $f.SAMPLE $f;
-done
