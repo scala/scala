85057d542c7e201191544415ff454afb243aa104
Simon Ochsenreither <simon@ochsenreither.de>
2016-05-19 18:46:49 +0200

    Add documentation to @deprecated

--- src/library/scala/deprecatedOverriding.scala
+++ src/library/scala/deprecatedOverriding.scala
@@ -12,9 +12,28 @@ package scala
  *
  *  Overriding such a member in  a sub-class then generates a warning.
  *
+ *  {{{
+ *  class Foo {
+ *    @deprecatedOverriding("this method will be made final", "2.12")
+ *    def add(x: Int, y: Int) = x + y
+ *  }
+ *  }}}
+ *
+ *  {{{
+ *  class Bar extends Foo // no deprecation warning
+ *  class Baz extends Foo {
+ *    override def add(x: Int, y: Int) = x - y
+ *  }
+ *  // warning: overriding method add in class Foo is deprecated (since 2.12): this method will be made final
+ *  // override def add(x: Int, y: Int) = x - y
+ *  //              ^
+ *  }}}
+ *
  *  @param  message the message to print during compilation if the member was overridden
  *  @param  since   a string identifying the first version in which overriding was deprecated
  *  @since  2.10
+ *  @see    [[scala.deprecated]]
  *  @see    [[scala.deprecatedInheritance]]
+ *  @see    [[scala.deprecatedName]]
  */
 class deprecatedOverriding(message: String = "", since: String = "") extends scala.annotation.StaticAnnotation
