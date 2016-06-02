f283bd5bd56f9bef7a735864464407c7b3b1d731
Adriaan Moors <adriaan.moors@typesafe.com>
2016-06-02 00:34:08 -0700

    drop fields for lazy and preinit vals

--- src/compiler/scala/tools/nsc/transform/Mixin.scala
+++ src/compiler/scala/tools/nsc/transform/Mixin.scala
@@ -172,13 +172,14 @@ abstract class Mixin extends InfoTransform with ast.TreeDSL {
       assert(phase == currentRun.mixinPhase, phase)
 
       for (member <- clazz.info.decls) {
-        if (!member.isMethod && !member.isModule && !member.isModuleVar) {
+        if (member.isMethod) publicizeTraitMethod(member)
+        else {
           assert(member.isTerm && !member.isDeferred, member)
-          assert(member hasFlag LAZY, member)
+          assert(member hasFlag LAZY | PRESUPER, member)
           // lazy vals still leave field symbols lying around in traits -- TODO: never emit them to begin with
           clazz.info.decls.unlink(member)
         }
-        else if (member.isMethod) publicizeTraitMethod(member)
+
       }
       debuglog("new defs of " + clazz + " = " + clazz.info.decls)
     }
