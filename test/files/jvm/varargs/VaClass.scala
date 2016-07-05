0d2760dce189cdcb363e54868381175af4b2646f
Lukas Rytz <lukas.rytz@gmail.com>
2016-07-05 14:22:00 +0200

    SI-8786 fix generic signature for @varargs forwarder methods
    When generating a varargs forwarder for
    
        def foo[T](a: T*)
    
    the parameter type of the forwarder needs to be Array[Object]. If we
    gnerate Array[T] in UnCurry, that would be erased to plain Object, and
    the method would not be a valid varargs.
    
    Unfortunately, setting the parameter type to Array[Object] lead to
    an invalid generic signature - the generic signature should reflect the
    real signature.
    
    This change adds an attachment to the parameter symbol in the varargs
    forwarder method and special-cases signature generation.
    
    Also cleanes up the code to produce the varargs forwarder. For example,
    type parameter and parameter symbols in the forwarder's method type were
    not clones, but the same symbols from the original method were re-used.

--- test/files/jvm/varargs/VaClass.scala
+++ test/files/jvm/varargs/VaClass.scala
@@ -1,15 +1,8 @@
-
-
 import annotation.varargs
 
-
-
 class VaClass {
-
   @varargs def vs(a: Int, b: String*) = println(a + b.length)
   @varargs def vi(a: Int, b: Int*) = println(a + b.sum)
   @varargs def vt[T](a: Int, b: T*) = println(a + b.length)
-
-  // TODO remove type bound after fixing SI-8786, see also https://github.com/scala/scala/pull/3961
-  @varargs def vt1[T <: String](a: Int, b: T*): T = b.head
+  @varargs def vt1[T](a: Int, b: T*): T = b.head
 }
