ffe1829708d9c7c320ae441b45bb699045f4add1
Pavel Petlinsky <p.petlinsky@rambler-co.ru>
2016-07-08 16:50:49 +0300

    SI-9813 Add Java 1.6. and 1.8 java.lang.Math methods into scala.math.package

--- src/library/scala/math/package.scala
+++ src/library/scala/math/package.scala
@@ -199,4 +199,96 @@ package object math {
 
   /** Returns the size of an ulp of the given `Float` value. */
   def ulp(x: Float): Float = java.lang.Math.ulp(x)
+
+  /** Returns the sum of its arguments, throwing an exception if the result overflows an `Int`. */
+  def addExact(x: Int, y: Int): Int = java.lang.Math.addExact(x, y)
+
+  /** Returns the sum of its arguments, throwing an exception if the result overflows a `Long`. */
+  def addExact(x: Long, y: Long): Long = java.lang.Math.addExact(x, y)
+
+  /** Returns the difference of the arguments, throwing an exception if the result overflows an `Int`. */
+  def subtractExact(x: Int, y: Int): Int = java.lang.Math.subtractExact(x, y)
+
+  /** Returns the difference of the arguments, throwing an exception if the result overflows a `Long`. */
+  def subtractExact(x: Long, y: Long): Long = java.lang.Math.subtractExact(x, y)
+
+  /** Returns the product of the arguments, throwing an exception if the result overflows an `Int`. */
+  def multiplyExact(x: Int, y: Int): Int = java.lang.Math.multiplyExact(x, y)
+
+  /** Returns the product of the arguments, throwing an exception if the result overflows a `Long`. */
+  def multiplyExact(x: Long, y: Long): Long = java.lang.Math.multiplyExact(x, y)
+
+  /** Returns the argument incremented by one, throwing an exception if the result overflows an `Int`. */
+  def incrementExact(a: Int): Int = java.lang.Math.incrementExact(a)
+
+  /** Returns the argument incremented by one, throwing an exception if the result overflows a `Long`. */
+  def incrementExact(a: Long) =  java.lang.Math.incrementExact(a)
+
+  /** Returns the argument decremented by one, throwing an exception if the result overflows an `Int`. */
+  def decrementExact(a: Int) =  java.lang.Math.decrementExact(a)
+
+  /** Returns the argument decremented by one, throwing an exception if the result overflows a `Long`. */
+  def decrementExact(a: Long) =  java.lang.Math.decrementExact(a)
+
+  /** Returns the negation of the argument, throwing an exception if the result overflows an `Int`. */
+  def negateExact(a: Int) =  java.lang.Math.negateExact(a)
+
+  /** Returns the negation of the argument, throwing an exception if the result overflows a `Long`. */
+  def negateExact(a: Long) =  java.lang.Math.negateExact(a)
+
+  /** Returns the value of the long argument; throwing an exception if the value overflows an `Int`. */
+  def toIntExact(value: Long): Int = java.lang.Math.toIntExact(value)
+
+  /** Returns the largest (closest to positive infinity) `Int` value
+    * that is less than or equal to the algebraic quotient. */
+  def floorDiv(x: Int, y: Int): Int = java.lang.Math.floorDiv(x, y)
+
+  /** Returns the largest (closest to positive infinity) `Long` value
+    * that is less than or equal to the algebraic quotient. */
+  def floorDiv(x: Long, y: Long): Long = java.lang.Math.floorDiv(x, y)
+
+  /** Returns the floor modulus of the `Int` arguments. */
+  def floorMod(x: Int, y: Int): Int = java.lang.Math.floorMod(x, y)
+
+  /** Returns the floor modulus of the `Long` arguments. */
+  def floorMod(x: Long, y: Long): Long = java.lang.Math.floorMod(x, y)
+
+  /** Returns the first floating-point argument with the sign of the second floating-point argument. */
+  def copySign(magnitude: Double, sign: Double): Double = java.lang.Math.copySign(magnitude, sign)
+
+  /** Returns the first floating-point argument with the sign of the second floating-point argument. */
+  def copySign(magnitude: Float, sign: Float): Float = java.lang.Math.copySign(magnitude, sign)
+
+  /** Returns the unbiased exponent used in the representation of a `Double`. */
+  def getExponent(f: Float): Int = java.lang.Math.getExponent(f)
+
+  /** Returns the unbiased exponent used in the representation of a `Float`. */
+  def getExponent(f: Double): Int = java.lang.Math.getExponent(f)
+
+  /** Returns the floating-point number adjacent to the first argument in the direction of the second argument. */
+  def nextAfter(start: Double, direction: Double): Double = java.lang.Math.nextAfter(start, direction)
+
+  /** Returns the floating-point number adjacent to the first argument in the direction of the second argument. */
+  def nextAfter(start: Float, direction: Float): Float = java.lang.Math.nextAfter(start, direction)
+
+  /** Returns the floating-point value adjacent to d in the direction of positive infinity. */
+  def nextUp(d: Double): Double = java.lang.Math.nextUp(d)
+
+  /** Returns the floating-point value adjacent to f in the direction of positive infinity. */
+  def nextUp(d: Float): Float = java.lang.Math.nextUp(d)
+
+  /** Returns the floating-point value adjacent to d in the direction of negative infinity. */
+  def nextDown(d: Double): Double = java.lang.Math.nextUp(d)
+
+  /** Returns the floating-point value adjacent to f in the direction of negative infinity. */
+  def nextDown(d: Float): Float = java.lang.Math.nextUp(d)
+
+  /** Returns d × 2scaleFactor rounded as if performed by a single correctly rounded floating-point
+    * multiply to a member of the `Double` value set. */
+  def scalb(d: Double, scaleFactor: Int): Double = java.lang.Math.scalb(d, scaleFactor)
+
+  /** Returns d × 2scaleFactor rounded as if performed by a single correctly rounded floating-point
+    * multiply to a member of the `Float` value set. */
+  def scalb(d: Float, scaleFactor: Int): Float = java.lang.Math.scalb(d, scaleFactor)
+
 }
