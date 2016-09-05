0d74b87ea68102305b1f93026236b7b03539fdd9
Jason Zaugg <jzaugg@gmail.com>
2016-09-05 16:57:13 +1000

    Cache setting-derived values, like isScala211, in the Run
    The computation of these performs a regex match on the value of
    `-Xsource`. Let's just do that once per run, rather than on each lookup.

--- src/reflect/scala/reflect/internal/SymbolTable.scala
+++ src/reflect/scala/reflect/internal/SymbolTable.scala
@@ -210,6 +210,18 @@ abstract class SymbolTable extends macros.Universe
   /** The run identifier of the given period. */
   final def runId(period: Period): RunId = period >> 8
 
+  trait RunSettings {
+    val runSettings: PerRunSettings = PerRunSettings
+  }
+
+  type PerRunSettings <: PerRunSettingsBase
+  protected def PerRunSettings: PerRunSettings
+  abstract class PerRunSettingsBase {
+    def isScala211: Boolean
+    def isScala212: Boolean
+  }
+  def currentRun: RunSettings with RunReporting
+
   /** The phase identifier of the given period. */
   final def phaseId(period: Period): Phase#Id = period & 0xFF
 
