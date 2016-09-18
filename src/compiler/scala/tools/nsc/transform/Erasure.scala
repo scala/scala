87b3d2cc41b175742da9301210299c3b1524a287
Jason Zaugg <jzaugg@gmail.com>
2016-09-18 15:31:15 +1000

    Optimize javaBinaryName callers
    ... by calling javaBinaryNameString, instead.
    
    They all are happy with a throw away String, there is no advantage
    to interning this into the name table.

--- src/compiler/scala/tools/nsc/transform/Erasure.scala
+++ src/compiler/scala/tools/nsc/transform/Erasure.scala
@@ -258,7 +258,7 @@ abstract class Erasure extends InfoTransform
     // Anything which could conceivably be a module (i.e. isn't known to be
     // a type parameter or similar) must go through here or the signature is
     // likely to end up with Foo<T>.Empty where it needs Foo<T>.Empty$.
-    def fullNameInSig(sym: Symbol) = "L" + enteringJVM(sym.javaBinaryName)
+    def fullNameInSig(sym: Symbol) = "L" + enteringJVM(sym.javaBinaryNameString)
 
     def jsig(tp0: Type, existentiallyBound: List[Symbol] = Nil, toplevel: Boolean = false, primitiveOK: Boolean = true): String = {
       val tp = tp0.dealias
