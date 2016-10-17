eb5c51383a63c5c3420e53ef021607ff5fd20296
Rory Graves <rory.graves@fieldmark.co.uk>
2016-10-17 20:25:31 +0100

    Optimised implementation of List.filter/filterNot

--- src/library/scala/collection/immutable/List.scala
+++ src/library/scala/collection/immutable/List.scala
@@ -86,6 +86,7 @@ sealed abstract class List[+A] extends AbstractSeq[A]
                                   with Product
                                   with GenericTraversableTemplate[A, List]
                                   with LinearSeqOptimized[A, List[A]]
+                                  with FilteredTraversableInternal[A, List[A]]
                                   with Serializable {
   override def companion: GenericCompanion[List] = List
 
@@ -416,6 +417,7 @@ sealed abstract class List[+A] extends AbstractSeq[A]
   // Create a proxy for Java serialization that allows us to avoid mutation
   // during de-serialization.  This is the Serialization Proxy Pattern.
   protected final def writeReplace(): AnyRef = new List.SerializationProxy(this)
+
 }
 
 /** The empty list.
