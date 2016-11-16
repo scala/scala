5370f271dfcd82ae3fb6675e7f055430a281381e
Tamer Mohammed Abdul-Radi <tamerr@cakesolutions.net>
2016-11-16 17:27:26 +0000

    fixup! SI-10060 Fixes NumericRange.max bug on empty ranges

--- test/junit/scala/collection/immutable/RangeTest.scala
+++ test/junit/scala/collection/immutable/RangeTest.scala
@@ -11,17 +11,29 @@ class RangeTest {
   import AssertUtil._
 
   @Test
-  def test_SI10060_long_max(): Unit = {
+  def test_SI10060_numeric_range_min_max(): Unit = {
+    assertEquals(Range.Long.inclusive(1, 9, 1).min, 1)
+    assertEquals(Range.Long.inclusive(1, 9, 1).max, 9)
+    assertEquals(Range.Long.inclusive(9, 1, -1).min, 1)
+    assertEquals(Range.Long.inclusive(9, 1, -1).max, 9)
+    assertThrows[java.util.NoSuchElementException](Range.Long.inclusive(1, 9, -1).min)
+    assertThrows[java.util.NoSuchElementException](Range.Long.inclusive(1, 9, -1).max)
+    assertThrows[java.util.NoSuchElementException](Range.Long.inclusive(9, 1, 1).min)
+    assertThrows[java.util.NoSuchElementException](Range.Long.inclusive(9, 1, 1).max)
+
     assertEquals(Range.Int.inclusive(1, 9, 1).min, 1)
     assertEquals(Range.Int.inclusive(1, 9, 1).max, 9)
+    assertEquals(Range.Int.inclusive(9, 1, -1).min, 1)
+    assertEquals(Range.Int.inclusive(9, 1, -1).max, 9)
     assertThrows[java.util.NoSuchElementException](Range.Int.inclusive(1, 9, -1).min)
     assertThrows[java.util.NoSuchElementException](Range.Int.inclusive(1, 9, -1).max)
     assertThrows[java.util.NoSuchElementException](Range.Int.inclusive(9, 1, 1).min)
     assertThrows[java.util.NoSuchElementException](Range.Int.inclusive(9, 1, 1).max)
 
-
     assertEquals(Range.inclusive(1, 9, 1).min, 1)
     assertEquals(Range.inclusive(1, 9, 1).max, 9)
+    assertEquals(Range.inclusive(9, 1, -1).min, 1)
+    assertEquals(Range.inclusive(9, 1, -1).max, 9)
     assertThrows[java.util.NoSuchElementException](Range.inclusive(1, 9, -1).min)
     assertThrows[java.util.NoSuchElementException](Range.inclusive(1, 9, -1).max)
     assertThrows[java.util.NoSuchElementException](Range.inclusive(9, 1, 1).min)
