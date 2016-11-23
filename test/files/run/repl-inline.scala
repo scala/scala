1b2cd1be9790bf9c14fd68c78f784d6cb4f7c907
Jason Zaugg <jzaugg@gmail.com>
2016-11-23 15:54:03 +1000

    Support inlining under -Yrepl-class-based REPL
    By marking the wrapper classes as sealed, the inliner will be
    able to assume finality of defs introduces in the REPL without
    requiring the user to mark them as `final`, which is an odd thing
    to do in single line of REPL input.

--- test/files/run/repl-inline.scala
+++ test/files/run/repl-inline.scala
@@ -1,7 +1,8 @@
 import scala.tools.nsc._
 
 object Test {
-  val testCode = """
+  val testCode =
+    """
 def callerOfCaller = Thread.currentThread.getStackTrace.drop(2).head.getMethodName
 def g = callerOfCaller
 def h = g
@@ -12,10 +13,15 @@ assert(h == "h", h)
   """
 
   def main(args: Array[String]) {
-    val settings = new Settings()
-    settings.processArgumentString("-opt:l:classpath")
-    settings.usejavacp.value = true
-    val repl = new interpreter.IMain(settings)
-    testCode.linesIterator.foreach(repl.interpret(_))
+    def test(f: Settings => Unit): Unit = {
+      val settings = new Settings()
+      settings.processArgumentString("-opt:l:classpath")
+      f(settings)
+      settings.usejavacp.value = true
+      val repl = new interpreter.IMain(settings)
+      testCode.linesIterator.foreach(repl.interpret(_))
+    }
+    test(_ => ())
+    test(_.Yreplclassbased.value = true)
   }
 }
