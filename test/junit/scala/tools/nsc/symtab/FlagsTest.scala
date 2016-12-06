scala/scala

commit c8e6050c3c190dd064642b6b77fc179f27b0495d

New trait encoding: use default methods, jettison impl classes
Until now, concrete methods in traits were encoded with
"trait implementation classes".

  - Such a trait would compile to two class files
    - the trait interface, a Java interface, and
    - the implementation class, containing "trait implementation methods"
  - trait implementation methods are static methods has an explicit self
    parameter.
  - some methods don't require addition of an interface method, such as
    private methods. Calls to these directly call the implementation method
  - classes that mixin a trait install "trait forwarders", which implement
    the abstract method in the interface by forwarding to the trait
    implementation method.

The new encoding:
  - no longer emits trait implementation classes or trait implementation
    methods.
  - instead, concrete methods are simply retained in the interface, as JVM 8
    default interface methods (the JVM spec changes in
    [JSR-335](http://download.oracle.com/otndocs/jcp/lambda-0_9_3-fr-eval-spec/index.html)
    pave the way)
  - use `invokespecial` to call private or particular super implementations
    of a method (rather `invokestatic`)
    - in cases when we `invokespecial` to a method in an indirect ancestor, we add
    that ancestor redundantly as a direct parent. We are investigating alternatives
    approaches here.
  - we still emit trait fowrarders, although we are
    [investigating](https://github.com/scala/scala-dev/issues/98) ways to only do
    this when the JVM would be unable to resolve the correct method using its rules
    for default method resolution.

Here's an example:

```
trait T {
  println("T")
  def m1 = m2
  private def m2 = "m2"
}
trait U extends T {
  println("T")
  override def m1 = super[T].m1
}
class C extends U {
  println("C")
  def test = m1
}
```

The old and new encodings are displayed and diffed here: https://gist.github.com/retronym/f174d23f859f0e053580

Some notes in the implementation:

src/compiler/scala/tools/nsc/transform/Constructors.scala
@@ -73,7 +73,7 @@ abstract class Constructors extends Statics with Transform with ast.TreeDSL {
 
     override def transform(tree: Tree): Tree = {
       tree match {
-        case cd @ ClassDef(mods0, name0, tparams0, impl0) if !cd.symbol.isInterface && !isPrimitiveValueClass(cd.symbol) =>
+        case cd @ ClassDef(mods0, name0, tparams0, impl0) if !isPrimitiveValueClass(cd.symbol) && cd.symbol.primaryConstructor != NoSymbol =>
           if(cd.symbol eq AnyValClass) {
             cd
           }
@@ -456,7 +456,7 @@ abstract class Constructors extends Statics with Transform with ast.TreeDSL {
 
     // find and dissect primary constructor
     private val (primaryConstr, _primaryConstrParams, primaryConstrBody) = stats collectFirst {
-      case dd@DefDef(_, _, _, vps :: Nil, _, rhs: Block) if dd.symbol.isPrimaryConstructor => (dd, vps map (_.symbol), rhs)
+      case dd@DefDef(_, _, _, vps :: Nil, _, rhs: Block) if dd.symbol.isPrimaryConstructor || dd.symbol.isMixinConstructor => (dd, vps map (_.symbol), rhs)
     } getOrElse {
       abort("no constructor in template: impl = " + impl)
     }
@@ -517,9 +517,11 @@ abstract class Constructors extends Statics with Transform with ast.TreeDSL {
         case Apply(Select(This(_), _), List()) =>
           // references to parameter accessor methods of own class become references to parameters
           // outer accessors become references to $outer parameter
-          if (canBeSupplanted(tree.symbol))
+          if (clazz.isTrait)
+            super.transform(tree)
+          else if (canBeSupplanted(tree.symbol))
             gen.mkAttributedIdent(parameter(tree.symbol.accessed)) setPos tree.pos
-          else if (tree.symbol.outerSource == clazz && !clazz.isImplClass)
+          else if (tree.symbol.outerSource == clazz)
             gen.mkAttributedIdent(parameterNamed(nme.OUTER)) setPos tree.pos
           else
             super.transform(tree)
@@ -566,7 +568,7 @@ abstract class Constructors extends Statics with Transform with ast.TreeDSL {
     }
 
     // Constant typed vals are not memoized.
-    def memoizeValue(sym: Symbol) = !sym.info.isInstanceOf[ConstantType]
+    def memoizeValue(sym: Symbol) = !sym.info.resultType.isInstanceOf[ConstantType]
 
     /** Triage definitions and statements in this template into the following categories.
       * The primary constructor is treated separately, as it is assembled in part from these pieces.
@@ -624,12 +626,12 @@ abstract class Constructors extends Statics with Transform with ast.TreeDSL {
 
         stat match {
           // recurse on class definition, store in defBuf
-          case _: ClassDef => defBuf += new ConstructorTransformer(unit).transform(stat)
+          case _: ClassDef if !stat.symbol.isInterface => defBuf += new ConstructorTransformer(unit).transform(stat)
 
           // Triage methods -- they all end up in the template --
           // regular ones go to `defBuf`, secondary contructors go to `auxConstructorBuf`.
           // The primary constructor is dealt with separately (we're massaging it here).
-          case _: DefDef if statSym.isPrimaryConstructor => ()
+          case _: DefDef if statSym.isPrimaryConstructor || statSym.isMixinConstructor => ()
           case _: DefDef if statSym.isConstructor => auxConstructorBuf += stat
           case _: DefDef => defBuf += stat
 

src/reflect/scala/reflect/internal/Flags.scala
@@ -157,7 +157,6 @@ class Flags extends ModifierFlags {
   final val MIXEDIN       = 1L << 35      // term member has been mixed in
   final val EXISTENTIAL   = 1L << 35      // type is an existential parameter or skolem
   final val EXPANDEDNAME  = 1L << 36      // name has been expanded with class suffix
-  final val IMPLCLASS     = 1L << 37      // symbol is an implementation class
   final val TRANS_FLAG    = 1L << 38      // transient flag guaranteed to be reset after each phase.
 
   final val LOCKED        = 1L << 39      // temporary flag to catch cyclic dependencies
@@ -199,8 +198,7 @@ class Flags extends ModifierFlags {
   // 17: CONTRAVARIANT/M INCONSTRUCTOR       LABEL
   // 25:  DEFAULTPARAM/M       TRAIT/M
   // 35:     EXISTENTIAL       MIXEDIN
-  // 37:       IMPLCLASS    PRESUPER/M
-  val OverloadedFlagsMask = 0L | BYNAMEPARAM | CONTRAVARIANT | DEFAULTPARAM | EXISTENTIAL | IMPLCLASS
+  val OverloadedFlagsMask = 0L | BYNAMEPARAM | CONTRAVARIANT | DEFAULTPARAM | EXISTENTIAL
 
   // ------- late flags (set by a transformer phase) ---------------------------------
   //
@@ -210,7 +208,7 @@ class Flags extends ModifierFlags {
   //     refchecks   7  [START] <latemethod>
   //    specialize  13  [START] <latefinal> <notprivate>
   // explicitouter  14  [START] <notprotected>
-  //       erasure  15  [START] <latedeferred> <lateinterface>
+  //       erasure  15  [START] <latedeferred>
   //         mixin  20  [START] <latemodule> <notoverride>
   //
   // lateMETHOD set in RefChecks#transformInfo.
@@ -218,13 +216,11 @@ class Flags extends ModifierFlags {
   // notPRIVATE set in Symbols#makeNotPrivate, IExplicitOuter#transform, Inliners.
   // notPROTECTED set in ExplicitOuter#transform.
   // lateDEFERRED set in AddInterfaces, Mixin, etc.
-  // lateINTERFACE set in AddInterfaces#transformMixinInfo.
   // lateMODULE set in Mixin#transformInfo.
   // notOVERRIDE set in Mixin#preTransform.
 
   final val lateDEFERRED  = (DEFERRED: Long) << LateShift
   final val lateFINAL     = (FINAL: Long) << LateShift
-  final val lateINTERFACE = (INTERFACE: Long) << LateShift
   final val lateMETHOD    = (METHOD: Long) << LateShift
   final val lateMODULE    = (MODULE: Long) << LateShift
 
@@ -439,7 +435,7 @@ class Flags extends ModifierFlags {
     case              LIFTED => "<lifted>"                            // (1L << 34)
     case         EXISTENTIAL => "<existential/mixedin>"               // (1L << 35)
     case        EXPANDEDNAME => "<expandedname>"                      // (1L << 36)
-    case           IMPLCLASS => "<implclass/presuper>"                // (1L << 37)
+    case            PRESUPER => "<presuper>"                          // (1L << 37)
     case          TRANS_FLAG => "<trans_flag>"                        // (1L << 38)
     case              LOCKED => "<locked>"                            // (1L << 39)
     case         SPECIALIZED => "<specialized>"                       // (1L << 40)
@@ -456,7 +452,7 @@ class Flags extends ModifierFlags {
     case      `lateDEFERRED` => "<latedeferred>"                      // (1L << 51)
     case         `lateFINAL` => "<latefinal>"                         // (1L << 52)
     case        `lateMETHOD` => "<latemethod>"                        // (1L << 53)
-    case     `lateINTERFACE` => "<lateinterface>"                     // (1L << 54)
+    case   0x80000000000000L => ""                                    // (1L << 54)
     case        `lateMODULE` => "<latemodule>"                        // (1L << 55)
     case      `notPROTECTED` => "<notprotected>"                      // (1L << 56)
     case       `notOVERRIDE` => "<notoverride>"                       // (1L << 57)

test/junit/scala/tools/nsc/symtab/FlagsTest.scala
@@ -33,7 +33,6 @@ class FlagsTest {
   def testTimedFlags(): Unit = {
     testLate(lateDEFERRED, _.isDeferred)
     testLate(lateFINAL, _.isFinal)
-    testLate(lateINTERFACE, _.isInterface)
     testLate(lateMETHOD, _.isMethod)
     testLate(lateMODULE, _.isModule)
     testNot(PROTECTED | notPROTECTED, _.isProtected)

src/compiler/scala/tools/nsc/ast/TreeGen.scala
@@ -145,6 +145,7 @@ abstract class TreeGen extends scala.reflect.internal.TreeGen with TreeDSL {
   override def mkCast(tree: Tree, pt: Type): Tree = {
     debuglog("casting " + tree + ":" + tree.tpe + " to " + pt + " at phase: " + phase)
     assert(!tree.tpe.isInstanceOf[MethodType], tree)
+    assert(!pt.isInstanceOf[MethodType], tree)
     assert(pt eq pt.normalize, tree +" : "+ debugString(pt) +" ~>"+ debugString(pt.normalize))
     atPos(tree.pos) {
       mkAsInstanceOf(tree, pt, any = !phase.next.erasedTypes, wrapInApply = isAtPhaseAfter(currentRun.uncurryPhase))

src/compiler/scala/tools/nsc/backend/jvm/BCodeBodyBuilder.scala
@@ -535,6 +535,22 @@ abstract class BCodeBodyBuilder extends BCodeSkelBuilder {
     private def genApply(app: Apply, expectedType: BType): BType = {
       var generatedType = expectedType
       lineNumber(app)
+
+      def genSuperApply(hostClass: Symbol, fun: Symbol, args: List[Tree]) = {
+        // 'super' call: Note: since constructors are supposed to
+        // return an instance of what they construct, we have to take
+        // special care. On JVM they are 'void', and Scala forbids (syntactically)
+        // to call super constructors explicitly and/or use their 'returned' value.
+        // therefore, we can ignore this fact, and generate code that leaves nothing
+        // on the stack (contrary to what the type in the AST says).
+
+        val invokeStyle = InvokeStyle.Super
+        mnode.visitVarInsn(asm.Opcodes.ALOAD, 0)
+        genLoadArguments(args, paramTKs(app))
+        genCallMethod(fun, invokeStyle, app.pos, hostClass)
+        generatedType = methodBTypeFromSymbol(fun).returnType
+      }
+
       app match {
 
         case Apply(TypeApply(fun, targs), _) =>
@@ -582,19 +598,19 @@ abstract class BCodeBodyBuilder extends BCodeSkelBuilder {
 
           generatedType = genTypeApply()
 
-        // 'super' call: Note: since constructors are supposed to
-        // return an instance of what they construct, we have to take
-        // special care. On JVM they are 'void', and Scala forbids (syntactically)
-        // to call super constructors explicitly and/or use their 'returned' value.
-        // therefore, we can ignore this fact, and generate code that leaves nothing
-        // on the stack (contrary to what the type in the AST says).
-        case Apply(fun @ Select(Super(_, _), _), args) =>
-          val invokeStyle = InvokeStyle.Super
-          // if (fun.symbol.isConstructor) Static(true) else SuperCall(mix);
-          mnode.visitVarInsn(asm.Opcodes.ALOAD, 0)
-          genLoadArguments(args, paramTKs(app))
-          genCallMethod(fun.symbol, invokeStyle, app.pos)
-          generatedType = methodBTypeFromSymbol(fun.symbol).returnType
+        case Apply(fun @ Select(Super(qual, mix), _), args) =>
+          val hostClass = qual.symbol.parentSymbols.filter(_.name == mix) match {
+            case Nil =>
+              // We get here for trees created by SuperSelect which use tpnme.EMPTY as the super qualifier
+              // Subsequent code uses the owner of fun.symbol to target the call.
+              null
+            case parent :: Nil=>
+              parent
+            case parents =>
+              devWarning("ambiguous parent class qualifier: " + qual.symbol.parentSymbols)
+              null
+          }
+          genSuperApply(hostClass, fun.symbol, args)
 
         // 'new' constructor call: Note: since constructors are
         // thought to return an instance of what they construct,
@@ -1050,19 +1066,26 @@ abstract class BCodeBodyBuilder extends BCodeSkelBuilder {
       hostSymbol.info ; methodOwner.info
 
       def needsInterfaceCall(sym: Symbol) = (
-           sym.isInterface
+           sym.isTraitOrInterface
         || sym.isJavaDefined && sym.isNonBottomSubClass(definitions.ClassfileAnnotationClass)
       )
 
+      val isTraitCallToObjectMethod =
+        hostSymbol != methodOwner && methodOwner.isTraitOrInterface && ObjectTpe.decl(method.name) != NoSymbol && method.overrideChain.last.owner == ObjectClass
+
       // whether to reference the type of the receiver or
       // the type of the method owner
-      val useMethodOwner = (
+      val useMethodOwner = ((
            !style.isVirtual
         || hostSymbol.isBottomClass
         || methodOwner == definitions.ObjectClass
-      )
+      ) && !(style.isSuper && hostSymbol != null)) || isTraitCallToObjectMethod
       val receiver = if (useMethodOwner) methodOwner else hostSymbol
       val jowner   = internalName(receiver)
+
+      if (style.isSuper && (isTraitCallToObjectMethod || receiver.isTraitOrInterface) && !cnode.interfaces.contains(jowner))
+        cnode.interfaces.add(jowner)
+
       val jname    = method.javaSimpleName.toString
       val bmType   = methodBTypeFromSymbol(method)
       val mdescr   = bmType.descriptor
@@ -1342,7 +1365,7 @@ abstract class BCodeBodyBuilder extends BCodeSkelBuilder {
       def asmType(sym: Symbol) = classBTypeFromSymbol(sym).toASMType
 
       val implMethodHandle =
-        new asm.Handle(if (lambdaTarget.hasFlag(Flags.STATIC)) asm.Opcodes.H_INVOKESTATIC else asm.Opcodes.H_INVOKEVIRTUAL,
+        new asm.Handle(if (lambdaTarget.hasFlag(Flags.STATIC)) asm.Opcodes.H_INVOKESTATIC else if (lambdaTarget.owner.isTrait) asm.Opcodes.H_INVOKEINTERFACE else asm.Opcodes.H_INVOKEVIRTUAL,
           classBTypeFromSymbol(lambdaTarget.owner).internalName,
           lambdaTarget.name.toString,
           methodBTypeFromSymbol(lambdaTarget).descriptor)

src/compiler/scala/tools/nsc/backend/jvm/BCodeHelpers.scala
@@ -32,7 +32,7 @@ abstract class BCodeHelpers extends BCodeIdiomatic with BytecodeWriters {
    * the InnerClass / EnclosingMethod classfile attributes. See comment in BTypes.
    */
   def considerAsTopLevelImplementationArtifact(classSym: Symbol) =
-    classSym.isImplClass || classSym.isSpecialized
+    classSym.isSpecialized
 
   /**
    * Cache the value of delambdafy == "inline" for each run. We need to query this value many
@@ -145,15 +145,12 @@ abstract class BCodeHelpers extends BCodeIdiomatic with BytecodeWriters {
     assert(classSym.isClass, classSym)
 
     def doesNotExist(method: Symbol) = {
-      // (1) SI-9124, some trait methods don't exist in the generated interface. see comment in BTypes.
-      // (2) Value classes. Member methods of value classes exist in the generated box class. However,
+      //     Value classes. Member methods of value classes exist in the generated box class. However,
       //     nested methods lifted into a value class are moved to the companion object and don't exist
       //     in the value class itself. We can identify such nested methods: the initial enclosing class
       //     is a value class, but the current owner is some other class (the module class).
-      method.owner.isTrait && method.isImplOnly || { // (1)
-        val enclCls = nextEnclosingClass(method)
-        exitingPickler(enclCls.isDerivedValueClass) && method.owner != enclCls // (2)
-      }
+      val enclCls = nextEnclosingClass(method)
+      exitingPickler(enclCls.isDerivedValueClass) && method.owner != enclCls
     }
 
     def enclosingMethod(sym: Symbol): Option[Symbol] = {
@@ -248,7 +245,7 @@ abstract class BCodeHelpers extends BCodeIdiomatic with BytecodeWriters {
    * Build the [[InlineInfo]] for a class symbol.
    */
   def buildInlineInfoFromClassSymbol(classSym: Symbol, classSymToInternalName: Symbol => InternalName, methodSymToDescriptor: Symbol => String): InlineInfo = {
-    val traitSelfType = if (classSym.isTrait && !classSym.isImplClass) {
+    val traitSelfType = if (classSym.isTrait) {
       // The mixin phase uses typeOfThis for the self parameter in implementation class methods.
       val selfSym = classSym.typeOfThis.typeSymbol
       if (selfSym != classSym) Some(classSymToInternalName(selfSym)) else None
@@ -259,7 +256,7 @@ abstract class BCodeHelpers extends BCodeIdiomatic with BytecodeWriters {
     val isEffectivelyFinal = classSym.isEffectivelyFinal
 
     val sam = {
-      if (classSym.isImplClass || classSym.isEffectivelyFinal) None
+      if (classSym.isEffectivelyFinal) None
       else {
         // Phase travel necessary. For example, nullary methods (getter of an abstract val) get an
         // empty parameter list in later phases and would therefore be picked as SAM.
@@ -284,41 +281,15 @@ abstract class BCodeHelpers extends BCodeIdiomatic with BytecodeWriters {
           val name      = methodSym.javaSimpleName.toString // same as in genDefDef
           val signature = name + methodSymToDescriptor(methodSym)
 
-          // Some detours are required here because of changing flags (lateDEFERRED, lateMODULE):
+          // Some detours are required here because of changing flags (lateDEFERRED):
           // 1. Why the phase travel? Concrete trait methods obtain the lateDEFERRED flag in Mixin.
           //    This makes isEffectivelyFinalOrNotOverridden false, which would prevent non-final
           //    but non-overridden methods of sealed traits from being inlined.
-          // 2. Why the special case for `classSym.isImplClass`? Impl class symbols obtain the
-          //    lateMODULE flag during Mixin. During the phase travel to exitingPickler, the late
-          //    flag is ignored. The members are therefore not isEffectivelyFinal (their owner
-          //    is not a module). Since we know that all impl class members are static, we can
-          //    just take the shortcut.
-          val effectivelyFinal = classSym.isImplClass || exitingPickler(methodSym.isEffectivelyFinalOrNotOverridden)
-
-          // Identify trait interface methods that have a static implementation in the implementation
-          // class. Invocations of these methods can be re-wrired directly to the static implementation
-          // if they are final or the receiver is known.
-          //
-          // Using `erasure.needsImplMethod` is not enough: it keeps field accessors, module getters
-          // and super accessors. When AddInterfaces creates the impl class, these methods are
-          // initially added to it.
-          //
-          // The mixin phase later on filters out most of these members from the impl class (see
-          // Mixin.isImplementedStatically). However, accessors for concrete lazy vals remain in the
-          // impl class after mixin. So the filter in mixin is not exactly what we need here (we
-          // want to identify concrete trait methods, not any accessors). So we check some symbol
-          // properties manually.
-          val traitMethodWithStaticImplementation = {
-            import symtab.Flags._
-            classSym.isTrait && !classSym.isImplClass &&
-              erasure.needsImplMethod(methodSym) &&
-              !methodSym.isModule &&
-              !(methodSym hasFlag (ACCESSOR | SUPERACCESSOR))
-          }
+          val effectivelyFinal = exitingPickler(methodSym.isEffectivelyFinalOrNotOverridden) && !(methodSym.owner.isTrait && methodSym.isModule)
 
           val info = MethodInlineInfo(
             effectivelyFinal                    = effectivelyFinal,
-            traitMethodWithStaticImplementation = traitMethodWithStaticImplementation,
+            traitMethodWithStaticImplementation = false,
             annotatedInline                     = methodSym.hasAnnotation(ScalaInlineClass),
             annotatedNoInline                   = methodSym.hasAnnotation(ScalaNoInlineClass)
           )
@@ -866,7 +837,6 @@ abstract class BCodeHelpers extends BCodeIdiomatic with BytecodeWriters {
       || sym.isArtifact
       || sym.isLiftedMethod
       || sym.isBridge
-      || (sym.ownerChain exists (_.isImplClass))
     )
 
     /* @return

src/compiler/scala/tools/nsc/backend/jvm/BCodeSkelBuilder.scala
@@ -174,7 +174,7 @@ abstract class BCodeSkelBuilder extends BCodeHelpers {
           if (lmoc != NoSymbol) {
             // it must be a top level class (name contains no $s)
             val isCandidateForForwarders = {
-              exitingPickler { !(lmoc.name.toString contains '$') && lmoc.hasModuleFlag && !lmoc.isImplClass && !lmoc.isNestedClass }
+              exitingPickler { !(lmoc.name.toString contains '$') && lmoc.hasModuleFlag && !lmoc.isNestedClass }
             }
             if (isCandidateForForwarders) {
               log(s"Adding static forwarders from '$claszSymbol' to implementations in '$lmoc'")
@@ -563,7 +563,7 @@ abstract class BCodeSkelBuilder extends BCodeHelpers {
       }
 
       val isNative         = methSymbol.hasAnnotation(definitions.NativeAttr)
-      val isAbstractMethod = (methSymbol.isDeferred || methSymbol.owner.isInterface) && !methSymbol.hasFlag(Flags.JAVA_DEFAULTMETHOD)
+      val isAbstractMethod = rhs == EmptyTree
       val flags = GenBCode.mkFlags(
         javaFlags(methSymbol),
         if (isAbstractMethod)        asm.Opcodes.ACC_ABSTRACT   else 0,

commit 34dad77513d08535adaf26989bd0cd84993ceab1

SI-10093 don't move member traits to constructor body in constructors
Fixes a regression introduced in c8e6050. Member traits with only
abstract definitions (`isInterface`) were moved into the primary
constructor by mistake. (Flatten moved the classes back.)

The member trait was duplicated into the constructor of specialized
subclasses, causing it to be generated multiple times.

Also removes some unnecessary `isMixinConstructor` checks: the mixin
constructor is always the primary constructor.

This commit also clarifies (and tests) what `isInterface` means: for
scala-defined traits, it means there are only abstract members. For
java-defined interfaces, it is always true.

src/compiler/scala/tools/nsc/transform/Constructors.scala
@@ -462,7 +462,7 @@ abstract class Constructors extends Statics with Transform with TypingTransforme
 
     // find and dissect primary constructor
     private val (primaryConstr, _primaryConstrParams, primaryConstrBody) = stats collectFirst {
-      case dd@DefDef(_, _, _, vps :: Nil, _, rhs: Block) if dd.symbol.isPrimaryConstructor || dd.symbol.isMixinConstructor => (dd, vps map (_.symbol), rhs)
+      case dd@DefDef(_, _, _, vps :: Nil, _, rhs: Block) if dd.symbol.isPrimaryConstructor => (dd, vps map (_.symbol), rhs)
     } getOrElse {
       abort("no constructor in template: impl = " + impl)
     }
@@ -646,14 +646,14 @@ abstract class Constructors extends Statics with Transform with TypingTransforme
 
           stat match {
             // recurse on class definition, store in defBuf
-            case _: ClassDef if !statSym.isInterface =>
-              defBuf += new ConstructorTransformer(unit).transform(stat)
+            case _: ClassDef =>
+              if (statSym.isInterface) defBuf += stat
+              else defBuf += new ConstructorTransformer(unit).transform(stat)
 
             // primary constructor is already tracked as `primaryConstr`
             // non-primary constructors go to auxConstructorBuf
-            // mixin constructors are suppressed (!?!?)
             case _: DefDef if statSym.isConstructor =>
-              if ((statSym ne primaryConstrSym) && !statSym.isMixinConstructor) auxConstructorBuf += stat
+              if (statSym ne primaryConstrSym) auxConstructorBuf += stat
 
             // If a val needs a field, an empty valdef goes into the template.
             // Except for lazy and ConstantTyped vals, the field is initialized by an assignment in:

src/reflect/scala/reflect/internal/Flags.scala
@@ -92,7 +92,9 @@ class ModifierFlags {
   final val ABSTRACT      = 1 << 3        // abstract class, or used in conjunction with abstract override.
                                           // Note difference to DEFERRED!
   final val DEFERRED      = 1 << 4        // was `abstract' for members | trait is virtual
-  final val INTERFACE     = 1 << 7        // symbol is an interface (i.e. a trait which defines only abstract methods)
+  final val INTERFACE     = 1 << 7        // symbol is an interface. the flag is set for:
+                                          //  - scala-defined traits with only abstract methods or fields
+                                          //  - any java-defined interface (even if it has default methods)
   final val MUTABLE       = 1 << 12       // symbol is a mutable variable.
   final val PARAM         = 1 << 13       // symbol is a (value or type) parameter to a method
   final val MACRO         = 1 << 15       // symbol is a macro definition

test/files/pos/t10093.flags
@@ -1,0 +1,1 @@
+-Xfatal-warnings

test/files/pos/t10093.scala
@@ -1,0 +1,5 @@
+class A[@specialized(Int) T](val value: T) {
+  trait B
+  def useValue(x:T): Unit = ()
+  useValue(value)
+}

test/junit/scala/tools/nsc/symtab/FlagsTest.scala
@@ -2,15 +2,17 @@ package scala.tools.nsc
 package symtab
 
 import org.junit.Assert._
-import scala.tools.testing.AssertUtil._
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
+import scala.tools.testing.BytecodeTesting
+
 @RunWith(classOf[JUnit4])
-class FlagsTest {
+class FlagsTest extends BytecodeTesting {
   object symbolTable extends SymbolTableForUnitTesting
   import symbolTable._
+
   import Flags._
 
   def sym = NoSymbol.newTermSymbol(nme.EMPTY)
@@ -86,5 +88,39 @@ class FlagsTest {
     import scala.reflect.runtime.universe._
     val dep = typeOf[java.lang.Deprecated].typeSymbol
     assertTrue(dep.isJavaAnnotation && dep.isJava)
+  }
+
+  @Test
+  def interfaceFlag(): Unit = {
+    // scala traits are `isInterface` if they have only type defs and abstract methods / fields.
+    // java interfaces are always `isInterface`.
+    val scalaCode =
+      """package p
+        |trait T1 {
+        |  import scala.collection
+        |  def m: Int
+        |  val f: Int
+        |  type T <: AnyRef
+        |}
+        |trait T2 {
+        |  def m = 1
+        |}
+        |trait T3 {
+        |  val f = 1
+        |}
+        |trait T4 {
+        |  println()
+        |}
+      """.stripMargin
+    val javaI1 = "package p; interface I1 { int m(); }"
+    val javaI2 = "package p; interface I2 { default int m() { return 1; } }"
+    compiler.compileClasses(code = scalaCode, javaCode = (javaI1, "I1.java") :: (javaI2, "I2.java") :: Nil)
+    import compiler.global.rootMirror._
+    assert( getRequiredClass("p.T1").isInterface)
+    assert(!getRequiredClass("p.T2").isInterface)
+    assert(!getRequiredClass("p.T3").isInterface)
+    assert(!getRequiredClass("p.T4").isInterface)
+    assert( getRequiredClass("p.I1").isInterface)
+    assert( getRequiredClass("p.I2").isInterface)
   }
 }