f841dab6c2eb8d9002c286d57ea80df637a0d4bc
Adriaan Moors <adriaan@lightbend.com>
2017-01-09 15:29:33 -0800

    support --show-log for partest command

--- project/PartestUtil.scala
+++ project/PartestUtil.scala
@@ -29,7 +29,7 @@ object PartestUtil {
     val knownUnaryOptions = List(
       "--pos", "--neg", "--run", "--jvm", "--res", "--ant", "--scalap", "--specialized",
       "--scalacheck", "--instrumented", "--presentation", "--failed", "--update-check",
-      "--show-diff", "--verbose", "--terse", "--debug", "--version", "--self-test", "--help")
+      "--show-diff", "--show-log", "--verbose", "--terse", "--debug", "--version", "--self-test", "--help")
     val srcPathOption = "--srcpath"
     val grepOption = "--grep"
 
