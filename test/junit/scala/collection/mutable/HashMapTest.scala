898fa00a76286786427b093f0161ea0d3d3bae29
Jason Zaugg <jzaugg@gmail.com>
2017-02-19 14:47:02 +1000

    SI-10187 Support mutation of mutable.HashMap in getOrElseUpdate
    Scala 2.12.1 included optimizations to `HashMape.getOrElseUpdate`
    to avoid recomputing the index in the hash table when adding an
    the element.
    
    However, this index could be stale if the callback added elements
    to the map and triggered a resize.
    
    This commit checks that the table is unchanged before reusing
    the index, restoring the 2.12.0 behaviour.

---
+++ test/junit/scala/collection/mutable/HashMapTest.scala
@@ -0,0 +1,38 @@
+package scala.collection
+package mutable
+
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(classOf[JUnit4])
+class HashMapTest {
+
+  @Test
+  def getOrElseUpdate_mutationInCallback() {
+    val hm = new mutable.HashMap[String, String]()
+    // add enough elements to resize the hash table in the callback
+    def add() = 1 to 100000 foreach (i => hm(i.toString) = "callback")
+    hm.getOrElseUpdate("0", {
+      add()
+      ""
+    })
+    assertEquals(Some(""), hm.get("0"))
+  }
+
+  @Test
+  def getOrElseUpdate_evalOnce(): Unit = {
+    var i = 0
+    val hm = new mutable.HashMap[Int, Int]()
+    hm.getOrElseUpdate(0, {i += 1; i})
+    assertEquals(1, hm(0))
+  }
+
+  @Test
+  def getOrElseUpdate_noEval(): Unit = {
+    val hm = new mutable.HashMap[Int, Int]()
+    hm.put(0, 0)
+    hm.getOrElseUpdate(0, throw new AssertionError())
+  }
+}
