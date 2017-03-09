050811bd84523dd90cec9d490aa746043973b13d
Som Snytt <som.snytt@gmail.com>
2017-03-09 09:15:14 -0800

    SI-8969 Accept poly+implicit for assignment syntax
    Follow-up to fb061f22d4c35df626d9651e017820a11f8fe56e
    which allowed the type param only.
    
    Reported:
    ```
    scala> object Test {
         |   def a[R](implicit s: List[R]):Int = 0
         |   def a_=[R](v: Int)(implicit s: List[R]) = ()
         | }
    ```

--- test/files/pos/t4237.scala
+++ test/files/pos/t4237.scala
@@ -2,5 +2,16 @@ class A {
   (new { def field = 0; def field_=(i: Int) = () }).field = 5 // compiles as expected
   (new { def field(implicit i: Int) = 0; def field_=(i: Int) = () }).field = 5 // compiles even with implicit params on getter
   (new { def field = 0; def field_=[T](i: Int) = () }).field = 5 // compiles with type param on setter
-  (new { def field[T] = 0; def field_=(i: Int) = () }).field = 5 // DOESN'T COMPILE
+  (new { def field[T] = 0; def field_=(i: Int) = () }).field = 5 // DIDN'T COMPILE
+
+  class Imp
+  implicit val imp: Imp = new Imp
+  implicit val implicitList: List[Int] = null
+
+  // compiles even with implicit params on setter
+  (new { def field(implicit i: Int) = 0; def field_=(i: Int)(implicit j: Imp) = () }).field = 5
+  (new { def field(implicit i: Int) = 0; def field_=[T <: Imp](i: Int)(implicit j: T) = () }).field = 5
+  // was reassignment to val
+  (new { def field[T](implicit ts: List[T]) = 0; def field_=[T](i: Int)(implicit ts: List[T]) = () }).field = 5
+  (new { def field[T](implicit ts: List[T]) = 0; def field_=[T](i: T)(implicit ts: List[T]) = () }).field = 5
 }
