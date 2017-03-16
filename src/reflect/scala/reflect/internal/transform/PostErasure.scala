7a31e8bf73e067457da02de8ec844c385cb418f4
Jason Zaugg <jzaugg@gmail.com>
2017-03-16 13:40:18 +1000

    Optimize postErasure info transform

--- src/reflect/scala/reflect/internal/transform/PostErasure.scala
+++ src/reflect/scala/reflect/internal/transform/PostErasure.scala
@@ -8,7 +8,10 @@ trait PostErasure {
 
   object elimErasedValueType extends TypeMap {
     def apply(tp: Type) = tp match {
-      case ConstantType(Constant(tp: Type)) => ConstantType(Constant(apply(tp)))
+      case ConstantType(Constant(tp1: Type)) =>
+        val tp1Mapped = apply(tp1)
+        if (tp1 eq tp1Mapped) tp
+        else ConstantType(Constant(tp1Mapped))
       case ErasedValueType(_, underlying)   => underlying
       case _                                => mapOver(tp)
     }
