scala/scala

commit 63f7b357f787b96ddf0440c779b1937844cbbb52

SI-8873 don't propagate primary ctor arg to case class's apply
Final implementation based on feedback by Jason

src/compiler/scala/tools/nsc/typechecker/Namers.scala
@@ -686,6 +686,8 @@ trait Namers extends MethodSynthesis {
 
         if (name == nme.copy && sym.isSynthetic)
           enterCopyMethod(tree)
+        else if (name == nme.apply && sym.hasAllFlags(SYNTHETIC | CASE))
+          sym setInfo caseApplyMethodCompleter(tree, completerOf(tree).asInstanceOf[LockingTypeCompleter])
         else
           sym setInfo completerOf(tree)
     }
@@ -813,6 +815,15 @@ trait Namers extends MethodSynthesis {
       classSym setAnnotations (annotations filter annotationFilter(ClassTargetClass, defaultRetention = true))
     }
 
+    def caseApplyMethodCompleter(tree: DefDef, sigCompleter: LockingTypeCompleter) = mkTypeCompleter(tree) { methSym =>
+      sigCompleter.completeImpl(methSym)
+
+      // don't propagate e.g. @volatile annot to apply's argument
+      def retainOnlyParamAnnots(param: Symbol) =
+        param setAnnotations (param.annotations filter AnnotationInfo.mkFilter(ParamTargetClass, defaultRetention = false))
+
+      methSym.info.paramss.foreach(_.foreach(retainOnlyParamAnnots))
+    }
 
     // complete the type of a value definition (may have a method symbol, for those valdefs that never receive a field,
     // as specified by Field.noFieldFor)

src/compiler/scala/tools/nsc/typechecker/Unapplies.scala
@@ -128,6 +128,7 @@ trait Unapplies extends ast.TreeDSL {
    */
   def factoryMeth(mods: Modifiers, name: TermName, cdef: ClassDef): DefDef = {
     val tparams   = constrTparamsInvariant(cdef)
+
     val cparamss  = constrParamss(cdef)
     def classtpe = classType(cdef, tparams)
     atPos(cdef.pos.focus)(

test/files/pos/t8873.scala
@@ -1,0 +1,1 @@
+case class X(@volatile var x:Int)

commit 379e113e568c3d3193aace81fc37d7279eff4f8c

`CompleterWrapper` delegates `typeParams`.
Fixes the problem reported with #5730 by xuwei-k in scala/scala-dev#352.

The problem was already present before the introduction of
`applyUnapplyMethodCompleter`, as 63f7b35 (in #5294) introduced
a similar bug where the `PolyTypeCompleter`'s `typeParams` override
was masked.

src/compiler/scala/tools/nsc/typechecker/Namers.scala
@@ -614,6 +614,9 @@ trait Namers extends MethodSynthesis {
     }
 
     class CompleterWrapper(completer: TypeCompleter) extends TypeCompleter {
+      // override important when completer.isInstanceOf[PolyTypeCompleter]!
+      override val typeParams = completer.typeParams
+
       val tree = completer.tree
 
       override def complete(sym: Symbol): Unit = {

test/files/pos/userdefined_apply_poly_overload.scala
@@ -1,0 +1,13 @@
+object Foo {
+  // spurious error if:
+  //   - this definition precedes that of apply (which is overloaded with the synthetic one derived from the case class)
+  //   - AND `Foo.apply` is explicitly applied to `[A]` (no error if `[A]` is inferred)
+  //
+  def referToPolyOverloadedApply[A]: Foo[A] = Foo.apply[A]("bla")
+  //                                                   ^
+  //  found   : String("bla")
+  //  required: Int
+
+  def apply[A](x: Int): Foo[A] = ???
+}
+case class Foo[A](x: String) // must be polymorphic