809e8c555b93abd612af56320c0eb05f41dd719f
Dale Wijnand <dale.wijnand@gmail.com>
2017-05-03 14:31:32 +0100

    Cleanup ConsoleInterface
    Rewritten from sbt/zinc@1abf6ca3bfe0628321e1562a9b4cfe58e19ab7b7

--- src/main/scala/xsbt/ConsoleInterface.scala
+++ src/main/scala/xsbt/ConsoleInterface.scala
@@ -8,9 +8,9 @@
 package xsbt
 
 import xsbti.Logger
-import scala.tools.nsc.{ GenericRunnerCommand, Settings }
-import scala.tools.nsc.interpreter.{ IMain, InteractiveReader, ILoop }
+import scala.tools.nsc.interpreter.{ ILoop, IMain, InteractiveReader }
 import scala.tools.nsc.reporters.Reporter
+import scala.tools.nsc.{ GenericRunnerCommand, Settings }
 
 class ConsoleInterface {
   def commandArguments(
@@ -35,20 +35,17 @@ class ConsoleInterface {
     lazy val interpreterSettings = MakeSettings.sync(args.toList, log)
     val compilerSettings = MakeSettings.sync(args, bootClasspathString, classpathString, log)
 
-    if (!bootClasspathString.isEmpty)
-      compilerSettings.bootclasspath.value = bootClasspathString
-    compilerSettings.classpath.value = classpathString
     log.info(Message("Starting scala interpreter..."))
     log.info(Message(""))
+
     val loop = new ILoop {
-
       override def createInterpreter() = {
-
         if (loader ne null) {
           in = InteractiveReader.apply()
           intp = new IMain(settings) {
             override protected def parentClassLoader =
               if (loader eq null) super.parentClassLoader else loader
+
             override protected def newCompiler(settings: Settings, reporter: Reporter) =
               super.newCompiler(compilerSettings, reporter)
           }
@@ -56,37 +53,30 @@ class ConsoleInterface {
         } else
           super.createInterpreter()
 
-        def bind(values: Seq[(String, Any)]): Unit = {
-          // for 2.8 compatibility
-          final class Compat {
-            def bindValue(id: String, value: Any) =
-              intp.bind(id, value.asInstanceOf[AnyRef].getClass.getName, value)
-          }
-          implicit def compat(a: AnyRef): Compat = new Compat
-
-          for ((id, value) <- values)
-            intp.beQuietDuring(intp.bindValue(id, value))
-        }
-
-        bind(bindNames zip bindValues)
+        for ((id, value) <- bindNames zip bindValues)
+          intp.beQuietDuring(intp.bind(id, value))
 
         if (!initialCommands.isEmpty)
           intp.interpret(initialCommands)
 
         ()
       }
+
       override def closeInterpreter(): Unit = {
         if (!cleanupCommands.isEmpty)
           intp.interpret(cleanupCommands)
         super.closeInterpreter()
       }
     }
+
     loop.process(if (loader eq null) compilerSettings else interpreterSettings)
+
     ()
   }
 }
+
 object MakeSettings {
-  def apply(args: List[String], log: Logger) = {
+  def apply(args: List[String], log: Logger): Settings = {
     val command = new GenericRunnerCommand(args, message => log.error(Message(message)))
     if (command.ok)
       command.settings
@@ -107,15 +97,8 @@ object MakeSettings {
     compilerSettings
   }
 
-  def sync(options: List[String], log: Logger) = {
+  def sync(options: List[String], log: Logger): Settings = {
     val settings = apply(options, log)
-
-    // -Yrepl-sync is only in 2.9.1+
-    final class Compat {
-      def Yreplsync = settings.BooleanSetting("-Yrepl-sync", "For compatibility only.")
-    }
-    implicit def compat(s: Settings): Compat = new Compat
-
     settings.Yreplsync.value = true
     settings
   }
