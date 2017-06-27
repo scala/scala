d693f2664af1fb0b3e37dbecbc8473d586ffdac7
Julien Richard-Foy <julien@richard-foy.fr>
2017-06-27 11:40:36 +0200

    Add most common operations of Iterable

--- src/main/scala/strawman/collection/Iterator.scala
+++ src/main/scala/strawman/collection/Iterator.scala
@@ -519,6 +519,28 @@ trait Iterator[+A] extends IterableOnce[A] { self =>
       else Iterator.empty.next()
   }
 
+  /** Takes longest prefix of values produced by this iterator that satisfy a predicate.
+    *
+    *  @param   p  The predicate used to test elements.
+    *  @return  An iterator returning the values produced by this iterator, until
+    *           this iterator produces a value that does not satisfy
+    *           the predicate `p`.
+    *  @note    Reuse: $consumesAndProducesIterator
+    */
+  def takeWhile(p: A => Boolean): Iterator[A] = new Iterator[A] {
+    private var hd: A = _
+    private var hdDefined: Boolean = false
+    private var tail: Iterator[A] = self
+
+    def hasNext = hdDefined || tail.hasNext && {
+      hd = tail.next()
+      if (p(hd)) hdDefined = true
+      else tail = Iterator.empty
+      hdDefined
+    }
+    def next() = if (hasNext) { hdDefined = false; hd } else Iterator.empty.next()
+  }
+
   def zip[B](that: IterableOnce[B]): Iterator[(A, B)] = new Iterator[(A, B)] {
     val thatIterator = that.iterator()
     def hasNext = self.hasNext && thatIterator.hasNext
