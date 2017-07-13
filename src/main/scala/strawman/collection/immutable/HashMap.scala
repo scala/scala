a1e849a89852d065ddd4f96ea01b4d9218150a51
Stephen Nancekivell <stephennancekivell@gmail.com>
2017-07-13 22:03:28 +1000

    Clean up Match case Unreachable Warning
    Probably dont need this case
    https://travis-ci.org/scala/collection-strawman#L1056
    
    ```
    [warn] -- [E029] Match case Unreachable Warning: /home/travis/build/scala/collection-strawman/src/main/scala/strawman/collection/immutable/HashMap.scala:575:13 
    [warn] 575 |      case _ => sys.error("section supposed to be unreachable.")
    [warn]     |             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    [warn]     |             unreachable code
    [warn] one warning found
    ```

--- src/main/scala/strawman/collection/immutable/HashMap.scala
+++ src/main/scala/strawman/collection/immutable/HashMap.scala
@@ -572,7 +572,6 @@ object HashMap extends MapFactory[HashMap] {
         new HashTrieMap[K, V1](this.bitmap | that.bitmap, merged, totalelems)
       case hm: HashMapCollision1[_, _] => that.merge0(this, level, merger.invert)
       case hm: HashMap[_, _] => this
-      case _ => sys.error("section supposed to be unreachable.")
     }
   }
 
