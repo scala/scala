e16b280a7fe3ae00ef98ec2b4ea01ce533174c28
Julien Richard-Foy <julien@richard-foy.fr>
2017-07-25 16:38:56 +0200

    Enable some Iterator tests. Add missing methods on iterators. Optimized concat.

--- test/junit/src/test/scala/scala/collection/IteratorTest.scala
+++ test/junit/src/test/scala/scala/collection/IteratorTest.scala
@@ -1,5 +1,5 @@
 
-package scala.collection
+package strawman.collection
 
 import org.junit.Assert._
 import org.junit.Test
@@ -8,7 +8,7 @@ import org.junit.runners.JUnit4
 
 import scala.tools.testing.AssertUtil._
 
-import Seq.empty
+import strawman.collection.immutable.{Range, List, LazyList}
 
 @RunWith(classOf[JUnit4])
 class IteratorTest {
@@ -35,31 +35,32 @@ class IteratorTest {
     assertEquals(it.drop(1).next.getStackTrace.length, it.drop(1).drop(1).next.getStackTrace.length)
   }
 
-  @Test def dropIsChainable(): Unit = {
-    assertSameElements(1 to 4, Iterator from 0 take 5 drop 1)
-    assertSameElements(3 to 4, Iterator from 0 take 5 drop 3)
-    assertSameElements(empty,  Iterator from 0 take 5 drop 5)
-    assertSameElements(empty,  Iterator from 0 take 5 drop 10)
-    assertSameElements(0 to 4, Iterator from 0 take 5 drop 0)
-    assertSameElements(0 to 4, Iterator from 0 take 5 drop -1)
-    assertSameElements(2 to 8 by 2, Iterator from 0 take 5 drop 1 map (2 * _))
-    assertSameElements(2 to 8 by 2, Iterator from 0 take 5 map (2 * _) drop 1)
-    assertSameElements(3 to 4, Iterator from 0 take 5 drop 1 drop 2)
-    assertSameElements(3 to 4, Iterator from 0 take 5 drop 2 drop 1)
-  }
+// Disabled because we have no syntax for ranges yet and no default Seq
+//  @Test def dropIsChainable(): Unit = {
+//    assertSameElements(1 to 4, Iterator from 0 take 5 drop 1)
+//    assertSameElements(3 to 4, Iterator from 0 take 5 drop 3)
+//    assertSameElements(empty,  Iterator from 0 take 5 drop 5)
+//    assertSameElements(empty,  Iterator from 0 take 5 drop 10)
+//    assertSameElements(0 to 4, Iterator from 0 take 5 drop 0)
+//    assertSameElements(0 to 4, Iterator from 0 take 5 drop -1)
+//    assertSameElements(2 to 8 by 2, Iterator from 0 take 5 drop 1 map (2 * _))
+//    assertSameElements(2 to 8 by 2, Iterator from 0 take 5 map (2 * _) drop 1)
+//    assertSameElements(3 to 4, Iterator from 0 take 5 drop 1 drop 2)
+//    assertSameElements(3 to 4, Iterator from 0 take 5 drop 2 drop 1)
+//  }
 
-  @Test def sliceIsChainable(): Unit = {
-    assertSameElements(3 to 6, Iterator from 0 slice (3, 7))
-    assertSameElements(empty,  Iterator from 0 slice (3, 3))
-    assertSameElements(0 to 2, Iterator from 0 slice (-1, 3))
-    assertSameElements(empty,  Iterator from 0 slice (3, -1))
-    assertSameElements(6 to 12 by 2, Iterator from 0 slice (3, 7) map (2 * _))
-    assertSameElements(6 to 12 by 2, Iterator from 0 map (2 * _) slice (3, 7))
-    assertSameElements(4 to 6, Iterator from 0 slice (3, 7) drop 1)
-    assertSameElements(4 to 7, Iterator from 0 drop 1 slice (3, 7))
-    assertSameElements(4 to 5, Iterator from 0 slice (3, 7) slice (1, 3))
-    assertSameElements(4 to 6, Iterator from 0 slice (3, 7) slice (1, 10))
-  }
+//  @Test def sliceIsChainable(): Unit = {
+//    assertSameElements(3 to 6, Iterator from 0 slice (3, 7))
+//    assertSameElements(empty,  Iterator from 0 slice (3, 3))
+//    assertSameElements(0 to 2, Iterator from 0 slice (-1, 3))
+//    assertSameElements(empty,  Iterator from 0 slice (3, -1))
+//    assertSameElements(6 to 12 by 2, Iterator from 0 slice (3, 7) map (2 * _))
+//    assertSameElements(6 to 12 by 2, Iterator from 0 map (2 * _) slice (3, 7))
+//    assertSameElements(4 to 6, Iterator from 0 slice (3, 7) drop 1)
+//    assertSameElements(4 to 7, Iterator from 0 drop 1 slice (3, 7))
+//    assertSameElements(4 to 5, Iterator from 0 slice (3, 7) slice (1, 3))
+//    assertSameElements(4 to 6, Iterator from 0 slice (3, 7) slice (1, 10))
+//  }
 
   // test/files/run/iterator-concat.scala
   @Test def concatIsStackFriendly(): Unit = {
@@ -71,10 +72,10 @@ class IteratorTest {
       //closures.foldLeft(Iterator.empty: Iterator[Int])((res, f) => res ++ f())
       List.fill(size)(() => Iterator(1)).foldLeft(Iterator.empty: Iterator[Int])((res, f) => res ++ f())
     }
-    assertEquals(100,    mk(100).sum)
-    assertEquals(1000,   mk(1000).sum)
-    assertEquals(10000,  mk(10000).sum)
-    assertEquals(100000, mk(100000).sum)
+    assertEquals(100,    View.fromIterator(mk(100)).sum)
+    assertEquals(1000,   View.fromIterator(mk(1000)).sum)
+    assertEquals(10000,  View.fromIterator(mk(10000)).sum)
+    assertEquals(100000, View.fromIterator(mk(100000)).sum)
   }
 
   @Test def from(): Unit = {
@@ -124,74 +125,79 @@ class IteratorTest {
     var xs4 = a.slice(0, 4).iterator
     assertEquals(14, xs0.size + xs1.size + xs2.size + xs3.size + xs4.size)
   }
-  @Test def toSeq(): Unit = {
-    assertEquals("1x2x3x4x5", List(1, 2, 3, 4, 5).iterator.mkString("x"))
-  }
+
+// Disabled because we don’t have mkString on Iterators
+//  @Test def toSeq(): Unit = {
+//    assertEquals("1x2x3x4x5", List(1, 2, 3, 4, 5).iterator().mkString("x"))
+//  }
   @Test def indexOf(): Unit = {
-    assertEquals(3, List(1, 2, 3, 4, 5).iterator.indexOf(4))
-    assertEquals(-1, List(1, 2, 3, 4, 5).iterator.indexOf(16))
+    assertEquals(3, List(1, 2, 3, 4, 5).iterator().indexOf(4))
+    assertEquals(-1, List(1, 2, 3, 4, 5).iterator().indexOf(16))
   }
   @Test def indexWhere(): Unit = {
-    assertEquals(3, List(1, 2, 3, 4, 5).iterator.indexWhere { x: Int => x >= 4 })
-    assertEquals(-1, List(1, 2, 3, 4, 5).iterator.indexWhere { x: Int => x >= 16 })
+    assertEquals(3, List(1, 2, 3, 4, 5).iterator().indexWhere { x: Int => x >= 4 })
+    assertEquals(-1, List(1, 2, 3, 4, 5).iterator().indexWhere { x: Int => x >= 16 })
   }
   @Test def indexOfFrom(): Unit = {
-    assertEquals(1, List(1, 2, 3, 4, 5).iterator.indexOf(2, 0))
-    assertEquals(1, List(1, 2, 3, 4, 5).iterator.indexOf(2, 1))
-    assertEquals(-1, List(1, 2, 3, 4, 5).iterator.indexOf(2, 2))
-    assertEquals(4, List(1, 2, 3, 2, 1).iterator.indexOf(1, 1))
-    assertEquals(1, List(1, 2, 3, 2, 1).iterator.indexOf(2, 1))
+    assertEquals(1, List(1, 2, 3, 4, 5).iterator().indexOf(2, 0))
+    assertEquals(1, List(1, 2, 3, 4, 5).iterator().indexOf(2, 1))
+    assertEquals(-1, List(1, 2, 3, 4, 5).iterator().indexOf(2, 2))
+    assertEquals(4, List(1, 2, 3, 2, 1).iterator().indexOf(1, 1))
+    assertEquals(1, List(1, 2, 3, 2, 1).iterator().indexOf(2, 1))
   }
   @Test def indexWhereFrom(): Unit = {
-    assertEquals(1, List(1, 2, 3, 4, 5).iterator.indexWhere(_ == 2, 0))
-    assertEquals(1, List(1, 2, 3, 4, 5).iterator.indexWhere(_ == 2, 1))
-    assertEquals(-1, List(1, 2, 3, 4, 5).iterator.indexWhere(_ == 2, 2))
-    assertEquals(4, List(1, 2, 3, 2, 1).iterator.indexWhere(_ < 2, 1))
-    assertEquals(1, List(1, 2, 3, 2, 1).iterator.indexWhere(_ <= 2, 1))
+    assertEquals(1, List(1, 2, 3, 4, 5).iterator().indexWhere(_ == 2, 0))
+    assertEquals(1, List(1, 2, 3, 4, 5).iterator().indexWhere(_ == 2, 1))
+    assertEquals(-1, List(1, 2, 3, 4, 5).iterator().indexWhere(_ == 2, 2))
+    assertEquals(4, List(1, 2, 3, 2, 1).iterator().indexWhere(_ < 2, 1))
+    assertEquals(1, List(1, 2, 3, 2, 1).iterator().indexWhere(_ <= 2, 1))
   }
   // iterator-iterate-lazy.scala
   // was java.lang.UnsupportedOperationException: tail of empty list
   @Test def iterateIsSufficientlyLazy(): Unit = {
     //Iterator.iterate((1 to 5).toList)(_.tail).takeWhile(_.nonEmpty).toList  // suffices
-    Iterator.iterate((1 to 5).toList)(_.tail).takeWhile(_.nonEmpty).map(_.head).toList
+    View.fromIterator(Iterator.iterate((1 to 5).toList)(_.tail).takeWhile(_.nonEmpty).map(_.head)).to(List)
   }
   // scala/bug#3516
-  @Test def toStreamIsSufficientlyLazy(): Unit = {
-    val results = collection.mutable.ListBuffer.empty[Int]
-    def mkIterator = (1 to 5).iterator map (x => { results += x ; x })
+  @Test def lazyListIsLazy(): Unit = {
+    val results = mutable.ListBuffer.empty[Int]
+    def mkIterator = Range.inclusive(1, 5).iterator() map (x => { results += x ; x })
     def mkInfinite = Iterator continually { results += 1 ; 1 }
 
-    // Stream is strict in its head so we should see 1 from each of them.
-    val s1 = mkIterator.toStream
-    val s2 = mkInfinite.toStream
+    val s1 = LazyList.fromIterator(mkIterator)
+    val s2 = LazyList.fromIterator(mkInfinite)
     // back and forth without slipping into nontermination.
-    results += (Stream from 1).toIterator.drop(10).toStream.drop(10).toIterator.next()
-    assertSameElements(List(1,1,21), results)
+    // TODO Uncomment and adapt with LazyList
+//    results += (Stream from 1).toIterator.drop(10).toStream.drop(10).toIterator.next()
+    assertSameElements(List.empty, results)
   }
+
   // scala/bug#8552
   @Test def indexOfShouldWorkForTwoParams(): Unit = {
-    assertEquals(1, List(1, 2, 3).iterator.indexOf(2, 0))
-    assertEquals(-1, List(5 -> 0).iterator.indexOf(5, 0))
-    assertEquals(0, List(5 -> 0).iterator.indexOf((5, 0)))
-    assertEquals(-1, List(5 -> 0, 9 -> 2, 0 -> 3).iterator.indexOf(9, 2))
-    assertEquals(1, List(5 -> 0, 9 -> 2, 0 -> 3).iterator.indexOf(9 -> 2))
+    assertEquals(1, List(1, 2, 3).iterator().indexOf(2, 0))
+    assertEquals(-1, List(5 -> 0).iterator().indexOf(5, 0))
+    assertEquals(0, List(5 -> 0).iterator().indexOf((5, 0)))
+    assertEquals(-1, List(5 -> 0, 9 -> 2, 0 -> 3).iterator().indexOf(9, 2))
+    assertEquals(1, List(5 -> 0, 9 -> 2, 0 -> 3).iterator().indexOf(9 -> 2))
   }
-  // scala/bug#9332
-  @Test def spanExhaustsLeadingIterator(): Unit = {
-    def it = Iterator.iterate(0)(_ + 1).take(6)
-    val (x, y) = it.span(_ != 1)
-    val z = x.toList
-    assertEquals(1, z.size)
-    assertFalse(x.hasNext)
-    assertEquals(1, y.next)
-    assertFalse(x.hasNext)   // was true, after advancing underlying iterator
-  }
-  // scala/bug#9913
-  @Test def `span leading iterator finishes at state -1`(): Unit = {
-    val (yes, no) = Iterator(1, 2, 3).span(_ => true)
-    assertFalse(no.hasNext)
-    assertTrue(yes.hasNext)
-  }
+
+// Disabled because we don’t have span on Iterators
+//  // scala/bug#9332
+//  @Test def spanExhaustsLeadingIterator(): Unit = {
+//    def it = Iterator.iterate(0)(_ + 1).take(6)
+//    val (x, y) = it.span(_ != 1)
+//    val z = x.toList
+//    assertEquals(1, z.size)
+//    assertFalse(x.hasNext)
+//    assertEquals(1, y.next)
+//    assertFalse(x.hasNext)   // was true, after advancing underlying iterator
+//  }
+//  // scala/bug#9913
+//  @Test def `span leading iterator finishes at state -1`(): Unit = {
+//    val (yes, no) = Iterator(1, 2, 3).span(_ => true)
+//    assertFalse(no.hasNext)
+//    assertTrue(yes.hasNext)
+//  }
   // scala/bug#9623
   @Test def noExcessiveHasNextInJoinIterator: Unit = {
     var counter = 0
@@ -220,32 +226,34 @@ class IteratorTest {
     assertSameElements(exp, res)
     assertEquals(8, counter) // was 14
   }
-  // scala/bug#9691
-  @Test def bufferedHeadOptionReturnsValueWithHeadOrNone(): Unit = {
-    // Checks BufferedIterator returns Some(value) when there is a value
-    val validHeadOption = List(1,2,3).iterator.buffered.headOption
-    assertEquals(Some(1), validHeadOption)
-    // Checks BufferedIterator returns None when there is no value
-    val invalidHeadOption = List(1,2,3).iterator.drop(10).buffered.headOption
-    assertEquals(None: Option[Int], invalidHeadOption)
-    // Checks BufferedIterator returns Some(value) in the last position with a value
-    val validHeadOptionAtTail = List(1,2,3).iterator.drop(2).buffered.headOption
-    assertEquals(Some(3), validHeadOptionAtTail)
-    // Checks BufferedIterator returns None at the first position without a value
-    val invalidHeadOptionOnePastTail = List(1,2,3).iterator.drop(3).buffered.headOption
-    assertEquals(None, invalidHeadOptionOnePastTail)
-    // Checks BufferedIterator returns Some(null) if the next value is null.
-    val nullHandingList = List(null, "yellow").iterator.buffered.headOption
-    assertEquals(Some(null), nullHandingList)
-    // Checks that BufferedIterator is idempotent. That the head is not
-    // changed by its invocation, nor the headOption by the next call to head.
-    val it = List(1,2,3).iterator.buffered
-    val v1 = it.head
-    val v2 = it.headOption
-    val v3 = it.head
-    val v4 = it.headOption
-    assertEquals(v1, v3)
-    assertEquals(v2, v4)
-    assertEquals(Some(v1), v2)
-  }
+
+// Disabled because we don’t have buffered on Iterators
+//  // scala/bug#9691
+//  @Test def bufferedHeadOptionReturnsValueWithHeadOrNone(): Unit = {
+//    // Checks BufferedIterator returns Some(value) when there is a value
+//    val validHeadOption = List(1,2,3).iterator.buffered.headOption
+//    assertEquals(Some(1), validHeadOption)
+//    // Checks BufferedIterator returns None when there is no value
+//    val invalidHeadOption = List(1,2,3).iterator.drop(10).buffered.headOption
+//    assertEquals(None: Option[Int], invalidHeadOption)
+//    // Checks BufferedIterator returns Some(value) in the last position with a value
+//    val validHeadOptionAtTail = List(1,2,3).iterator.drop(2).buffered.headOption
+//    assertEquals(Some(3), validHeadOptionAtTail)
+//    // Checks BufferedIterator returns None at the first position without a value
+//    val invalidHeadOptionOnePastTail = List(1,2,3).iterator.drop(3).buffered.headOption
+//    assertEquals(None, invalidHeadOptionOnePastTail)
+//    // Checks BufferedIterator returns Some(null) if the next value is null.
+//    val nullHandingList = List(null, "yellow").iterator.buffered.headOption
+//    assertEquals(Some(null), nullHandingList)
+//    // Checks that BufferedIterator is idempotent. That the head is not
+//    // changed by its invocation, nor the headOption by the next call to head.
+//    val it = List(1,2,3).iterator.buffered
+//    val v1 = it.head
+//    val v2 = it.headOption
+//    val v3 = it.head
+//    val v4 = it.headOption
+//    assertEquals(v1, v3)
+//    assertEquals(v2, v4)
+//    assertEquals(Some(v1), v2)
+//  }
 }
