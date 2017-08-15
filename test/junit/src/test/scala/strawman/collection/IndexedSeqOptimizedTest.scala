8e29f0f867ef4ea4e2e030fba27cb54cdafaa81d
Julien Richard-Foy <julien@richard-foy.fr>
2017-08-15 15:11:28 +0200

    Enable IndexedSeqOptimizedTest

--- test/junit/src/test/scala/strawman/collection/IndexedSeqOptimizedTest.scala
+++ test/junit/src/test/scala/strawman/collection/IndexedSeqOptimizedTest.scala
@@ -1,16 +1,18 @@
-package scala.collection
+package strawman.collection
 
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
+import scala.Predef.{augmentString => _, classOf, charWrapper}
+
 import org.junit.Assert._
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
 @RunWith(classOf[JUnit4])
 class IndexedSeqOptimizedTest {
 
   @Test
   def notThrowsAnExceptionInLastIndexOf() {
-    assertEquals(0, (Array(2): collection.mutable.WrappedArray[Int]).lastIndexWhere(_ => true, 1))
+    assertEquals(0, Array(2).lastIndexWhere(_ => true, 1))
     assertEquals(2, "abc123".lastIndexWhere(_.isLetter, 6))
   }
 
