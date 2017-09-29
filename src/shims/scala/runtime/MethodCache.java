c509c9d7fbb86e15f11cdfe1604b6be5d6314b0c
Lukas Rytz <lukas.rytz@gmail.com>
2017-09-29 20:17:39 +0200

    shims to allow JavaThenScala

---
+++ src/shims/scala/runtime/MethodCache.java
@@ -0,0 +1,8 @@
+package scala.runtime;
+
+import java.lang.reflect.Method;
+
+public class MethodCache {
+  public Method find(Class<?> forReceiver) { return null; }
+  public MethodCache add(Class<?> forReceiver, Method forMethod) { return null; }
+}
