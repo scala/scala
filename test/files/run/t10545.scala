658617ef325f65300b940b8e8df0c0098e15d0e9
Miles Sabin <miles@milessabin.com>
2017-10-11 10:16:56 +0100

    When testing for presence of a Symbol in a Type don't skip aliases.

---
+++ test/files/run/t10545.scala
@@ -0,0 +1,15 @@
+import scala.language.higherKinds
+
+class D[T]
+
+class C[F[_]](val i: Int)
+object C {
+  def apply[F[_]](implicit cf: C[F]): Int = cf.i
+
+  implicit def c0[F[_]]: C[F] = new C[F](0)
+  implicit def c1: C[D] = new C[D](1)
+}
+
+object Test extends App {
+  assert(C[D] == 1) // Works in Dotty ...
+}
