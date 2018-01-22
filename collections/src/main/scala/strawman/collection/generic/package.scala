8e5d613affcf99caa4b089341d505e906920cb44
Julien Richard-Foy <julien@richard-foy.fr>
2018-01-22 16:21:54 +0100

    Add deprecated CanBuildFrom alias to BuildFrom

--- collections/src/main/scala/strawman/collection/generic/package.scala
+++ collections/src/main/scala/strawman/collection/generic/package.scala
@@ -5,4 +5,6 @@ import scala.deprecated
 package object generic {
   @deprecated("Clearable was moved from collection.generic to collection.mutable", "2.13.0")
   type Clearable = strawman.collection.mutable.Clearable
+  @deprecated("Use scala.collection.BuildFrom instead", "2.13.0")
+  type CanBuildFrom[-From, -A, +C] = strawman.collection.BuildFrom[From, A, C]
 }
