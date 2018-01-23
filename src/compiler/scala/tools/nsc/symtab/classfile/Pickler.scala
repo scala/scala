e3722fa5b039a33e321634868fc3725b86255dc3
Jason Zaugg <jzaugg@gmail.com>
2018-01-23 14:59:08 +1000

    Refactor pickle phase
    Small refactoring designed to make the subsequent
    commit more reviewable.
    
    Tightens up the definition of "companion"-s so that
    we no longer add a type alias in a package object into
    the pickle of a same-named module.
    
    (cherry picked from commit b41e6516321da2cda0441bec1c7d3d66ae2dab42)

--- src/compiler/scala/tools/nsc/symtab/classfile/Pickler.scala
+++ src/compiler/scala/tools/nsc/symtab/classfile/Pickler.scala
@@ -33,26 +33,30 @@ abstract class Pickler extends SubComponent {
   def newPhase(prev: Phase): StdPhase = new PicklePhase(prev)
 
   class PicklePhase(prev: Phase) extends StdPhase(prev) {
-    def apply(unit: CompilationUnit) {
-      def pickle(tree: Tree) {
-        def add(sym: Symbol, pickle: Pickle) = {
-          if (currentRun.compiles(sym) && !currentRun.symData.contains(sym)) {
-            debuglog("pickling " + sym)
-            pickle putSymbol sym
-            currentRun.symData(sym) = pickle
-          }
-        }
-
+    def apply(unit: CompilationUnit): Unit = {
+      def pickle(tree: Tree): Unit = {
         tree match {
           case PackageDef(_, stats) =>
             stats foreach pickle
           case ClassDef(_, _, _, _) | ModuleDef(_, _, _) =>
             val sym = tree.symbol
-            val pickle = new Pickle(sym)
-            add(sym, pickle)
-            add(sym.companionSymbol, pickle)
-            pickle.writeArray()
-            currentRun registerPickle sym
+            def shouldPickle(sym: Symbol) = currentRun.compiles(sym) && !currentRun.symData.contains(sym)
+            if (shouldPickle(sym)) {
+              val pickle = new Pickle(sym)
+              def pickleSym(sym: Symbol) = {
+                pickle.putSymbol(sym)
+                currentRun.symData(sym) = pickle
+              }
+
+              val companion = sym.companionSymbol.filter(_.owner == sym.owner) // exclude companionship between package- and package object-owned symbols.
+              val syms = sym :: (if (shouldPickle(companion)) companion :: Nil else Nil)
+              syms.foreach { sym =>
+                pickle.putSymbol(sym)
+                currentRun.symData(sym) = pickle
+              }
+              pickle.writeArray()
+              currentRun registerPickle sym
+            }
           case _ =>
         }
       }
