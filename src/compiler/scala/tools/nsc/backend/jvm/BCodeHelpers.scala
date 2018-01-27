10b09dd228de6e5ee403e4bd816a40cc8948c606
Harrison Houghton <hhoughton@learningobjects.com>
2018-01-27 18:57:34 -0500

    Deprecate static forwarders along with their forwardees.
    Evaluates one of the flags that was being dropped on the
    floor in `addForwarder`, and finds it being unjustly so.
    
    The attached test case shows the issue: javac won't warn
    on deprecation as scalac would.
    
    Fixes scala/bug#10701.

--- src/compiler/scala/tools/nsc/backend/jvm/BCodeHelpers.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/BCodeHelpers.scala
@@ -818,9 +818,9 @@ abstract class BCodeHelpers extends BCodeIdiomatic {
        */
       // TODO: evaluate the other flags we might be dropping on the floor here.
       // TODO: ACC_SYNTHETIC ?
-      val flags = GenBCode.PublicStatic | (
-        if (m.isVarargsMethod) asm.Opcodes.ACC_VARARGS else 0
-      )
+      val flags = GenBCode.PublicStatic |
+        (if (m.isVarargsMethod) asm.Opcodes.ACC_VARARGS else 0) |
+        (if (m.isDeprecated) asm.Opcodes.ACC_DEPRECATED else 0)
 
       // TODO needed? for(ann <- m.annotations) { ann.symbol.initialize }
       val jgensig = staticForwarderGenericSignature
