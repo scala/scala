9d38db062f37361708b1f2d5392e2118955d20b9
Julien Richard-Foy <julien@richard-foy.fr>
2018-02-09 11:28:31 +0100

    Implement some IndexedViews in terms of SeqViews

--- collections/src/main/scala/strawman/collection/IndexedView.scala
+++ collections/src/main/scala/strawman/collection/IndexedView.scala
@@ -20,6 +20,7 @@ trait IndexedView[+A] extends IndexedSeqOps[A, View, View[A]] with SeqView[A] { 
     }
   }
 
+  override def prepended[B >: A](elem: B): IndexedView[B] = new IndexedView.Prepended(elem, this)
   override def take(n: Int): IndexedView[A] = new IndexedView.Take(this, n)
   override def takeRight(n: Int): IndexedView[A] = new IndexedView.TakeRight(this, n)
   override def drop(n: Int): IndexedView[A] = new IndexedView.Drop(this, n)
@@ -30,17 +31,14 @@ trait IndexedView[+A] extends IndexedSeqOps[A, View, View[A]] with SeqView[A] { 
 
 object IndexedView {
 
-  class Id[+A](underlying: IndexedSeqOps[A, AnyConstr, _]) extends IndexedView[A] {
-    def length: Int = underlying.length
-    def apply(idx: Int): A = underlying(idx)
-  }
+  class Id[+A](underlying: IndexedSeqOps[A, AnyConstr, _])
+    extends SeqView.Id(underlying) with IndexedView[A]
 
-  class Take[A](underlying: IndexedView[A], n: Int) extends IndexedView[A] {
-    private[this] val normN = n max 0
-    def length = underlying.size min normN
-    @throws[IndexOutOfBoundsException]
-    def apply(i: Int) = underlying.apply(i)
-  }
+  class Prepended[+A](elem: A, underlying: Iterable[A] with IndexedSeqOps[A, AnyConstr, _])
+    extends SeqView.Prepended(elem, underlying) with IndexedView[A]
+
+  class Take[A](underlying: IndexedView[A], n: Int)
+    extends SeqView.Take(underlying, n) with IndexedView[A]
 
   class TakeRight[A](underlying: IndexedView[A], n: Int) extends IndexedView[A] {
     private[this] val delta = (underlying.size - (n max 0)) max 0
@@ -49,8 +47,7 @@ object IndexedView {
     def apply(i: Int) = underlying.apply(i + delta)
   }
 
-  class Drop[A](underlying: IndexedView[A], n: Int) extends IndexedView[A] {
-    protected val normN = n max 0
+  class Drop[A](underlying: IndexedView[A], n: Int) extends View.Drop[A](underlying, n) with IndexedView[A] {
     def length = (underlying.size - normN) max 0
     @throws[IndexOutOfBoundsException]
     def apply(i: Int) = underlying.apply(i + normN)
@@ -63,11 +60,8 @@ object IndexedView {
     def apply(i: Int) = underlying.apply(i)
   }
 
-  class Map[A, B](underlying: IndexedView[A], f: A => B) extends IndexedView[B] {
-    def length = underlying.size
-    @throws[IndexOutOfBoundsException]
-    def apply(n: Int) = f(underlying.apply(n))
-  }
+  class Map[A, B](underlying: IndexedView[A], f: A => B)
+    extends SeqView.Map(underlying, f) with IndexedView[B]
 
   class Reverse[A](underlying: IndexedView[A]) extends IndexedView[A] {
     def length = underlying.size
