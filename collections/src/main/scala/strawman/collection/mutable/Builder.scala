615a53f8fc24a8c45c480d371afd6d52b4d57fa0
Lukas Rytz <lukas.rytz@gmail.com>
2018-02-12 16:09:04 +0100

    override sizeHint in Builder.mapResult

--- collections/src/main/scala/strawman/collection/mutable/Builder.scala
+++ collections/src/main/scala/strawman/collection/mutable/Builder.scala
@@ -67,6 +67,7 @@ trait Builder[-A, +To] extends Growable[A] { self =>
     def addOne(x: A): this.type = { self += x; this }
     def clear(): Unit = self.clear()
     override def addAll(xs: IterableOnce[A]): this.type = { self ++= xs; this }
+    override def sizeHint(size: Int): Unit = self.sizeHint(size)
     def result(): NewTo = f(self.result())
   }
 
