2fef510c01d5f88fd42426c3b0e7621f6dc02c44
Lukas Rytz <lukas.rytz@gmail.com>
2018-02-14 13:41:39 +0100

    IsIterableLike for array
    Otherwise the pattern matcher crashes when compiling
    value-class-extractor-seq.scala

--- collections/src/main/scala/strawman/collection/generic/IsIterableLike.scala
+++ collections/src/main/scala/strawman/collection/generic/IsIterableLike.scala
@@ -1,7 +1,7 @@
 package strawman.collection
 package generic
 
-import scala.{Any, Char}
+import scala.{Any, Char, Array}
 import scala.Predef.{String, implicitly}
 
 /** A trait which can be used to avoid code duplication when defining extension
@@ -110,6 +110,12 @@ object IsIterableLike {
     new IsIterableLike[String] {
       type A = Char
       val conversion = implicitly[String => IterableOps[Char, Iterable, String]]
+    }
+
+  implicit def arrayRepr[T](implicit conv: Array[T] => IterableOps[T, Iterable, Array[T]]): IsIterableLike[Array[T]] =
+    new IsIterableLike[Array[T]] {
+      override type A = T
+      override val conversion: Array[T] => IterableOps[A, Iterable, Array[T]] = conv
     }
 
   implicit def iterableRepr[C[X] <: Iterable[X], A0](implicit conv: C[A0] => IterableOps[A0, C, C[A0]]): IsIterableLike[C[A0]] { type A = A0 } =
