3d2b1076048ba521cf49d3f452b6f585ac4c89bc
Lukas Rytz <lukas.rytz@gmail.com>
2018-02-19 23:21:06 +0100

    Support implicitNotFound on parameters
    This allows a more specific message for individual implicit parameters
    that are not found.
    
    This allows improving error message, for example in overloading
    resolution:
    
    ```
    scala> trait Iterable[A] { def map[B](f: A => B): Iterable[B] }
    scala> trait SortedSet[A] { def map[B: Ordering](f: A => B): SortedSet[B] }
    
    scala> def t(s: SortedSet[Int]) = s.map(x => x) // OK
    t: (s: SortedSet[Int])SortedSet[Int]
    
    scala> def t[T](s: SortedSet[T]) = s.map(x => x) // Confusing error
                                            ^
           error: No implicit Ordering defined for T.
    ```
    
    better:
    
    ```
    scala> trait SortedSet[A] { def map[B](f: A => B)(implicit @annotation.implicitNotFound("No implicit Ordering[${B}] found to build a SortedSet[${B}]. You can build an unordered Set[${B}] by calling `unsorted` before `map`.") ord: Ordering[B]): SortedSet[B] }
    defined trait SortedSet
    
    scala> def t[T](s: SortedSet[T]) = s.map(x => x)
                                            ^
           error: No implicit Ordering[T] found to build a SortedSet[T]. You can build an unordered Set[T] by calling `unsorted` before `map`.
    ```

--- test/files/neg/t2462b.scala
+++ test/files/neg/t2462b.scala
@@ -7,3 +7,15 @@ trait Meh[-From, +To]
 
 @implicitNotFound(msg = "Cannot construct a collection of type ${To} ${Elem}.")
 trait Meh2[-From, +To]
+
+class C[T]
+trait T {
+  def m[Aaa](implicit @implicitNotFound("I see no C[${Uuh}]") theC: C[Aaa]) = ???
+  def n[Aaa](implicit @implicitNotFound("I see no C[${Aaa}]") theC: C[Aaa]) = ???
+}
+
+trait U[X, Y[_], Z[_, ZZ]] {
+  class I[R] {
+    def m[S](implicit @implicitNotFound("${X} ${Y} ${ Z } ${R} ${S} -- ${XX} ${ZZ} ${ Nix }") i: Int) = ???
+  }
+}
