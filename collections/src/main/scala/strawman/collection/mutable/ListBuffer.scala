lrytz/collection-strawman

include index in IOOBE

collections/src/main/scala/strawman/collection/mutable/ListBuffer.scala
@@ -161,14 +161,14 @@ class ListBuffer[A]
 
   def update(idx: Int, elem: A): Unit = {
     ensureUnaliased()
-    if (idx < 0 || idx >= len) throw new IndexOutOfBoundsException
+    if (idx < 0 || idx >= len) throw new IndexOutOfBoundsException(idx.toString)
     val p = locate(idx)
     setNext(p, elem :: getNext(p).tail)
   }
 
   def insert(idx: Int, elem: A): Unit = {
     ensureUnaliased()
-    if (idx < 0 || idx > len) throw new IndexOutOfBoundsException
+    if (idx < 0 || idx > len) throw new IndexOutOfBoundsException(idx.toString)
     if (idx == len) +=(elem)
     else {
       val p = locate(idx)
@@ -198,7 +198,7 @@ class ListBuffer[A]
     val it = elems.iterator()
     if (it.hasNext) {
       ensureUnaliased()
-      if (idx < 0 || idx > len) throw new IndexOutOfBoundsException
+      if (idx < 0 || idx > len) throw new IndexOutOfBoundsException(idx.toString)
       if (idx == len) ++=(elems)
       else insertAfter(locate(idx), it)
     }
@@ -206,7 +206,7 @@ class ListBuffer[A]
 
   def remove(idx: Int): A = {
     ensureUnaliased()
-    if (idx < 0 || idx >= len) throw new IndexOutOfBoundsException
+    if (idx < 0 || idx >= len) throw new IndexOutOfBoundsException(idx.toString)
     len -= 1
     val p = locate(idx)
     val nx = getNext(p)
@@ -217,7 +217,7 @@ class ListBuffer[A]
   def remove(idx: Int, n: Int): Unit =
     if (n > 0) {
       ensureUnaliased()
-      if (idx < 0 || idx + n > len) throw new IndexOutOfBoundsException
+      if (idx < 0 || idx + n > len) throw new IndexOutOfBoundsException(idx.toString)
       removeAfter(locate(idx), n)
     }
 

scala/scala

collections/src/main/scala/strawman/collection/mutable/ListBuffer.scala
@@ -161,14 +161,14 @@ class ListBuffer[A]
 
   def update(idx: Int, elem: A): Unit = {
     ensureUnaliased()
-    if (idx < 0 || idx >= len) throw new IndexOutOfBoundsException
+    if (idx < 0 || idx >= len) throw new IndexOutOfBoundsException(idx.toString)
     val p = locate(idx)
     setNext(p, elem :: getNext(p).tail)
   }
 
   def insert(idx: Int, elem: A): Unit = {
     ensureUnaliased()
-    if (idx < 0 || idx > len) throw new IndexOutOfBoundsException
+    if (idx < 0 || idx > len) throw new IndexOutOfBoundsException(idx.toString)
     if (idx == len) +=(elem)
     else {
       val p = locate(idx)
@@ -199,7 +199,7 @@ class ListBuffer[A]
     val it = elems.iterator()
     if (it.hasNext) {
       ensureUnaliased()
-      if (idx < 0 || idx > len) throw new IndexOutOfBoundsException
+      if (idx < 0 || idx > len) throw new IndexOutOfBoundsException(idx.toString)
       if (idx == len) ++=(elems)
       else insertAfter(locate(idx), it)
     }
@@ -207,7 +207,7 @@ class ListBuffer[A]
 
   def remove(idx: Int): A = {
     ensureUnaliased()
-    if (idx < 0 || idx >= len) throw new IndexOutOfBoundsException
+    if (idx < 0 || idx >= len) throw new IndexOutOfBoundsException(idx.toString)
     val p = locate(idx)
     val nx = getNext(p)
     setNext(p, nx.tail)
@@ -218,7 +218,7 @@ class ListBuffer[A]
   def remove(idx: Int, count: Int): Unit =
     if (count > 0) {
       ensureUnaliased()
-      if (idx < 0 || idx + count > len) throw new IndexOutOfBoundsException
+      if (idx < 0 || idx + count > len) throw new IndexOutOfBoundsException(idx.toString)
       removeAfter(locate(idx), count)
     } else if (count < 0) {
       throw new IllegalArgumentException("removing negative number of elements: " + count)