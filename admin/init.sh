8fc5de23fb00c59c7f475b0f58a58f8f328d3bc7
Adriaan Moors <adriaan@lightbend.com>
2018-03-07 15:27:21 +0100

    Review feedback from lrytz

--- admin/init.sh
+++ admin/init.sh
@@ -5,14 +5,9 @@ sensitive() {
   perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/credentials-private-repo > ~/.credentials-private-repo
   perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/credentials-sonatype     > ~/.credentials-sonatype
   perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/sonatype-curl            > ~/.sonatype-curl
-  # perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/m2-settings.xml          > ~/.m2/settings.xml  -- not needed anymore (used for ide integration?)
 
   openssl aes-256-cbc -d -pass "pass:$GPG_SUBKEY_SECRET" -in files/gpg_subkey.enc | gpg --import
 }
-
-# directories needed by sensitive part
-# mkdir -p ~/.m2 -- not needed anymore (used for ide integration?)
-mkdir -p ~/.ssh
 
 # don't let anything escape from the sensitive part (e.g. leak environment var by echoing to log on failure)
 sensitive >/dev/null 2>&1
