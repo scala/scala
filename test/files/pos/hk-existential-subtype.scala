afb54835da97267a8a7b17fd083c90871461f946
Georgi Krastev <joro.kr.21@gmail.com>
2018-03-18 21:28:08 +0100

    Fix bounds of higher-kinded existentials pt. 2
    I missed that in #6123

--- test/files/pos/hk-existential-subtype.scala
+++ test/files/pos/hk-existential-subtype.scala
@@ -1,4 +1,5 @@
 class Functor[F[_]]
 object Functor {
   val someF: Functor[F] forSome { type F[_] } = new Functor[Option]
+  val someG: Functor[G] forSome { type G[x] <: List[x] } = new Functor[::]
 }
