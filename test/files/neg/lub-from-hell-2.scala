0b32def000d7ebcf0219a9452a8760b90735bc3e
Lukas Rytz <lukas.rytz@gmail.com>
2018-04-05 13:21:29 +0200

    Consolidate tests for invalid lubs

--- test/files/neg/lub-from-hell-2.scala
+++
@@ -1,5 +0,0 @@
-// f-bounded types involved in LUBs can sometimes produce an ill-bounded LUB.
-class Test {
-  trait Tree
-  def foo(a: Boolean, b: List[Any], c: collection.mutable.ListBuffer[Any]) = if (a) b else c
-}
