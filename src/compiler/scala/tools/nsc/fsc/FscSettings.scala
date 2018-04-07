23206a2a277926991db9a488b685ef63efcb2f38
taku0 <mxxouy6x3m_github@tatapa.org>
2018-04-07 14:38:59 +0900

    Support JEP 293 style long command-line options
    JEP 293 specifies GNU-like styles for command-line options, which is
    supported from OpenJDK/Oracle JDK 9.
    
    This patch adds synonyms with two hyphens for each options.
    Option names consist with multiple words are separated by hyphens.

--- src/compiler/scala/tools/nsc/fsc/FscSettings.scala
+++ src/compiler/scala/tools/nsc/fsc/FscSettings.scala
@@ -17,15 +17,15 @@ class FscSettings(error: String => Unit) extends Settings(error) {
     disable(resident)
   }
 
-  val currentDir   = StringSetting ("-current-dir", "path", "Base directory for resolving relative paths", "").internalOnly()
-  val reset        = BooleanSetting("-reset",    "Reset compile server caches")
-  val shutdown     = BooleanSetting("-shutdown", "Shutdown compile server")
-  val server       = StringSetting ("-server",   "hostname:portnumber", "Specify compile server socket", "")
+  val currentDir   = StringSetting ("-current-dir", "path", "Base directory for resolving relative paths", "").internalOnly() withAbbreviation "--current-directory"
+  val reset        = BooleanSetting("-reset",    "Reset compile server caches") withAbbreviation "--reset"
+  val shutdown     = BooleanSetting("-shutdown", "Shutdown compile server") withAbbreviation "--shutdown"
+  val server       = StringSetting ("-server",   "hostname:portnumber", "Specify compile server socket", "") withAbbreviation "--server"
   val port         = IntSetting    ("-port",     "Search and start compile server in given port only",
-                                       0, Some((0, Int.MaxValue)), (_: String) => None)
-  val preferIPv4   = BooleanSetting("-ipv4",     "Use IPv4 rather than IPv6 for the server socket")
+                                       0, Some((0, Int.MaxValue)), (_: String) => None) withAbbreviation "--port"
+  val preferIPv4   = BooleanSetting("-ipv4",     "Use IPv4 rather than IPv6 for the server socket") withAbbreviation "--ipv4"
   val idleMins     = IntSetting    ("-max-idle", "Set idle timeout in minutes for fsc (use 0 for no timeout)",
-                                       30, Some((0, Int.MaxValue)), (_: String) => None)
+                                       30, Some((0, Int.MaxValue)), (_: String) => None) withAbbreviation "--max-idle"
 
   // For improved help output, separating fsc options from the others.
   def fscSpecific = Set[Settings#Setting](
