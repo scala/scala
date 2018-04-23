1c7625e8fcd12e8c5e4566d666c7809898afcb19
Lukas Rytz <lukas.rytz@gmail.com>
2018-04-23 13:35:32 +0200

    Test case for fixed bug in mutable queues

--- test/junit/scala/collection/mutable/QueueTest.scala
+++ test/junit/scala/collection/mutable/QueueTest.scala
@@ -15,4 +15,18 @@ class QueueTest {
     val q2: Queue[Int] = q1.reverse
     assertEquals("Queue", q2.className)
   }
+
+  @Test
+  def t9005(): Unit = {
+    val q = Queue(1, 2, 3)
+    assertEquals(q, List(1, 2, 3))
+    val r = q.tail
+    assertEquals(r, List(2, 3))
+    assertEquals(q.enqueue(4), List(1, 2, 3, 4))
+    assertEquals(r, List(2, 3))
+    val s = q.drop(1)
+    assertEquals(s, List(2, 3, 4))
+    assertEquals(q.enqueue(5), List(1, 2, 3, 4, 5))
+    assertEquals(s, List(2, 3, 4))
+  }
 }
