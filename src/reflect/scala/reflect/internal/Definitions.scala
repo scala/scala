18bc693f67266fbb1ff4e126f7a84b79d35f4d1b
Jason Zaugg <jzaugg@gmail.com>
2018-05-02 11:14:56 +1000

    Optimize isStable by using normalize, which is cached
    (cherry picked from commit 9e533873f87535fc28ac7d315f04bf37697cc44e)

--- src/reflect/scala/reflect/internal/Definitions.scala
+++ src/reflect/scala/reflect/internal/Definitions.scala
@@ -758,7 +758,7 @@ trait Definitions extends api.StandardDefinitions {
       case TypeRef(_, NothingClass | SingletonClass, _) => true
       case TypeRef(_, sym, _) if sym.isAbstractType     => tp.bounds.hi.typeSymbol isSubClass SingletonClass
       case TypeRef(pre, sym, _) if sym.isModuleClass    => isStable(pre)
-      case TypeRef(_, _, _)                             => val dealiased = tp.dealias; (dealiased ne tp) && isStable(dealiased)
+      case TypeRef(_, _, _)                             => val normalize = tp.normalize; (normalize ne tp) && isStable(normalize)
       case TypeVar(origin, _)                           => isStable(origin)
       case AnnotatedType(_, atp)                        => isStable(atp)    // Really?
       case _: SimpleTypeProxy                           => isStable(tp.underlying)
