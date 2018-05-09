fa39e5a4338aa8b7c3cab2e7cacc5d8649e124d6
Jason Zaugg <jzaugg@gmail.com>
2018-05-09 19:44:40 +1000

    Expunge the transformed field from Typer
    (cherry picked from commit 708f5990381f149564d96b47abe3a432345f08f2)

--- src/compiler/scala/tools/nsc/typechecker/Typers.scala
+++ src/compiler/scala/tools/nsc/typechecker/Typers.scala
@@ -38,11 +38,6 @@ trait Typers extends Adaptations with Tags with TypersTracking with PatternTyper
   final def forArgMode(fun: Tree, mode: Mode) =
     if (treeInfo.isSelfOrSuperConstrCall(fun)) mode | SCCmode else mode
 
-  // namer calls typer.computeType(rhs) on DefDef / ValDef when tpt is empty. the result
-  // is cached here and re-used in typedDefDef / typedValDef
-  // Also used to cache imports type-checked by namer.
-  val transformed = new mutable.AnyRefMap[Tree, Tree]
-
   final val shortenImports = false
 
   // allows override of the behavior of the resetTyper method w.r.t comments
@@ -193,7 +188,7 @@ trait Typers extends Adaptations with Tags with TypersTracking with PatternTyper
     val runDefinitions = currentRun.runDefinitions
     import runDefinitions._
 
-    private val transformed: mutable.Map[Tree, Tree] = unit.transformed
+    private def transformed: mutable.Map[Tree, Tree] = unit.transformed
 
     val infer = new Inferencer {
       def context = Typer.this.context
