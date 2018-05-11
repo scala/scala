3bd718d114ce5b157976ef848fea06a84ec385b3
Som Snytt <som.snytt@gmail.com>
2018-05-11 15:08:34 -0700

    Xlint nonlocal-return
    Don't just log it, Xlint it!
    
    Mention where control is returning to.

---
+++ test/files/neg/t10820-warn.scala
@@ -0,0 +1,12 @@
+
+import util.Try
+
+trait Test {
+
+  /*
+   * Inspired by
+   * https://stackoverflow.com/questions/50297478/why-scala-allows-return-from-recoverwith
+   * "I actually saw the post Don't Use Return in Scala long ago never paid attention."
+   */
+  def result: Option[Int] = Try(0/0).recoverWith { case _ => return Some(-1) }.toOption
+}
