a9c2b57af3fa7642790f6c56c30b8a25f88436f6
Som Snytt <som.snytt@gmail.com>
2018-06-08 09:46:55 -0700

    Tests for issue 2458

---
+++ test/files/neg/t2458c/test.scala
@@ -0,0 +1,14 @@
+
+import q.X._
+
+package p {
+  object Test {
+    def main(args: Array[String]): Unit = println {
+      // both defined in p in other unit, and imported from q.X = ambiguous
+      f() + g()
+    }
+  }
+}
+package q {
+  object X { def f = "bye" ; def g = "hi" }
+}
