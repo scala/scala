7dd39e106fc039c7cbea0f723cb4c2841b866458
Jason Zaugg <jzaugg@gmail.com>
2018-06-10 13:42:10 +1000

    Customise label handling without needing to modify ASM directly

---
+++ src/compiler/scala/tools/nsc/backend/jvm/LabelNode1.java
@@ -0,0 +1,19 @@
+/* NSC -- new Scala compiler
+ * Copyright 2018 LAMP/EPFL
+ * @author Martin Odersky
+ */
+package scala.tools.nsc.backend.jvm;
+
+import scala.tools.asm.Label;
+import scala.tools.asm.tree.LabelNode;
+
+public class LabelNode1 extends LabelNode {
+    public LabelNode1() {
+    }
+
+    public LabelNode1(Label label) {
+        super(label);
+    }
+
+    public int flags;
+}
