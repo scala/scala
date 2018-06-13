7b3babe1b6d12df0ffabc8ca1a0fbfe544318145
xuwei-k <6b656e6a69@gmail.com>
2018-06-13 18:19:06 +0900

    add deprecated DefaultMap in immutable package object

--- src/library/scala/collection/immutable/package.scala
+++ src/library/scala/collection/immutable/package.scala
@@ -11,4 +11,7 @@ package object immutable {
   type Traversable[+X] = Iterable[X]
   @deprecated("Use Iterable instead of Traversable", "2.13.0")
   val Traversable = Iterable
+
+  @deprecated("Use Map instead of DefaultMap", "2.13.0")
+  type DefaultMap[K, +V] = scala.collection.immutable.Map[K, V]
 }
