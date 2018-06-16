d0241517fc3c773befd13f87b7600e9000691497
Eugene Yokota <eed3si9n@gmail.com>
2018-06-16 06:08:21 -0400

    Improve the tuple adaptation warning message

--- src/compiler/scala/tools/nsc/typechecker/Adaptations.scala
+++ src/compiler/scala/tools/nsc/typechecker/Adaptations.scala
@@ -77,7 +77,7 @@ trait Adaptations {
         }
       } else if (settings.warnAdaptedArgs)
         context.warning(t.pos, adaptWarningMessage(
-          s"Adapting argument list by creating a ${args.size}-tuple: this may not be what you want.")
+          s"adapted the argument list to the expected ${args.size}-tuple: add additional parens instead")
         )
 
       // return `true` if the adaptation should be kept
