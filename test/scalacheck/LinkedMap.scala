76b3be07dff3bc91928a8d56a1221140b3ae7b71
Matthew de Detrich <mdedetrich@gmail.com>
2018-06-27 01:19:45 +0200

    Add ordering on withDefault test

--- test/scalacheck/LinkedMap.scala
+++ test/scalacheck/LinkedMap.scala
@@ -18,10 +18,17 @@ object LinkedMapTest extends Properties("LinkedMap") {
 
   property("ordering append") = forAll(arbitrary[Map[Int,Int]] suchThat (_.size > 1))
     { (m: Map[Int, Int]) =>
-      val list = m.toList
-      val first = list.init
-      val elem = list.last
-      val lm = LinkedMap.from(first)
-      (lm + elem).iterator.toList == first :+ elem
-    }
+      val (m1, m2) = m.splitAt(m.size / 2)
+      val first = m1.toList
+      val second = m2.toList
+      (LinkedMap.from(first) ++ second).iterator.toList == first ++ second
+  }
+
+  property("ordering on withDefault") = forAll(arbitrary[Map[Int,Int]] suchThat (_.size > 1))
+    { (m: Map[Int, Int]) =>
+      val (m1, m2) = m.splitAt(m.size / 2)
+      val first = m1.toList
+      val second = m2.toList
+      (LinkedMap.from(first).withDefaultValue(1) ++ second).iterator.toList == first ++ second
+  }
 }
