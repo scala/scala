f5b40fdccb8bf9483603e99e62073510e79ad0cf
wholock <prithviraj.m.billa@gmail.com>
2018-07-08 12:11:34 -0400

    Added aliases for missing classes in collection.generic

--- src/library/scala/collection/generic/package.scala
+++ src/library/scala/collection/generic/package.scala
@@ -4,6 +4,13 @@ package scala.collection
 package object generic {
   @deprecated("Clearable was moved from collection.generic to collection.mutable", "2.13.0")
   type Clearable = scala.collection.mutable.Clearable
+
   @deprecated("Use scala.collection.BuildFrom instead", "2.13.0")
   type CanBuildFrom[-From, -A, +C] = scala.collection.BuildFrom[From, A, C]
+
+  @deprecated("Growable was moved from collection.generic to collection.mutable", "2.13.0")
+  type Growable[-A] = scala.collection.mutable.Growable[A]
+
+  @deprecated("Shrinkable was moved from collection.generic to collection.mutable", "2.13.0")
+  type Shrinkable[-A] = scala.collection.mutable.Shrinkable[A]
 }
