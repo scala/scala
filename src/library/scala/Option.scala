b904e269480a15f31b620151a21bb42745c267ca
Mike Skells <mike.skells@talk21.com>
2018-07-25 17:40:45 -0400

    make Option.WithFilter private ctor

--- src/library/scala/Option.scala
+++ src/library/scala/Option.scala
@@ -216,7 +216,7 @@ sealed abstract class Option[+A] extends Product with Serializable {
    *  collection" contract even though it seems unlikely to matter much in a
    *  collection with max size 1.
    */
-  class WithFilter(p: A => Boolean) {
+  class WithFilter private[Option] (p: A => Boolean) {
     def map[B](f: A => B): Option[B] = self filter p map f
     def flatMap[B](f: A => Option[B]): Option[B] = self filter p flatMap f
     def foreach[U](f: A => U): Unit = self filter p foreach f
