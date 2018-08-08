36baab8279074092a97363d0b80af82d76e8d2a5
Stefan Zeiger <szeiger@novocode.com>
2018-08-08 14:32:27 +0200

    Add deprecated `updated` method to s.c.m.Map
    Fixes https://github.com/scala/bug/issues/10990

--- src/library/scala/collection/mutable/AnyRefMap.scala
+++ src/library/scala/collection/mutable/AnyRefMap.scala
@@ -381,12 +381,9 @@ class AnyRefMap[K <: AnyRef, V] private[collection] (defaultEntry: K => V, initi
 
   override def ++[V2 >: V](xs: scala.collection.Iterable[(K, V2)]): AnyRefMap[K, V2] = concat(xs)
 
-  @deprecated("Use AnyRefMap.from(m).add(k,v) instead of m.updated(k, v)", "2.13.0")
-  def updated[V1 >: V](key: K, value: V1): AnyRefMap[K, V1] = {
-    val arm = clone().asInstanceOf[AnyRefMap[K, V1]]
-    arm += ((key, value))
-    arm
-  }
+  @deprecated("Use m.clone().addOne(k,v) instead of m.updated(k, v)", "2.13.0")
+  override def updated[V1 >: V](key: K, value: V1): AnyRefMap[K, V1] =
+    clone().asInstanceOf[AnyRefMap[K, V1]].addOne(key, value)
 
   private[this] def foreachElement[A,B](elems: Array[AnyRef], f: A => B): Unit = {
     var i,j = 0
