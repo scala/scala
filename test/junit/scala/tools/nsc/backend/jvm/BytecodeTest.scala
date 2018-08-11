0763168ca6501cf28e826e6a46688eb5b4bbb6d3
Eugene Yokota <eed3si9n@gmail.com>
2018-08-10 22:32:24 -0400

    [nomerge] Emit bridge method forwarders with BRIDGE flag
    Fixes scala/bug#11061
    Ref scala/bug#10812
    
    On 2.13.x branch #6531 removed the mirror class forwarders for bridge methods. I would like to do same in 2.12.x since Java 11-ea started to find them ambiguous as seen in akka/akka#25449 / scala/bug#11061.
    
    To keep binary compatibility, I am still emitting the forwarders for bridge methods, but with `ACC_BRIDGE` flag.

--- test/junit/scala/tools/nsc/backend/jvm/BytecodeTest.scala
+++ test/junit/scala/tools/nsc/backend/jvm/BytecodeTest.scala
@@ -16,6 +16,42 @@ class BytecodeTest extends BytecodeTesting {
   import compiler._
 
   @Test
+  def bridgeFlag(): Unit = {
+    val code =
+      """ A { def f: Object = null }
+        |object B extends A { override def f: String = "b" }
+      """.stripMargin
+    for (base <- List("trait", "class")) {
+      val List(a, bMirror, bModule) = compileClasses(base + code)
+      assertEquals("B", bMirror.name)
+      assertEquals(List("f()Ljava/lang/Object;0x49", "f()Ljava/lang/String;0x9"),
+        bMirror.methods.asScala
+          .filter(_.name == "f")
+          .map(m => m.name + m.desc + "0x" + Integer.toHexString(m.access)).toList.sorted)
+    }
+  }
+
+  @Test
+  def varArg(): Unit = {
+    val code =
+      """ A { @annotation.varargs def f(i: Int*): Object = null }
+        |object B extends A { @annotation.varargs override def f(i: Int*): String = "b" }
+      """.stripMargin
+    for (base <- List("trait", "class")) {
+      val List(a, bMirror, bModule) = compileClasses(base + code)
+      assertEquals("B", bMirror.name)
+      assertEquals(List(
+        "f(Lscala/collection/Seq;)Ljava/lang/Object;0x49",
+        "f(Lscala/collection/Seq;)Ljava/lang/String;0x9",
+        "f([I)Ljava/lang/Object;0xc9",
+        "f([I)Ljava/lang/String;0x89"),
+        bMirror.methods.asScala
+          .filter(_.name == "f")
+          .map(m => m.name + m.desc + "0x" + Integer.toHexString(m.access)).toList.sorted)
+    }
+  }
+
+  @Test
   def t6288bJumpPosition(): Unit = {
     val code =
       """object Case3 {                                 // 01
