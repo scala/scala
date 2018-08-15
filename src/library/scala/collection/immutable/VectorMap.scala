fb1a71ba15d9730b09eca4cb1d03cc19ebbfd4eb
Matthew de Detrich <mdedetrich@gmail.com>
2018-08-15 23:54:23 +0200

    Fix transitive equality on VectorMap

--- src/library/scala/collection/immutable/VectorMap.scala
+++ src/library/scala/collection/immutable/VectorMap.scala
@@ -106,7 +106,8 @@ final class VectorMap[K, +V] private[immutable] (
   // Only care about content, not ordering for equality
   override def equals(that: Any): Boolean =
     that match {
-      case vmap: VectorMap[_, _] => underlying == vmap.underlying
+      case vmap: VectorMap[_, _] =>
+        underlying.mapValues(_._2).toMap == vmap.underlying.mapValues(_._2).toMap
       case _ => super.equals(that)
     }
 
