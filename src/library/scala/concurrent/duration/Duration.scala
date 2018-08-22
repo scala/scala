b1482c07495c5b690734722394043c66c2142333
Allan Renucci <allan.renucci@gmail.com>
2018-08-22 19:26:53 +0200

    Drop Duration::isFinite parameter list

--- src/library/scala/concurrent/duration/Duration.scala
+++ src/library/scala/concurrent/duration/Duration.scala
@@ -102,7 +102,7 @@ object Duration {
    * Extract length and time unit out of a duration, if it is finite.
    */
   def unapply(d: Duration): Option[(Long, TimeUnit)] =
-    if (d.isFinite()) Some((d.length, d.unit)) else None
+    if (d.isFinite) Some((d.length, d.unit)) else None
 
   /**
    * Construct a possibly infinite or undefined Duration from the given number of nanoseconds.
@@ -208,7 +208,7 @@ object Duration {
       case x           => Double.PositiveInfinity * (if ((this > Zero) ^ (divisor >= Zero)) -1 else 1)
     }
 
-    final def isFinite() = false
+    final def isFinite = false
 
     private[this] def fail(what: String) = throw new IllegalArgumentException(s"$what not allowed on infinite Durations")
     final def length: Long    = fail("length")
@@ -470,7 +470,7 @@ sealed abstract class Duration extends Serializable with Ordered[Duration] {
    * This method returns whether this duration is finite, which is not the same as
    * `!isInfinite` for Double because this method also returns `false` for [[Duration.Undefined]].
    */
-  def isFinite(): Boolean
+  def isFinite: Boolean
   /**
    * Return the smaller of this and that duration as determined by the natural ordering.
    */
@@ -639,7 +639,7 @@ final class FiniteDuration(val length: Long, val unit: TimeUnit) extends Duratio
   // if this is made a constant, then scalac will elide the conditional and always return +0.0, scala/bug#6331
   private[this] def minusZero = -0d
   def /(divisor: Duration): Double =
-    if (divisor.isFinite()) toNanos.toDouble / divisor.toNanos
+    if (divisor.isFinite) toNanos.toDouble / divisor.toNanos
     else if (divisor eq Undefined) Double.NaN
     else if ((length < 0) ^ (divisor > Zero)) 0d
     else minusZero
@@ -704,7 +704,7 @@ final class FiniteDuration(val length: Long, val unit: TimeUnit) extends Duratio
 
   def unary_- = Duration(-length, unit)
 
-  final def isFinite() = true
+  final def isFinite = true
 
   final override def toCoarsest: FiniteDuration = {
     def loop(length: Long, unit: TimeUnit): FiniteDuration = {
