504e362f6d3a8a769a79881e1a9df4dff076e051
Miles Sabin <miles@milessabin.com>
2018-09-06 12:34:44 +0100

    Added actual error case.

--- test/files/pos/t6317.scala
+++ test/files/pos/t6317.scala
@@ -11,5 +11,8 @@ class ScalaCompilerKiller {
   val sps = List[(String, String)]()
 
   // to repro, need: implicit conversion, overloading, pair in synthetic scrutinee in function passed to higher-order method
+  (null: C).overloaded(sps.map(/* _ match */ { case (r, _) => r }))
+
+  // workaround ...
   (null: C).overloaded(sps.map(_ match { case (r, _) => r }))
 }
