e927ebae14b0dc145cb88ae9e1f60d67c8ecd7a2
Josh Lemer <joshlemer@gmail.com>
2018-09-23 11:47:20 -0400

    Add property tests for HashMap#merged
    Revert ImmutableChampHashMapProperties.K and V to String
    
    Revert HashMap properties K, V to String
    
    Sort out merge conflicts
    
    Fix merge conflicts

--- test/scalacheck/scala/collection/immutable/ImmutableChampHashMapProperties.scala
+++ test/scalacheck/scala/collection/immutable/ImmutableChampHashMapProperties.scala
@@ -3,15 +3,17 @@ package scala.collection.immutable
 import org.scalacheck.Arbitrary._
 import org.scalacheck.Prop.forAll
 import org.scalacheck._
-import scala.language.higherKinds
 
 object ImmutableChampHashMapProperties extends Properties("immutable.HashMap") {
 
-  type K = Int
-  type V = Int
+  type K = String
+  type V = String
   type T = (K, V)
 
-  property("convertToScalaMapAndCheckSize") = forAll { input: HashMap[K, V] =>
+  //  override def overrideParameters(p: org.scalacheck.Test.Parameters) =
+  //    p.withMinSuccessfulTests(1000)
+
+  property("convertToScalaMapAndCheckSize") = forAll { (input: HashMap[K, V]) =>
     convertToScalaMapAndCheckSize(input)
   }
 
@@ -126,10 +128,38 @@ object ImmutableChampHashMapProperties extends Properties("immutable.HashMap") {
   property("transform(f) == map { (k, v) => (k, f(k, v)) }") = forAll { (xs: HashMap[K, V], f: (K, V) => String) =>
     xs.transform(f) == xs.map{ case (k, v) => (k, f(k, v)) }
   }
+
+  // Warning: This is not necessarily true if `V` is a primitive, because equality is checked
+  // by `eq`, which may not return `true` for boxed primitives that otherwise would be equal.
   property("xs.transform((_, v) => v) eq xs") = forAll { xs: HashMap[K, V] =>
     xs.transform((_, v) => v) eq xs
   }
+
+  property("left.merged(right) { select left } is the same as right concat left") =
+    forAll { (left: HashMap[K, V], right: HashMap[K, V]) =>
+      left.merged(right)((left, _) => left) == right.concat(left)
+    }
+  property("left.merged(right) { select right } is the same as left concat right") =
+    forAll { (left: HashMap[K, V], right: HashMap[K, V]) =>
+      left.merged(right)((_, right) => right) == left.concat(right)
+    }
+  property("merged passes through all key-values in either map that aren't in the other") =
+    forAll { (left: HashMap[K, V], right: HashMap[K, V], mergedValue: V) =>
+      val onlyInLeft = left.filter { case (k, _) => !right.contains(k) }
+      val onlyInRight = right.filter { case (k, _) => !left.contains(k) }
+
+      val merged = left.merged(right){ case (_, (k, _)) => k -> mergedValue }
+
+      onlyInLeft.forall{ case (k, v) => merged.get(k).contains(v) } &&
+      onlyInRight.forall{ case (k, v) => merged.get(k).contains(v) }
+    }
+
+  property("merged results in a merged value for all keys that appear in both maps") =
+    forAll { (left: HashMap[K, V], right: HashMap[K, V], mergedValue: V) =>
+      val intersected = left.filter { case (k, _) => right.contains(k) }
+
+      val merged = left.merged(right){ case (_, (k, _)) => k -> mergedValue }
+
+      intersected.forall { case (k, _) => merged.get(k).contains(mergedValue) }
+    }
 }
-
-
-
