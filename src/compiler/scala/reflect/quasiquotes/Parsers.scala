650bf9b4fe765a8487d852918744ce6e7f9abc92
Kobenko Mikhail <m.kobenko@tinkoff.ru>
2018-10-15 12:47:31 +0500

    Revert "small optimization for one call value"
    This reverts commit 956be8e

--- src/compiler/scala/reflect/quasiquotes/Parsers.scala
+++ src/compiler/scala/reflect/quasiquotes/Parsers.scala
@@ -6,9 +6,9 @@ import scala.tools.nsc.ast.parser.Tokens._
 import scala.reflect.internal.util.{BatchSourceFile, SourceFile, FreshNameCreator}
 
 /** Builds upon the vanilla Scala parser and teams up together with Placeholders.scala to emulate holes.
- *  A principled solution to splicing into Scala syntax would be a parser that natively supports holes.
- *  Unfortunately, that's outside of our reach in Scala 2.11, so we have to emulate.
- */
+  *  A principled solution to splicing into Scala syntax would be a parser that natively supports holes.
+  *  Unfortunately, that's outside of our reach in Scala 2.11, so we have to emulate.
+  */
 trait Parsers { self: Quasiquotes =>
   import global.{Try => _, _}
   import build.implodePatDefs
