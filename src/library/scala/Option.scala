8199dcc8fe63683e2ec01a4770671331f2dd737d
texasbruce <popoxee@hotmail.com>
2018-10-22 14:23:53 -0400

    Fix scala/bug#11222
    Define unzip and unzip3 for Option
    
    Changed constructing using Option to Some as per @SethTisue

--- src/library/scala/Option.scala
+++ src/library/scala/Option.scala
@@ -323,6 +323,43 @@ sealed abstract class Option[+A] extends Product with Serializable {
   final def zip[A1 >: A, B](that: Option[B]): Option[(A1, B)] =
     if (isEmpty || that.isEmpty) None else Some((this.get, that.get))
 
+  /** Converts an Option of pairs into an Option of first elements and an Option of second elements.
+    *
+    *  @tparam A1    the type of the first half of the element pairs
+    *  @tparam A2    the type of the second half of the element pairs
+    *  @param asPair an implicit conversion which asserts that the element type
+    *                of this Option is a pair.
+    *  @return       a pair of Options, containing, respectively, the first and second half
+    *                of each element pair of this Option.
+    */
+  final def unzip[A1, A2](implicit asPair: A => (A1, A2)): (Option[A1], Option[A2]) = {
+    if (isEmpty)
+      (None, None)
+    else {
+      val e = asPair(this.get)
+      (Some(e._1), Some(e._2))
+    }
+  }
+
+  /** Converts an Option of triples into three Options, one containing the elements from each position of the triple.
+    *
+    *  @tparam A1      the type of the first of three elements in the triple
+    *  @tparam A2      the type of the second of three elements in the triple
+    *  @tparam A3      the type of the third of three elements in the triple
+    *  @param asTriple an implicit conversion which asserts that the element type
+    *                  of this Option is a triple.
+    *  @return         a triple of Options, containing, respectively, the first, second, and third
+    *                  elements from each element triple of this Option.
+    */
+  final def unzip3[A1, A2, A3](implicit asTriple: A => (A1, A2, A3)): (Option[A1], Option[A2], Option[A3]) = {
+    if (isEmpty)
+      (None, None, None)
+    else {
+      val e = asTriple(this.get)
+      (Some(e._1), Some(e._2), Some(e._3))
+    }
+  }
+
   /** Returns a singleton iterator returning the $option's value
    * if it is nonempty, or an empty iterator if the option is empty.
    */
--- test/junit/scala/OptionTest.scala
+++ test/junit/scala/OptionTest.scala
@@ -11,6 +11,14 @@ class OptionTest extends RunTesting {
   @Test def testNoneZipSome: Unit = assertEquals(None zip Some("bar"), None)
   @Test def testNoneZipNone: Unit = assertEquals(None zip None, None)
 
+  @Test def testSomeUnzipToSomePair: Unit = assertEquals(Some(("foo", "bar")) unzip, (Some("foo"), Some("bar")))
+  @Test def testSomeUnzipToSomeNone: Unit = assertEquals(Some(("foo", null)) unzip, (Some("foo"), Some(null)))
+  @Test def testNoneUnzipToNonePair: Unit = assertEquals(None unzip, (None, None))
+
+  @Test def testSomeUnzip3ToSomeTriple: Unit = assertEquals(Some(("foo", "bar", "z")) unzip3, (Some("foo"), Some("bar"), Some("z")))
+  @Test def testSomeUnzip3ToSomeNone: Unit = assertEquals(Some(("foo", null, null)) unzip3, (Some("foo"), Some(null), Some(null)))
+  @Test def testNoneUnzip3ToNoneTriple: Unit = assertEquals(None unzip3, (None, None, None))
+
   import runner._
   import scala.tools.reflect.{ ToolBoxError => E }
 
