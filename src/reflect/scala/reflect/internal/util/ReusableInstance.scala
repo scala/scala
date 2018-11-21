f4ec73898629f4e7b5cb0c5f5a32781a8fd9f835
Harrison Houghton <hhoughton@learningobjects.com>
2018-11-21 11:12:29 -0500

    Avoid allocation of *Ref objects for vars in Context#lookupSymbol
    Even after 30+ iterations, I was unable to get the JIT to eliminate
    these allocations.
    
    Add in the single-element cache that could be used for scala/scala-dev#498.

---
+++ src/reflect/scala/reflect/internal/util/ReusableInstance.scala
@@ -0,0 +1,37 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala
+package reflect
+package internal
+package util
+
+/** A wrapper for a re-entrant, cached instance of a value of type `T`.
+  *
+  * Not thread safe.
+  */
+final class ReusableInstance[T <: AnyRef](make: () => T) {
+  private val cached = make()
+  private var taken = false
+
+  @inline def using[R](action: T => R): R =
+    if (taken) action(make())
+    else try {
+      taken = true
+      action(cached)
+    } finally taken = false
+}
+
+object ReusableInstance {
+  def apply[T <: AnyRef](make: => T): ReusableInstance[T] =
+    new ReusableInstance[T](make _)
+}
