e7eca326c31611046a0dd33b71cc07598dfe7be7
Anthony Tjuatja <anthonytjuatja@gmail.com>
2018-12-12 02:27:34 +0700

    Implemented IndexedSeqView's Iterator and Reverse iterator `slice` operation with constant time complexity, and added tests for both type of iterator
    Added tests for 1 element and 2 elements, code organizations for previous test with 0 elements
    
    Overriden sliceIterator instead of slice
    
    Used protected access modifier for sliceIterator
    
    Re-added `private` keyword accidentally removed
    
    Removed guarding of overflow for `pos` on ReverseIterator and `current` on forward iterator, added line break for import statements between `scala`-based package and non-`scala`-based package

--- test/junit/scala/collection/immutable/IndexedSeqTest.scala
+++ test/junit/scala/collection/immutable/IndexedSeqTest.scala
@@ -3,6 +3,9 @@ package scala.collection.immutable
 import org.junit._
 import Assert._
 
+import scala.collection.Seq.empty
+import scala.language.postfixOps
+import scala.tools.testing.AssertUtil._
 import scala.util.AllocationTest
 
 class IndexedSeqTest extends AllocationTest {
@@ -93,6 +96,48 @@ class IndexedSeqTest extends AllocationTest {
     assertNotEquals(Range(0,1), new MyRange(0, 1))
     assertEquals(new MyRange(0, 1), new MyRange(0, 1))
   }
+
+  @Test def testforwardSliceEquals(): Unit = {
+
+    def generateTestIter = (0 to 10).view iterator
+
+
+    assertSameElements(empty toList,  generateTestIter slice (3, -1))
+    assertSameElements(empty toList,  generateTestIter slice (0, 0))
+    assertSameElements(empty,  generateTestIter slice (3, 3))
+    assertSameElements(List(0) ,  generateTestIter slice (0, 1))
+    assertSameElements(0 to 1 ,  generateTestIter slice (0, 2))
+    assertSameElements(3 to 6 toList, generateTestIter slice(3,7))
+    assertSameElements(0 to 2 toList, generateTestIter slice (-1, 3))
+    assertSameElements(0 to 10 toList, generateTestIter slice (0, 11))
+    assertSameElements(6 to 12 by 2 toList, generateTestIter slice (3, 7) map (2 * _))
+    assertSameElements(6 to 12 by 2 toList, generateTestIter map (2 * _) slice (3, 7))
+    assertSameElements(4 to 6 toList, generateTestIter slice (3, 7) drop 1)
+    assertSameElements(4 to 7 toList, generateTestIter drop 1 slice (3, 7))
+    assertSameElements(4 to 5 toList, generateTestIter slice (3, 7) slice (1, 3))
+    assertSameElements(4 to 6 toList, generateTestIter slice (3, 7) slice (1, 10))
+  }
+
+  @Test def testbackwardSliceEquals(): Unit = {
+
+    def generateTestIter = (0 to 10).view reverseIterator
+
+    assertSameElements(empty toList,  generateTestIter slice (3, -1))
+    assertSameElements(empty toList,  generateTestIter slice (3, 2))
+    assertSameElements(empty,  generateTestIter slice (0, 0))
+    assertSameElements(empty,  generateTestIter slice (3, 3))
+    assertSameElements(List(10) ,  generateTestIter slice (0, 1))
+    assertSameElements(10 to 9 by -1 ,  generateTestIter slice (0, 2))
+    assertSameElements(7 to 4 by -1 toList, generateTestIter slice(3,7))
+    assertSameElements(10 to 8 by -1 toList, generateTestIter slice (-1, 3))
+    assertSameElements(14 to 8 by -2 toList, generateTestIter slice (3, 7) map (2 * _))
+    assertSameElements(14 to 8 by -2 toList, generateTestIter map (2 * _) slice (3, 7))
+    assertSameElements(6 to 4 by -1 toList, generateTestIter slice (3, 7) drop 1)
+    assertSameElements(6 to 3 by -1 toList, generateTestIter drop 1 slice (3, 7))
+    assertSameElements(6 to 5 by -1 toList, generateTestIter slice (3, 7) slice (1, 3))
+    assertSameElements(6 to 4 by -1 toList, generateTestIter slice (3, 7) slice (1, 10))
+  }
+
 }
 final class MyRange(val start: Int, val end: Int) extends IndexedSeq[Int] {
   def apply(idx: Int) = start + idx
