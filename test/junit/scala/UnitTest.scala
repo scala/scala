52ad2dfc39086aad84d5d56265761517fbcd994c
Dale Wijnand <dale.wijnand@gmail.com>
2018-12-17 10:14:50 +0000

    Fix UnitTest testVoid

--- test/junit/scala/UnitTest.scala
+++ test/junit/scala/UnitTest.scala
@@ -4,5 +4,5 @@ import org.junit.Assert._
 import org.junit.Test
 
 final class UnitTest {
-  @Test def testVoid: Unit = Unit.void(1)
+  @Test def testVoid(): Unit = assertTrue(().getClass == Unit.void(1).getClass)
 }
