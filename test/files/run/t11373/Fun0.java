f248e3b891b0a92b8a78007470329ffb6d4809aa
Adriaan Moors <adriaan@lightbend.com>
2019-01-21 13:39:36 +0100

    LMF only builds bridges for SAMmy

---
+++ test/files/run/t11373/Fun0.java
@@ -0,0 +1,5 @@
+public interface Fun0 {
+    String ap();
+
+    default Fun0 test(Fun0 b) { return null; }
+}
---
+++ test/files/run/t11373/Fun0Impl.java
@@ -0,0 +1,3 @@
+public interface Fun0Impl extends Fun0 {
+    default Fun0Impl test(Fun0 b) { return null; }
+}
