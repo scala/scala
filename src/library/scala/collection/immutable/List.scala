1d0b166078d5dd70c556dff07775c5485a65e4ba
Jason Zaugg <jzaugg@gmail.com>
2019-03-06 17:05:58 +1000

    Allow List.isEmpty to be an invokevirtual
    Declare a single implementation in List comparing to Nil.

--- src/library/scala/collection/immutable/List.scala
+++ src/library/scala/collection/immutable/List.scala
@@ -132,6 +132,8 @@ sealed abstract class List[+A]
     these
   }
 
+  override final def isEmpty: Boolean = this eq Nil
+
   override def prepended[B >: A](elem: B): List[B] = elem :: this
 
   // When calling prependAll with another list `prefix`, avoid copying `this`
@@ -591,13 +593,11 @@ sealed abstract class List[+A]
 final case class :: [+A](override val head: A, private[scala] var next: List[A @uncheckedVariance]) // sound because `next` is used only locally
   extends List[A] {
   releaseFence()
-  override def isEmpty: Boolean = false
   override def headOption: Some[A] = Some(head)
   override def tail: List[A] = next
 }
 
 case object Nil extends List[Nothing] {
-  override def isEmpty: Boolean = true
   override def head: Nothing = throw new NoSuchElementException("head of empty list")
   override def headOption: None.type = None
   override def tail: Nothing = throw new UnsupportedOperationException("tail of empty list")
