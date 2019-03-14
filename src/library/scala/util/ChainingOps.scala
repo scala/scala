0c7684b8e9c85e871ef06081fa59845a0e5db78b
Lukas Rytz <lukas.rytz@gmail.com>
2019-03-14 09:22:54 +0100

    improve scaladocs for ChainingOps

--- src/library/scala/util/ChainingOps.scala
+++ src/library/scala/util/ChainingOps.scala
@@ -21,37 +21,43 @@ trait ChainingSyntax {
  */
 final class ChainingOps[A](private val self: A) extends AnyVal {
   /** Applies `f` to the value for its side effects, and returns the original value.
-   *
-   *    {{{
-   *    val xs = List(1, 2, 3)
-   *               .tap(ys => println("debug " + ys.toString))
-   *    // xs == List(1, 2, 3)
-   *    }}}
-   *
-   *  @param f      the function to apply to the value.
-   *  @tparam U     the result type of the function `f`.
-   *  @return       the original value `self`.
-   */
+    *
+    * {{{
+    *   scala> import scala.util.chaining._
+    *
+    *   scala> val xs = List(1, 2, 3).tap(ys => println("debug " + ys.toString))
+    *   debug List(1, 2, 3)
+    *   xs: List[Int] = List(1, 2, 3)
+    * }}}
+    *
+    *  @param f      the function to apply to the value.
+    *  @tparam U     the result type of the function `f`.
+    *  @return       the original value `self`.
+    */
   def tap[U](f: A => U): A = {
     f(self)
     self
   }
 
   /** Converts the value by applying the function `f`.
-   *
-   *    {{{
-   *    val times6 = (_: Int) * 6
-   *    val i = (1 - 2 - 3).pipe(times6).pipe(scala.math.abs)
-   *    // i == 24
-   *    }}}
-   *
-   * Note: `(1 - 2 - 3).pipe(times6)` may have a small amount of overhead at
-   * runtime compared to the equivalent  `{ val temp = 1 - 2 - 3; times6(temp) }`.
-   *
-   *  @param f      the function to apply to the value.
-   *  @tparam B     the result type of the function `f`.
-   *  @return       a new value resulting from applying the given function
-   *                `f` to this value.
-   */
+    *
+    * {{{
+    *   scala> import scala.util.chaining._
+    *
+    *   scala> val times6 = (_: Int) * 6
+    *   times6: Int => Int = $$Lambda$2023/975629453@17143b3b
+    *
+    *   scala> val i = (1 - 2 - 3).pipe(times6).pipe(scala.math.abs)
+    *   i: Int = 24
+    * }}}
+    *
+    * Note: `(1 - 2 - 3).pipe(times6)` may have a small amount of overhead at
+    * runtime compared to the equivalent  `{ val temp = 1 - 2 - 3; times6(temp) }`.
+    *
+    *  @param f      the function to apply to the value.
+    *  @tparam B     the result type of the function `f`.
+    *  @return       a new value resulting from applying the given function
+    *                `f` to this value.
+    */
   def pipe[B](f: A => B): B = f(self)
 }
