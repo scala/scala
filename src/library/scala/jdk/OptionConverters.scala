8f742f0b6bdbcf97685766976517e8242e4f8902
Lukas Rytz <lukas.rytz@gmail.com>
2019-03-15 13:37:23 +0100

    Add jdk.OptionConverters
    Clean up conversion methods, name them `.toScala`/`.toJava` instead
    of `.asScala`/`.asJava`. The conversions create new objects, they don't
    wrap the original optional.

---
+++ src/library/scala/jdk/OptionConverters.scala
@@ -0,0 +1,107 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.jdk
+
+import java.util.{Optional, OptionalDouble, OptionalInt, OptionalLong}
+
+/** This object contains methods that enable converting between Scala `Option` and Java `Optional`
+  * types.
+  *
+  * The [[OptionConverters.Ops]] object contains extension methods that are recommended to be used
+  * when writing Scala code. The explicit conversion methods in [[OptionConverters]] are practical
+  * when writing Java code.
+  *
+  * Scala `Option` is extended with a `toJava` method that creates a corresponding `Optional`, and
+  * a `toJavaPrimitive` method that creates a specialized variant (e.g., `OptionalInt`) if
+  * applicable.
+  *
+  * Java `Optional` is extended with a `toScala` method and a `toJavaPrimitive` method.
+  *
+  * Finally, specialized `Optional` types are extended with `toScala` and `toJavaGeneric` methods.
+  *
+  * Example usage:
+  *
+  * {{{
+  *   import scala.jdk.OptionConverters.Ops._
+  *   val a = Option("example").toJava      // Creates java.util.Optional[String] containing "example"
+  *   val b = (None: Option[String]).toJava // Creates an empty java.util.Optional[String]
+  *   val c = a.toScala                     // Back to Option("example")
+  *   val d = b.toScala                     // Back to None typed as Option[String]
+  *   val e = Option(2.7).toJava            // java.util.Optional[Double] containing boxed 2.7
+  *   val f = Option(2.7).toPrimitive       // java.util.OptionalDouble containing 2.7 (not boxed)
+  *   val g = f.toScala                     // Back to Option(2.7)
+  *   val h = f.toGeneric                   // Same as e
+  *   val i = e.toPrimitive                 // Same as f
+  * }}}
+  */
+object OptionConverters {
+  object Ops {
+    /** Provides conversions from Java `Optional` to Scala `Option` and specialized `Optional` types */
+    implicit class RichOptional[A](val o: java.util.Optional[A]) extends AnyVal {
+      /** Convert a Java `Optional` to a Scala `Option` */
+      def toScala: Option[A] = if (o.isPresent) Some(o.get) else None
+      /** Convert a generic Java `Optional` to a specialized variant */
+      def toJavaPrimitive[O](implicit shape: OptionShape[A, O]): O = shape.fromJava(o)
+    }
+
+    /** Provides conversions from Scala `Option` to Java `Optional` types */
+    implicit class RichOption[A](val o: Option[A]) extends AnyVal {
+      /** Convert a Scala `Option` to a generic Java `Optional` */
+      def toJava: Optional[A] = o match { case Some(a) => Optional.ofNullable(a); case _ => Optional.empty[A] }
+      /** Convert a Scala `Option` to a specialized Java `Optional` */
+      def toJavaPrimitive[O](implicit shape: OptionShape[A, O]): O = shape.fromScala(o)
+    }
+
+    /** Provides conversions from `OptionalDouble` to Scala `Option` and the generic `Optional` */
+    implicit class RichOptionalDouble(val o: OptionalDouble) extends AnyVal {
+      /** Convert a Java `OptionalDouble` to a Scala `Option` */
+      def toScala: Option[Double] = if (o.isPresent) Some(o.getAsDouble) else None
+      /** Convert a Java `OptionalDouble` to a generic Java `Optional` */
+      def toJavaGeneric: Optional[Double] = if (o.isPresent) Optional.of(o.getAsDouble) else Optional.empty[Double]
+    }
+
+    /** Provides conversions from `OptionalInt` to Scala `Option` and the generic `Optional` */
+    implicit class RichOptionalInt(val o: OptionalInt) extends AnyVal {
+      /** Convert a Java `OptionalInt` to a Scala `Option` */
+      def toScala: Option[Int] = if (o.isPresent) Some(o.getAsInt) else None
+      /** Convert a Java `OptionalInt` to a generic Java `Optional` */
+      def toJavaGeneric: Optional[Int] = if (o.isPresent) Optional.of(o.getAsInt) else Optional.empty[Int]
+    }
+
+    /** Provides conversions from `OptionalLong` to Scala `Option` and the generic `Optional` */
+    implicit class RichOptionalLong(val o: OptionalLong) extends AnyVal {
+      /** Convert a Java `OptionalLong` to a Scala `Option` */
+      def toScala: Option[Long] = if (o.isPresent) Some(o.getAsLong) else None
+      /** Convert a Java `OptionalLong` to a generic Java `Optional` */
+      def toJavaGeneric: Optional[Long] = if (o.isPresent) Optional.of(o.getAsLong) else Optional.empty[Long]
+    }
+  }
+
+  /** Convert a Scala `Option` to a Java `Optional` */
+  def toJava[A](o: Option[A]): Optional[A] = o match {
+    case Some(a) => Optional.ofNullable(a)
+    case _ => Optional.empty[A]
+  }
+
+  /** Convert a Java `Optional` to a Scala `Option` */
+  def toScala[A](o: Optional[A]): Option[A] = if (o.isPresent) Some(o.get) else None
+
+  /** Convert a Java `OptionalDouble` to a Scala `Option` */
+  def toScala(o: OptionalDouble): Option[Double] = if (o.isPresent) Some(o.getAsDouble) else None
+
+  /** Convert a Java `OptionalInt` to a Scala `Option` */
+  def toScala(o: OptionalInt): Option[Int] = if (o.isPresent) Some(o.getAsInt) else None
+
+  /** Convert a Java `OptionalLong` to a Scala `Option` */
+  def toScala(o: OptionalLong): Option[Long] = if (o.isPresent) Some(o.getAsLong) else None
+}
--- test/junit/scala/OptionConvertersTest.scala
+++ test/junit/scala/OptionConvertersTest.scala
@@ -16,12 +16,11 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.util._
+import scala.jdk.OptionConverters.Ops._
 
 @RunWith(classOf[JUnit4])
 class OptionConvertersTest {
-  import java.util._
-  import JavaConvertersOption._
-
   @Test
   def scalaToEverything(): Unit = {
     val o = Option("fish")
@@ -32,20 +31,20 @@ class OptionConvertersTest {
     val ni = (None: Option[Int])
     val ol = Option(-1L)
     val nl = (None: Option[Long])
-    assertEquals(o.asJava, Optional.of(o.get))
-    assertEquals(n.asJava, Optional.empty[String])
-    assertEquals(od.asJava.get: Double, Optional.of(od.get).get: Double, 0)
-    assertEquals(nd.asJava, Optional.empty[Double])
-    assertEquals(od.asPrimitive, OptionalDouble.of(od.get))
-    assertEquals(nd.asPrimitive, OptionalDouble.empty)
-    assertEquals(oi.asJava.get: Int, Optional.of(oi.get).get: Int)
-    assertEquals(ni.asJava, Optional.empty[Int])
-    assertEquals(oi.asPrimitive, OptionalInt.of(oi.get))
-    assertEquals(ni.asPrimitive, OptionalInt.empty)
-    assertEquals(ol.asJava.get: Long, Optional.of(ol.get).get: Long)
-    assertEquals(nl.asJava, Optional.empty[Long])
-    assertEquals(ol.asPrimitive, OptionalLong.of(ol.get))
-    assertEquals(nl.asPrimitive, OptionalLong.empty)
+    assertEquals(o.toJava, Optional.of(o.get))
+    assertEquals(n.toJava, Optional.empty[String])
+    assertEquals(od.toJava.get: Double, Optional.of(od.get).get: Double, 0)
+    assertEquals(nd.toJava, Optional.empty[Double])
+    assertEquals(od.toJavaPrimitive, OptionalDouble.of(od.get))
+    assertEquals(nd.toJavaPrimitive, OptionalDouble.empty)
+    assertEquals(oi.toJava.get: Int, Optional.of(oi.get).get: Int)
+    assertEquals(ni.toJava, Optional.empty[Int])
+    assertEquals(oi.toJavaPrimitive, OptionalInt.of(oi.get))
+    assertEquals(ni.toJavaPrimitive, OptionalInt.empty)
+    assertEquals(ol.toJava.get: Long, Optional.of(ol.get).get: Long)
+    assertEquals(nl.toJava, Optional.empty[Long])
+    assertEquals(ol.toJavaPrimitive, OptionalLong.of(ol.get))
+    assertEquals(nl.toJavaPrimitive, OptionalLong.empty)
   }
 
   @Test
@@ -58,63 +57,64 @@ class OptionConvertersTest {
     val ni = Optional.empty[Int]
     val ol = Optional.of(-1L)
     val nl = Optional.empty[Long]
-    assertEquals(o.asScala, Option(o.get))
-    assertEquals(n.asScala, Option.empty[String])
-    assertEquals(od.asScala, Option(od.get))
-    assertEquals(nd.asScala, Option.empty[Double])
-    assertEquals(od.asPrimitive, OptionalDouble.of(od.get))
-    assertEquals(nd.asPrimitive, OptionalDouble.empty)
-    assertEquals(oi.asScala, Option(oi.get))
-    assertEquals(ni.asScala, Option.empty[Int])
-    assertEquals(oi.asPrimitive, OptionalInt.of(oi.get))
-    assertEquals(ni.asPrimitive, OptionalInt.empty)
-    assertEquals(ol.asScala, Option(ol.get))
-    assertEquals(nl.asScala, Option.empty[Long])
-    assertEquals(ol.asPrimitive, OptionalLong.of(ol.get))
-    assertEquals(nl.asPrimitive, OptionalLong.empty)
+    assertEquals(o.toScala, Option(o.get))
+    assertEquals(n.toScala, Option.empty[String])
+    assertEquals(od.toScala, Option(od.get))
+    assertEquals(nd.toScala, Option.empty[Double])
+    assertEquals(od.toJavaPrimitive, OptionalDouble.of(od.get))
+    assertEquals(nd.toJavaPrimitive, OptionalDouble.empty)
+    assertEquals(oi.toScala, Option(oi.get))
+    assertEquals(ni.toScala, Option.empty[Int])
+    assertEquals(oi.toJavaPrimitive, OptionalInt.of(oi.get))
+    assertEquals(ni.toJavaPrimitive, OptionalInt.empty)
+    assertEquals(ol.toScala, Option(ol.get))
+    assertEquals(nl.toScala, Option.empty[Long])
+    assertEquals(ol.toJavaPrimitive, OptionalLong.of(ol.get))
+    assertEquals(nl.toJavaPrimitive, OptionalLong.empty)
   }
 
   @Test
   def javaOptionalDoubleToEverything(): Unit = {
     val o = OptionalDouble.of(2.7)
     val n = OptionalDouble.empty
-    assertEquals(o.asScala, Option(o.getAsDouble))
-    assertEquals(o.asGeneric.get: Double, Optional.of(o.getAsDouble).get: Double, 0)
-    assertEquals(n.asScala, None: Option[Double])
-    assertEquals(n.asGeneric, Optional.empty[Double])
+    assertEquals(o.toScala, Option(o.getAsDouble))
+    assertEquals(o.toJavaGeneric.get: Double, Optional.of(o.getAsDouble).get: Double, 0)
+    assertEquals(n.toScala, None: Option[Double])
+    assertEquals(n.toJavaGeneric, Optional.empty[Double])
   }
 
   @Test
   def javaOptionalIntToEverything(): Unit = {
     val o = OptionalInt.of(4)
     val n = OptionalInt.empty
-    assertEquals(o.asScala, Option(o.getAsInt))
-    assertEquals(o.asGeneric.get: Int, Optional.of(o.getAsInt).get: Int)
-    assertEquals(n.asScala, None: Option[Int])
-    assertEquals(n.asGeneric, Optional.empty[Int])
+    assertEquals(o.toScala, Option(o.getAsInt))
+    assertEquals(o.toJavaGeneric.get: Int, Optional.of(o.getAsInt).get: Int)
+    assertEquals(n.toScala, None: Option[Int])
+    assertEquals(n.toJavaGeneric, Optional.empty[Int])
   }
 
   @Test
   def javaOptionalLongToEverything(): Unit = {
     val o = OptionalLong.of(-1)
     val n = OptionalLong.empty
-    assertEquals(o.asScala, Option(o.getAsLong))
-    assertEquals(o.asGeneric.get: Long, Optional.of(o.getAsLong).get: Long)
-    assertEquals(n.asScala, None: Option[Long])
-    assertEquals(n.asGeneric, Optional.empty[Long])
+    assertEquals(o.toScala, Option(o.getAsLong))
+    assertEquals(o.toJavaGeneric.get: Long, Optional.of(o.getAsLong).get: Long)
+    assertEquals(n.toScala, None: Option[Long])
+    assertEquals(n.toJavaGeneric, Optional.empty[Long])
   }
 
   @Test
   def nonExtensionConversions(): Unit = {
-    assertEquals(toScala(Optional.of("fish")), Option("fish"))
-    assertEquals(toScala(Optional.empty[String]), None)
-    assertEquals(toJava(Option("fish")), Optional.of("fish"))
-    assertEquals(toJava(None: Option[String]), Optional.empty[String])
-    assertEquals(toScala(OptionalDouble.of(2.7)), Option(2.7))
-    assertEquals(toScala(OptionalDouble.empty), None)
-    assertEquals(toScala(OptionalInt.of(4)), Option(4))
-    assertEquals(toScala(OptionalInt.empty), None)
-    assertEquals(toScala(OptionalLong.of(-1L)), Option(-1L))
-    assertEquals(toScala(OptionalLong.empty), None)
+    import scala.jdk.{OptionConverters => conv}
+    assertEquals(conv.toScala(Optional.of("fish")), Option("fish"))
+    assertEquals(conv.toScala(Optional.empty[String]), None)
+    assertEquals(conv.toJava(Option("fish")), Optional.of("fish"))
+    assertEquals(conv.toJava(None: Option[String]), Optional.empty[String])
+    assertEquals(conv.toScala(OptionalDouble.of(2.7)), Option(2.7))
+    assertEquals(conv.toScala(OptionalDouble.empty), None)
+    assertEquals(conv.toScala(OptionalInt.of(4)), Option(4))
+    assertEquals(conv.toScala(OptionalInt.empty), None)
+    assertEquals(conv.toScala(OptionalLong.of(-1L)), Option(-1L))
+    assertEquals(conv.toScala(OptionalLong.empty), None)
   }
 }
