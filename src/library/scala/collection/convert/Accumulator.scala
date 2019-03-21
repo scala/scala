d295c1ca9d5c777cf51daba17dc39e015b31786a
Lukas Rytz <lukas.rytz@gmail.com>
2019-03-21 16:20:29 +0100

    Move accumulator shape type classes to where they're used

--- src/library/scala/collection/convert/Accumulator.scala
+++ src/library/scala/collection/convert/Accumulator.scala
@@ -12,7 +12,6 @@
 
 package scala.collection.convert
 
-import scala.collection.convert.impl.AccumulatorFactoryShape
 import scala.collection.mutable
 
 
@@ -65,11 +64,21 @@ trait Accumulator[@specialized(Double, Int, Long) A, +CC[_], +C]
   }
 }
 
-/** Contains factory methods to build Accumulators. Depending on the element types, a generic
-  * [[AnyAccumulator]] or a specialized primitive Accumulator like [[IntAccumulator]] is created.
+/** Contains factory methods to build Accumulators.
+  *
+  * Note that the `Accumulator` object itself is not a factory, but it is implicitly convert to
+  * a factory according to the element type, see [[Accumulator.toFactory]].
+  *
+  * This allows passing the `Accumulator` object as argument when a [[collection.Factory]], and
+  * the implicit [[Accumulator.AccumulatorFactoryShape]] instance is used to build a specialized
+  * Accumulator according to the element type:
+  *
   * {{{
   *   scala> val intAcc = Accumulator(1,2,3)
   *   intAcc: scala.collection.convert.IntAccumulator = IntAccumulator(1, 2, 3)
+  *
+  *   scala> val anyAccc = Accumulator("K")
+  *   anyAccc: scala.collection.convert.AnyAccumulator[String] = AnyAccumulator(K)
   * }}}
   *
   * @define coll Accumulator
@@ -168,7 +177,7 @@ object Accumulator {
     *  @return  An $coll that contains the results of `n1 x n2` evaluations of `elem`.
     */
   def fill[A, C](n1: Int, n2: Int)(elem: => A)(implicit canAccumulate: AccumulatorFactoryShape[A, C]): AnyAccumulator[C] = 
-    fill(n1)(fill(n2)(elem)(canAccumulate))(impl.AccumulatorFactoryShape.anyAccumulatorFactoryShape[C])
+    fill(n1)(fill(n2)(elem)(canAccumulate))(AccumulatorFactoryShape.anyAccumulatorFactoryShape[C])
 
   /** Produces a three-dimensional $coll containing the results of some element computation a number of times.
     *  @param   n1  the number of elements in the 1st dimension
@@ -178,7 +187,7 @@ object Accumulator {
     *  @return  An $coll that contains the results of `n1 x n2 x n3` evaluations of `elem`.
     */
   def fill[A, C](n1: Int, n2: Int, n3: Int)(elem: => A)(implicit canAccumulate: AccumulatorFactoryShape[A, C]): AnyAccumulator[AnyAccumulator[C]] =
-    fill(n1)(fill(n2, n3)(elem)(canAccumulate))(impl.AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[C]])
+    fill(n1)(fill(n2, n3)(elem)(canAccumulate))(AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[C]])
 
   /** Produces a four-dimensional $coll containing the results of some element computation a number of times.
     *  @param   n1  the number of elements in the 1st dimension
@@ -189,7 +198,7 @@ object Accumulator {
     *  @return  An $coll that contains the results of `n1 x n2 x n3 x n4` evaluations of `elem`.
     */
   def fill[A, C](n1: Int, n2: Int, n3: Int, n4: Int)(elem: => A)(implicit canAccumulate: AccumulatorFactoryShape[A, C]): AnyAccumulator[AnyAccumulator[AnyAccumulator[C]]] =
-    fill(n1)(fill(n2, n3, n4)(elem)(canAccumulate))(impl.AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[AnyAccumulator[C]]])
+    fill(n1)(fill(n2, n3, n4)(elem)(canAccumulate))(AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[AnyAccumulator[C]]])
 
   /** Produces a five-dimensional $coll containing the results of some element computation a number of times.
     *  @param   n1  the number of elements in the 1st dimension
@@ -201,7 +210,7 @@ object Accumulator {
     *  @return  An $coll that contains the results of `n1 x n2 x n3 x n4 x n5` evaluations of `elem`.
     */
   def fill[A, C](n1: Int, n2: Int, n3: Int, n4: Int, n5: Int)(elem: => A)(implicit canAccumulate: AccumulatorFactoryShape[A, C]): AnyAccumulator[AnyAccumulator[AnyAccumulator[AnyAccumulator[C]]]] =
-    fill(n1)(fill(n2, n3, n4, n5)(elem)(canAccumulate))(impl.AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[AnyAccumulator[AnyAccumulator[C]]]])
+    fill(n1)(fill(n2, n3, n4, n5)(elem)(canAccumulate))(AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[AnyAccumulator[AnyAccumulator[C]]]])
 
   /** Produces an $coll containing values of a given function over a range of integer values starting from 0.
     *  @param  n   The number of elements in the $coll
@@ -219,7 +228,7 @@ object Accumulator {
     *          for `0 <= i1 < n1` and `0 <= i2 < n2`.
     */
   def tabulate[A, C](n1: Int, n2: Int)(f: (Int, Int) => A)(implicit canAccumulate: AccumulatorFactoryShape[A, C]): AnyAccumulator[C] =
-    tabulate(n1)(i1 => tabulate(n2)(f(i1, _))(canAccumulate))(impl.AccumulatorFactoryShape.anyAccumulatorFactoryShape[C])
+    tabulate(n1)(i1 => tabulate(n2)(f(i1, _))(canAccumulate))(AccumulatorFactoryShape.anyAccumulatorFactoryShape[C])
 
   /** Produces a three-dimensional $coll containing values of a given function over ranges of integer values starting from 0.
     *  @param   n1  the number of elements in the 1st dimension
@@ -230,7 +239,7 @@ object Accumulator {
     *          for `0 <= i1 < n1`, `0 <= i2 < n2`, and `0 <= i3 < n3`.
     */
   def tabulate[A, C](n1: Int, n2: Int, n3: Int)(f: (Int, Int, Int) => A)(implicit canAccumulate: AccumulatorFactoryShape[A, C]): AnyAccumulator[AnyAccumulator[C]] =
-    tabulate(n1)(i1 => tabulate(n2, n3)(f(i1, _, _))(canAccumulate))(impl.AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[C]])
+    tabulate(n1)(i1 => tabulate(n2, n3)(f(i1, _, _))(canAccumulate))(AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[C]])
 
   /** Produces a four-dimensional $coll containing values of a given function over ranges of integer values starting from 0.
     *  @param   n1  the number of elements in the 1st dimension
@@ -242,7 +251,7 @@ object Accumulator {
     *          for `0 <= i1 < n1`, `0 <= i2 < n2`, `0 <= i3 < n3`, and `0 <= i4 < n4`.
     */
   def tabulate[A, C](n1: Int, n2: Int, n3: Int, n4: Int)(f: (Int, Int, Int, Int) => A)(implicit canAccumulate: AccumulatorFactoryShape[A, C]): AnyAccumulator[AnyAccumulator[AnyAccumulator[C]]] =
-    tabulate(n1)(i1 => tabulate(n2, n3, n4)(f(i1, _, _, _))(canAccumulate))(impl.AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[AnyAccumulator[C]]])
+    tabulate(n1)(i1 => tabulate(n2, n3, n4)(f(i1, _, _, _))(canAccumulate))(AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[AnyAccumulator[C]]])
 
   /** Produces a five-dimensional $coll containing values of a given function over ranges of integer values starting from 0.
     *  @param   n1  the number of elements in the 1st dimension
@@ -255,7 +264,7 @@ object Accumulator {
     *          for `0 <= i1 < n1`, `0 <= i2 < n2`, `0 <= i3 < n3`, `0 <= i4 < n4`, and `0 <= i5 < n5`.
     */
   def tabulate[A, C](n1: Int, n2: Int, n3: Int, n4: Int, n5: Int)(f: (Int, Int, Int, Int, Int) => A)(implicit canAccumulate: AccumulatorFactoryShape[A, C]): AnyAccumulator[AnyAccumulator[AnyAccumulator[AnyAccumulator[C]]]] =
-    tabulate(n1)(i1 => tabulate(n2, n3, n4, n5)(f(i1, _, _, _, _))(canAccumulate))(impl.AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[AnyAccumulator[AnyAccumulator[C]]]])
+    tabulate(n1)(i1 => tabulate(n2, n3, n4, n5)(f(i1, _, _, _, _))(canAccumulate))(AccumulatorFactoryShape.anyAccumulatorFactoryShape[AnyAccumulator[AnyAccumulator[AnyAccumulator[C]]]])
 
   /** Concatenates all argument collections into a single $coll.
    *
@@ -269,4 +278,42 @@ object Accumulator {
       xss.foreach(b ++= _)
       b.result()
     }
+
+  /** An implicit `AccumulatorFactoryShape` is used in Accumulator factory method to return
+    * specialized variants according to the element type.
+    */
+  sealed trait AccumulatorFactoryShape[A, C] {
+    def factory: collection.Factory[A, C]
+    def empty: C
+  }
+
+  object AccumulatorFactoryShape extends LowPriorityAccumulatorFactoryShape {
+    implicit val doubleAccumulatorFactoryShape: AccumulatorFactoryShape[Double, DoubleAccumulator] = new AccumulatorFactoryShape[Double, DoubleAccumulator] {
+      def factory: collection.Factory[Double, DoubleAccumulator] = DoubleAccumulator
+      def empty: DoubleAccumulator = DoubleAccumulator.empty
+    }
+
+    implicit val intAccumulatorFactoryShape: AccumulatorFactoryShape[Int, IntAccumulator] = new AccumulatorFactoryShape[Int, IntAccumulator] {
+      def factory: collection.Factory[Int, IntAccumulator] = IntAccumulator
+      def empty: IntAccumulator = IntAccumulator.empty
+    }
+
+    implicit val longAccumulatorFactoryShape: AccumulatorFactoryShape[Long, LongAccumulator] = new AccumulatorFactoryShape[Long, LongAccumulator] {
+      def factory: collection.Factory[Long, LongAccumulator] = LongAccumulator
+      def empty: LongAccumulator = LongAccumulator.empty
+    }
+
+    implicit val javaDoubleAccumulatorFactoryShape: AccumulatorFactoryShape[java.lang.Double, DoubleAccumulator] = doubleAccumulatorFactoryShape.asInstanceOf[AccumulatorFactoryShape[java.lang.Double, DoubleAccumulator]]
+    implicit val javaIntegerAccumulatorFactoryShape: AccumulatorFactoryShape[java.lang.Integer, IntAccumulator] = intAccumulatorFactoryShape.asInstanceOf[AccumulatorFactoryShape[java.lang.Integer, IntAccumulator]]
+    implicit val javaLongAccumulatorFactoryShape: AccumulatorFactoryShape[java.lang.Long, LongAccumulator] = longAccumulatorFactoryShape.asInstanceOf[AccumulatorFactoryShape[java.lang.Long, LongAccumulator]]
+  }
+
+  sealed trait LowPriorityAccumulatorFactoryShape {
+    implicit def anyAccumulatorFactoryShape[A]: AccumulatorFactoryShape[A, AnyAccumulator[A]] = anyAccumulatorFactoryShapePrototype.asInstanceOf[AccumulatorFactoryShape[A, AnyAccumulator[A]]]
+
+    private val anyAccumulatorFactoryShapePrototype = new AccumulatorFactoryShape[AnyRef, AnyAccumulator[AnyRef]] {
+      def factory: collection.Factory[AnyRef, AnyAccumulator[AnyRef]] = collection.IterableFactory.toFactory(AnyAccumulator)
+      def empty: AnyAccumulator[AnyRef] = AnyAccumulator.empty[AnyRef]
+    }
+  }
 }
--- src/library/scala/collection/convert/StreamExtensions.scala
+++ src/library/scala/collection/convert/StreamExtensions.scala
@@ -15,7 +15,8 @@ package scala.collection.convert
 import java.util.stream._
 
 import scala.annotation.implicitNotFound
-import scala.collection.convert.impl.{AccumulatorFactoryInfo, StreamShape, StreamUnboxer}
+import scala.collection.convert.StreamExtensions.AccumulatorFactoryInfo
+import scala.collection.convert.impl.{StreamShape, StreamUnboxer}
 import scala.collection.{AnyConstr, Stepper}
 import scala.jdk.CollectionConverters.Ops._
 
@@ -271,10 +272,10 @@ trait StreamExtensions {
      * is lazy, the conversion is lazy as well.
      */
     def toScala[C1](factory: collection.Factory[Long, C1])(implicit info: AccumulatorFactoryInfo[Long, C1]): C1 = {
-      def intAcc = stream.collect(LongAccumulator.supplier, LongAccumulator.adder, LongAccumulator.merger)
+      def longAcc = stream.collect(LongAccumulator.supplier, LongAccumulator.adder, LongAccumulator.merger)
       if (info.companion == AnyAccumulator) stream.collect(AnyAccumulator.supplier[Long], AnyAccumulator.unboxedLongAdder, AnyAccumulator.merger[Long]).asInstanceOf[C1]
-      else if (info.companion == LongAccumulator) intAcc.asInstanceOf[C1]
-      else if (stream.isParallel) intAcc.to(factory)
+      else if (info.companion == LongAccumulator) longAcc.asInstanceOf[C1]
+      else if (stream.isParallel) longAcc.to(factory)
       else factory.fromSpecific(stream.iterator.asInstanceOf[java.util.Iterator[Long]].asScala)
     }
   }
@@ -298,11 +299,54 @@ trait StreamExtensions {
      * is lazy, the conversion is lazy as well.
      */
     def toScala[C1](factory: collection.Factory[Double, C1])(implicit info: AccumulatorFactoryInfo[Double, C1]): C1 = {
-      def intAcc = stream.collect(DoubleAccumulator.supplier, DoubleAccumulator.adder, DoubleAccumulator.merger)
+      def doubleAcc = stream.collect(DoubleAccumulator.supplier, DoubleAccumulator.adder, DoubleAccumulator.merger)
       if (info.companion == AnyAccumulator) stream.collect(AnyAccumulator.supplier[Double], AnyAccumulator.unboxedDoubleAdder, AnyAccumulator.merger[Double]).asInstanceOf[C1]
-      else if (info.companion == DoubleAccumulator) intAcc.asInstanceOf[C1]
-      else if (stream.isParallel) intAcc.to(factory)
+      else if (info.companion == DoubleAccumulator) doubleAcc.asInstanceOf[C1]
+      else if (stream.isParallel) doubleAcc.to(factory)
       else factory.fromSpecific(stream.iterator.asInstanceOf[java.util.Iterator[Double]].asScala)
     }
+  }
+}
+
+object StreamExtensions {
+
+  /** An implicit `AccumulatorFactoryInfo` connects primitive element types to the corresponding
+    * specialized [[Accumulator]] factory. This is used in the `stream.toScala` extension methods
+    * to ensure collecting a primitive stream into a primitive accumulator does not box.
+    *
+    * When converting to a collection other than `Accumulator`, the generic
+    * `noAccumulatorFactoryInfo` is passed.
+    */
+  trait AccumulatorFactoryInfo[A, C] {
+    val companion: AnyRef
+  }
+  trait LowPriorityAccumulatorFactoryInfo {
+    implicit def noAccumulatorFactoryInfo[A, C]: AccumulatorFactoryInfo[A, C] = noAccumulatorFactoryInfoPrototype.asInstanceOf[AccumulatorFactoryInfo[A, C]]
+    private val noAccumulatorFactoryInfoPrototype: AccumulatorFactoryInfo[AnyRef, AnyRef] = new AccumulatorFactoryInfo[AnyRef, AnyRef] {
+      val companion: AnyRef = null
+    }
+  }
+  object AccumulatorFactoryInfo extends LowPriorityAccumulatorFactoryInfo {
+    implicit def anyAccumulatorFactoryInfo[A]: AccumulatorFactoryInfo[A, AnyAccumulator[A]] = anyAccumulatorFactoryInfoPrototype.asInstanceOf[AccumulatorFactoryInfo[A, AnyAccumulator[A]]]
+
+    private object anyAccumulatorFactoryInfoPrototype extends AccumulatorFactoryInfo[AnyRef, AnyAccumulator[AnyRef]] {
+      val companion: AnyRef = AnyAccumulator
+    }
+
+    implicit val intAccumulatorFactoryInfo: AccumulatorFactoryInfo[Int, IntAccumulator] = new AccumulatorFactoryInfo[Int, IntAccumulator] {
+      val companion: AnyRef = IntAccumulator
+    }
+
+    implicit val longAccumulatorFactoryInfo: AccumulatorFactoryInfo[Long, LongAccumulator] = new AccumulatorFactoryInfo[Long, LongAccumulator] {
+      val companion: AnyRef = LongAccumulator
+    }
+
+    implicit val doubleAccumulatorFactoryInfo: AccumulatorFactoryInfo[Double, DoubleAccumulator] = new AccumulatorFactoryInfo[Double, DoubleAccumulator] {
+      val companion: AnyRef = DoubleAccumulator
+    }
+
+    implicit val javaIntegerAccumulatorFactoryInfo: AccumulatorFactoryInfo[java.lang.Integer, IntAccumulator] = intAccumulatorFactoryInfo.asInstanceOf[AccumulatorFactoryInfo[java.lang.Integer, IntAccumulator]]
+    implicit val javaLongAccumulatorFactoryInfo: AccumulatorFactoryInfo[java.lang.Long, IntAccumulator] = longAccumulatorFactoryInfo.asInstanceOf[AccumulatorFactoryInfo[java.lang.Long, IntAccumulator]]
+    implicit val javaDoubleAccumulatorFactoryInfo: AccumulatorFactoryInfo[java.lang.Double, IntAccumulator] = doubleAccumulatorFactoryInfo.asInstanceOf[AccumulatorFactoryInfo[java.lang.Double, IntAccumulator]]
   }
 }
