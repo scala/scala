52bb0c30545178592a71385375bd46453f18adfd
Lukas Rytz <lukas.rytz@gmail.com>
2019-05-06 13:23:16 +0200

    Revert "Test the interaction of mixin forwarders and overloads"
    This reverts commit 7df83bcad87b1e0ccefcb10bb81abb4e97bebbc3.

--- test/files/run/mixin-forwarder-overload/A.scala
+++
@@ -1,9 +0,0 @@
-case class Foo(x: Int)
-
-trait A[X]  {
-  def concat[Dummy](suffix: Int): Dummy = ???
-}
-
-class Bar extends A[Foo] {
-  def concat(suffix: Int): Foo = Foo(0)
-}
--- test/files/run/mixin-forwarder-overload/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-  public static void main(String[] args) {
-    Bar bar = new Bar();
-    Foo x = bar.concat(0);
-    System.out.println(x);
-  }
-}
