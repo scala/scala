0b28d2fb9e84e21ea3744a3f28a258661859de07
Jason Zaugg <jzaugg@gmail.com>
2019-05-17 12:57:50 +1000

    Fix scalap parsing/printing of enum and class constant types

---
+++ test/files/scalap/constants.scala
@@ -0,0 +1,17 @@
+class Constants {
+  final val UnitConstant = ()
+  final val ByteConstant = false
+  final val CharConstant = 'a'
+  final val ShortConstant = 1.toShort
+  final val IntConstant = 1
+  final val LongConstant = 1L
+  final val FloatConstant = 1f
+  final val DoubleConstant = 1d
+
+  final val NullConstant = null
+
+  final val ClassConstant = classOf[String]
+  final val ClassConstant2 = classOf[Some[_]]
+  final val EnumConstant = java.util.concurrent.TimeUnit.DAYS
+  final val StringConstant = classOf[String]
+}
