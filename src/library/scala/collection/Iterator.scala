1e89d8455b8b05e91ff7ba5721f5b5ea2bc81f0a
Martijn Hoekstra <martijn.hoekstra@cargoxs.com>
2019-05-24 16:18:03 +0200

    fix return type in docs

--- src/library/scala/collection/Iterator.scala
+++ src/library/scala/collection/Iterator.scala
@@ -341,13 +341,13 @@ trait Iterator[+A] extends IterableOnce[A] with IterableOnceOps[A, Iterator, Ite
    *
    *  Example usages:
    *  {{{
-   *    // Returns List(List(1, 2, 3), List(2, 3, 4), List(3, 4, 5))
+   *    // Returns List(ArraySeq(1, 2, 3), ArraySeq(2, 3, 4), ArraySeq(3, 4, 5))
    *    (1 to 5).iterator.sliding(3).toList
-   *    // Returns List(List(1, 2, 3, 4), List(4, 5))
+   *    // Returns List(ArraySeq(1, 2, 3, 4), ArraySeq(4, 5))
    *    (1 to 5).iterator.sliding(4, 3).toList
-   *    // Returns List(List(1, 2, 3, 4))
+   *    // Returns List(ArraySeq(1, 2, 3, 4))
    *    (1 to 5).iterator.sliding(4, 3).withPartial(false).toList
-   *    // Returns List(List(1, 2, 3, 4), List(4, 5, 20, 25))
+   *    // Returns List(ArraySeq(1, 2, 3, 4), ArraySeq(4, 5, 20, 25))
    *    // Illustrating that withPadding's argument is by-name.
    *    val it2 = Iterator.iterate(20)(_ + 5)
    *    (1 to 5).iterator.sliding(4, 3).withPadding(it2.next).toList
