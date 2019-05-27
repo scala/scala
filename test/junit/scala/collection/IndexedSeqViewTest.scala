0f645305d4ac28c1be459f409a3d5c6609a653d3
NthPortal <nthportal@gmail.com>
2019-05-26 20:14:46 -0400

    [squash] more tests

--- test/junit/scala/collection/IndexedSeqViewTest.scala
+++ test/junit/scala/collection/IndexedSeqViewTest.scala
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(classOf[JUnit4])
-class SeqViewTest {
+class IndexedSeqViewTest {
   @Test
   def _toString(): Unit = {
-    assertEquals("SeqView(<not computed>)", Seq(1, 2, 3).view.toString)
+    assertEquals("IndexedSeqView(<not computed>)", IndexedSeq(1, 2, 3).view.toString)
   }
 }
