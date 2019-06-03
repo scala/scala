ec80eb49f99c3870f5de6d45e0f10a6947eafe9c
Eugene Yokota <eed3si9n@gmail.com>
2019-06-03 18:05:26 -0400

    Display warning on equals comparing non-references
    Ref https://github.com/scala/bug/issues/11551
    Ref https://twitter.com/not_xuwei_k/status/1135374786593468416
    
    This extends sensible check to `equals`, to warn against using it as opposed to `==`.

--- test/files/neg/checksensible.scala
+++ test/files/neg/checksensible.scala
@@ -100,3 +100,21 @@ class EqEqRefTest {
     in.close
   }
 }
+
+class AnyEqualsTest {
+  1L equals 1
+  // ok, because it's between the same numeric types
+  1 equals 1
+  // ok
+  1L equals "string"
+  // ok
+  1L.equals(())
+  (1L: Any) equals 1
+  (1L: AnyVal) equals 1
+  (1L: AnyVal) equals (1: AnyVal)
+  // ok
+  "string" equals 1
+  def foo[A](a: A) = a.equals(1)
+  // ok
+  def bar[A <: AnyRef](a: A) = a.equals(1)
+}
