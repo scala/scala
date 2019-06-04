fc5132843fc8dc8cc96eef11d2de7ff1db737e58
Harrison Houghton <hora.rhino@gmail.com>
2019-06-04 17:16:13 -0400

    Also deprecate backing field symbols.
    Compiling
        @deprecated val foo: T = some.deprecated(call)
    yielded
        private[this] val `foo `: T = some.deprecated(call)
        @deprecated <accessor> def foo: T = this.`foo `
    where the `@deprecated` has been slapped on the def (where it'll incur
    deprecation warnings on callers) but not on the val (where it'll
    suppress deprecation warnings on the body.
    
    Just copy the annotation across.
    
    Fixes scala/bug#11538 in an expedient manner.

---
+++ test/files/pos/t11538.scala
@@ -0,0 +1,13 @@
+package t11538
+
+@deprecated("not for you", since = "just now")
+class Abhorrent
+
+object Bizzle {
+  @deprecated("use mipple instead", since = "recently")
+  val wibble: Abhorrent = mipple
+  @deprecated("use wobble instead", since = "recently")
+  def mipple: Abhorrent = wobble
+  @deprecated("use wibble instead", since = "recently")
+  var wobble: Abhorrent = wibble
+}
