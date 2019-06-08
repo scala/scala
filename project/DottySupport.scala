c4c9721d252a901840de06588bec7cff3b3c62e8
Anatolii <anatoliykmetyuk@gmail.com>
2019-06-08 22:31:31 +0200

    Update Dotty to 0.16.0-RC2

--- project/DottySupport.scala
+++ project/DottySupport.scala
@@ -13,7 +13,7 @@ import sbt.librarymanagement.{
  *  Dotty in .travis.yml.
  */
 object DottySupport {
-  val dottyVersion = "0.15.0-RC1"
+  val dottyVersion = "0.16.0-RC2"
   val compileWithDotty: Boolean =
     Option(System.getProperty("scala.build.compileWithDotty")).map(_.toBoolean).getOrElse(false)
   lazy val commonSettings = Seq(
