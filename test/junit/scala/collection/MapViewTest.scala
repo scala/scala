fc9c99573799fd38fb656afc3dd51d18fb0fdc9f
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-06-09 13:38:45 -0400

    Fix MapView.toString when overriden
    Since MapView isn't marked final and can be subclassed, the deprecated
    stringPrefix field needs to be set instead of className, for now,
    until it is removed.

--- test/junit/scala/collection/MapViewTest.scala
+++ test/junit/scala/collection/MapViewTest.scala
@@ -11,4 +11,22 @@ class MapViewTest {
   def _toString(): Unit = {
     assertEquals("MapView(<not computed>)", Map(1 -> 2).view.toString)
   }
+  @Test
+  def testStringPrefixToString(): Unit = {
+    val mapView = new collection.MapView[Int,Int] {
+      override def stringPrefix = "FooMapView" // !
+      def iterator: Iterator[(Int,Int)] = ???
+      def get(key: Int) = None
+    }
+    assertEquals("FooMapView(<not computed>)", mapView.toString)
+  }
+  @Test
+  def testClassNameToString(): Unit = {
+    val mapView = new collection.MapView[Int,Int] {
+      override def className = "FooMapView"         // !
+      def iterator: Iterator[(Int,Int)] = ???
+      def get(key: Int) = None
+    }
+    assertEquals("FooMapView(<not computed>)", mapView.toString)
+  }
 }
