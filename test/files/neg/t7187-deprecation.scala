4ce66551416b9a4446a62bb3848bebe93b563103
Eugene Yokota <eed3si9n@gmail.com>
2019-08-01 03:12:58 -0400

    Fixes x.toString and value classes causing auto-application warnings under -Xsource:2.14
    Fixes https://github.com/scala/bug/issues/11639
    Fixes https://github.com/scala/bug/issues/11657
    
    The compiler gets confused about the lineage of `toString`.
    This follows Dotty's `matchNullaryLoosely` implementation (https://github.com/lampepfl/dotty/blob/811dc19f418f26d6b830bb08bfb49aeee3853a49/compiler/src/dotty/tools/dotc/core/SymDenotations.scala#L950-L957), which allows `sym.owner == defn.AnyClass` and `sym == defn.Object_clone` in addition to the symbol being Java-defined.

---
+++ test/files/neg/t7187-deprecation.scala
@@ -0,0 +1,45 @@
+// scalac: -Xsource:2.14 -deprecation -Werror
+//
+trait AcciSamZero { def apply(): Int }
+
+@FunctionalInterface
+trait SamZero { def apply(): Int }
+
+class A {
+  def boom(): Unit
+}
+
+class EtaExpand214 {
+  def m1 = 1
+  def m2() = 1
+  def m3(x: Int) = x
+
+  val t1: () => Any  = m1   // error
+  val t2: () => Any  = m2   // eta-expanded with lint warning
+  val t2AcciSam: AcciSamZero = m2   // eta-expanded with lint warning + sam warning
+  val t2Sam: SamZero = m2   // eta-expanded with lint warning
+  val t3: Int => Any = m3   // ok
+
+  val t4 = m1 // apply
+  val t5 = m2 // apply, ()-insertion
+  val t6 = m3 // eta-expansion in 2.14
+
+  val t4a: Int        = t4 // ok
+  val t5a: Int        = t5 // ok
+  val t6a: Int => Any = t6 // ok
+
+  val t7 = m1 _
+  val t8 = m2 _
+  val t9 = m3 _
+
+  val t7a: () => Any  = t7 // ok
+  val t8a: () => Any  = t8 // ok
+  val t9a: Int => Any = t9 // ok
+
+  val a = new A
+  a.boom // error
+
+  import scala.collection.mutable.Map
+  val xs = Map(1 -> "foo")
+  val ys = xs.clone // ok
+}
