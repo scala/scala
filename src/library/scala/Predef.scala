ae5359e686a0cd3cf99b16f2d9088c481a44ba40
杨博 (Yang Bo) <pop.atry@gmail.com>
2019-08-15 20:43:57 -0700

    Make implicitly return a dependent type

--- src/library/scala/Predef.scala
+++ src/library/scala/Predef.scala
@@ -218,7 +218,7 @@ object Predef extends LowPriorityImplicits {
    *  @return the implicit value of type `T`
    *  @group utilities
    */
-  @inline def implicitly[T](implicit e: T): T = e // TODO: when dependent method types are on by default, give this result type `e.type`, so that inliner has better chance of knowing which method to inline in calls like `implicitly[MatchingStrategy[Option]].zero`
+  @inline def implicitly[T](implicit e: T): e.type = e // TODO: when dependent method types are on by default, give this result type `e.type`, so that inliner has better chance of knowing which method to inline in calls like `implicitly[MatchingStrategy[Option]].zero`
 
   /** Used to mark code blocks as being expressions, instead of being taken as part of anonymous classes and the like.
    *  This is just a different name for [[identity]].
