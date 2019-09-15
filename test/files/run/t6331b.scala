b98f16362ac54c07487c28964d083765d2d46a1f
Som Snytt <som.snytt@gmail.com>
2019-09-15 11:49:37 -0700

    Clean up test that test is not elided
    Remove reify. It's sufficient to test that the
    condition throws. The really cool at the time trace
    facility didn't catch on, as it turns out.

--- test/files/run/t6331b.scala
+++ test/files/run/t6331b.scala
@@ -1,13 +1,18 @@
-import scala.tools.partest.Util.trace
-import scala.util.control.Exception.allCatch
 
+import scala.tools.testkit.AssertUtil._
 
 object Test extends App {
-  def intercept = allCatch.withApply(_.getClass)
-  val t: Boolean = true
-  trace(if (t) -0d else 0d)
-  trace(if (t) 0d else -0d)
-  trace(intercept(if (???) -0d else 0d))
-  trace(intercept(if (???) 0d else 0d))
-  trace(intercept(if (???) () else ()))
+
+  assertThrows[NotImplementedError] {
+    if (???) 0.0d else -0.0d
+  }
+  assertThrows[NotImplementedError] {
+    if (???) -0.0d else 0.0d
+  }
+  assertThrows[NotImplementedError] {
+    if (???) () else ()
+  }
+  assertThrows[NotImplementedError] {
+    if (???) ()
+  }
 }
