636223aa0ba2a2f2b71cabd6d570c7c0c0e11d8a
Harrison Houghton <hora.rhino@gmail.com>
2019-09-18 07:25:47 -0400

    Stricten @varargs check.
    If you get it wrong, the JVM will complain. Now we complain first.
    
    Not a refcheck for some reason.
    
    I hope it's ok to do a "good first issue" as a "good first issue after
    several months' hiatus". For what it's worth, it was. Who doesn't like
    adding new compiler errors?
    
    Closes scala/bug#11714.

--- test/files/neg/varargs.scala
+++ test/files/neg/varargs.scala
@@ -1,9 +1,4 @@
-
-
-
 import annotation.varargs
-
-
 
 // Failing varargs annotation
 object Test {
@@ -13,15 +8,19 @@ object Test {
   }
 
   trait B extends A {
-    @varargs def v1(a: Int, b: String*) = a + b.length
+    @varargs def v1(a: Int, b: String*) = a + b.length // nok
   }
 
-  @varargs def nov(a: Int) = 0
-  @varargs def v(a: Int, b: String*) = a + b.length
-  @varargs def v2(a: Int, b: String*) = 0
+  @varargs def nov(a: Int) = 0 // nok
+  @varargs def v(a: Int, b: String*) = a + b.length // ok
+  @varargs def v2(a: Int, b: String*) = 0 // nok
   def v2(a: Int, b: Array[String]) = 0
 
-  def main(args: Array[String]) {
-  }
+  @varargs def v3(a: String*)(b: Int) = b + a.length // nok
+  @varargs def v4(a: String)(b: Int) = b + a.length // nok
+  @varargs def v5(a: String)(b: Int*) = a + b.sum // ok
+
+  @varargs def v6: Int = 1 // nok
+  @varargs def v7(i: Int*)() = i.sum // ok (?)
 
 }
