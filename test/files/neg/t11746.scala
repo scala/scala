2c5df00dd678f364880cf1225382c815dfc5ea7f
Som Snytt <som.snytt@gmail.com>
2019-10-02 11:13:11 -0700

    Test for opt-warnings:none

---
+++ test/files/neg/t11746.scala
@@ -0,0 +1,20 @@
+//
+// scalac: -Werror -opt:l:inline -opt-inline-from:'**' -opt-warnings:none,_
+//
+// compare -opt-warnings:none,at-inline-failed-summary
+
+trait Try
+
+object Try {
+  def apply(s: String): Try = Success(s)
+}
+
+case class Success(s: String) extends Try
+case class Failure(e: Throwable) extends Try
+
+class C {
+  private def get(a: String): Unit = Try(a) match {
+    case Failure(e: Exception) => 
+    case Failure(e) => println(e.toString)
+  }
+}
