d0d557dea9b807f96408477c9a6dd3b39047dd11
mkeskells <mike.skells@talk21.com>
2019-10-25 18:15:08 +0100

    make Long flags actually Long

--- src/reflect/scala/reflect/internal/Flags.scala
+++ src/reflect/scala/reflect/internal/Flags.scala
@@ -88,36 +88,36 @@ package internal
 /** Flags set on Modifiers instances in the parsing stage.
  */
 class ModifierFlags {
-  final val IMPLICIT      = 1 << 9
-  final val FINAL         = 1 << 5    // May not be overridden. Note that java final implies much more than scala final.
-  final val PRIVATE       = 1 << 2
-  final val PROTECTED     = 1 << 0
+  final val IMPLICIT      = 1L << 9
+  final val FINAL         = 1L << 5    // May not be overridden. Note that java final implies much more than scala final.
+  final val PRIVATE       = 1L << 2
+  final val PROTECTED     = 1L << 0
 
-  final val SEALED        = 1 << 10
-  final val OVERRIDE      = 1 << 1
-  final val CASE          = 1 << 11
-  final val ABSTRACT      = 1 << 3        // abstract class, or used in conjunction with abstract override.
+  final val SEALED        = 1L << 10
+  final val OVERRIDE      = 1L << 1
+  final val CASE          = 1L << 11
+  final val ABSTRACT      = 1L << 3       // abstract class, or used in conjunction with abstract override.
                                           // Note difference to DEFERRED!
-  final val DEFERRED      = 1 << 4        // was `abstract' for members | trait is virtual
-  final val INTERFACE     = 1 << 7        // symbol is an interface. the flag is set for:
+  final val DEFERRED      = 1L << 4       // was `abstract' for members | trait is virtual
+  final val INTERFACE     = 1L << 7       // symbol is an interface. the flag is set for:
                                           //  - scala-defined traits with only abstract methods or fields
                                           //  - any java-defined interface (even if it has default methods)
-  final val MUTABLE       = 1 << 12       // symbol is a mutable variable.
-  final val PARAM         = 1 << 13       // symbol is a (value or type) parameter to a method
-  final val MACRO         = 1 << 15       // symbol is a macro definition
+  final val MUTABLE       = 1L << 12      // symbol is a mutable variable.
+  final val PARAM         = 1L << 13      // symbol is a (value or type) parameter to a method
+  final val MACRO         = 1L << 15      // symbol is a macro definition
 
-  final val COVARIANT     = 1 << 16       // symbol is a covariant type variable
-  final val BYNAMEPARAM   = 1 << 16       // parameter is by name
-  final val CONTRAVARIANT = 1 << 17       // symbol is a contravariant type variable
-  final val ABSOVERRIDE   = 1 << 18       // combination of abstract & override
-  final val LOCAL         = 1 << 19       // symbol is local to current class (i.e. private[this] or protected[this]
+  final val COVARIANT     = 1L << 16      // symbol is a covariant type variable
+  final val BYNAMEPARAM   = 1L << 16      // parameter is by name
+  final val CONTRAVARIANT = 1L << 17      // symbol is a contravariant type variable
+  final val ABSOVERRIDE   = 1L << 18      // combination of abstract & override
+  final val LOCAL         = 1L << 19      // symbol is local to current class (i.e. private[this] or protected[this]
                                           // pre: PRIVATE or PROTECTED are also set
-  final val JAVA          = 1 << 20       // symbol was defined by a Java class
-  final val STATIC        = 1 << 23       // static field, method or class
-  final val CASEACCESSOR  = 1 << 24       // symbol is a case parameter (or its accessor, or a GADT skolem)
-  final val TRAIT         = 1 << 25       // symbol is a trait
-  final val DEFAULTPARAM  = 1 << 25       // the parameter has a default value
-  final val PARAMACCESSOR = 1 << 29       // for field definitions generated for primary constructor
+  final val JAVA          = 1L << 20      // symbol was defined by a Java class
+  final val STATIC        = 1L << 23      // static field, method or class
+  final val CASEACCESSOR  = 1L << 24      // symbol is a case parameter (or its accessor, or a GADT skolem)
+  final val TRAIT         = 1L << 25      // symbol is a trait
+  final val DEFAULTPARAM  = 1L << 25      // the parameter has a default value
+  final val PARAMACCESSOR = 1L << 29      // for field definitions generated for primary constructor
                                           //   parameters (no matter if it's a 'val' parameter or not)
                                           // for parameters of a primary constructor ('val' or not)
                                           // for the accessor methods generated for 'val' or 'var' parameters
@@ -141,22 +141,22 @@ object ModifierFlags extends ModifierFlags
 
 /** All flags and associated operations */
 class Flags extends ModifierFlags {
-  final val METHOD        = 1 << 6        // a method
-  final val MODULE        = 1 << 8        // symbol is module or class implementing a module
-  final val PACKAGE       = 1 << 14       // symbol is a java package
+  final val METHOD        = 1L << 6       // a method
+  final val MODULE        = 1L << 8       // symbol is module or class implementing a module
+  final val PACKAGE       = 1L << 14      // symbol is a java package
 
-  final val CAPTURED      = 1 << 16       // variable is accessed from nested function.  Set by LambdaLift.
-  final val LABEL         = 1 << 17       // method symbol is a label. Set by TailCall
-  final val INCONSTRUCTOR = 1 << 17       // class symbol is defined in this/superclass constructor.
-  final val SYNTHETIC     = 1 << 21       // symbol is compiler-generated (compare with ARTIFACT)
-  final val STABLE        = 1 << 22       // functions that are assumed to be stable
+  final val CAPTURED      = 1L << 16      // variable is accessed from nested function.  Set by LambdaLift.
+  final val LABEL         = 1L << 17      // method symbol is a label. Set by TailCall
+  final val INCONSTRUCTOR = 1L << 17      // class symbol is defined in this/superclass constructor.
+  final val SYNTHETIC     = 1L << 21      // symbol is compiler-generated (compare with ARTIFACT)
+  final val STABLE        = 1L << 22      // functions that are assumed to be stable
                                           // (typically, access methods for valdefs)
                                           // or classes that do not contain abstract types.
-  final val BRIDGE        = 1 << 26       // function is a bridge method. Set by Erasure
-  final val ACCESSOR      = 1 << 27       // a value or variable accessor (getter or setter)
+  final val BRIDGE        = 1L << 26      // function is a bridge method. Set by Erasure
+  final val ACCESSOR      = 1L << 27      // a value or variable accessor (getter or setter)
 
-  final val SUPERACCESSOR = 1 << 28       // a super accessor
-  final val MODULEVAR     = 1 << 30       // for variables: is the variable caching a module value
+  final val SUPERACCESSOR = 1L << 28      // a super accessor
+  final val MODULEVAR     = 1L << 30      // for variables: is the variable caching a module value
 
   final val IS_ERROR      = 1L << 32      // symbol is an error symbol
   final val OVERLOADED    = 1L << 33      // symbol is overloaded
