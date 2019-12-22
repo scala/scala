c8dd0dd19db144ea45c0c44e4fe7dbed4d69c965
Som Snytt <som.snytt@gmail.com>
2019-12-21 22:26:35 -0800

    Extend nullary override test

--- test/files/neg/nullary-override.scala
+++ test/files/neg/nullary-override.scala
@@ -1,5 +1,10 @@
-// scalac: -Xfatal-warnings -Xlint
+// scalac: -Werror -Xlint
 //
 class A { def x: Int = 3 }
 class B extends A { override def x(): Int = 4 }
 
+class C extends java.lang.CharSequence {
+  def charAt(x$1: Int): Char = ???
+  def length: Int = ???
+  def subSequence(x$1: Int, x$2: Int): CharSequence = ???
+}
