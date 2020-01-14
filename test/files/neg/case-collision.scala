eb377e60b1e365f9b3c6c7a0cb48ea03b8e3853d
Som Snytt <som.snytt@gmail.com>
2020-01-14 01:03:42 -0800

    Test more file name collisions

--- test/files/neg/case-collision.scala
+++ test/files/neg/case-collision.scala
@@ -1,6 +1,6 @@
-// scalac: -Xfatal-warnings
+// scalac: -Werror
 //
-package foo
+package foo {
 
 class Bippy
 
@@ -14,3 +14,23 @@ object HyRaX
 
 class Wacko
 object wackO
+
+package object bar
+package bar {
+  class Package
+}
+
+object O extends App {
+  object a_! {
+    def foo = 1
+  }
+
+  object a_ {
+    object bang {
+      def foo = 2
+    }
+  }
+  println(a_!.foo)
+  println(a_.bang.foo)
+}
+}
