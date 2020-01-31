1cd3e74376ac25ac41355c30b25bfdc0d2a1674c
Harrison Houghton <hora.rhino@gmail.com>
2020-01-31 17:54:33 -0500

    Clear my terminal of a pair of unused-import warnings
    I thought som-snytt had recently gone on some sort of warning-cleanup
    mission, but nowadays I can't compile 2.12.x without seeing scala tell
    me off for these. Well, now I can.

--- src/compiler/scala/tools/nsc/typechecker/Macros.scala
+++ src/compiler/scala/tools/nsc/typechecker/Macros.scala
@@ -73,7 +73,6 @@ trait Macros extends MacroRuntimes with Traces with Helpers {
     */
   protected def findMacroClassLoader(): ClassLoader = {
     import java.net.URL
-    import scala.tools.nsc.io.AbstractFile
 
     val classpath: Seq[URL] = if (settings.YmacroClasspath.isSetByUser) {
       for {
