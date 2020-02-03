10619eeb830e340bc03916a9f822ba704aac6ce8
Jamie Thompson <bishbashboshjt@gmail.com>
2020-02-03 17:55:04 +0100

    test consuming (* -> *) -> * from TASTy

--- test/tasty/run/src-2/tastytest/TestHKNest.scala
+++ test/tasty/run/src-2/tastytest/TestHKNest.scala
@@ -3,9 +3,11 @@ package tastytest
 import HKNest._
 
 object TestHKNest extends Suite("TestHKNest") {
-    class Consume[H[_[_]]] {
-        def foo[F[A]](x: H[F]): String = x.toString()
-    }
 
-    test(assert(new Consume[Foo].foo(new Foo[Box]) == "Foo"))
+  class ConsumeInScala2[H[_[_]]] {
+    def foo[F[A]](x: H[F]): String = x.toString()
+  }
+
+  test(assert(new ConsumeInScala2[Foo].foo(new Foo[Box]) == "Foo"))
+//   test(assert(new ConsumeInScala3[Foo].foo(new Foo[Box]) == "Foo")) // broken
 }
--- test/tasty/run/src-3/tastytest/HKNest.scala
+++ test/tasty/run/src-3/tastytest/HKNest.scala
@@ -2,6 +2,10 @@ package tastytest
 
 object HKNest {
 
+  class ConsumeInScala3[H[_[_]]] {
+    def foo[F[A]](x: H[F]): String = x.toString()
+  }
+
   class Box[A]
 
   class Foo[F[_]] {
