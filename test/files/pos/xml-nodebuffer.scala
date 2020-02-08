b718927580432ba0115ac811471886f8af0efa3d
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2020-02-07 20:07:25 -0500

    Test for scala.xml.NodeBuffer literal

---
+++ test/files/pos/xml-nodebuffer.scala
@@ -0,0 +1,11 @@
+// scalac: -Ystop-after:parser
+//
+
+import scala.xml.NodeBuffer
+
+object foo {
+  // SI-9027
+  // xml-nodebuffer.scala:10: error: ';' expected but '.' found.
+  val nodeBuffer: NodeBuffer = <hello/><world/>
+  nodeBuffer.foreach(println)
+}
