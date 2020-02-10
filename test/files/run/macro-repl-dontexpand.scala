88f1622eb191ee49b0c6aee9289849392a11184d
Dale Wijnand <dale.wijnand@gmail.com>
2020-02-10 16:45:34 +0000

    Default -Yrepl-class-based to true
    ... and thus -Yrepl-use-magic-imports too.

--- test/files/run/macro-repl-dontexpand.scala
+++ test/files/run/macro-repl-dontexpand.scala
@@ -1,6 +1,12 @@
+import scala.tools.nsc.Settings
 import scala.tools.partest.ReplTest
 
 object Test extends ReplTest {
+    override def transformSettings(s: Settings) = {
+    s.Yreplclassbased.value = false // macros are object-based only
+    s
+  }
+
   override def extraSettings = "-language:experimental.macros"
   def code = """
     |def bar1(c: scala.reflect.macros.blackbox.Context) = ???
