98420d0269ae7451b5afd4fb2e92e352c97e1364
Som Snytt <som.snytt@gmail.com>
2020-02-22 19:28:30 -0800

    More post-uncurry parens

--- test/files/pos/lazyref-autoapply.scala
+++ test/files/pos/lazyref-autoapply.scala
@@ -1,8 +1,12 @@
-// scalac: -Xsource:2.14
+// scalac: -Werror -Xsource:2.14 -Xlint
 
 object Test {
   def doti(i: Int): Product = {
     case class Dot(i: Int) // was: warning: auto-application to `()` is deprecated ...
     Dot(i)
   }
+}
+object t11890 {
+  // was: warning: Auto-application to `()` is deprecated. Supply the empty argument list `()` explicitly to invoke method unary_!, ...
+  lazy val x = 5
 }
