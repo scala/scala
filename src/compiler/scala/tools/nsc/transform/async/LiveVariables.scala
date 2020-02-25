e82ed140819be990df4c9bf0fa8fd5c8fb9ae8f3
Jason Zaugg <jzaugg@gmail.com>
2020-02-25 10:54:36 +1000

    Live variables: detect usages in Awaitable.expr

--- src/compiler/scala/tools/nsc/transform/async/LiveVariables.scala
+++ src/compiler/scala/tools/nsc/transform/async/LiveVariables.scala
@@ -131,6 +131,10 @@ trait LiveVariables extends ExprBuilder {
 
       val findUses = new FindUseTraverser
       findUses.traverse(Block(as.stats: _*))
+      as match {
+        case aswa: AsyncStateWithAwait => findUses.traverse(aswa.awaitable.expr)
+        case _ =>
+      }
       ReferencedFields(findUses.usedFields, findUses.capturedFields)
     }
     case class ReferencedFields(used: Set[Symbol], captured: Set[Symbol]) {
