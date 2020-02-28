cf3a6f1874863d59fa1fc5d972ab6d867917d114
Jason Zaugg <jzaugg@gmail.com>
2020-02-28 13:53:42 +1000

    Align {add,merge}{Common,Distinct}.
    ... in the way they construct the bitmap of the resulting node.
    
    No behaviour change.

--- src/library/scala/collection/immutable/HashMap.scala
+++ src/library/scala/collection/immutable/HashMap.scala
@@ -856,7 +856,7 @@ object HashMap extends ImmutableMapFactory[HashMap] with BitOperations.Int {
           }
           // we don't have to check whether the result is a leaf, since union will only make the set larger
           // and this is not a leaf to begin with.
-          new HashTrieMap(this.bitmap | that.bitmap, resultElems, rs)
+          new HashTrieMap(abm | bbm, resultElems, rs)
 
         }
 
