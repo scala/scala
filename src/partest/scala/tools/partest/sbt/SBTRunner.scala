27919f1e201ca227577edb6f57c88ec595758d31
Dale Wijnand <dale.wijnand@gmail.com>
2020-03-18 15:00:09 +0000

    Fix fallback of scalacOpts in partest's SBTRunner
    If I understand the code correctly this means that when
    -Dpartest.scalac_opts isn't specified as an argument of running
    partest, the system property "partest.scalac_opts" will be honoured rather
    than the "partest.java_opts" property.

--- src/partest/scala/tools/partest/sbt/SBTRunner.scala
+++ src/partest/scala/tools/partest/sbt/SBTRunner.scala
@@ -58,7 +58,7 @@ class SBTRunner(config: RunnerSpec.Config,
 
   override val scalacOpts = {
     val l = defs.collect { case ("partest.scalac_opts", v) => v }
-    if(l.isEmpty) PartestDefaults.javaOpts
+    if(l.isEmpty) PartestDefaults.scalacOpts
     else l.mkString(" ")
   }
 
