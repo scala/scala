15aa453cea808c3c4b74656e16264989aa7622a9
bishabosha <bishbashboshjt@gmail.com>
2020-03-24 18:00:43 +0100

    Add nested RECtype test case

--- test/tasty/run/src-2/tastytest/TestRecTypes.scala
+++ test/tasty/run/src-2/tastytest/TestRecTypes.scala
@@ -1,13 +1,31 @@
 package tastytest
 
 object TestRecTypes extends Suite("TestRecTypes") {
+
   class CInt extends RecTypes.C {
     type T1 = Int
     type T2 = Int
   }
-  test {
+
+  class CCInt extends RecTypes.C {
+    type T1 = CInt
+    type T2 = CInt
+  }
+
+  test("1 level RECtype") {
     val cdepTest = new RecTypes.CDep[CInt].test_==(new CInt)
     assert(cdepTest(1,1) === true)
     assert(cdepTest(1,2) === false)
   }
+
+  test("2 level nested RECtype") {
+    val cint = new CInt
+    val cdepNestTest = new RecTypes.CDepNest[CCInt].test_==(new CCInt)(cint, cint)
+    assert(cdepNestTest(1,1,1,1) === true)
+    assert(cdepNestTest(2,1,1,1) === false)
+    assert(cdepNestTest(1,2,1,1) === false)
+    assert(cdepNestTest(1,1,2,1) === false)
+    assert(cdepNestTest(1,1,1,2) === false)
+  }
+
 }
--- test/tasty/run/src-3/tastytest/RecTypes.scala
+++ test/tasty/run/src-3/tastytest/RecTypes.scala
@@ -1,8 +1,16 @@
 package tastytest
 
 object RecTypes {
+
   class C { type T1; type T2 }
-  class CDep[C2 <: C { type T1; type T2 = T1 }] {
+
+  class CDep[C2 <: C { type T1; type T2 = T1 }] { // 1 level RECtype
     def test_==(c: C2)(c1: c.T1, c2: c.T2): Boolean = c1 == c2
   }
+
+  class CDepNest[C2 <: C { type T1 <: C { type T1 = Int; type T2 = T1}; type T2 = T1 }] { // 2 level nested RECtype
+    def test_==(c: C2)(cc1: c.T1, cc2: c.T2)(c1: cc1.T1, c2: cc1.T2, c3: cc2.T1, c4: cc2.T2): Boolean =
+      c1 == c2 && c1 == c3 && c1 == c4 && c2 == c3 && c2 == c4 && c3 == c4
+  }
+
 }
