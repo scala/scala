2119b2e4b464314826dbf4d7fd4f69ec32c36c24
Jamie Thompson <bishbashboshjt@gmail.com>
2020-04-08 11:34:06 +0200

    Delete SourceFile.scala
    was an oversight

--- src/compiler/scala/annotation/internal/SourceFile.scala
+++
@@ -1,9 +0,0 @@
-package scala.annotation.internal
-
-import scala.annotation.Annotation
-
-/** An annotation to record a Scala2 pickled alias.
- */
-class SourceFile(path: String) extends Annotation {
-
-}
