32c4e8043d8fe3495d1d285299e05709d2973870
Som Snytt <som.snytt@gmail.com>
2020-04-09 03:40:25 -0700

    Cleanup the test

--- test/files/run/enums.scala
+++ test/files/run/enums.scala
@@ -1,6 +1,7 @@
-//############################################################################
-// Enumerations
-//############################################################################
+
+import scala.tools.testkit.AssertUtil.assertThrows
+import scala.util.Using
+import scala.util.control.NonFatal
 
 object Test1 {
 
@@ -8,29 +9,26 @@ object Test1 {
     val Mon, Tue, Wed, Thu, Fri, Sat, Sun = Value
   }
 
-  def isWorkingDay(d: WeekDays.Value) =
-    ! (d == WeekDays.Sat || d == WeekDays.Sun);
+  def isWorkingDay(d: WeekDays.Value) = d != WeekDays.Sat && d != WeekDays.Sun
 
-  def run: Int = {
-    val it = WeekDays.values filter (isWorkingDay);
-    it.toList.length
-  }
+  def run: Int = WeekDays.values.filter(isWorkingDay).size
 }
 
 object Test2 {
 
   object ThreadState extends Enumeration {
-    val New          = Value("NEW");
-    val Runnable     = Value("RUNNABLE");
-    val Blocked      = Value("BLOCKED");
-    val Waiting      = Value("WAITING");
-    val TimedWaiting = Value("TIMED_WAITING");
-    val Terminated   = Value("TERMINATED");
+    val New          = Value("NEW")
+    val Runnable     = Value("RUNNABLE")
+    val Blocked      = Value("BLOCKED")
+    val Waiting      = Value("WAITING")
+    val TimedWaiting = Value("TIMED_WAITING")
+    val Terminated   = Value("TERMINATED")
   }
 
   def run: Int = {
-    val it = for (s <- ThreadState.values; if s.id != 0) yield s;
-    it.toList.length
+    // immutable.SortedSet[Test2.ThreadState.Value]
+    val filtered = for (s <- ThreadState.values if s.id != 0) yield s
+    filtered.size
   }
 }
 
@@ -39,13 +37,13 @@ object Test3 {
   object Direction extends Enumeration {
     val North = Value("North")
     val South = Value("South")
-    val East = Value("East")
-    val West = Value("West")
+    val East  = Value("East")
+    val West  = Value("West")
   }
 
   def run: Int = {
-    val it = for (d <- Direction.values; if d.toString() startsWith "N") yield d;
-    it.toList.length
+    val filtered = for (d <- Direction.values if d.toString() startsWith "N") yield d
+    filtered.size
   }
 }
 
@@ -54,38 +52,32 @@ object Test4 {
   object Direction extends Enumeration {
     val North = Value("North")
     val South = Value("South")
-    val East = Value("East")
-    val West = Value("West")
+    val East  = Value("East")
+    val West  = Value("West")
   }
 
-  def run: Int = {
+  def run(): Unit = {
     val dir = Direction.withName("North")
     assert(dir.toString == "North")
-    try {
-      Direction.withName("Nord")
-      assert(false)
-    } catch {
-      case e: Exception => /* do nothing */
-    }
-    0
+    assertThrows[NoSuchElementException](Direction.withName("Nord"), _ == "No value found for 'Nord'")
   }
 }
 
 object Test5 {
 
   object D1 extends Enumeration(0) {
-    val North, South, East, West = Value;
+    val North, South, East, West = Value
   }
 
   object D2 extends Enumeration(-2) {
-    val North, South, East, West = Value;
+    val North, South, East, West = Value
   }
 
   object WeekDays extends Enumeration  {
     val Mon, Tue, Wed, Thu, Fri, Sat, Sun = Value
   }
 
-  def run: Unit = {
+  def run(): Unit = {
     val s1 = D1.ValueSet(D1.North, D1.East)
     val s2 = D2.North + D2.East
     println(s1)
@@ -96,67 +88,57 @@ object Test5 {
     println(s2.toBitMask.map(_.toBinaryString).toList)
     println(D1.ValueSet.fromBitMask(s1.toBitMask))
     println(D2.ValueSet.fromBitMask(s2.toBitMask))
-    val r: WeekDays.ValueSet = WeekDays.values.range(WeekDays.Tue, WeekDays.Sat)
-    println(r)
+    println(WeekDays.values.range(WeekDays.Tue, WeekDays.Sat): WeekDays.ValueSet)
   }
 }
 
 object SerializationTest {
   object Types extends Enumeration { val X, Y = Value }
-  class A extends java.io.Serializable { val types = Types.values }
-  class B extends java.io.Serializable { val types = Set(Types.X, Types.Y) }
+  class A extends Serializable { val types = Types.values }
+  class B extends Serializable { val types = Set(Types.X, Types.Y) }
 
-  def serialize(obj: AnyRef) = {
-    val baos = new java.io.ByteArrayOutputStream()
-    val oos = new java.io.ObjectOutputStream(baos)
-    oos.writeObject(obj)
-    oos.close()
-    val bais = new java.io.ByteArrayInputStream(baos.toByteArray)
-    val ois = new java.io.ObjectInputStream(bais)
-    val prime = ois.readObject()
-    ois.close()
-    prime
+  def serialize(obj: AnyRef): Unit = {
+    import java.io._
+    val baos = new ByteArrayOutputStream()
+    Using.resource(new ObjectOutputStream(baos))(_.writeObject(obj))
+    val bais = new ByteArrayInputStream(baos.toByteArray)
+    Using.resource(new ObjectInputStream(bais))(_.readObject())
+    ()
   }
 
-  def run: Unit = {
+  def run(): Unit = {
     serialize(new A())
     serialize(new B())
   }
 }
 
-//############################################################################
-// Test code
-
 object Test {
 
   def check_success(name: String, closure: => Int, expected: Int): Unit = {
-    Console.print("test " + name);
+    val title = s"test $name"
     try {
-      val actual: Int = closure;
-      if (actual == expected) {
-        Console.print(" was successful");
-      } else {
-        Console.print(" failed: expected "+ expected +", found "+ actual);
-      }
+      val actual: Int = closure
+      if (actual == expected)
+        println(s"$title was successful")
+      else
+        println(s"$title failed: expected $expected, found $actual")
     } catch {
-      case exception: Throwable => {
-        Console.print(" raised exception " + exception);
-        exception.printStackTrace();
-      }
+      case NonFatal(exception) =>
+        println(s"$title raised exception $exception")
+        exception.printStackTrace()
     }
-    Console.println;
+  }
+  def check_success(name: String, closure: => Unit): Unit = {
+    println(s"test $name")
+    closure
   }
 
   def main(args: Array[String]): Unit = {
-    check_success("Test1", Test1.run, 5);
-    check_success("Test2", Test2.run, 5);
-    check_success("Test3", Test3.run, 1);
-    check_success("Test4", Test4.run, 0);
-    Console.println;
-    Test5.run;
-    Console.println;
-    SerializationTest.run;
+    check_success("Test1", Test1.run, 5)
+    check_success("Test2", Test2.run, 5)
+    check_success("Test3", Test3.run, 1)
+    check_success("Test4", Test4.run())
+    check_success("Test5", Test5.run())
+    check_success("SerializationTest", SerializationTest.run())
   }
 }
-
-//############################################################################
