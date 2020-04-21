4d73f5f48f74ea5118ce4bc965b276ded5af24ac
Jason Zaugg <jzaugg@gmail.com>
2020-04-21 15:11:50 +1000

    Temporarily reinstate global.treeTpe for compat with scala-meta

--- src/reflect/scala/reflect/internal/Types.scala
+++ src/reflect/scala/reflect/internal/Types.scala
@@ -4758,6 +4758,8 @@ trait Types
     "scala.collection.IndexedSeq",
     "scala.collection.Iterator")
 
+  @deprecated("Use _.tpe", "2.12.12") // used by scala-meta, leave until they remove the dependency.
+  private[scala] val treeTpe = (t: Tree) => t.tpe
   private[scala] val typeContainsTypeVar = { val collector = new FindTypeCollector(_.isInstanceOf[TypeVar]); (tp: Type) => collector.collect(tp).isDefined }
   private[scala] val typeIsSubTypeOfSerializable = (tp: Type) => tp <:< SerializableTpe
 
