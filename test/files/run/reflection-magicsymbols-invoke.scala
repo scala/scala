96aa17712a4758a8f1f28902af3ec0fc7529763b
Harrison Houghton <hora.rhino@gmail.com>
2020-05-03 19:08:47 -0400

    Don't enter $isInstanceOf/$asInstanceOf
    Closes scala/bug#11843.
    
    Co-authored-by: A. P. Marki <som.snytt@gmail.com>

--- test/files/run/reflection-magicsymbols-invoke.scala
+++ test/files/run/reflection-magicsymbols-invoke.scala
@@ -23,7 +23,7 @@ object Test extends App {
           println(realex.getClass + ": " + realex.getMessage)
       }
     print(s"testing ${tpe.typeSymbol.name}.$method: ")
-    wrap({
+    wrap {
       if (method == termNames.CONSTRUCTOR.toString) {
         val ctor = tpe.decl(termNames.CONSTRUCTOR).asMethod
         cm.reflectClass(ctor.owner.asClass).reflectConstructor(ctor)(args: _*)
@@ -31,7 +31,7 @@ object Test extends App {
         val meth = tpe.decl(TermName(method).encodedName.toTermName).asMethod
         cm.reflect(receiver).reflectMethod(meth)(args: _*)
       }
-    })
+    }
   }
 
   println("============\nAny")
@@ -63,10 +63,6 @@ object Test extends App {
   typeOf[AnyRef].members.toList.sortBy(key).foreach(sym => println(key(sym)))
   test(typeOf[AnyRef], "2", "!=", "2")
   test(typeOf[AnyRef], "2", "##")
-  test(typeOf[AnyRef], "2", "$asInstanceOf")
-  test(typeOf[AnyRef], "2", "$asInstanceOf", typeOf[String])
-  test(typeOf[AnyRef], "2", "$isInstanceOf")
-  test(typeOf[AnyRef], "2", "$isInstanceOf", typeOf[String])
   test(typeOf[AnyRef], "2", "==", "2")
   test(typeOf[AnyRef], "2", "clone")
   test(typeOf[AnyRef], "2", "eq", "2")
