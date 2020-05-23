d5bb4858a0d373557fbd400e719b8b4f2eee5d47
Martijn Hoekstra <martijn.hoekstra@cargoxs.com>
2020-05-23 16:07:16 +0200

    [backport] fix back-quoted constructor params with identical prefixes

--- src/reflect/scala/reflect/internal/StdNames.scala
+++ src/reflect/scala/reflect/internal/StdNames.scala
@@ -441,6 +441,9 @@ trait StdNames {
      *  Look backward from the end of the string for "$$", and take the
      *  part of the string after that; but if the string is "$$$" or longer,
      *  be sure to retain the extra dollars.
+     *  If the name happens to be a back quoted name containing literal $$
+     *  or $ followed by an operator that gets encoded, go directly to compiler
+     *  crash. Do not pass go and don't even think about collecting any $$
      */
     def unexpandedName(name: Name): Name = {
       if (!name.containsChar('$')) name // lastIndexOf calls Name.toString, add a fast path to avoid that.
