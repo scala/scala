ffef4d32aac8f6c0cc7215b529740d09315075c8
Som Snytt <som.snytt@gmail.com>
2020-06-02 21:03:41 -0700

    All filters must match for nowarn
    (cherry picked from commit 0c52dc463623c429ece49c1e83a4e24ad8451000)

---
+++ test/files/neg/t12026.scala
@@ -0,0 +1,14 @@
+//scalac: -Ywarn-value-discard -Xfatal-warnings
+
+import annotation._
+
+//the following option works and warns for f but not g
+//-Wconf:cat=w-flag-value-discard&site=T.g:s
+
+@nowarn("cat=w-flag-value-discard&site=T.g")
+trait T {
+  def v(): Int
+  def f(): Unit = v()
+  def g(): Unit = v()
+}
+// was: no warning
