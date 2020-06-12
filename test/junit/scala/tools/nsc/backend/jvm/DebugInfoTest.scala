059394c21d572c74591e588bf2c1cc1bf0cabd1d
Ergys Dona <ergys.dona@epfl.ch>
2020-06-12 12:16:17 +0200

    Add some extra tests

--- test/junit/scala/tools/nsc/backend/jvm/DebugInfoTest.scala
+++ test/junit/scala/tools/nsc/backend/jvm/DebugInfoTest.scala
@@ -27,7 +27,7 @@ class DebugInfoTest {
     val expectedLines = Seq(
       "*F",
       "1 MyAwesomeClass.scala"
-    )
+      )
 
     assert(stratum.fileSectionLines == expectedLines)
   }
@@ -73,6 +73,22 @@ class DebugInfoTest {
   }
 
   @Test
+  def oneToOneMappingWithFileIdStratumTest(): Unit = {
+    val stratum = new ScalaStratum
+
+    stratum.addRawLineMapping(RawLineMapping(from = 42, toStart = 142, toEnd = 142, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 55, toStart = 155, toEnd = 155, sourceFileId = Some(2)))
+
+    val expectedLines = Seq(
+      "*L",
+      "42#1:142",
+      "55#2:155"
+      )
+
+    assert(stratum.lineSectionLines == expectedLines)
+  }
+
+  @Test
   def oneToManyMappingStratumTest(): Unit = {
     val stratum = new ScalaStratum
 
@@ -81,6 +97,22 @@ class DebugInfoTest {
     val expectedLines = Seq(
       "*L",
       "42:142,9"
+      )
+
+    assert(stratum.lineSectionLines == expectedLines)
+  }
+
+  @Test
+  def oneToManyMappingWithFileIdStratumTest(): Unit = {
+    val stratum = new ScalaStratum
+
+    stratum.addRawLineMapping(RawLineMapping(from = 42, toStart = 142, toEnd = 150, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 55, toStart = 155, toEnd = 163, sourceFileId = Some(2)))
+
+    val expectedLines = Seq(
+      "*L",
+      "42#1:142,9",
+      "55#2:155,9"
       )
 
     assert(stratum.lineSectionLines == expectedLines)
@@ -105,6 +137,31 @@ class DebugInfoTest {
   }
 
   @Test
+  def zipMappingWithFileIdStratumTest(): Unit = {
+    val stratum = new ScalaStratum
+
+    stratum.addRawLineMapping(RawLineMapping(from = 42, toStart = 142, toEnd = 142, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 43, toStart = 143, toEnd = 143, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 44, toStart = 144, toEnd = 144, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 45, toStart = 145, toEnd = 145, sourceFileId = Some(1)))
+
+    stratum.addRawLineMapping(RawLineMapping(from = 46, toStart = 146, toEnd = 146, sourceFileId = Some(2)))
+    stratum.addRawLineMapping(RawLineMapping(from = 47, toStart = 147, toEnd = 147, sourceFileId = Some(2)))
+
+    stratum.addRawLineMapping(RawLineMapping(from = 48, toStart = 148, toEnd = 148, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 49, toStart = 149, toEnd = 149, sourceFileId = Some(1)))
+
+    val expectedLines = Seq(
+      "*L",
+      "42#1,4:142",
+      "48#1,2:148",
+      "46#2,2:146"
+    )
+
+    assert(stratum.lineSectionLines == expectedLines)
+  }
+
+  @Test
   def multiZipMappingStratumTest(): Unit = {
     val stratum = new ScalaStratum
 
@@ -117,6 +174,54 @@ class DebugInfoTest {
     val expectedLines = Seq(
       "*L",
       "42,5:142,3"
+      )
+
+    assert(stratum.lineSectionLines == expectedLines)
+  }
+
+  @Test
+  def multiZipMappingWithBreakStratumTest(): Unit = {
+    val stratum = new ScalaStratum
+
+    stratum.addRawLineMapping(RawLineMapping(from = 42, toStart = 142, toEnd = 144))
+    stratum.addRawLineMapping(RawLineMapping(from = 43, toStart = 145, toEnd = 147))
+    stratum.addRawLineMapping(RawLineMapping(from = 44, toStart = 148, toEnd = 151))
+    stratum.addRawLineMapping(RawLineMapping(from = 45, toStart = 152, toEnd = 154))
+    stratum.addRawLineMapping(RawLineMapping(from = 46, toStart = 155, toEnd = 157))
+    stratum.addRawLineMapping(RawLineMapping(from = 47, toStart = 158, toEnd = 160))
+
+    val expectedLines = Seq(
+      "*L",
+      "42,2:142,3",
+      "44:148,4",
+      "45,3:152,3"
+      )
+
+    assert(stratum.lineSectionLines == expectedLines)
+  }
+
+  @Test
+  def multiZipMappingWithFileIdStratumTest(): Unit = {
+    val stratum = new ScalaStratum
+
+    stratum.addRawLineMapping(RawLineMapping(from = 42, toStart = 142, toEnd = 144, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 43, toStart = 145, toEnd = 147, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 44, toStart = 148, toEnd = 150, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 45, toStart = 151, toEnd = 153, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 46, toStart = 154, toEnd = 156, sourceFileId = Some(1)))
+
+    stratum.addRawLineMapping(RawLineMapping(from = 47, toStart = 157, toEnd = 159, sourceFileId = Some(2)))
+    stratum.addRawLineMapping(RawLineMapping(from = 48, toStart = 160, toEnd = 162, sourceFileId = Some(2)))
+    stratum.addRawLineMapping(RawLineMapping(from = 49, toStart = 163, toEnd = 165, sourceFileId = Some(2)))
+
+    stratum.addRawLineMapping(RawLineMapping(from = 50, toStart = 166, toEnd = 169, sourceFileId = Some(1)))
+    stratum.addRawLineMapping(RawLineMapping(from = 51, toStart = 170, toEnd = 173, sourceFileId = Some(1)))
+
+    val expectedLines = Seq(
+      "*L",
+      "42#1,5:142,3",
+      "50#1,2:166,4",
+      "47#2,3:157,3"
       )
 
     assert(stratum.lineSectionLines == expectedLines)
