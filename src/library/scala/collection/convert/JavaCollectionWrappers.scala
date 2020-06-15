dc22b37cd179a39bad9f51170d7608404051a08b
Lukas Rytz <lukas.rytz@gmail.com>
2020-06-15 16:15:08 +0200

    Add StrictOptimized parents to jkd converters

--- src/library/scala/collection/convert/JavaCollectionWrappers.scala
+++ src/library/scala/collection/convert/JavaCollectionWrappers.scala
@@ -55,14 +55,20 @@ private[collection] object JavaCollectionWrappers extends Serializable {
   case class IterableWrapper[A](underlying: Iterable[A]) extends ju.AbstractCollection[A] with IterableWrapperTrait[A] with Serializable
 
   @SerialVersionUID(3L)
-  case class JIterableWrapper[A](underlying: jl.Iterable[A]) extends AbstractIterable[A] with Serializable {
+  case class JIterableWrapper[A](underlying: jl.Iterable[A])
+    extends AbstractIterable[A]
+      with StrictOptimizedIterableOps[A, Iterable, Iterable[A]]
+      with Serializable {
     def iterator = underlying.iterator.asScala
     override def iterableFactory = mutable.ArrayBuffer
     override def isEmpty: Boolean = !underlying.iterator().hasNext
   }
 
   @SerialVersionUID(3L)
-  case class JCollectionWrapper[A](underlying: ju.Collection[A]) extends AbstractIterable[A] with Serializable {
+  case class JCollectionWrapper[A](underlying: ju.Collection[A])
+    extends AbstractIterable[A]
+      with StrictOptimizedIterableOps[A, Iterable, Iterable[A]]
+      with Serializable {
     def iterator = underlying.iterator.asScala
     override def size = underlying.size
     override def knownSize: Int = if (underlying.isEmpty) 0 else super.knownSize
@@ -94,8 +100,10 @@ private[collection] object JavaCollectionWrappers extends Serializable {
   }
 
   @SerialVersionUID(3L)
-  case class JListWrapper[A](underlying: ju.List[A]) extends mutable.AbstractBuffer[A]
+  case class JListWrapper[A](underlying: ju.List[A])
+    extends mutable.AbstractBuffer[A]
       with SeqOps[A, mutable.Buffer, mutable.Buffer[A]]
+      with StrictOptimizedSeqOps[A, mutable.Buffer, mutable.Buffer[A]]
       with IterableFactoryDefaults[A, mutable.Buffer]
       with Serializable {
     def length = underlying.size
@@ -169,7 +177,11 @@ private[collection] object JavaCollectionWrappers extends Serializable {
   }
 
   @SerialVersionUID(3L)
-  case class JSetWrapper[A](underlying: ju.Set[A]) extends mutable.AbstractSet[A] with mutable.SetOps[A, mutable.Set, mutable.Set[A]] with Serializable {
+  case class JSetWrapper[A](underlying: ju.Set[A])
+    extends mutable.AbstractSet[A]
+      with mutable.SetOps[A, mutable.Set, mutable.Set[A]]
+      with StrictOptimizedSetOps[A, mutable.Set, mutable.Set[A]]
+      with Serializable {
 
     override def size: Int = underlying.size
     override def isEmpty: Boolean = underlying.isEmpty
@@ -299,7 +311,9 @@ private[collection] object JavaCollectionWrappers extends Serializable {
       with JMapWrapperLike[K, V, mutable.Map, mutable.Map[K, V]] with Serializable
 
   trait JMapWrapperLike[K, V, +CC[X, Y] <: mutable.MapOps[X, Y, CC, _], +C <: mutable.MapOps[K, V, CC, C]]
-    extends mutable.MapOps[K, V, CC, C] {
+    extends mutable.MapOps[K, V, CC, C]
+      with StrictOptimizedMapOps[K, V, CC, C]
+      with StrictOptimizedIterableOps[(K, V), mutable.Iterable, C] {
 
     def underlying: ju.Map[K, V]
 
@@ -458,8 +472,13 @@ private[collection] object JavaCollectionWrappers extends Serializable {
   }
 
   @SerialVersionUID(3L)
-  case class JPropertiesWrapper(underlying: ju.Properties) extends mutable.AbstractMap[String, String]
-            with mutable.MapOps[String, String, mutable.Map, mutable.Map[String, String]] with Serializable {
+  case class JPropertiesWrapper(underlying: ju.Properties)
+    extends mutable.AbstractMap[String, String]
+      with mutable.MapOps[String, String, mutable.Map, mutable.Map[String, String]]
+      with StrictOptimizedMapOps[String, String, mutable.Map, mutable.Map[String, String]]
+      with StrictOptimizedIterableOps[(String, String), mutable.Iterable, mutable.Map[String, String]]
+      with Serializable {
+    unzip
 
     override def size = underlying.size
     override def isEmpty: Boolean = underlying.isEmpty
