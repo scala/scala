828defb65d4e93c722c6b64ddf7e90cbb1a47488
NagaChaitanya Vellanki <pnagato@protonmail.com>
2020-07-25 23:40:31 -0700

    Fixes reflection's CodePrinter outputs incorrect code

--- src/reflect/scala/reflect/internal/Printers.scala
+++ src/reflect/scala/reflect/internal/Printers.scala
@@ -562,7 +562,7 @@ trait Printers extends api.Printers { self: SymbolTable =>
 
       def addBackquotes(s: String) =
         if (decoded && (decName.exists(ch => brackets.contains(ch) || isWhitespace(ch) || isDot(ch)) ||
-          (name.isOperatorName && decName.exists(isOperatorPart) && decName.exists(isScalaLetter) && !decName.contains(bslash))))
+          ((name.isOperatorName || decName.exists(isOperatorPart)) && decName.exists(isScalaLetter) && !decName.contains(bslash))))
           s"`$s`" else s
 
       if (name == nme.CONSTRUCTOR) "this"
