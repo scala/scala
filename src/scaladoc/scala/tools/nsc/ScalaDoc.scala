scala/scala

commit 71c91d5cea6410a81001a362b6d74efc64b86877

Allow custom interpretation of path strings in settings

src/scaladoc/scala/tools/nsc/doc/Settings.scala
@@ -14,12 +14,14 @@ package scala.tools.nsc
 package doc
 
 import java.io.File
+
 import scala.language.postfixOps
+import scala.tools.nsc.settings.{DefaultPathFactory, PathFactory}
 
 /** An extended version of compiler settings, with additional Scaladoc-specific options.
   * @param error A function that prints a string to the appropriate error stream
   * @param printMsg A function that prints the string, without any extra boilerplate of error */
-class Settings(error: String => Unit, val printMsg: String => Unit = println(_)) extends scala.tools.nsc.Settings(error) {
+class Settings(error: String => Unit, val printMsg: String => Unit = println(_), pathFactory: PathFactory = DefaultPathFactory) extends scala.tools.nsc.Settings(error, pathFactory) {
 
   // TODO 2.13 Remove
   private def removalIn213 = "This flag is scheduled for removal in 2.13. If you have a case where you need this flag then please report a bug."

src/compiler/scala/tools/nsc/Settings.scala
@@ -12,11 +12,12 @@
 
 package scala.tools.nsc
 
-import settings.MutableSettings
+import settings.{DefaultPathFactory, MutableSettings, PathFactory}
 
 /** A compatibility stub.
  */
-class Settings(errorFn: String => Unit) extends MutableSettings(errorFn) {
+class Settings(errorFn: String => Unit, pathFactory: PathFactory) extends MutableSettings(errorFn, pathFactory) {
+  def this(errorFn: String => Unit) = this(errorFn, DefaultPathFactory)
   def this() = this(Console.println)
 
   override def withErrorFn(errorFn: String => Unit): Settings = {

src/compiler/scala/tools/nsc/GenericRunnerSettings.scala
@@ -13,9 +13,12 @@
 package scala.tools.nsc
 
 import java.net.URL
+
+import scala.tools.nsc.settings.{DefaultPathFactory, PathFactory}
 import scala.tools.util.PathResolver
 
-class GenericRunnerSettings(error: String => Unit) extends Settings(error) {
+class GenericRunnerSettings(error: String => Unit, pathFactory: PathFactory) extends Settings(error, pathFactory) {
+  def this(error: String => Unit) = this(error, DefaultPathFactory)
   lazy val classpathURLs: Seq[URL] = {
     val registry = new CloseableRegistry
     try {

src/compiler/scala/tools/nsc/Global.scala
@@ -367,7 +367,7 @@ class Global(var currentSettings: Settings, reporter0: Reporter)
   def getSourceFile(f: AbstractFile): BatchSourceFile = new BatchSourceFile(f, reader read f)
 
   def getSourceFile(name: String): SourceFile = {
-    val f = AbstractFile.getFile(name)
+    val f = settings.pathFactory.getFile(name)
     if (f eq null) throw new FileNotFoundException(
       "source file '" + name + "' could not be found")
     getSourceFile(f)
@@ -874,11 +874,11 @@ class Global(var currentSettings: Settings, reporter0: Reporter)
         case cp: ClassPath => Seq(cp)
       }
 
-      val dir = AbstractFile.getDirectory(path) // if path is a `jar`, this is a FileZipArchive (isDirectory is true)
+      val dir = settings.pathFactory.getDirectory(path) // if path is a `jar`, this is a FileZipArchive (isDirectory is true)
       val canonical = dir.canonicalPath         // this is the canonical path of the .jar
       def matchesCanonical(e: ClassPath) = origin(e) match {
         case Some(opath) =>
-          AbstractFile.getDirectory(opath).canonicalPath == canonical
+          settings.pathFactory.getDirectory(opath).canonicalPath == canonical
         case None =>
           false
       }

src/compiler/scala/tools/nsc/classpath/ClassPathFactory.scala
@@ -38,7 +38,7 @@ class ClassPathFactory(settings: Settings, closeableRegistry: CloseableRegistry 
   def sourcesInPath(path: String): List[ClassPath] =
     for {
       file <- expandPath(path, expandStar = false)
-      dir <- Option(AbstractFile getDirectory file)
+      dir <- Option(settings.pathFactory.getDirectory(file))
     } yield createSourcePath(dir)
 
 
@@ -50,7 +50,7 @@ class ClassPathFactory(settings: Settings, closeableRegistry: CloseableRegistry 
     for {
       dir <- expandPath(path, expandStar = false)
       name <- expandDir(dir)
-      entry <- Option(AbstractFile.getDirectory(name))
+      entry <- Option(settings.pathFactory.getDirectory(name))
     } yield newClassPath(entry)
 
   def classesInExpandedPath(path: String): IndexedSeq[ClassPath] =
@@ -67,8 +67,8 @@ class ClassPathFactory(settings: Settings, closeableRegistry: CloseableRegistry 
     for {
       file <- expandPath(path, expand)
       dir <- {
-        def asImage = if (file.endsWith(".jimage")) Some(AbstractFile.getFile(file)) else None
-        Option(AbstractFile.getDirectory(file)).orElse(asImage)
+        def asImage = if (file.endsWith(".jimage")) Some(settings.pathFactory.getFile(file)) else None
+        Option(settings.pathFactory.getDirectory(file)).orElse(asImage)
       }
     } yield newClassPath(dir)
 

src/compiler/scala/tools/nsc/settings/FscSettings.scala
@@ -17,7 +17,7 @@ package settings
 import util.ClassPath
 import io.{ Path, AbstractFile }
 
-class FscSettings(error: String => Unit) extends Settings(error) {
+class FscSettings(error: String => Unit, pathFactory: PathFactory = DefaultPathFactory) extends Settings(error, pathFactory) {
   outer =>
 
   locally {

src/compiler/scala/tools/nsc/settings/MutableSettings.scala
@@ -24,15 +24,16 @@ import scala.reflect.{ ClassTag, classTag }
 
 /** A mutable Settings object.
  */
-class MutableSettings(val errorFn: String => Unit)
+class MutableSettings(val errorFn: String => Unit, val pathFactory: PathFactory)
               extends scala.reflect.internal.settings.MutableSettings
                  with AbsSettings
                  with ScalaSettings
                  with Mutable {
+  def this(errorFn: String => Unit) = this(errorFn, DefaultPathFactory)
   type ResultOfTryToSet = List[String]
 
   def withErrorFn(errorFn: String => Unit): MutableSettings = {
-    val settings = new MutableSettings(errorFn)
+    val settings = new MutableSettings(errorFn, pathFactory)
     copyInto(settings)
     settings
   }
@@ -273,8 +274,8 @@ class MutableSettings(val errorFn: String => Unit)
      *  Both directories should exist.
      */
     def add(srcDir: String, outDir: String): Unit = // used in ide?
-      add(checkDir(AbstractFile.getDirectory(srcDir), srcDir),
-          checkDir(AbstractFile.getDirectory(outDir), outDir))
+      add(checkDir(pathFactory.getDirectory(srcDir), srcDir),
+          checkDir(pathFactory.getDirectory(outDir), outDir))
 
     /** Check that dir is exists and is a directory. */
     private def checkDir(dir: AbstractFile, name: String, allowJar: Boolean = false): AbstractFile = (
@@ -290,7 +291,7 @@ class MutableSettings(val errorFn: String => Unit)
      *  be dumped in there, regardless of previous calls to 'add'.
      */
     def setSingleOutput(outDir: String) {
-      val dst = AbstractFile.getDirectory(outDir)
+      val dst = pathFactory.getDirectory(outDir)
       setSingleOutput(checkDir(dst, outDir, allowJar = true))
     }
 

src/compiler/scala/tools/nsc/settings/PathFactory.scala
@@ -1,0 +1,27 @@
+/*
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
+ */
+
+package scala.tools.nsc.settings
+
+import scala.reflect.io.{AbstractFile, PlainFile}
+
+/** Converts paths provided in compiler options (e.g elements of `-classpath` or the target directory of `-d`) to
+ *  an `AbstractFile`. */
+trait PathFactory {
+  def getDirectory(path: String): AbstractFile
+  def getFile(path: String): AbstractFile
+}
+
+object DefaultPathFactory extends PathFactory {
+  override def getDirectory(path: String): AbstractFile = AbstractFile.getDirectory(path)
+  override def getFile(path: String): AbstractFile = new PlainFile(path)
+}

commit ce8e3dfbfd657f484d0a607c29406d8e2eac2274

Restore binary compatibility for doc.Settings
71c91d5cea6410a81001a362b6d74efc64b86877 broke binary compatibility for
scala.tools.nsc.doc.Settings.<init>. This api is used by
https://github.com/tkawachi/sbt-doctest which was broken by the change
to the signature.

src/scaladoc/scala/tools/nsc/ScalaDoc.scala
@@ -14,6 +14,7 @@ package scala.tools.nsc
 
 import scala.tools.nsc.doc.DocFactory
 import scala.tools.nsc.reporters.ConsoleReporter
+import scala.tools.nsc.settings.DefaultPathFactory
 import scala.reflect.internal.Reporter
 import scala.reflect.internal.util.{ FakePos, NoPosition, Position }
 
@@ -26,7 +27,8 @@ class ScalaDoc {
   def process(args: Array[String]): Boolean = {
     var reporter: ScalaDocReporter = null
     val docSettings = new doc.Settings(msg => reporter.error(FakePos("scaladoc"), msg + "\n  scaladoc -help  gives more information"),
-                                       msg => reporter.printMessage(msg))
+                                       msg => reporter.printMessage(msg),
+                                       DefaultPathFactory)
     reporter = new ScalaDocReporter(docSettings)
     val command = new ScalaDoc.Command(args.toList, docSettings)
     def hasFiles = command.files.nonEmpty || docSettings.uncompilableFiles.nonEmpty

src/scaladoc/scala/tools/nsc/doc/Settings.scala
@@ -22,6 +22,8 @@ import scala.tools.nsc.settings.{DefaultPathFactory, PathFactory}
   * @param error A function that prints a string to the appropriate error stream
   * @param printMsg A function that prints the string, without any extra boilerplate of error */
 class Settings(error: String => Unit, val printMsg: String => Unit = println(_), pathFactory: PathFactory = DefaultPathFactory) extends scala.tools.nsc.Settings(error, pathFactory) {
+  // https://github.com/tkawachi/sbt-doctest depends on this constructor being available
+  def this(error: String => Unit, printMsg: String => Unit) = this(error, printMsg, DefaultPathFactory)
 
   // TODO 2.13 Remove
   private def removalIn213 = "This flag is scheduled for removal in 2.13. If you have a case where you need this flag then please report a bug."