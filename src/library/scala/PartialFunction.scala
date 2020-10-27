9d8e0ad972b8670a0e7f00e279c26cbd3386f5f4
Karl_Wang <wskqing@gmail.com>
2020-10-27 23:25:13 +0800

    Update PartialFunction.scala

--- src/library/scala/PartialFunction.scala
+++ src/library/scala/PartialFunction.scala
@@ -86,7 +86,7 @@ trait PartialFunction[-A, +B] extends (A => B) { self =>
   /** Checks if a value is contained in the function's domain.
    *
    *  @param  x   the value to test
-   *  @return `'''true'''`, iff `x` is in the domain of this function, `'''false'''` otherwise.
+   *  @return `'''true'''`, if `x` is in the domain of this function, `'''false'''` otherwise.
    */
   def isDefinedAt(x: A): Boolean
 
