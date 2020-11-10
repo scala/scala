728fa37826cb2328db5e974555158d5720d2ecb1
Jason Zaugg <jzaugg@gmail.com>
2020-11-10 13:34:02 +1000

    Adapt toolbox/async test for 2.13.x

--- test/async/jvm/toolbox.scala
+++ test/async/jvm/toolbox.scala
@@ -1,11 +1,12 @@
-import tools.reflect._
-import reflect.runtime._
+import scala.tools.reflect._
+import scala.reflect.runtime._
 object Test extends App {
   val box = currentMirror.mkToolBox(options = "-Xasync")
+
   val code =
     """
     import scala.concurrent._, scala.concurrent.duration._, ExecutionContext.Implicits._
-    import scala.tools.partest.async._
+    import scala.tools.testkit.async._
     val f1 = Future(1)
     val f2 = Future(2)
     val res = Async.async { Async.await(f1) + Async.await(f2) }
