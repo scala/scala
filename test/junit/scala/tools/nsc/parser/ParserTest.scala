892a56d9855d5c8fee6f49ab409b2f5849719abd
Jason Zaugg <jzaugg@gmail.com>
2020-12-02 10:09:33 +1000

    Test showing position bug with `var x = _`
    The `= _` should be include in the range position
    of the ValDef.

--- test/junit/scala/tools/nsc/parser/ParserTest.scala
+++ test/junit/scala/tools/nsc/parser/ParserTest.scala
@@ -3,6 +3,7 @@ package scala.tools.nsc.parser
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.junit.Assert._
 
 import scala.tools.testing.BytecodeTesting
 
@@ -28,5 +29,20 @@ class ParserTest extends BytecodeTesting{
     run.compileSources(newSourceFile(lfCode) :: Nil)
     assert(!reporter.hasErrors)
     run.compileSources(newSourceFile(crlfCode) :: Nil)
+  }
+
+  @Test
+  def rangePosOfDefaultInitializer_t12213(): Unit = {
+    val code =
+      """object Other { var x: Int = _; var y: Int = 42 }"""
+    import compiler._, global._
+    val run = new Run
+    run.compileSources(newSourceFile(code) :: Nil)
+    assert(!reporter.hasErrors)
+    val unit = run.units.toList.head
+    def codeOf(pos: Position) = new String(pos.source.content.slice(pos.start, pos.end))
+    val List(x, y) = unit.body.collect { case vd : ValDef => vd }.takeRight(2)
+    assertEquals("var y: Int = 42", codeOf(y.pos))
+    assertEquals("var x: Int", codeOf(x.pos)) // TODO fix parser to include `= _`
   }
 }
