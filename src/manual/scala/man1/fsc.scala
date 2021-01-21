007ed27e45bbb3be54a2f3daf0dc6fcb3e66442c
Seth Tisue <seth@tisue.net>
2021-01-20 17:53:12 -0800

    update some antiquated language in our man pages

--- src/manual/scala/man1/fsc.scala
+++ src/manual/scala/man1/fsc.scala
@@ -123,9 +123,9 @@ object fsc extends Command {
           "Specify the options to be passed to the " & MBold("java") &
           " command defined by " & MBold("JAVACMD") & ".",
 
-          "With Java 1.5 (or newer) one may for example configure the " &
-          "memory usage of the JVM as follows: " &
-          Mono("JAVA_OPTS=\"-Xmx512M -Xms16M -Xss16M\"")
+          "One might for example configure the " &
+          "memory usage of the JVM with: " &
+          Mono("JAVA_OPTS=\"-Xmx2G -Xss16M\"")
         ))))
 
   val exitStatus = Section("EXIT STATUS",
