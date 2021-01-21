1236e7956f3dd6d16ae03281b4a95309a307d29a
Som Snytt <som.snytt@gmail.com>
2021-01-21 09:46:32 -0800

    Improve REPL output filtering for color escapes

---
+++ test/files/run/t12276.scala
@@ -0,0 +1,22 @@
+import scala.tools.nsc.Settings
+import scala.tools.nsc.interpreter.shell.{ILoop, ShellConfig}
+import scala.tools.partest.{hexdump, ReplTest}
+
+object Test extends ReplTest {
+  def code = """
+    |java.nio.CharBuffer.allocate(5)
+    |java.nio.CharBuffer.allocate(6)
+    |class C
+    |classOf[C]
+    |val esc = 0x1b.toChar
+    |classOf[C].toString + esc + "[3z"
+    |classOf[C].toString + esc + "[3!"
+    |classOf[C].toString + scala.io.AnsiColor.YELLOW
+    |""".stripMargin
+
+  override protected def shellConfig(testSettings: Settings) =
+    new ILoop.TestConfig(ShellConfig(testSettings)) {
+      override val colorOk = true
+    }
+  override def normalize(s: String) = hexdump(s).mkString("", "\n", "\n")
+}
