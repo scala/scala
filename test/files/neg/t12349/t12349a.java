206af8a5254c137c0ad31536e2848b3955bb80c6
Hatano Yuusuke <56590357+kynthus@users.noreply.github.com>
2021-02-26 09:57:22 +0900

    Test case for override access check when overridign Java methods

---
+++ test/files/neg/t12349/t12349a.java
@@ -0,0 +1,45 @@
+package t12349;
+
+public class t12349a {
+
+       public void a1() { System.out.println("t12349a#a1()"); }
+       public void a2() { System.out.println("t12349a#a2()"); }
+       public void a3() { System.out.println("t12349a#a3()"); }
+       public void a4() { System.out.println("t12349a#a4()"); }
+       public void a5() { System.out.println("t12349a#a5()"); }
+       public void a6() { System.out.println("t12349a#a6()"); }
+       public void a7() { System.out.println("t12349a#a7()"); }
+       public void a8() { System.out.println("t12349a#a8()"); }
+       public void a9() { System.out.println("t12349a#a9()"); }
+
+    protected void b1() { System.out.println("t12349a#b1()"); }
+    protected void b2() { System.out.println("t12349a#b2()"); }
+    protected void b3() { System.out.println("t12349a#b3()"); }
+    protected void b4() { System.out.println("t12349a#b4()"); }
+    protected void b5() { System.out.println("t12349a#b5()"); }
+    protected void b6() { System.out.println("t12349a#b6()"); }
+    protected void b7() { System.out.println("t12349a#b7()"); }
+    protected void b8() { System.out.println("t12349a#b8()"); }
+    protected void b9() { System.out.println("t12349a#b9()"); }
+
+              void c1() { System.out.println("t12349a#c1()"); }
+              void c2() { System.out.println("t12349a#c2()"); }
+              void c3() { System.out.println("t12349a#c3()"); }
+              void c4() { System.out.println("t12349a#c4()"); }
+              void c5() { System.out.println("t12349a#c5()"); }
+              void c6() { System.out.println("t12349a#c6()"); }
+              void c7() { System.out.println("t12349a#c7()"); }
+              void c8() { System.out.println("t12349a#c8()"); }
+              void c9() { System.out.println("t12349a#c9()"); }
+
+      private void d1() { System.out.println("t12349a#d1()"); }
+      private void d2() { System.out.println("t12349a#d2()"); }
+      private void d3() { System.out.println("t12349a#d3()"); }
+      private void d4() { System.out.println("t12349a#d4()"); }
+      private void d5() { System.out.println("t12349a#d5()"); }
+      private void d6() { System.out.println("t12349a#d6()"); }
+      private void d7() { System.out.println("t12349a#d7()"); }
+      private void d8() { System.out.println("t12349a#d8()"); }
+      private void d9() { System.out.println("t12349a#d9()"); }
+
+}
---
+++ test/files/neg/t12349/t12349b.scala
@@ -0,0 +1,47 @@
+package t12349
+
+object t12349b {
+
+  class Inner12349b extends t12349a {
+                       override def a1(): Unit = println("Inner12349b#a1()")
+    protected          override def a2(): Unit = println("Inner12349b#a2()") // [#12349]
+      private          override def a3(): Unit = println("Inner12349b#a3()") // weaker access privileges
+    protected[t12349b] override def a4(): Unit = println("Inner12349b#a4()") // [#12349]
+      private[t12349b] override def a5(): Unit = println("Inner12349b#a5()") // [#12349]
+    protected[t12349]  override def a6(): Unit = println("Inner12349b#a6()") // [#12349]
+      private[t12349]  override def a7(): Unit = println("Inner12349b#a7()") // [#12349]
+    protected[this]    override def a8(): Unit = println("Inner12349b#a8()") // [#12349]
+      private[this]    override def a9(): Unit = println("Inner12349b#a9()") // [#9334]
+
+                       override def b1(): Unit = println("Inner12349b#b1()")
+    protected          override def b2(): Unit = println("Inner12349b#b2()")
+      private          override def b3(): Unit = println("Inner12349b#b3()") // weaker access privileges
+    protected[t12349b] override def b4(): Unit = println("Inner12349b#b4()")
+      private[t12349b] override def b5(): Unit = println("Inner12349b#b5()") // weaker access privileges
+    protected[t12349]  override def b6(): Unit = println("Inner12349b#b6()")
+      private[t12349]  override def b7(): Unit = println("Inner12349b#b7()") // weaker access privileges
+    protected[this]    override def b8(): Unit = println("Inner12349b#b8()") // [#12349] - not fixed by PR #9525
+      private[this]    override def b9(): Unit = println("Inner12349b#b9()") // [#9334]
+
+                       override def c1(): Unit = println("Inner12349b#c1()")
+    protected          override def c2(): Unit = println("Inner12349b#c2()") // [#12349]
+      private          override def c3(): Unit = println("Inner12349b#c3()") // weaker access privileges
+    protected[t12349b] override def c4(): Unit = println("Inner12349b#c4()") // [#12349]
+      private[t12349b] override def c5(): Unit = println("Inner12349b#c5()") // [#12349]
+    protected[t12349]  override def c6(): Unit = println("Inner12349b#c6()")
+      private[t12349]  override def c7(): Unit = println("Inner12349b#c7()")
+    protected[this]    override def c8(): Unit = println("Inner12349b#c8()") // [#12349]
+      private[this]    override def c9(): Unit = println("Inner12349b#c9()") // [#9334]
+
+                       override def d1(): Unit = println("Inner12349b#d1()") // overrides nothing
+    protected          override def d2(): Unit = println("Inner12349b#d2()") // overrides nothing
+      private          override def d3(): Unit = println("Inner12349b#d3()") // overrides nothing
+    protected[t12349b] override def d4(): Unit = println("Inner12349b#d4()") // overrides nothing
+      private[t12349b] override def d5(): Unit = println("Inner12349b#d5()") // overrides nothing
+    protected[t12349]  override def d6(): Unit = println("Inner12349b#d6()") // overrides nothing
+      private[t12349]  override def d7(): Unit = println("Inner12349b#d7()") // overrides nothing
+    protected[this]    override def d8(): Unit = println("Inner12349b#d8()") // overrides nothing
+      private[this]    override def d9(): Unit = println("Inner12349b#d9()") // overrides nothing
+  }
+
+}
