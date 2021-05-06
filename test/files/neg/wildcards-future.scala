1185f84584f64f3b2b0482cbd7dd266079cebfd2
Kai <kai@7mind.io>
2021-05-06 12:36:54 +0100

    Differentiate Scala 2 and Scala 3 wildcard identifier names
    This change names wildcards written with Scala 3 `?` syntax with `?$N` pattern instead of `_$N` used for Scala 2 wildcards
    
    There are two reasons for it:
    
    - To allow `kind-projector` to implement Scala 3 underscore syntax for type lambdas by transforming old-style underscores, but leaving Scala 3 underscores intact
    - To show a mildly more relevant error message, since a wildcard introduced by `?` will now also have a name with `?` in the error message

---
+++ test/files/neg/wildcards-future.scala
@@ -0,0 +1,11 @@
+// scalac: -Xsource:3
+//
+object Test {
+  val underscores: Map[_ <: AnyRef, _ >: Null] = Map()
+  val qmarks: Map[? <: AnyRef, ? >: Null] = Map()
+
+  underscores : Map[String, String] // error wildcard variables starting with `_`
+
+  qmarks : Map[String, String] // error – wildcard variables should start with `?` to differentiate from the old syntax
+                               // (and have a mildly more readable error...)
+}
