6ab61aafd9fb6bf04f5f30808b6b90d12ceb9fad
Som Snytt <som.snytt@gmail.com>
2021-05-20 10:24:22 -0700

    Handle Scala 3 star in import braces

---
+++ test/files/neg/import-syntax.scala
@@ -0,0 +1,12 @@
+// scalac: -Xsource:3
+
+class D {
+  def *(y: Int): Int = y
+  def unrelated(y: Int): Int = y
+}
+
+object nope {
+  val d = new D
+  import d.{* => huh}
+  import d.{_ => also_no}
+}
