d1984ea7eedfb343918c2ac7476dd9c01dc5d579
Som Snytt <som.snytt@gmail.com>
2021-06-14 13:53:28 -0700

    Retry sealed traits for relatedness

---
+++ test/files/neg/t12414b/a_1.scala
@@ -0,0 +1,6 @@
+
+sealed trait Trait1
+sealed trait Trait2
+
+class Class1 extends Trait1
+class Class2 extends Trait2
---
+++ test/files/neg/t12414b/b_2.scala
@@ -0,0 +1,9 @@
+// scalac: -Werror
+
+object Test extends App {
+  def test(x: Trait1): Unit =
+    x match {
+      case y: Trait2 =>
+      case _ =>
+    }
+}
