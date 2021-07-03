956dc4f662d4ca9c2bcfdd29451ca82729058a26
Jason Zaugg <jzaugg@gmail.com>
2021-07-03 15:02:10 +1000

    Configure OSGI bnd tool for reproducible builds
    We already set use the Git timestamp for the OSGi version number,
    but we were still getting diffs in the META-INF/MANIFEST.MF
    file in the `Bnd-LastModified` header.
    
    This commit sets the REPRODUCIBLE flag to tell the OGSi bundler tool
    to omit that header and ensure that the current timestamp does not
    leak into other Zip entry metadata.
    
    Together with the upgrade to SBT 1.5.x this seems to make the
    resulting JARs successively builds from a given commit identical.
    
    There have been some behaviour changes in bnd that affect our
    output. These seem to be valid bug fixes, but I have opted to
    stay with our status quo with a little extra configuration.

--- project/Osgi.scala
+++ project/Osgi.scala
@@ -8,11 +8,11 @@ import sbt.Keys._
 import collection.JavaConverters._
 import VersionUtil.versionProperties
 
-/** OSGi packaging for the Scala build, distilled from sbt-osgi. We do not use sbt-osgi because it
-  * depends on a newer version of BND which gives slightly different output (probably OK to upgrade
-  * in the future, now that the Ant build has been removed) and does not allow a crucial bit of
+/** OSGi packaging for the Scala build, distilled from sbt-osgi.
+ *
+ * We don't use sbt-osgi (yet) because it does not allow a crucial bit of
   * configuration that we need: Setting the classpath for BND. In sbt-osgi this is always
-  *  `fullClasspath in Compile` whereas we want `products in Compile in packageBin`. */
+  * `fullClasspath in Compile` whereas we want `products in Compile in packageBin`. */
 object Osgi {
   val bundle = TaskKey[File]("osgiBundle", "Create an OSGi bundle.")
   val bundleName = SettingKey[String]("osgiBundleName", "The Bundle-Name for the manifest.")
@@ -29,11 +29,30 @@ object Osgi {
         "Bundle-Name" -> bundleName.value,
         "Bundle-SymbolicName" -> bundleSymbolicName.value,
         "ver" -> v,
-        "Export-Package" -> "*;version=${ver};-split-package:=merge-first",
+
+        // bnd 3.0 fixes for https://github.com/bndtools/bnd/issues/971. This changes our OSGi
+        // metadata by adding Import-Package automatically for all of our exported packages.
+        // Supposedly this is the right thing to do: https://blog.osgi.org/2007/04/importance-of-exporting-nd-importing.html
+        // but I'm disabling the feature (`-noimport:=true`) to avoid changing this detail of
+        // our little understood OSGi metadata for now.
+        "Export-Package" -> "*;version=${ver};-noimport:=true;-split-package:=merge-first",
+
         "Import-Package" -> "scala.*;version=\"${range;[==,=+);${ver}}\",*",
         "Bundle-Version" -> v,
         "Bundle-RequiredExecutionEnvironment" -> "JavaSE-1.8",
-        "-eclipse" -> "false"
+        "-eclipse" -> "false",
+
+        // Great new feature in modern bnd versions: reproducible builds.
+        // Omits the Bundle-LastModified header and avoids using System.currentTimeMillis
+        // for ZIP metadata.
+        "-reproducible" -> "true",
+
+        // https://github.com/bndtools/bnd/commit/2f1d89428559d21857b87b6d5b465a18a300becc (bndlib 4.2.0)
+        // seems to have fixed a bug in its detection class references in Class.forName("some.Class")
+        // For our build, this adds an import on the package "com.cloudius.util" (referred to by an optional
+        // part of JLine. This directive disables the Class.forName scanning. An alternative fix would be
+        // direct this to be an optional dependency (as we do for jline itself with `"Import-Package" -> ("jline.*;resolution:=optional," + ... )`)
+        "-noclassforname" -> "true" //
       )
     },
     jarlist := false,
