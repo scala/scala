dec86a116040c1923bf161ec4e31cd5f4072d065
Som Snytt <som.snytt@gmail.com>
2021-08-09 13:59:06 -0700

    Issue warnings in silent mode

---
+++ test/files/neg/t12441.scala
@@ -0,0 +1,15 @@
+
+// scalac: -Xlint -Werror
+
+trait T {
+  def k(u: => Unit): Unit = u
+  def f: Any = Option.empty[String].contains(1234)
+  def g = Option.empty[String].contains(1234)
+  def h() = k(Option.empty[String].contains(1234))
+
+  def s0 = List(1, 2, 3).toSet()  // adapt mistaken arg () to apply and infer AnyVal
+
+  val p = (42, 27)
+  def pesky = p == (42, 17)
+  def peskier = p == (42, List(17, ""))
+}
