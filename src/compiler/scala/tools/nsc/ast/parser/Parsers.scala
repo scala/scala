c16fc01d385dafea8ba9f1c80666c42218d214f7
Guillaume Martres <smarter@ubuntu.com>
2021-08-10 15:15:42 +0200

    Allow `import x.{*, given}` under -Xsource:3
    Imagine a Scala 3 library containing:
    ```
    object A {
      val a: Int = 1
      given b: Int = 2
    }
    ```
    To import all members of `A` from Scala 2, we write `import A.*`,
    but to do the same from Scala 3, we need to write `import A.{*, given}`
    instead. This complicates cross-compilation for projects which depend on
    Scala 3 libraries (unless these libraries exclusively use `implicit`
    which is not something we want to encourage).
    
    This commit remedies this by allowing `import x.{*, given}` (and `import
    x.{given, *}`), this is easy to do since we can just pretend the user
    wrote `import x.*` which will give us both regular and given members in
    Scala 2 code and therefore match the semantics of Scala 3.

--- src/compiler/scala/tools/nsc/ast/parser/Parsers.scala
+++ src/compiler/scala/tools/nsc/ast/parser/Parsers.scala
@@ -2618,7 +2618,16 @@ self =>
      *  }}}
      */
     def importSelectors(): List[ImportSelector] = {
-      val selectors = inBracesOrNil(commaSeparated(importSelector()))
+      val selectors0 = inBracesOrNil(commaSeparated(importSelector()))
+
+      // Treat an import of `*, given` or `given, *` as if it was an import of `*`
+      // since the former in Scala 3 has the same semantics as the latter in Scala 2.
+      val selectors =
+        if (currentRun.isScala3 && selectors0.exists(_.name eq nme.WILDCARD))
+          selectors0.filterNot(sel => sel.name == nme.`given` && sel.rename == sel.name)
+        else
+          selectors0
+
       selectors.init foreach {
         case ImportSelector(nme.WILDCARD, pos, _, _)  => syntaxError(pos, "Wildcard import must be in last position")
         case _                                        => ()
