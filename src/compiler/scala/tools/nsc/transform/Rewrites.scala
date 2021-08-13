5cf8cfd8585f8f6ecd483c6bbed01cbd5bec04bf
Lukas Rytz <lukas.rytz@gmail.com>
2021-08-13 10:25:30 +0200

    Write out patches back to the source file

--- src/compiler/scala/tools/nsc/transform/Rewrites.scala
+++ src/compiler/scala/tools/nsc/transform/Rewrites.scala
@@ -1,7 +1,8 @@
 package scala.tools.nsc
 package transform
 
-import scala.reflect.internal.util.{Position, RangePosition}
+import scala.annotation.tailrec
+import scala.reflect.internal.util.{Position, SourceFile}
 import scala.tools.nsc.Reporting.WarningCategory
 
 abstract class Rewrites extends SubComponent {
@@ -23,11 +24,60 @@ abstract class Rewrites extends SubComponent {
     override def apply(unit: CompilationUnit): Unit = {
       val rewriter = new RewriteTraverser()
       rewriter.traverse(unit.body)
-      println(rewriter.patches)
+      writePatches(unit.source, rewriter.patches.toArray)
     }
   }
 
-  case class Patch(span: Position, replacement: String)
+  case class Patch(span: Position, replacement: String) {
+    def delta: Int = replacement.length - (span.end - span.start)
+  }
+
+  def checkNoOverlap(patches: Array[Patch]): Boolean = {
+    var ok = true
+    if (patches.nonEmpty)
+      patches.reduceLeft { (p1, p2) =>
+        if (p1.span.end > p2.span.start) {
+          ok = false
+          runReporting.warning(NoPosition, s"overlapping patches:\n - $p1\n - $p2", WarningCategory.Other, "")
+        }
+        p2
+      }
+    ok
+  }
+
+  def applyPatches(source: SourceFile, patches: Array[Patch]): String = {
+    val sourceChars = source.content
+    val patchedChars = new Array[Char](sourceChars.length + patches.foldLeft(0)(_ + _.delta))
+
+    @tailrec def loop(pIdx: Int, inIdx: Int, outIdx: Int): Unit = {
+      def copy(upTo: Int): Int = {
+        val untouched = upTo - inIdx
+        System.arraycopy(sourceChars, inIdx, patchedChars, outIdx, untouched)
+        outIdx + untouched
+      }
+      if (pIdx < patches.length) {
+        val p = patches(pIdx)
+        val outNew = copy(p.span.start)
+        p.replacement.copyToArray(patchedChars, outNew)
+        loop(pIdx + 1, p.span.end, outNew + p.replacement.length)
+      } else {
+        val outNew = copy(sourceChars.length)
+        assert(outNew == patchedChars.length, s"$outNew != ${patchedChars.length}")
+      }
+    }
+    loop(0, 0, 0)
+    new String(patchedChars)
+  }
+
+  def writePatches(source: SourceFile, patches: Array[Patch]): Unit = if (patches.nonEmpty) {
+    java.util.Arrays.sort(patches, Ordering.by[Patch, Int](_.span.start))
+    if (checkNoOverlap(patches)) {
+      val bytes = applyPatches(source, patches).getBytes(settings.encoding.value)
+      val out = source.file.output
+      out.write(bytes)
+      out.close()
+    }
+  }
 
   lazy val breakOutSym = {
     import definitions._
