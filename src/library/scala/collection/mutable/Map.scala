b13ebd7c97738708187dd3e7b2390fa93767270e
NthPortal <nthportal@gmail.com>
2021-08-20 21:19:05 -0400

    Fix concurrent.Map#{filterInPlace,mapValuesInPlace}
    Fix the behaviour of `concurrent.Map#filterInPlace` and
    `concurrent.Map#mapValuesInPlace` to respect atomic entry changes.

--- src/library/scala/collection/mutable/Map.scala
+++ src/library/scala/collection/mutable/Map.scala
@@ -171,17 +171,19 @@ trait MapOps[K, V, +CC[X, Y] <: MapOps[X, Y, CC, _], +C <: MapOps[K, V, CC, C]]
     * @param p  The test predicate
     */
   def filterInPlace(p: (K, V) => Boolean): this.type = {
-    if (nonEmpty) {
-      val array = this.toArray[Any] // scala/bug#7269 toArray avoids ConcurrentModificationException
-      val arrayLength = array.length
-      var i = 0
-      while (i < arrayLength) {
-        val (k, v) = array(i).asInstanceOf[(K, V)]
-        if (!p(k, v)) {
-          this -= k
+    if (!isEmpty) this match {
+      case tm: concurrent.Map[_, _] => tm.asInstanceOf[concurrent.Map[K, V]].filterInPlaceImpl(p)
+      case _ =>
+        val array = this.toArray[Any] // scala/bug#7269 toArray avoids ConcurrentModificationException
+        val arrayLength = array.length
+        var i = 0
+        while (i < arrayLength) {
+          val (k, v) = array(i).asInstanceOf[(K, V)]
+          if (!p(k, v)) {
+            this -= k
+          }
+          i += 1
         }
-        i += 1
-      }
     }
     this
   }
@@ -197,8 +199,9 @@ trait MapOps[K, V, +CC[X, Y] <: MapOps[X, Y, CC, _], +C <: MapOps[K, V, CC, C]]
     * @return   the map itself.
     */
   def mapValuesInPlace(f: (K, V) => V): this.type = {
-    if (nonEmpty) this match {
+    if (!isEmpty) this match {
       case hm: mutable.HashMap[_, _] => hm.asInstanceOf[mutable.HashMap[K, V]].mapValuesInPlaceImpl(f)
+      case tm: concurrent.Map[_, _] => tm.asInstanceOf[concurrent.Map[K, V]].mapValuesInPlaceImpl(f)
       case _ =>
         val array = this.toArray[Any]
         val arrayLength = array.length
