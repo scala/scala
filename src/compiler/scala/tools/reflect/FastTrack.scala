36aa13c6a59ae589f852950b507f691835f71ee1
Som Snytt <som.snytt@gmail.com>
2022-02-27 08:46:02 -0800

    Text alignment

--- src/compiler/scala/tools/reflect/FastTrack.scala
+++ src/compiler/scala/tools/reflect/FastTrack.scala
@@ -64,8 +64,8 @@ class FastTrack[MacrosAndAnalyzer <: Macros with Analyzer](val macros: MacrosAnd
       makeBlackbox(           ApiUniverseReify) { case Applied(_, ttag :: Nil, (expr :: _) :: _)  => c => c.materializeExpr(c.prefix.tree, EmptyTree, expr) },
       makeWhitebox(            StringContext_f) { case _                                          => _.interpolateF },
       makeWhitebox(            StringContext_s) { case _                                          => _.interpolateS },
-      makeWhitebox(            StringContext_raw) { case _                                        => _.interpolateRaw },
-      makeBlackbox(ReflectRuntimeCurrentMirror) { case _                                          => c => currentMirror(c).tree },
+      makeWhitebox(          StringContext_raw) { case _                                          => _.interpolateRaw },
+      makeBlackbox(ReflectRuntimeCurrentMirror) { case _                                          => currentMirror(_).tree },
       makeWhitebox(  QuasiquoteClass_api_apply) { case _                                          => _.expandQuasiquote },
       makeWhitebox(QuasiquoteClass_api_unapply) { case _                                          => _.expandQuasiquote }
      )
