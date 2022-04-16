00fd7503b6f6ef12460173bfc8b6c80e72ccb9a4
Som Snytt <som.snytt@gmail.com>
2022-04-16 07:48:34 -0700

    Remove the unused productIterator synthesis only

--- src/compiler/scala/tools/nsc/typechecker/SyntheticMethods.scala
+++ src/compiler/scala/tools/nsc/typechecker/SyntheticMethods.scala
@@ -24,7 +24,6 @@ import symtab.Flags._
  *    def productArity: Int
  *    def productElement(n: Int): Any
  *    def productPrefix: String
- *    ~def productIterator: Iterator[Any]~
  *
  *  Selectively added to case classes/objects, unless a non-default
  *  implementation already exists:
@@ -114,13 +113,6 @@ trait SyntheticMethods extends ast.TreeDSL {
         (m0 ne meth) && !m0.isDeferred && !m0.isSynthetic && (m0.owner != AnyValClass) && (typeInClazz(m0) matches typeInClazz(meth))
       }
     }
-    /*
-    def productIteratorMethod =
-      createMethod(nme.productIterator, iteratorOfType(AnyTpe))(_ =>
-        Select(Super(This(tpnme.EMPTY), tpnme.EMPTY), definitions.Product_iterator))
-        //gen.mkRuntimeCall(nme.typedProductIterator, List(AnyTpe), List(mkThis))
-      )
-     */
 
     def perElementMethod(name: Name, returnType: Type)(caseFn: Symbol => Tree): Tree = 
       createSwitchMethod(name, accessors.indices, returnType)(idx => caseFn(accessors(idx)))
@@ -267,7 +259,6 @@ trait SyntheticMethods extends ast.TreeDSL {
         Product_productPrefix -> (() => constantNullary(nme.productPrefix, clazz.name.decode)),
         Product_productArity -> (() => constantNullary(nme.productArity, arity)),
         Product_productElement -> (() => perElementMethod(nme.productElement, AnyTpe)(mkThisSelect)),
-        //Product_iterator -> (() => productIteratorMethod),
         Product_canEqual -> (() => canEqualMethod)
       )
     }
