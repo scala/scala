5808a53e48067c29c0f1c0c1a02001764ff6c9d5
Som Snytt <som.snytt@gmail.com>
2022-08-01 16:52:39 -0700

    Show expected -Wvalue-discard with type params

---
+++ test/files/neg/t11379b.scala
@@ -0,0 +1,12 @@
+// scalac: -Werror -Wvalue-discard
+class OneTypeParam[B](value: B) {
+  def map[B1](fn: B => B1): OneTypeParam[B1] = new OneTypeParam(fn(value))
+  def unitValue: OneTypeParam[Unit] = new OneTypeParam(())
+  def checkCompiler: OneTypeParam[Unit] = unitValue.map(_ => unitValue)
+}
+
+class TwoTypeParam[A, B](value: B) {
+  def map[B1](fn: B => B1): TwoTypeParam[A, B1] = new TwoTypeParam(fn(value))
+  def unitValue[C]: TwoTypeParam[C, Unit] = new TwoTypeParam(())
+  def checkCompiler: TwoTypeParam[String, Unit] = unitValue.map(_ => unitValue)
+}
