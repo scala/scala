422590abc60e6c708d998780fc9d6a7e897e5300
Luc Henninger <luc.henninger@orange.fr>
2022-10-01 14:59:38 +0200

    Deprecate "isTraversableAgain" and replace by "isIterableagain" (if this really make sens).

--- src/library/scala/collection/IterableOnce.scala
+++ src/library/scala/collection/IterableOnce.scala
@@ -572,7 +572,7 @@ trait IterableOnceOps[+A, +CC[_], +C] extends Any { this: IterableOnce[A] =>
    *
    *  @return   `true` if it is repeatedly traversable, `false` otherwise.
     */
-  @depreceted("Use isIterableAgain instead", "2.13.0")
+  @deprecated("Use isIterableAgain instead", "2.13.0")
   def isTraversableAgain: Boolean = isIterableAgain
 
   /** Apply `f` to each element for its side effects
