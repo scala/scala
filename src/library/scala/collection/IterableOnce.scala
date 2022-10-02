32531e035deb1070d6260bcfed0e81071881a7a4
Luc Henninger <luc.henninger@orange.fr>
2022-10-02 22:22:59 +0200

    Deprecate "isTraversableAgain".

--- src/library/scala/collection/IterableOnce.scala
+++ src/library/scala/collection/IterableOnce.scala
@@ -565,7 +565,7 @@ trait IterableOnceOps[+A, +CC[_], +C] extends Any { this: IterableOnce[A] =>
    *
    *  @return   `true` if it is repeatedly traversable, `false` otherwise.
    */
-  @deprecated("Could be replaced by isInstanceOf[Iterable]", "2.13.0")
+  @deprecated("Use .isInstanceOf[Iterable] instead", "2.13.0")
   def isTraversableAgain: Boolean = false
 
   /** Apply `f` to each element for its side effects
