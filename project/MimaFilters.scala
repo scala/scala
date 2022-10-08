ace81da2b6f72e5665b3b842f25bd8f3aa34f818
Seth Tisue <seth@tisue.net>
2022-10-08 15:46:35 -0300

    new reference compiler is Scala 2.13.10

--- project/MimaFilters.scala
+++ project/MimaFilters.scala
@@ -13,7 +13,7 @@ object MimaFilters extends AutoPlugin {
   import autoImport._
 
   override val globalSettings = Seq(
-    mimaReferenceVersion := Some("2.13.9"),
+    mimaReferenceVersion := Some("2.13.10"),
   )
 
   val mimaFilters: Seq[ProblemFilter] = Seq[ProblemFilter](
