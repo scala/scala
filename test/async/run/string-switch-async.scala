1e27f99e66e67b1ce48299ebf98a4f76a7885e5a
Lukas Rytz <lukas.rytz@gmail.com>
2022-11-08 14:46:42 +0100

    Fix interaction between string switching and async
    ```
    def testSwitch(s: String) = optionally {
      s match {
        case "" => ""
        case p =>
          value(Some(p))
          p
      }
    }
    ```
    
    Pattern translation introduces a local for the scrutinee (`x1`) and
    replaces uses of `p` in the default case by `x1`. After patmat:
    
    ```
    case <synthetic> val x1: String = s;
    x1 match {
      case "" => ""
      case _ => {
        scala.tools.partest.async.OptionAwait.value[String](new Some[String](x1));
        x1
      }
    }
    ```
    
    The `x1` variable is accessed in different async stages, and therefore
    transformed into a state machine field. After async:
    
    ```
    (stateMachine$async.this.x1: String) match {
    case "" => ...
      ...
    case _ => ...
    }
    ```
    
    This breaks the translation of string switches in `CleanUp`, which
    assumes the scrutinee AST is a `Literal` or an `Ident`. The fix is to
    introduce another local variable if needed.

---
+++ test/async/run/string-switch-async.scala
@@ -0,0 +1,19 @@
+// scalac: -Xasync
+
+import scala.tools.partest.async.OptionAwait._
+import org.junit.Assert._
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    assertEquals(Some(""), testSwitch(""))
+    assertEquals(Some("aa"), testSwitch("a"))
+  }
+
+  private def testSwitch(s: String) = optionally {
+    s match {
+      case "" => ""
+      case p =>
+        value(Some(p)) + p
+    }
+  }
+}
