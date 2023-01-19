ec07b49f92fc32d473eb97aef2b109ff94d3ba14
Dale Wijnand <dale.wijnand@gmail.com>
2023-01-19 15:26:52 +0000

    Patmat: Fix over-prunning of private children

---
+++ test/files/neg/t6159.scala
@@ -0,0 +1,13 @@
+// scalac: -Werror
+// like test/files/pos/t6159.scala
+// but with T2 not private
+trait A {
+  sealed abstract class X
+  private class X1 extends X with X2 { }
+  trait X2 extends X
+  sealed trait X3 extends X
+
+  def f(x: X) = x match {
+    case _: X1 => 0
+  }
+}
---
+++ test/files/pos/t12712.scala
@@ -0,0 +1,16 @@
+// scalac: -Werror
+object T {
+  private sealed trait T
+  private object O extends T
+  private trait U extends T
+  private object P extends U
+
+  private def t(t: T) = t match {
+    case O => ()
+    case _: U => println("hai")
+  }
+
+  def main(args: Array[String]): Unit = {
+    t(P)
+  }
+}
