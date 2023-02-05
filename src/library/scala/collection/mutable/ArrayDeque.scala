c7a16c918f10fe52ce5ee5d08b8ef469b16c7f08
João Costa <jdpc557@gmail.com>
2023-02-05 22:27:26 +0100

    Replace removeFirst with removeHead in ArrayDeque documentation

--- src/library/scala/collection/mutable/ArrayDeque.scala
+++ src/library/scala/collection/mutable/ArrayDeque.scala
@@ -21,7 +21,7 @@ import scala.reflect.ClassTag
 
 /** An implementation of a double-ended queue that internally uses a resizable circular buffer.
   *
-  *  Append, prepend, removeFirst, removeLast and random-access (indexed-lookup and indexed-replacement)
+  *  Append, prepend, removeHead, removeLast and random-access (indexed-lookup and indexed-replacement)
   *  take amortized constant time. In general, removals and insertions at i-th index are O(min(i, n-i))
   *  and thus insertions and removals from end/beginning are fast.
   *
