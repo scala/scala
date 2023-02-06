b6a26e531306b78984e20b1a64a434a0de5304e7
Lukas Rytz <lukas.rytz@gmail.com>
2023-02-06 14:19:43 +0100

    [backport] warn when inherited takes precedence over outer definitions
    Under `-Xsource:2.13`, warn when an inherited member takes precedence
    over an outer definition.

---
+++ test/files/neg/t11921-alias.scala
@@ -0,0 +1,70 @@
+// scalac: -Xfatal-warnings -Xsource:2.13
+
+object t1 {
+  class C[T] { type TT = T }
+  object O {
+    type TT = String
+    class D extends C[TT] {
+      def n(x: TT) = x // OK
+    }
+  }
+}
+
+object t2 {
+  class C[T] { type TT <: T }
+  object O {
+    type TT = String
+    class D extends C[TT] {
+      def n(x: TT) = x // ambiguous
+    }
+  }
+}
+
+object t3 {
+  trait Context
+  class A[C <: Context](val c: C)
+  class B(val c: Context) { b =>
+    val a = new A[c.type](c) {
+      def n = c // OK
+    }
+  }
+}
+
+object t4 {
+  trait Context
+  class A[C <: Context](val c: C)
+  class B(val c: Context) { b =>
+    val a = new A(c) {
+      def n = c // ambiguous
+    }
+  }
+}
+
+object t5 {
+  trait TT
+  class K[T <: TT](val t: T)
+  class C {
+    def f(t: TT) = new K[t.type](t) {
+      def test = t
+    }
+  }
+}
+
+object t6 {
+  class C(val name: String)
+  object Test {
+    def m(name: String) = new C(name) {
+      println(name)
+    }
+  }
+}
+
+object t7 {
+  abstract class A(val name: String)
+  class C(name: String) extends A(name)
+  object Test {
+    def m(name: String) = new C(name) {
+      println(name)
+    }
+  }
+}
