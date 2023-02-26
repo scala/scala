d50b823d67e25f54d7461528f04509c9b1aa23d4
Som Snytt <som.snytt@gmail.com>
2023-02-26 14:36:11 -0800

    Accept implicit _, implicit (x: Int) in lambda

--- test/files/pos/t3672.scala
+++ test/files/pos/t3672.scala
@@ -1,4 +1,22 @@
 object Test {
-  def foo(f: Int => Int) = () ; foo { implicit x : Int => x + 1 }
-  def bar(f: Int => Int) = () ; foo { x : Int => x + 1 }
+  def foo(f: Int => Int) = ()
+  def test(): Unit = {
+    foo { x => x + 1 }
+    foo { implicit x => x + 1 }
+    foo { x: Int => x + 1 }
+    foo { implicit x: Int => x + 1 }
+    foo { _ => 42 }
+    foo { implicit _ => implicitly[Int] + 1 }   // scala 2 deficit
+    foo { _: Int => 42 }
+    foo { implicit _: Int => implicitly[Int] + 1 }    // scala 2 deficit
+
+    foo(x => x + 1)
+    foo(implicit x => x + 1)
+    foo((x: Int) => x + 1)
+    foo(implicit (x: Int) => x + 1)   // scala 3
+    foo(_ => 42)
+    foo(implicit _ => implicitly[Int] + 1)    // scala 2 deficit
+    foo((_: Int) => 42)
+    foo(implicit (_: Int) => implicitly[Int] + 1)   // scala 3
+  }
 }
