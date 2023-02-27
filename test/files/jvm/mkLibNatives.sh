fa84e2aa9f1533b2c0a24a02613549f60f40ae31
Seth Tisue <seth@tisue.net>
2023-02-26 16:28:04 -0800

    fix `test/files/jvm/natives.scala` on ARM Macs
    fixes scala/scala-dev#833

--- test/files/jvm/mkLibNatives.sh
+++ test/files/jvm/mkLibNatives.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 ##############################################################################
 # Author  : Stephane Micheloud
@@ -11,14 +11,24 @@
 debug=
 
 cygwin=false;
-darwin=false;
+darwin_x86=false;
+darwin_arm=false;
 case "`uname`" in
-    CYGWIN*) cygwin=true ;;
-    Darwin*) darwin=true ;;
+  CYGWIN*) cygwin=true ;;
+  Darwin*) case "`uname -m`" in
+    x86_64*) darwin_x86=true ;;
+    arm64*) darwin_arm=true ;;
+  esac
 esac
 
 CLASS_NAME=Test\$
 CLASS_DIR=natives-jvm.obj
+
+if [ ! -f "${CLASS_DIR}/${CLASS_NAME}.class" ]; then
+  echo "first you need to run this within sbt:"
+  echo "partest --debug test/files/jvm/natives.scala"
+  exit
+fi
 
 OBJ_NAME=natives
 LIB_NAME=libnatives
@@ -36,11 +46,20 @@ JAVAH_OPTIONS="-jni -force -classpath ${CLASS_DIR} -o ${OBJ_NAME}.h"
 
 CC=gcc
 
-if $darwin; then
-  CC_OPTIONS="-c -arch ppc -arch i386 -arch x86_64"
+if $darwin_x86; then
+  # not sure if this stuff still works on current MacOS -- the
+  # generated .jnilib file is already in version control and we're not
+  # likely to need to generate it again, so I didn't bother to see if this
+  # needs the same changes that are in the darwin_arm section below
+  CC_OPTIONS="-c -arch i386 -arch x86_64"
   CC_INCLUDES="-I/System/Library/Frameworks/JavaVM.framework/Headers"
   LNK_OPTIONS="-dynamiclib -framework JavaVM"
-  FULL_LIB_NAME=${LIB_NAME}.jnilib
+  FULL_LIB_NAME=${LIB_NAME}-x86.jnilib
+elif $darwin_arm; then
+  CC_OPTIONS="-c -arch arm64"
+  CC_INCLUDES="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin"
+  LNK_OPTIONS="-L${JAVA_HOME}/jre/lib/server -dynamiclib -ljvm"
+  FULL_LIB_NAME=${LIB_NAME}-arm.jnilib
 else
   CC_OPTIONS=-c
   CC_INCLUDES="-I${JAVA_HOME}/include -I${JAVA_HOME}/include/${OSTYPE}"
