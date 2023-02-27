ba25d9045b2cc138afa4a4d13768da34ea921500
Seth Tisue <seth@tisue.net>
2023-02-26 16:28:04 -0800

    fix `test/files/jvm/natives.scala` on ARM Macs
    fixes scala/scala-dev#833

--- test/files/jvm/natives.scala
+++ test/files/jvm/natives.scala
@@ -2,14 +2,18 @@ object Test {
 
   //println("java.library.path=" + System.getProperty("java.library.path"))
 
-  val sysWordSize = System.getProperty("sun.arch.data.model", "32")
-  val sysType = System.getProperty("os.name")
+  val os = System.getProperty("os.name")
+  val arch = System.getProperty("os.arch")
 
-  val libName =
-    if (sysType == "Mac OS X")
-      "natives"
-    else
-      "natives-" + sysWordSize
+  val libName = (os, arch) match {
+    case ("Mac OS X", "aarch64") =>
+      "natives-arm"
+    case ("Mac OS X", "x86_64") =>
+      "natives-x86"
+    case _ =>
+       val wordSize = System.getProperty("sun.arch.data.model", "32")
+      "natives-" + wordSize
+  }
 
   System.loadLibrary(libName)
 
