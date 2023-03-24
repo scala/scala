2cbff24f4c26828f72e798c56a23a2bf565685e5
Liang Yan <ckgppl_yan@sina.cn>
2023-03-24 10:36:49 +0800

    Fix tasty/TestLogarithms.scala failed in some platform
    Signed-off-by: Liang Yan <ckgppl_yan@sina.cn>

--- test/tasty/run/src-2/tastytest/TestLogarithms.scala
+++ test/tasty/run/src-2/tastytest/TestLogarithms.scala
@@ -4,10 +4,10 @@ import Logarithms._
 
 object TestLogarithms extends Suite("TestLogarithms") {
 
-  val Some(l1) = Logarithm.of(2)
-  val Some(l2) = Logarithm.of(3)
+  val Some(l1) = Logarithm.of(10)
+  val Some(l2) = Logarithm.of(100)
 
-  test(assert((l1 + l2).toDouble == 4.999999999999999))
-  test(assert((l1 * l2).toDouble == 6.0))
+  test(assert((l1 + l2).toDouble == 109.99999999999997))
+  test(assert((l1 * l2).toDouble == 1000.0))
 
 }
