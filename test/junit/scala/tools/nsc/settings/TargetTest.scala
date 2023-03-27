c44b8c6b9d9f07e636922887f854adeabd3920a6
Som Snytt <som.snytt@gmail.com>
2023-03-27 10:35:37 -0700

    Respect user target always

--- test/junit/scala/tools/nsc/settings/TargetTest.scala
+++ test/junit/scala/tools/nsc/settings/TargetTest.scala
@@ -19,11 +19,16 @@ import org.junit.runners.JUnit4
 
 import scala.collection.mutable.ListBuffer
 import scala.tools.nsc.settings.StandardScalaSettings._
+import scala.tools.testkit.AssertUtil.assertFails
 import scala.util.Properties.isJavaAtLeast
 import scala.util.Try
 
 @RunWith(classOf[JUnit4])
 class TargetTest {
+
+  private def lately[A](body: => A): Unit = if (isJavaAtLeast(11)) body: Unit
+
+  private def eightly[A](body: => A): Unit = if (!isJavaAtLeast(9)) body: Unit
 
   @Test def testSettingTargetSetting(): Unit = {
     def goodVersion(v: String): Boolean = Try(isJavaAtLeast(v)).getOrElse(false)
@@ -64,5 +69,36 @@ class TargetTest {
     checkFail("-target:jvm-7")    // no longer
     checkFail("-target:jvm-3000") // not in our lifetime
     checkFail("-target:msil")     // really?
+  }
+  @Test def `respect user target`: Unit = lately {
+    val settings = new Settings(err => fail(s"Error output: $err"))
+    val (ok, _) = settings.processArgumentString("--release:11 --target:8")
+    assertTrue(ok)
+    assertEquals("8", settings.target.value)
+    assertEquals("11", settings.release.value)
+    assertEquals("8", settings.targetValue)
+    assertEquals(Some("11"), settings.releaseValue)
+  }
+  @Test def `target is release if specified`: Unit = lately {
+    val settings = new Settings(err => fail(s"Error output: $err"))
+    val (ok, _) = settings.processArgumentString("--release:11")
+    assertTrue(ok)
+    assertEquals("8", settings.target.value)        // default
+    assertEquals(None, settings.target.valueSetByUser)
+    assertEquals("11", settings.release.value)
+    assertEquals("11", settings.targetValue)        // because --release:11
+    assertEquals(Some("11"), settings.releaseValue)
+  }
+  @Test def `disrespect user target`: Unit = lately {
+    val settings = new Settings(err => fail(s"Error output: $err"))
+    assertFails(_.contains("-release cannot be less than -target")) {
+      settings.processArgumentString("--release:8 --target:11")
+    }
+  }
+  @Test def `cannot bump release on jdk 8`: Unit = eightly {
+    val settings = new Settings(err => fail(s"Error output: $err"))
+    assertFails(_.contains("'9' is not a valid choice for '-release'")) {
+      settings.processArgumentString("--release:9")
+    }
   }
 }
