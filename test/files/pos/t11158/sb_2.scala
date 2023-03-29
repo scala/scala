b5ccdaf0b37b0a41a20c0e278caaadb2dc26cbc8
Som Snytt <som.snytt@gmail.com>
2023-03-28 21:40:59 -0700

    Test for assignment rewrite

---
+++ test/files/pos/t11158/sb_2.scala
@@ -0,0 +1,7 @@
+
+// sbt is an acronym for StringBuilder test
+class sbt {
+  val sb = new StringBuilder("There once was a man from Killarney / whose comments were nothing but blarney")
+
+  def reset() = sb.length = 42
+}
