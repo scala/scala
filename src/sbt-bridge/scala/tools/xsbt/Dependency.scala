9cd09ac89f722800a1a23f4e086b6e48c09816ba
Lukas Rytz <lukas.rytz@gmail.com>
2023-07-13 10:11:00 +0200

    sbtBridge/headerCreate

--- src/sbt-bridge/scala/tools/xsbt/Dependency.scala
+++ src/sbt-bridge/scala/tools/xsbt/Dependency.scala
@@ -2,8 +2,11 @@
  * Zinc - The incremental compiler for Scala.
  * Copyright Scala Center, Lightbend, and Mark Harrah
  *
+ * Scala (https://www.scala-lang.org)
+ * Copyright EPFL and Lightbend, Inc.
+ *
  * Licensed under Apache License 2.0
- * SPDX-License-Identifier: Apache-2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
  *
  * See the NOTICE file distributed with this work for
  * additional information regarding copyright ownership.
