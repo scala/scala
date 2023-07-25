2f4a674dc3cf39daad5c19628ed637d9fba108d2
Som Snytt <som.snytt@gmail.com>
2023-07-24 17:39:00 -0700

    Remove ParserTest

--- test/files/run/t9944.scala
+++ test/files/run/t9944.scala
@@ -1,7 +1,11 @@
 
-import scala.tools.partest.ParserTest
+import scala.tools.partest.DirectTest
 
-object Test extends ParserTest {
+object Test extends DirectTest {
+
+  override def extraSettings: String = "-usejavacp -Ystop-after:parser -Vprint:parser"
 
   def code = s"""class C { def g = 42 ; def f = s""\"123\r\n$${ g }\r\n123\r\n""\"}"""
+
+  override def show(): Unit = if (!compile()) println("Compilation failed!")
 }
