4868bd3d9679e62739ae38f44cec5afc12bf450d
Gabriel Jones <18059585+gabrieljones@users.noreply.github.com>
2023-08-04 12:35:08 -0400

    ChainingOpsTest: tapIf, pipeIf

--- test/junit/scala/util/ChainingOpsTest.scala
+++ test/junit/scala/util/ChainingOpsTest.scala
@@ -20,6 +20,46 @@ class ChainingOpsTest extends RunTesting {
     assertEquals(List(1, 2, 3), result)
   }
 
+  @Test
+  def testAnyTapIfFalse(): Unit = {
+    var x: Int = 0
+    val result = List(1, 2, 3)
+      .tapIf(false)(xs => x = xs.head)
+
+    assertEquals(0, x)
+    assertEquals(List(1, 2, 3), result)
+  }
+
+  @Test
+  def testAnyTapIfFalseLambda(): Unit = {
+    var x: Int = 0
+    val result = List(1, 2, 3)
+      .tapIf(_.size > 4)(xs => x = xs.head)
+
+    assertEquals(0, x)
+    assertEquals(List(1, 2, 3), result)
+  }
+
+  @Test
+  def testAnyTapIfTrue(): Unit = {
+    var x: Int = 0
+    val result = List(1, 2, 3)
+      .tapIf(true)(xs => x = xs.head)
+
+    assertEquals(1, x)
+    assertEquals(List(1, 2, 3), result)
+  }
+
+  @Test
+  def testAnyTapIfTrueLambda(): Unit = {
+    var x: Int = 0
+    val result = List(1, 2, 3)
+      .tapIf(_.size < 4)(xs => x = xs.head)
+
+    assertEquals(1, x)
+    assertEquals(List(1, 2, 3), result)
+  }
+
   @Test def testAnyValTap(): Unit = assertEquals(42.tap(x => x), 42)
 
   @Test
@@ -31,6 +71,48 @@ class ChainingOpsTest extends RunTesting {
 
     assertEquals(24, result)
   }
+
+  @Test
+  def testAnyPipeIfFalse(): Unit = {
+    val times6 = (_: Int) * 6
+    val result = (1 - 2 - 3)
+      .pipeIf(false)(times6)
+      .pipe(scala.math.abs)
+
+    assertEquals(4, result)
+  }
+
+  @Test
+  def testAnyPipeIfFalseLambda(): Unit = {
+    val times6 = (_: Int) * 6
+    val result = (1 - 2 - 3)
+      .pipeIf(_ > 0)(times6)
+      .pipe(scala.math.abs)
+
+    assertEquals(4, result)
+  }
+
+  @Test
+  def testAnyPipeIfTrue(): Unit = {
+    val times6 = (_: Int) * 6
+    val result = (1 - 2 - 3)
+      .pipeIf(false)(times6)
+      .pipe(scala.math.abs)
+
+    assertEquals(24, result)
+  }
+
+  @Test
+  def testAnyPipeIfTrueLambda(): Unit = {
+    val times6 = (_: Int) * 6
+    val result = (1 - 2 - 3)
+      .pipeIf(_ < 0)(times6)
+      .pipe(scala.math.abs)
+
+    assertEquals(24, result)
+  }
+
+  //TODO: testPipeIf where [A] != [B]
 
   @Test def testNoSelf(): Unit =
     assertThrows[ToolBoxError] {
