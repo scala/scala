feff88b7361a35536c9559d33c73774268ce00a2
Georgi Krastev <joro.kr.21@gmail.com>
2023-08-11 09:57:08 +0300

    Handle AnnotatedType in mkAttributedQualifierIfPossible

---
+++ test/files/pos/annotated-outer.scala
@@ -0,0 +1,12 @@
+// scalac: -Werror
+object Test {
+  trait MySet[A]
+  trait MyMap[K, +V] {
+    class Keys extends MySet[K]
+  }
+
+  def areKeys[A](xs: MySet[A]) = xs match {
+    case _: (MyMap[A, _] @unchecked)#Keys => true
+    case _ => false
+  }
+}
