24fcaebe9f657e12940f84f42cb178ad37df5f85
Som Snytt <som.snytt@gmail.com>
2023-08-26 03:55:18 -0700

    Improve namePos for type aliases and valdefs
    Use namePos for symbol of derived trees like accessors

---
+++ test/files/neg/t12735c.scala
@@ -0,0 +1,26 @@
+// scalac: -Werror -Xlint -Xreporter:scala.tools.partest.nest.PlainReporter
+
+class UnusedVal {
+  private val v: String =
+    List(1) match {
+      case Nil => "nil"
+    }
+}
+
+class UnusedVals {
+  private val v, w, x, y, z: String =
+    List(1) match {
+      case Nil => "nil"
+    }
+}
+
+class UnusedIdents {
+  private val v, w, x, y, z =
+    List(1) match {
+      case Nil => "nil"
+    }
+}
+
+object UnusedAlias {
+  private type int = Int
+}
