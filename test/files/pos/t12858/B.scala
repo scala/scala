96d74acde2747d515d7a5a41d02000fe516282cf
Lukas Rytz <lukas.rytz@gmail.com>
2023-09-01 13:59:56 +0200

    Fix spurious "nullary overrides nilary" warning
    ```
    interface A { int f(); }          // Java
    trait B extends A { def f: Int }  // Scala
    trait C { def f = 2 }
    class T extends B with C
    ```
    
    Namer adds an empty parameter list `()` to `B.f`.
    There's no override warning at `B.f` because `A.f` is defined in Java.
    
    In class `T`, `C.f` (without parameter list) overrides `B.f()` (with an
    empty parameter list - added by Namer). There should be no warning.

---
+++ test/files/pos/t12858/B.scala
@@ -0,0 +1,9 @@
+// scalac: -Werror
+
+trait B1 extends A { def f: Int }
+trait C1 { def f = 2 }
+class T1 extends B1 with C1
+
+trait B2 extends A { def f: Int = 1}
+trait C2 { self: B2 => override def f = 2 }
+class T2 extends B2 with C2
