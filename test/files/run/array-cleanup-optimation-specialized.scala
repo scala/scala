1c32603ad242a9dd542c01d025654651b16a4112
Som Snytt <som.snytt@gmail.com>
2024-01-02 16:31:47 -0800

    Consolidate tests

--- test/files/run/array-cleanup-optimation-specialized.scala
+++ test/files/run/array-cleanup-optimation-specialized.scala
@@ -4,9 +4,11 @@ object Test {
   def main(args: Array[String]): Unit = {
     assert(apply[String]("") == classOf[Array[String]])
     assert(apply[Double](1d) == classOf[Array[Double]])
+
+    assert(applyBound[Double](1d) == classOf[Array[Double]])
   }
 
-  def apply[@specialized(Double) C: ClassTag](c: C): Class[_] = {
-    Array(c).getClass
-  }
+  def apply[@specialized(Double) C: ClassTag](c: C): Class[_] = Array(c).getClass
+
+  def applyBound[D <: Double: ClassTag](d: D): Class[_] = Array.apply[D](d).getClass
 }
