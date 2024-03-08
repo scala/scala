aeaacf8785e84fcac9fc4ffe8e10ec58e37a0593
Som Snytt <som.snytt@gmail.com>
2024-03-08 14:11:53 -0800

    Adjust help text

--- src/compiler/scala/tools/nsc/Reporting.scala
+++ src/compiler/scala/tools/nsc/Reporting.scala
@@ -522,6 +522,7 @@ object Reporting {
     def includes(o: WarningCategory): Boolean = this eq o
     def summaryCategory: WarningCategory = this
     lazy val name: String = WarningCategory.nameOf(this)
+    override def toString = name
   }
 
   object WarningCategory {
@@ -722,7 +723,7 @@ object Reporting {
   }
 
   object MessageFilter {
-    object Any extends MessageFilter {
+    case object Any extends MessageFilter {
       def matches(message: Message): Boolean = true
     }
 
@@ -768,7 +769,9 @@ object Reporting {
     }
   }
 
-  sealed trait Action
+  sealed trait Action {
+    override def toString = s"Action[${getClass.getSimpleName.stripSuffix("$")}]"
+  }
 
   object Action {
     object Error extends Action
