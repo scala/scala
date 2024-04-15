698369d8b66107dee750aeddff3b8c2110324276
Lukas Rytz <lukas.rytz@gmail.com>
2024-04-15 15:09:32 +0200

    Align WConf category for WFlagUnnamedBooleanLiteral

--- src/compiler/scala/tools/nsc/Reporting.scala
+++ src/compiler/scala/tools/nsc/Reporting.scala
@@ -571,7 +571,7 @@ object Reporting {
         WFlagExtraImplicit,
         WFlagNumericWiden,
         WFlagSelfImplicit,
-        WFlagNamedLiteral,
+        WFlagUnnamedBooleanLiteral,
         WFlagValueDiscard
       = wflag()
 
