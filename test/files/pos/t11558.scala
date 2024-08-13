081de87441efd89a0b1eedc72144807d00012115
Som Snytt <som.snytt@gmail.com>
2024-08-12 19:21:26 -0700

    Augment regression test

--- test/files/pos/t11558.scala
+++ test/files/pos/t11558.scala
@@ -3,3 +3,11 @@ class Test {
   trait S[T] { def map[R](f: F1[_ >: T, _ <: R]): S[R] }
   (??? : S[Int]).map(_.toString) // check that map's type param is inferred (should be String)
 }
+
+object Test_9244 {
+  trait F1[T1, R] { def apply(a1: T1): R }
+
+  def cycle[T](function: F1[? >: T, ? <: T]): Unit = ???
+
+  def t9244 = cycle((_: String) => "")
+}
