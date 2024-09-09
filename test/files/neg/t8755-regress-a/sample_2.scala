ff827f321c22066d08b6ae897435fd74bf74fd7c
Som Snytt <som.snytt@gmail.com>
2024-09-08 19:57:04 -0700

    Test reachable component

---
+++ test/files/neg/t8755-regress-a/sample_2.scala
@@ -0,0 +1,6 @@
+//> using options -Xplugin:. -Xplugin-require:"Testing phase assembly" -Vphases -Werror
+package sample
+
+// just a sample that is compiled with the sample plugin enabled
+object Sample extends App {
+}
