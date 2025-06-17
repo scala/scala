3e9e83aded3fd8e6f9cf4424a1689f8a5ddf1635
Som Snytt <som.snytt@gmail.com>
2025-06-17 09:59:51 -0700

    TastyFormat doc format

--- src/compiler/scala/tools/tasty/TastyFormat.scala
+++ src/compiler/scala/tools/tasty/TastyFormat.scala
@@ -15,12 +15,14 @@ package scala.tools.tasty
 // revision: https://github.com/scala/scala3/commit/24bff2a019afcf0f45ddfd3af6b213e7e228471c
 object TastyFormat {
 
-  /** The first four bytes of a TASTy file, followed by four values:
-    * - `MajorVersion: Int` - see definition in `TastyFormat`
-    * - `MinorVersion: Int` - see definition in `TastyFormat`
-    * - `ExperimentalVersion: Int` - see definition in `TastyFormat`
-    * - `ToolingVersion: String` - arbitrary length string representing the tool that produced the TASTy.
-    */
+  /** The TASTy file header.
+   *
+   *  The first four bytes of a TASTy file, followed by four values:
+   *  - `MajorVersion: Int` - see definition in `TastyFormat`
+   *  - `MinorVersion: Int` - see definition in `TastyFormat`
+   *  - `ExperimentalVersion: Int` - see definition in `TastyFormat`
+   *  - `ToolingVersion: String` - arbitrary length string representing the tool that produced the TASTy.
+   */
   final val header: Array[Int] = Array(0x5C, 0xA1, 0xAB, 0x1F)
 
   /** Natural number. Each increment of the `MajorVersion` begins a
@@ -54,39 +56,39 @@ object TastyFormat {
    */
   final val ExperimentalVersion: Int = 0
 
-  /**This method implements a binary relation (`<:<`) between two TASTy versions.
+  /** This method implements a binary relation (`<:<`) between two TASTy versions.
    *
-   * We label the lhs `file` and rhs `compiler`.
-   * if `file <:< compiler` then the TASTy file is valid to be read.
+   *  We label the lhs `file` and rhs `compiler`.
+   *  if `file <:< compiler` then the TASTy file is valid to be read.
    *
-   * A TASTy version, e.g. `v := 28.0-3` is composed of three fields:
-   *   - v.major == 28
-   *   - v.minor == 0
-   *   - v.experimental == 3
+   *  A TASTy version, e.g. `v := 28.0-3` is composed of three fields:
+   *    - v.major == 28
+   *    - v.minor == 0
+   *    - v.experimental == 3
    *
-   * TASTy versions have a partial order, for example,
-   * `a <:< b` and `b <:< a` are both false if
-   *   - `a` and `b` have different `major` fields.
-   *   - `a` and `b` have the same `major` & `minor` fields,
-   *     but different `experimental` fields, both non-zero.
+   *  TASTy versions have a partial order, for example,
+   *  `a <:< b` and `b <:< a` are both false if
+   *    - `a` and `b` have different `major` fields.
+   *    - `a` and `b` have the same `major` & `minor` fields,
+   *      but different `experimental` fields, both non-zero.
    *
-   * A TASTy version with a zero value for its `experimental` field
-   * is considered to be stable. Files with a stable TASTy version
-   * can be read by a compiler with an unstable TASTy version,
-   * (where the compiler's TASTy version has a higher `minor` field).
+   *  A TASTy version with a zero value for its `experimental` field
+   *  is considered to be stable. Files with a stable TASTy version
+   *  can be read by a compiler with an unstable TASTy version,
+   *  (where the compiler's TASTy version has a higher `minor` field).
    *
-   * A compiler with a stable TASTy version can never read a file
-   * with an unstable TASTy version.
+   *  A compiler with a stable TASTy version can never read a file
+   *  with an unstable TASTy version.
    *
-   * We follow the given algorithm:
+   *  We follow the given algorithm:
    *
-   * ```
-   * (fileMajor, fileMinor, fileExperimental) match
-   *   case (`compilerMajor`, `compilerMinor`, `compilerExperimental`) => true // full equality
-   *   case (`compilerMajor`, minor, 0) if minor < compilerMinor       => true // stable backwards compatibility
-   *   case _                                                          => false
-   * ```
-   * @syntax markdown
+   *  {{{
+   *  (fileMajor, fileMinor, fileExperimental) match
+   *    case (`compilerMajor`, `compilerMinor`, `compilerExperimental`) => true // full equality
+   *    case (`compilerMajor`, minor, 0) if minor < compilerMinor       => true // stable backwards compatibility
+   *    case _                                                          => false
+   *  }}}
+   *  @syntax markdown
    */
   def isVersionCompatible(
     fileMajor: Int,
